f7f5336b715bacffce0b8c838e3a8f776759e2e3
Benjamin Glatzel <benjamin.glatzel@me.com>
2011-04-01 00:29:07 +0200

    Rewrite of the lighting(!) Looks much better now. New trees. Added a sun. Stone is now being randomly placed within hills. [...]

--- src/blockmania/Configuration.java
+++ src/blockmania/Configuration.java
@@ -26,7 +26,6 @@ import org.lwjgl.util.vector.Vector3f;
  */
 public class Configuration {
     public static Vector3f viewingDistanceInChunks = new Vector3f(32.0f,1.0f,32.0f);
-    public static int viewingDistance = 256;
 
     public static Vector2f calcTextureMapCoords(int x, int y) {
         return new Vector2f(1.0f/(float) x,1.0f/(float) y);
