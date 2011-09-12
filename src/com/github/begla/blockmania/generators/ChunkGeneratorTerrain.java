4dbf5892ce68eee99d1207683f2bf4fb6b22df1f
Benjamin Glatzel <benjamin.glatzel@me.com>
2011-09-12 15:35:44 +0200

    Fixes #43-Fix ice on top of water in the snow biome

--- src/com/github/begla/blockmania/generators/ChunkGeneratorTerrain.java
+++ src/com/github/begla/blockmania/generators/ChunkGeneratorTerrain.java
@@ -85,10 +85,10 @@ public class ChunkGeneratorTerrain extends ChunkGenerator {
                         break;
                     }
 
-                    if (y < 32 && y > 0) { // Ocean
+                    if (y <= 32 && y > 0) { // Ocean
                         c.setBlock(x, y, z, (byte) 0x4);
 
-                        if (y == 29) {
+                        if (y == 32) {
                             // Ice layer
                             if (type == BIOME_TYPE.SNOW)
                                 c.setBlock(x, y, z, (byte) 0x11);
