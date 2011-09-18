5c7d294e388a983ecb7f3570f1214c6eb8e1af91
Benjamin Glatzel <benjamin.glatzel@me.com>
2011-09-18 14:17:47 +0200

    - Fixes empty VBOs

--- src/com/github/begla/blockmania/world/chunk/ChunkMesh.java
+++ src/com/github/begla/blockmania/world/chunk/ChunkMesh.java
@@ -84,7 +84,7 @@ public class ChunkMesh {
         VBOManager.getInstance().bufferVboData(_vertexBuffers[id], _vertexElements[id].vertices, GL15.GL_STATIC_DRAW);
     }
 
-    private void renderVbo(int id) {
+    private synchronized void renderVbo(int id) {
         if (_vertexBuffers[id] <= 0)
             return;
 
@@ -148,7 +148,7 @@ public class ChunkMesh {
         }
     }
 
-    public void freeBuffers() {
+    public synchronized void freeBuffers() {
         for (int i = 0; i < _vertexBuffers.length; i++) {
             int id = _vertexBuffers[i];
 
