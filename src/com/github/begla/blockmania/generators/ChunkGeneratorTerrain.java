2678c950496bf1533bf9d62a3bc6cfc29ac313d9
Benjamin Glatzel <benjamin.glatzel@me.com>
2011-11-25 00:36:16 +0100

    Fixes weird looking "mountain chunks". Hopefully.

--- src/com/github/begla/blockmania/generators/ChunkGeneratorTerrain.java
+++ src/com/github/begla/blockmania/generators/ChunkGeneratorTerrain.java
@@ -28,7 +28,7 @@ public class ChunkGeneratorTerrain extends ChunkGenerator {
 
     /* CONST */
     protected static final int SAMPLE_RATE_3D_HOR = 2;
-    protected static final int SAMPLE_RATE_3D_VERT = 16;
+    protected static final int SAMPLE_RATE_3D_VERT = 8;
 
     /**
      * Available types of biomes.
@@ -225,23 +225,23 @@ public class ChunkGeneratorTerrain extends ChunkGenerator {
 
         if (type == BIOME_TYPE.DESERT) {
             freqY *= 1.0;
-            amp *= 0.1;
+            amp *= 0.0;
         } else if (type == BIOME_TYPE.PLAINS) {
             freqY *= 1.0;
-            amp *= 0.1;
+            amp *= 0.0;
         } else if (type == BIOME_TYPE.MOUNTAINS) {
             freqY *= 1.0;
             amp *= 1.0;
         } else if (type == BIOME_TYPE.SNOW) {
             freqY *= 1.0;
-            amp *= 0.3;
+            amp *= 0.9;
         } else if (type == BIOME_TYPE.FOREST) {
             freqY *= 1.0;
-            amp *= 0.5;
+            amp *= 0.9;
         }
 
         double density = calcMountainDensity(x, y * freqY, z) * amp;
-        return -y + ((height * 64.0 + 16.0) + (density * height) * 256.0);
+        return -y + ((height * 64.0 + 16.0) + density * 64.0);
     }
 
     public double calcBaseTerrain(double x, double z) {
