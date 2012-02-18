af7441dada2651cce5ee1def1d67bfa1bcfacd8c
Immortius <immortius@gmail.com>
2012-02-18 14:57:00 +1100

    * Updated Torch block shape  * Improved bender addon:  	- New properties panel 	- Can export AABBs 	- Exports author and export date

--- blender_addons/io_mesh_terasology/export_block_shape.py
+++ blender_addons/io_mesh_terasology/export_block_shape.py
@@ -13,19 +13,95 @@ Each side can be given a custom property of "full" to denote that it fills that 
 
 import bpy
 import os
+import datetime
+import math
+
+def convertVec3d(v):
+		return -v[0], v[2], v[1]
+		
+def convertVec3dAbs(v):
+	return v[0], v[2], v[1]
+
+def writeAutoCollider( 
+		fw,
+		scene	
+		):
+	parts = ["Center", "Top", "Bottom", "Front", "Back", "Left", "Right"]
+
+	min = [100000.0,100000.0,100000.0]
+	max = [-100000.0,-100000.0,-100000.0]
+	
+	for part in parts:
+		if part in bpy.data.objects:
+			mesh = bpy.data.objects[part].data
+			for faceNum, f in enumerate(mesh.faces):
+				faceVerts = f.vertices
+				for vertNum, index in enumerate(faceVerts):
+					vert = mesh.vertices[index].co
+					for i in range(3):
+						if vert[i] > max[i]:
+							max[i] = vert[i]
+						elif vert[i] < min[i]:
+							min[i] = vert[i]
+	
+	pos = [0.0,0.0,0.0]
+	dim = [0.0,0.0,0.0]
+	
+	for i in range(3):
+		pos[i] = 0.5 * (max[i] + min[i])
+		dim[i] = 0.5 * (max[i] - min[i])
+	fw("	Colliders = [\n")	
+	fw("		[\n")
+	fw("			position : [%.6f, %.6f, %.6f],\n" % convertVec3d(pos))
+	fw("			extents : [%.6f, %.6f, %.6f]\n" % convertVec3dAbs(dim))
+	fw("		]\n")	
+	fw("	]\n")
+		
+def writeCollider( 
+		obj, 
+		fw,
+		scene	
+		):
+	if not obj:
+		return
+		
+	mesh = obj.data
+		
+	min = [100000.0,100000.0,100000.0]
+	max = [-100000.0,-100000.0,-100000.0]
+		
+	for faceNum, f in enumerate(mesh.faces):
+		faceVerts = f.vertices
+		for vertNum, index in enumerate(faceVerts):
+			vert = mesh.vertices[index].co
+			for i in range(3):
+				if vert[i] > max[i]:
+					max[i] = vert[i]
+				elif vert[i] < min[i]:
+					min[i] = vert[i]
+	
+	pos = [0.0,0.0,0.0]
+	dim = [0.0,0.0,0.0]
+	
+	for i in range(3):
+		pos[i] = 0.5 * (max[i] + min[i])
+		dim[i] = 0.5 * (max[i] - min[i])
+		
+	fw("		[\n")
+	fw("			position : [%.6f, %.6f, %.6f],\n" % convertVec3d(pos))
+	fw("			extents : [%.6f, %.6f, %.6f]\n" % convertVec3dAbs(dim))
+	fw("		]")	
 
 def writeMeshPart(name, 
 		obj, 
 		fw,
 		scene,
-		apply_modifiers
+		apply_modifiers		
 		):
 		
 	def roundVec3d(v):
 		return round(v[0], 6), round(v[1], 6), round(v[2], 6)
 		
-	def convertVec3d(v):
-		return -v[0], v[2], v[1]
 
 	def roundVec2d(v):
 		return round(v[0], 6), round(v[1], 6)
@@ -55,7 +131,7 @@ def writeMeshPart(name,
 			processedFaces[i].append(len(processedVerts))
 			processedVerts.append((vert, normal, uvs))
 		
-			
+	
 	fw("	%s {\n" % name)
 	
 	fw("		vertices = [")
@@ -101,8 +177,11 @@ def writeMeshPart(name,
 		fw("]")
 		firstFace = False
 	fw("\n		]\n")
-	if "full" in obj or "full" in obj.data:
-		fw("		fullSide = true\n");
+	if "teraFullSide" in obj:
+		if obj.teraFullSide:
+			fw("		fullSide = true\n");
+		else:
+			fw("		fullSide = false\n");
 	else:
 		fw("		fullSide = false\n");
 	
@@ -114,14 +193,23 @@ def writeMeshPart(name,
 def save(operator,
 		 context,
 		 filepath="",
-		 apply_modifiers=True
+		 apply_modifiers=True,
+		 for_embed=False
 		 ):
 
 	scene = context.scene
 
 	file = open(filepath, "w", encoding="utf8", newline="\n")
 	fw = file.write
+	if for_embed:
+		fw("package org.terasology.data.shapes\n\n")
 	fw("shape {\n")
+	
+	fw('	author = "%s"\n' % scene.teraAuthor)
+	
+	now = datetime.datetime.now()
+	
+	fw('	exportDate = "%s"\n' % '{:%Y-%m-%d %H:%M:%S}'.format(now))
 	
 	bpy.ops.object.mode_set(mode='OBJECT')
 	
@@ -130,6 +218,20 @@ def save(operator,
 	for part in parts:
 		if part in bpy.data.objects:
 			writeMeshPart(part, bpy.data.objects[part], fw, scene, apply_modifiers);
+	if scene.teraAutoCollider:
+		writeAutoCollider(fw, scene)
+	else: 
+		first = True
+		for object in bpy.data.objects:
+			if object.teraAABB:
+				if first:
+					fw("	Colliders = [\n")
+					first = False
+				else:
+					fw(",\n")
+				writeCollider(object, fw, scene)
+		if not first:
+			fw("\n	]\n")
 	
 	fw("}\n")
 	file.close()
