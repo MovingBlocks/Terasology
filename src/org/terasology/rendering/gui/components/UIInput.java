3ec5106534f66896b08df81dac12f51264ae5e27
small-jeeper <adeon.k87@gmail.com>
2012-02-20 17:35:14 +1100

    Commit from workspace

--- src/org/terasology/rendering/gui/components/UIInput.java
+++ src/org/terasology/rendering/gui/components/UIInput.java
@@ -31,9 +31,17 @@ public class UIInput extends UIDisplayContainer {
   
   private int    _cursorPosition = 0;
   private String _prevInputValue = new String();
+  
+  private int _maxLength = 42;
+  //Dirty Hack =)
+    
+  private int[] _cursorPosInPix;
+  
   public UIInput(Vector2f size) {
       setSize(size);
       setCrop(true);
+
+      _cursorPosInPix = new int[_maxLength];
 
       _defaultTexture = new UIGraphicsElement("gui_menu");
       _defaultTexture.setVisible(true);
@@ -72,7 +80,7 @@ public class UIInput extends UIDisplayContainer {
         }
 
         if(_mouseDown){
-            clicked();
+            clicked(mousePos);
         }
 
     } else {
@@ -104,9 +112,35 @@ public class UIInput extends UIDisplayContainer {
     }
   }
   
-  public void clicked() {
+  public void clicked(Vector2f mousePos) {
       _focused = true;
+
+      return;
+
+      //TO DO add to UITextCursor
+      /*int localCoordX = (int)(mousePos.x - getPosition().x);
+      int textWidth = _inputText.getTextWidth();
+
+      if(localCoordX>textWidth){
+          _cursorPosition = _inputValue.length();
+          _textCursor.setPosition(new Vector2f(_padding.x + _inputText.getTextWidth()/2, (_textCursor.getPosition().y)));
+      }else{
+          boolean continueSearch = true;
+          int tempPos = _cursorPosition;
+          int tempTextWidth = textWidth;
+          while(continueSearch){
+              textWidth /= 2;
+              tempTextWidth /= 2;
+              if(localCoordX<textWidth){
+
+              }else if(localCoordX>textWidth){
+
+              }
+          }
+      } */
+
   }
+
 //TO DO  Fix it
   public void processKeyboardInput(int key){
     if(_focused){
@@ -141,7 +175,7 @@ public class UIInput extends UIDisplayContainer {
             _cursorPosition--;
             if(_cursorPosition<0){
                 _cursorPosition = 0;
-            }else{
+            }else{      //TO DO add to UITextCursor
                 cursorPosition =  new Vector2f(_padding.x + (_inputText.getTextWidth() - getChangePosition(_cursorPosition))/2, (_textCursor.getPosition().y));
             }
         }else if(key == Keyboard.KEY_RIGHT){
@@ -152,6 +186,9 @@ public class UIInput extends UIDisplayContainer {
                 cursorPosition =  new Vector2f(_padding.x + (_inputText.getTextWidth() - getChangePosition(_cursorPosition))/2, (_textCursor.getPosition().y));
             }
         }else{
+            if(_inputValue.length()>_maxLength){
+                return;
+            }
             char c = Keyboard.getEventCharacter();
             if (c >= 'a' && c < 'z' + 1 || c >= '0' && c < '9' + 1 || c >= 'A' && c < 'Z' + 1 || c == ' ' || c == '_' || c == '.' || c == ',' || c == '!' || c == '-' || c == '(' || c == ')' || c == '"' || c == '\'' || c == ';' || c == '+') {
                 _prevInputValue = _inputValue.toString();
@@ -163,7 +200,7 @@ public class UIInput extends UIDisplayContainer {
            cursorPosition = new Vector2f(_padding.x + _inputText.getTextWidth()/2, (_textCursor.getPosition().y));
         }
         _inputText.setText(_inputValue.toString());
-
+        //TO DO add to UITextCursor
         //TO DO WTF?! What happened with getPositon? Fix it
         _textCursor.setPosition(new Vector2f(cursorPosition));
         //System.out.println(_textCursor.getPosition());
@@ -187,7 +224,7 @@ public class UIInput extends UIDisplayContainer {
   public void setColor(Color color){
     _inputText.setColor(color);
   }
-    
+    //TO DO add to UITextCursor
   private int getChangePosition(int cursorPos){
       if(cursorPos==1){
           int prevWidth    = _inputText.getFont().getWidth(_prevInputValue);
@@ -198,6 +235,27 @@ public class UIInput extends UIDisplayContainer {
                   -
                   _inputText.getFont().getWidth(_inputValue.toString().substring(0,(cursorPos>0?cursorPos-1:0)));
       }
+  }
+
+  /*private void addIndexPos(int index, int posPix){
+      if(index<(_cursorPosInPix.length-1)){
+        _cursorPosInPix[index] = posPix;
+      }
+  }
+
+  private void delteIndexPos(int index, int posPix){
+      if(index<(_cursorPosInPix.length-1)){
+        _cursorPosInPix[index] = posPix;
+      }
+  }*/
+
+  public void setMaxLength(int max){
+      _cursorPosInPix = new int[_maxLength];
+      _maxLength = max;
+  }
+    
+  public int getMaxLength(){
+      return _maxLength;
   }
 
 }
