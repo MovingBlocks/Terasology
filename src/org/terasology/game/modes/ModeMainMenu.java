a4e3ef306bf9740cb98f2508344b8c4260dc001e
Benjamin Glatzel <benjamin.glatzel@me.com>
2012-02-22 21:43:44 +0100

    Adds missing licensing headers and general license file to the repository.

--- src/org/terasology/game/modes/ModeMainMenu.java
+++ src/org/terasology/game/modes/ModeMainMenu.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 Benjamin Glatzel <benjamin.glatzel@me.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.game.modes;
 
 import org.terasology.game.Terasology;
@@ -16,7 +31,6 @@ import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 
 import static org.lwjgl.opengl.GL11.*;
-
 
 /**
  * The class implements the main game menu. The class contains the following screens: game settings,
