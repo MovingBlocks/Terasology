b6315761034c92052c151a0afcc285dc5faa66b1
t3hk0d3 <clouster@yandex.ru>
2012-03-01 13:03:01 +0400

    Improved error checking a bit

--- src/org/terasology/logic/audio/OpenALException.java
+++ src/org/terasology/logic/audio/OpenALException.java
@@ -2,17 +2,28 @@ package org.terasology.logic.audio;
 
 import org.lwjgl.openal.AL10;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class OpenALException extends IllegalStateException {
 
     public static void checkState(String message) {
         int error = AL10.alGetError();
         
         if (error != AL10.AL_NO_ERROR) {
-            throw new OpenALException(message, error);
+            OpenALException exception = new OpenALException(message, error);
+
+            exception.fillInStackTrace();
+
+            List<StackTraceElement> stackTrace = Arrays.asList(exception.getStackTrace());
+            stackTrace.remove(0); // remove first element in stack
+            exception.setStackTrace(stackTrace.toArray(new StackTraceElement[stackTrace.size()]));
+
+            throw exception;
         }
     }
     
     protected OpenALException(String message, int errorCode) {
-        super("OpenAL Error at " + message + " - " + AL10.alGetString(errorCode));
+        super("OpenAL Error (" + errorCode + ") at " + message + " - " + AL10.alGetString(errorCode));
     }
 }
