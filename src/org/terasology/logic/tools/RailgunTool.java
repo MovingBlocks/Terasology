b28679c95cab8f051092e5ec1e0a8a7ace1b46d1
Kai Kratz <kaikratz@googlemail.com>
2012-03-04 00:39:16 +0100

    fixes range

--- src/org/terasology/logic/tools/RailgunTool.java
+++ src/org/terasology/logic/tools/RailgunTool.java
@@ -50,7 +50,7 @@ public class RailgunTool extends SimpleTool {
         //Vector3d origin = blockPos.toVector3d();
         Vector3d dir = _player.getViewingDirection();
         Vector3d origin = (Vector3d)_player.getPosition().clone();
-        for(int s = 4; s <= 2000; s+=30){
+        for(int s = 4; s <= 10000; s+=30){
 
             origin.add(dir);
 
