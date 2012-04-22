c6fc1d8dcb01fba3b67baf46c337bd270542e323
Larry Miller <larrymiller777@yahoo.com>
2012-04-21 21:00:06 -0500

    Add a bunch more new blocks! Yay blocks :-)

---
+++ src/main/groovy/org/terasology/data/blocks/definitions/ClayStone.groovy
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2011 Benjamin Glatzel <benjamin.glatzel@me.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.data.blocks.definitions
+
+/**
+ * ClayStone is a geological term used to describe a clastic sedimentary rock
+ * that is composed primarily of clay-sized particles. Claystones are distinct
+ * from mudstones, which are partly hardened muds that slake when wetted;
+ * claystone is fully hardened material.
+ */
+block {
+    version = 1
+    shape = "cube"
+
+    hardness = 2
+}
