ae007178280b85486f7b06d4a98af15b481a5f62
Immortius <immortius@gmail.com>
2012-05-13 00:23:49 +1000

    Basic network test

---
+++ src/main/java/org/terasology/network/Server.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.network;
+
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFactory;
+import org.jboss.netty.channel.group.ChannelGroup;
+import org.jboss.netty.channel.group.ChannelGroupFuture;
+import org.jboss.netty.channel.group.DefaultChannelGroup;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Immortius
+ */
+public class Server {
+
+    static final ChannelGroup allChannels = new DefaultChannelGroup("tera-server");
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    private ChannelFactory factory;
+    private Channel listenChannel;
+
+    public void start(int port) {
+        factory = new NioServerSocketChannelFactory(Executors.newCachedThreadPool(), Executors.newCachedThreadPool());
+        ServerBootstrap bootstrap = new ServerBootstrap(factory);
+        bootstrap.setPipelineFactory(new TerasologyServerPipelineFactory());
+        bootstrap.setOption("child.tcpNoDelay", true);
+        bootstrap.setOption("child.keepAlive", true);
+        listenChannel = bootstrap.bind(new InetSocketAddress(port));
+        allChannels.add(listenChannel);
+    }
+
+    public void shutdown() {
+        ChannelGroupFuture future = allChannels.close();
+        future.awaitUninterruptibly();
+        factory.releaseExternalResources();
+        logger.log(Level.INFO, "Server shutdown");
+    }
+
+
+
+}
---
+++ src/main/java/org/terasology/network/TerasologyServerHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2012
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.network;
+
+import org.jboss.netty.channel.*;
+import org.terasology.protobuf.NetData;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Immortius
+ */
+public class TerasologyServerHandler extends SimpleChannelUpstreamHandler {
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Override
+    public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e) {
+        Server.allChannels.add(e.getChannel());
+    }
+
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        NetData.NetMessage message = NetData.NetMessage.newBuilder().setConnection(NetData.ConnectMessage.newBuilder().setName("Test Send To Client")).build();
+        e.getChannel().write(message);
+    }
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
+        NetData.NetMessage message = (NetData.NetMessage) e.getMessage();
+        logger.log(Level.INFO, "Received message: " + message.getConnection().getName());
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
+        logger.log(Level.WARNING, "Unexpected exception from client", e.getCause());
+        e.getChannel().close();
+    }
+}
