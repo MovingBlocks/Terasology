6c3cd0dea1a4a441b845ac1b1f041340d3e46c52
Mathias Kalb <mail@mkalb.de>
2012-08-31 10:25:49 +0200

    TerasologyDevelopment and correct gitignore

--- src/dev/java/org/terasology/world/chunks/store/ChunkCachePerformanceTest.java
+++ src/dev/java/org/terasology/world/chunks/store/ChunkCachePerformanceTest.java
@@ -1,14 +1,13 @@
 package org.terasology.world.chunks.store;
 
-import java.io.File;
-
+import org.terasology.TerasologyDevelopment;
 import org.terasology.world.chunks.Chunk;
 import org.terasology.world.chunks.ChunkStore;
 
 public final class ChunkCachePerformanceTest {
 
-    private static final String FOLDER_BENCHMARK_TEMP = "benchmark-temp";
-    private static final int SIZE = 200;
+    private static final String SUB_FOLDER = ChunkCachePerformanceTest.class.getSimpleName();
+    private static final int SIZE = 1000;
 
     private final Chunk[] chunks;
 
@@ -23,39 +22,39 @@ public final class ChunkCachePerformanceTest {
         benchmarkCache(new ChunkStoreGZip());
         benchmarkCache(new ChunkStoreUncompressed());
         benchmarkCache(new ChunkStoreDeflate());
-        benchmarkCache(new ChunkStoreFileSystem(new File(FOLDER_BENCHMARK_TEMP)));
+        benchmarkCache(new ChunkStoreFileSystem(TerasologyDevelopment.getOutputFolder(SUB_FOLDER)));
     }
 
-    private void benchmarkCache(ChunkStore chunkStore) {
+    private void benchmarkCache(final ChunkStore chunkStore) {
         final int size = chunks.length;
         System.out.println("Start test: " + chunkStore + " with size=" + size);
         try {
-            long t1 = System.nanoTime();
+            final long t1 = System.nanoTime();
             for (int k = 0; k < size; ++k) {
                 chunkStore.put(chunks[k]);
             }
-            long t2 = System.nanoTime();
+            final long t2 = System.nanoTime();
             for (int k = 0; k < size; ++k) {
                 if (chunkStore.get(chunks[k].getPos()) == null) {
                     throw new RuntimeException("Cannot load chunk! k=" + k + ", chunkStore=" + chunkStore);
                 }
             }
-            long t3 = System.nanoTime();
+            final long t3 = System.nanoTime();
 
             System.out.printf("%s in memory cache size is %fMb\n", chunkStore.getClass(), chunkStore.size());
-            System.out.printf("%s write time per chunk is %f ms\n", chunkStore.getClass(), ((t2 - t1) / 10e9) / size * 1000);
-            System.out.printf("%s read time per chunk is %f ms\n", chunkStore.getClass(), ((t3 - t2) / 10e9) / size * 1000);
+            System.out.printf("%s write time per chunk is %f ms\n", chunkStore.getClass(), (((t2 - t1) / 10e9) / size) * 1000);
+            System.out.printf("%s read time per chunk is %f ms\n", chunkStore.getClass(), (((t3 - t2) / 10e9) / size) * 1000);
         } finally {
-            long t4 = System.nanoTime();
+            final long t4 = System.nanoTime();
             chunkStore.dispose();
-            long t5 = System.nanoTime();
-            System.out.printf("%s dispose time per chunk is %f ms\n", chunkStore.getClass(), ((t5 - t4) / 10e9) / size * 1000);
+            final long t5 = System.nanoTime();
+            System.out.printf("%s dispose time per chunk is %f ms\n", chunkStore.getClass(), (((t5 - t4) / 10e9) / size) * 1000);
         }
     }
 
-    public static final void main(String[] args) {
+    public static final void main(final String[] args) {
         try {
-            ChunkCachePerformanceTest chunkCachePerformanceTest = new ChunkCachePerformanceTest(SIZE);
+            final ChunkCachePerformanceTest chunkCachePerformanceTest = new ChunkCachePerformanceTest(SIZE);
             chunkCachePerformanceTest.testChunkCash();
         } catch (final RuntimeException e) {
             System.err.println("ChunkCachePerformanceTest failed! " + e.getMessage());
