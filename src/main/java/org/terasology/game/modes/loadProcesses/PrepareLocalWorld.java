cc5d5c70c86b5a14ec09637b4db7e35da613eb03
Immortius <immortius@gmail.com>
2012-12-07 11:05:43 +1100

    Fixed issue with PrepareLocalWorld not pulling in enough of the world around the spawn chunk.

--- src/main/java/org/terasology/game/modes/loadProcesses/PrepareLocalWorld.java
+++ src/main/java/org/terasology/game/modes/loadProcesses/PrepareLocalWorld.java
@@ -78,7 +78,7 @@ public class PrepareLocalWorld implements LoadProcess {
         if (spawningPlayer) {
             spawnZoneEntity = entityManager.create();
             spawnZoneEntity.addComponent(new LocationComponent(new Vector3f(Chunk.SIZE_X / 2, Chunk.SIZE_Y / 2, Chunk.SIZE_Z / 2)));
-            worldRenderer.getChunkProvider().addRegionEntity(spawnZoneEntity, 1);
+            worldRenderer.getChunkProvider().addRegionEntity(spawnZoneEntity, 4);
             targetPos = Vector3i.zero();
         } else {
             CoreRegistry.get(LocalPlayer.class).setEntity(iterator.next());
