925a54e22639c4922ed0079f756eb8a2bf56928a
esa-petri <esereja@yahoo.co.uk>
2012-12-10 17:14:53 +0200

    small music files

---
+++ src/main/java/org/terasology/componentSystem/worldSimulation/WeatherSimulator.java
@@ -0,0 +1,13 @@
+package org.terasology.componentSystem.worldSimulation;
+
+public class WeatherSimulator {
+	//TODO This class will cause lightning to hit you(because thunderstorm) to head while playing. 
+	/*And in desert you vision will be blocked by sand storm(should be easy to code), also snow storms
+	 *  will be your friends in snowy areas.*/
+	//TODO this class could also rarely cause massive damages like meteorites, earthquakes, landslides,
+	//forestfires, land disortions/destructions and other not so player friendly events. And especially some really odd, unexpected, rare and cool events. 
+	//herd of gelatinous cubes suddenly spawning in middle of cubeland wrecking chaos. ]:-)
+	//maybe we need natural disaster class for that. that kind of events will make game more unpredictable(which is interesting)
+	//cow hers spawning in middle of city would be cool too
+	//TODO Remove this class if code should be some where else
+}
