e72b90d142610a788e059cf4622928145e67e2a4
Penn Su <pennsuaccount@gmail.com>
2012-12-22 21:12:25 +0800

    Changed name to functional, also adding the files back

---
+++ mods/functional/src/main/java/org/terasology/functional/components/LocomotiveComponent.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2012 Benjamin Glatzel <benjamin.glatzel@me.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.functional.components;
+
+import com.bulletphysics.collision.dispatch.PairCachingGhostObject;
+import com.google.common.collect.Lists;
+import org.terasology.entitySystem.Component;
+import org.terasology.physics.CollisionGroup;
+import org.terasology.physics.StandardCollisionGroup;
+
+import javax.vecmath.Vector3f;
+import java.util.List;
+
+/**
+ * @author Pencilcheck <pennsu@gmail.com>
+ */
+public final class LocomotiveComponent implements Component {
+
+    public enum LocomotiveType {
+        Train,
+        Boat
+    }
+
+    public int id;
+
+    public CollisionGroup collisionGroup = StandardCollisionGroup.CHARACTER;
+    public List<CollisionGroup> collidesWith = Lists.<CollisionGroup>newArrayList(StandardCollisionGroup.WORLD, StandardCollisionGroup.SENSOR);
+
+    public LocomotiveType locomotiveType = LocomotiveType.Train;
+
+    public Vector3f currentVelocity = new Vector3f();
+
+    public float maximumSpeed = 1f;
+
+    public transient PairCachingGhostObject collider;
+
+    // Movement settings
+    //public float stepHeight = 0.35f;
+    public float stepHeight = 1.4f;
+    public float slopeFactor = 0.6f; // Cosine of the maximum slope traversable. 1 is no slope, 0 is any slope
+
+    public boolean shouldMove = false;
+
+    public boolean activated = false;
+
+    public void toggle() {
+        if(activated) activated = false;
+        else activated = true;
+    }
+
+    public float getId() {
+        return id;
+    }
+
+    public void setId(int new_id) {
+        id = new_id;
+    }
+
+    public LocomotiveType getLocomotiveType() {
+        return locomotiveType;
+    }
+
+    public void setLocomotiveType(LocomotiveType new_locomotiveType) {
+        locomotiveType = new_locomotiveType;
+    }
+
+    public float getMaximumSpeed() {
+        return maximumSpeed;
+    }
+
+    public void setMaximumSpeed(float new_maximumSpeed) {
+        maximumSpeed = new_maximumSpeed;
+    }
+}
---
+++ mods/functional/src/main/java/org/terasology/functional/componentsystem/entityfactory/LocomotiveFactory.java
@@ -0,0 +1,65 @@
+package org.terasology.functional.componentsystem.entityfactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.components.world.LocationComponent;
+import org.terasology.entitySystem.EntityManager;
+import org.terasology.entitySystem.EntityRef;
+import org.terasology.entitySystem.Prefab;
+import org.terasology.functional.componentsystem.controllers.FunctionalSystem;
+import org.terasology.rendering.logic.MeshComponent;
+import org.terasology.utilities.FastRandom;
+
+import javax.vecmath.Color4f;
+import javax.vecmath.Vector3f;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Pencilcheck
+ * Date: 12/22/12
+ * Time: 6:14 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LocomotiveFactory {
+
+    private static final Vector3f[] COLORS = {new Vector3f(1.0f, 1.0f, 0.2f), new Vector3f(1.0f, 0.2f, 0.2f), new Vector3f(0.2f, 1.0f, 0.2f), new Vector3f(1.0f, 1.0f, 0.2f)};
+
+    private static final Logger logger = LoggerFactory.getLogger(FunctionalSystem.class);
+
+    private FastRandom random;
+    private EntityManager entityManager;
+
+    public EntityRef generateLocomotiveCube(Vector3f position, int index) {
+        EntityRef entity = null;
+        switch (index) {
+            case 0: {
+                entity = entityManager.create("functional:train");
+                break;
+            }
+            case 1: {
+                entity = entityManager.create("functional:boat");
+                break;
+            }
+            default:
+                entity = entityManager.create("functional:train");
+        }
+        if (entity == null) {
+            return null;
+        }
+        LocationComponent loc = entity.getComponent(LocationComponent.class);
+        if (loc != null) {
+            loc.setWorldPosition(position);
+            entity.saveComponent(loc);
+        }
+
+        return entity;
+    }
+
+    public void setRandom(FastRandom random) {
+        this.random = random;
+    }
+
+    public void setEntityManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+}
