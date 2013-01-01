1e117a4e2f2f4cd3cce9d697e639e160187cb339
Manuel Brotz <manu.brotz@gmx.ch>
2013-01-01 20:38:40 +0100

    Added a simple framework for benchmarking and implemented benchmarks for tera arrays.

---
+++ src/dev/java/org/terasology/benchmark/BenchmarkCallback.java
@@ -0,0 +1,23 @@
+package org.terasology.benchmark;
+
+/**
+ * BenchmarkCallback allows to watch the progress of the execution of one or many benchmarks.
+ * 
+ * @author Manuel Brotz <manu.brotz@gmx.ch>
+ *
+ */
+public interface BenchmarkCallback {
+    
+    public void begin(Benchmark benchmark, int benchmarkIndex, int benchmarkCount);
+    
+    public void progress(Benchmark benchmark, double percent);
+    
+    public void success(BenchmarkResult result);
+    
+    public void aborted(BenchmarkResult result);
+    
+    public void error(BenchmarkError.Type type, Exception e, BenchmarkResult result);
+    
+    public void fatal(Exception e);
+    
+}
---
+++ src/dev/java/org/terasology/benchmark/BenchmarkResult.java
@@ -0,0 +1,170 @@
+package org.terasology.benchmark;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+
+import com.google.common.base.Preconditions;
+
+/**
+ * BenchmarkResult records the results and the errors of the execution of one particular benchmark.
+ * It also maintains a list of columns which are very useful for pretty printing the results. 
+ * 
+ * @author Manuel Brotz <manu.brotz@gmx.ch>
+ *
+ */
+public abstract class BenchmarkResult {
+    
+    private final List<Column<?>> columns = new LinkedList<Column<?>>();
+
+    private final Class<?> benchmarkClass;
+    private final String title;
+    private final int[] repetitions;
+    private final long[] starttime;
+    private final long[] runtime;
+    private boolean aborted;
+    private final List<BenchmarkError> errors = new LinkedList<BenchmarkError>();
+
+    public static enum Alignment {
+        Left {
+            @Override
+            public String pad(String value, int size) {
+                String result = (value == null ? "" : value);
+                while (result.length() < size) 
+                    result = result + " ";
+                return result;
+            }
+        },
+        
+        Right {
+            @Override
+            public String pad(String value, int size) {
+                String result = (value == null ? "" : value);
+                while (result.length() < size) 
+                    result = " " + result;
+                return result;
+            }
+        };
+        
+        public abstract String pad(String value, int size);
+    }
+    
+    public static abstract class Column<T extends BenchmarkResult> {
+        
+        private final int reps;
+        private final String[] cache;
+        private boolean hasMaxWidth = false;
+        private int maxWidth = 0;
+        
+        public final T owner;
+        public final Alignment alignment;
+        public final String name;
+        
+        protected abstract String getValueInternal(int rep);
+        
+        public Column(T owner, Alignment alignment, String name) {
+            this.owner = Preconditions.checkNotNull(owner);
+            this.alignment = Preconditions.checkNotNull(alignment);
+            this.name = Preconditions.checkNotNull(name);
+            this.reps = owner.getRepetitions();
+            this.cache = new String[reps];
+        }
+        
+        public final String getValue(int rep) {
+            Preconditions.checkElementIndex(rep, reps, "Parameter 'rep'");
+            if (cache[rep] == null) {
+                String v = Preconditions.checkNotNull(getValueInternal(rep), "BenchmarkResult::Column::getValueInternal() must never return null (rep=" + rep + ")");
+                cache[rep] = v;
+                return v;
+            }
+            return cache[rep];
+        }
+        
+        public final int computeMaxWidth() {
+            if (!hasMaxWidth) {
+                int max = name.length();
+                for (int i = 0; i < reps; i++) {
+                    String v = getValue(i);
+                    if (v.length() > max) max = v.length();
+                }
+                hasMaxWidth = true;
+                maxWidth = max;
+            }
+            return maxWidth;
+        }
+    }
+    
+    public BenchmarkResult(Benchmark benchmark) {
+        Preconditions.checkNotNull(benchmark, "Parameter 'benchmark' must not be null");
+        this.benchmarkClass = benchmark.getClass();
+        this.title = benchmark.getTitle();
+        this.repetitions = Preconditions.checkNotNull(benchmark.getRepetitions());
+        Preconditions.checkArgument(repetitions.length > 0, "The length of the parameter 'repetitions' has to be greater than zero");
+        int reps = repetitions.length;
+        this.starttime = new long[reps];
+        this.runtime = new long[reps];
+    }
+    
+    public final Iterator<Column<?>> getColumnsIterator() {
+        return columns.iterator();
+    }
+    
+    public final void addColumn(Column<?> column) {
+        columns.add(Preconditions.checkNotNull(column));
+    }
+    
+    public final int getNumColumns() {
+        return columns.size();
+    }
+    
+    public final Class<?> getBenchmarkClass() {
+        return benchmarkClass;
+    }
+    
+    public final String getTitle() {
+        return title;
+    }
+     
+    public final int getRepetitions() {
+        return repetitions.length;
+    }
+    
+    public final int getRepetitions(int rep) {
+        return repetitions[rep];
+    }
+    
+    public final long getStartTime(int rep) {
+        return starttime[rep];
+    }
+    
+    public final void setStartTime(int rep, long value) {
+        starttime[rep] = value;
+    }
+    
+    public final long getRunTime(int rep) {
+        return runtime[rep];
+    }
+    
+    public final void setRunTime(int rep, long value) {
+        runtime[rep] = value;
+    }
+    
+    public final boolean isAborted() {
+        return aborted;
+    }
+    
+    public final Iterator<BenchmarkError> getErrorsIterator() {
+        return errors.iterator();
+    }
+    
+    public final int getNumErrors() {
+        return errors.size();
+    }
+    
+    public final void addError(BenchmarkError.Type type, Exception error) {
+        this.aborted = this.aborted || Preconditions.checkNotNull(type).abort;
+        errors.add(new BenchmarkError(type, error));
+    }
+
+}
