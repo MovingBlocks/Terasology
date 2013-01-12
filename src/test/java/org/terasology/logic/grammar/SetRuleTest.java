f761a2bddec97df73d253ece16f7b7c07e8c4322
Tobias <skaldarnar@googlemail.com>
2013-01-12 22:15:41 +0100

    Grammar test suite update:   - added test cases for split rules   - added more test cases for the other rules   - moved the building generator test into tests (tests combination of rules)

--- src/test/java/org/terasology/logic/grammar/SetRuleTest.java
+++ src/test/java/org/terasology/logic/grammar/SetRuleTest.java
@@ -2,9 +2,15 @@ package org.terasology.logic.grammar;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.terasology.logic.grammar.shapes.Shape;
+import org.terasology.logic.grammar.shapes.TerminalShape;
+import org.terasology.logic.grammar.shapes.complex.SetRule;
+import org.terasology.math.Vector3i;
 import org.terasology.model.structures.BlockCollection;
 import org.terasology.model.structures.BlockPosition;
+import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockUri;
+import org.terasology.world.block.family.SymmetricFamily;
 import org.terasology.world.block.management.BlockManager;
 
 import java.util.List;
@@ -12,35 +18,45 @@ import java.util.List;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
-/**
- * Created with IntelliJ IDEA.
- * User: tobias
- * Date: 29.08.12
- * Time: 18:04
- * To change this template use File | Settings | File Templates.
- */
+/** Created with IntelliJ IDEA. User: tobias Date: 29.08.12 Time: 18:04 To change this template use File | Settings | File Templates. */
 public class SetRuleTest {
 
-    SetRule successor;
-    ShapeSymbol scope;
-    BlockCollection collection;
-    BlockUri uri;
+    private static final Vector3i position = new Vector3i(1, 1, 1);
+    private static final Vector3i dimension = new Vector3i(2, 3, 4);
+
+    private static final Vector3i negDimension = new Vector3i(2, 3, -4);
+
+    private SetRule setRule, negSetRule;
+    private BlockCollection collection, negCollection;
 
     @Before
     public void setUp() throws Exception {
-        uri = new BlockUri("engine", "CobbleStone");
-        scope = new ShapeSymbol("scope");
+        BlockManager.getInstance().addBlockFamily(new SymmetricFamily(new BlockUri("some:uri"), new Block()));
+        //Block block = BlockManager.getInstance().getBlock("some:uri");
+        BlockUri uri = new BlockUri("some:uri");
 
-        scope.setDimension(2, 4, 6);
+        setRule = new SetRule(uri);
+        setRule.setPosition(position);
+        setRule.setDimension(dimension);
 
-        successor = new SetRule(uri);
-        successor.setDimension(2, 4, 6);
+        negSetRule = new SetRule(uri);
+        negSetRule.setPosition(position);
+        negSetRule.setDimension(negDimension);
 
         collection = new BlockCollection();
-        for (int x = 0; x < 2; x++) {
-            for (int y = 0; y < 4; y++) {
-                for (int z = 0; z < 6; z++) {
+        for (int x = position.x; x < position.x + dimension.x; x++) {
+            for (int y = position.y; y < position.y + dimension.y; y++) {
+                for (int z = position.z; z > position.z - dimension.z; z--) {
                     collection.addBlock(new BlockPosition(x, y, z), BlockManager.getInstance().getBlock(uri));
+                }
+            }
+        }
+
+        negCollection = new BlockCollection();
+        for (int x = position.x; x < position.x + negDimension.x; x++) {
+            for (int y = position.y; y < position.y + negDimension.y; y++) {
+                for (int z = position.z; z < position.z + Math.abs(negDimension.z); z++) {
+                    negCollection.addBlock(new BlockPosition(x, y, z), BlockManager.getInstance().getBlock(uri));
                 }
             }
         }
@@ -48,16 +64,31 @@ public class SetRuleTest {
 
     @Test
     public void testToString() throws Exception {
-        assertEquals("Set ( \"engine:cobblestone\" );", successor.toString());
+        assertEquals("Set ( \"some:uri\" );", setRule.toString());
     }
 
     @Test
-    public void testGetElements() throws Exception {
-        List<Shape> elements = successor.getElements();
+    public void testSetRule() throws Exception {
+        List<Shape> elements = setRule.getElements();
         assertEquals(1, elements.size());
+
         Shape s = elements.get(0);
         assertTrue(s instanceof TerminalShape);
+
         TerminalShape t = (TerminalShape) s;
         assertEquals(collection, t.getValue());
+    }
+
+    @Test
+    public void testNegativeDimension() throws Exception {
+        List<Shape> elements = negSetRule.getElements();
+        assertEquals(1, elements.size());
+
+        Shape s = elements.get(0);
+        assertTrue(s instanceof TerminalShape);
+
+        TerminalShape t = (TerminalShape) s;
+        assertEquals(24, t.getValue().getBlocks().size());
+        assertEquals(negCollection, t.getValue());
     }
 }
