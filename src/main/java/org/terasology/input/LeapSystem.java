a03d9db261e875f522a1f85a53b5c324aae56900
Rasmus Praestholm <cervator@gmail.com>
2013-02-09 00:47:42 -0500

    First crude version that works with full player movement including jumping based purely on the Leap Motion! Still implemented almost entirely wrong and really rough, but hey :-)

--- src/main/java/org/terasology/input/LeapSystem.java
+++ src/main/java/org/terasology/input/LeapSystem.java
@@ -16,12 +16,14 @@
 package org.terasology.input;
 
 import com.leapmotion.leap.*;
+import org.terasology.entitySystem.EntityRef;
 import org.terasology.entitySystem.EventHandlerSystem;
 import org.terasology.game.CoreRegistry;
 import org.terasology.input.binds.ForwardsMovementAxis;
+import org.terasology.input.binds.StrafeMovementAxis;
 import org.terasology.logic.LocalPlayer;
-import org.terasology.logic.manager.GUIManager;
 import org.terasology.performanceMonitor.PerformanceMonitor;
+import org.terasology.physics.character.CharacterMovementComponent;
 
 /**
  * Test implementation for the Leap Motion Controller.
@@ -29,19 +31,29 @@ import org.terasology.performanceMonitor.PerformanceMonitor;
  */
 public class LeapSystem implements EventHandlerSystem {
 
-    private LocalPlayer localPlayer;
-    private GUIManager guiManager;
-    private Controller leapController;
+    /** Max width of the motion space from the *center* (so times 2 total) - 'x' */
+    private static final int MOTION_SPACE_WIDTH_MAX = 150;
+
+    /** Max height of the motion space from the *ground* (total) - 'y' */
+    private static final int MOTION_SPACE_HEIGHT_MAX = 500;
+
+    /** Max depth of the motion space from the *center* (so times 2 total) - 'z' */
+    private static final int MOTION_SPACE_DEPTH_MAX = 150;
+
+    /** Min width of the space we want to be sensitive toward (closer is "dead space") */
+    private static final int MOTION_SPACE_WIDTH_MIN = 40;
+
+    /** Min height of the space we want to be sensitive toward (closer is "dead space") */
+    private static final int MOTION_SPACE_HEIGHT_MIN = 200;
+
+    /** Min depth of the space we want to be sensitive toward (closer is "dead space") */
+    private static final int MOTION_SPACE_DEPTH_MIN = 40;
+
+    /** Counter for total frames processed */
     private int framesTotal = 0;
 
     public void initialise() {
-        // Doesn't seem to persist variables? Have to set them again inside update() :-(
-
-        localPlayer = CoreRegistry.get(LocalPlayer.class);
-        guiManager = CoreRegistry.get(GUIManager.class);
-        leapController = CoreRegistry.get(Controller.class);
-
-        //CoreRegistry.get(EventSystem.class).registerEventHandler(guiManager); // In or out makes no difference?
+        // Doesn't seem to persist variables right the way I use it? Have to set them again inside update() :-(
     }
 
     @Override
@@ -60,10 +72,11 @@ public class LeapSystem implements EventHandlerSystem {
             return;
         }
 
-        leapController = CoreRegistry.get(Controller.class);
+        Controller leapController = CoreRegistry.get(Controller.class);
+        EntityRef playerEntity = CoreRegistry.get(LocalPlayer.class).getEntity();
 
         // Grab the previous frame (that we care about) for comparisons
-        Frame oldFrame = leapController.frame(59);
+        //Frame oldFrame = leapController.frame(59);
 
         // Get the most recent frame and report some basic information
         Frame frame = leapController.frame();
@@ -77,7 +90,7 @@ public class LeapSystem implements EventHandlerSystem {
         if (!frame.hands().empty()) {
             // Get the first hand
             Hand hand = frame.hands().get(0);
-
+/*
             // Check if the hand has any fingers
             FingerList fingers = hand.fingers();
             if (!fingers.empty()) {
@@ -94,7 +107,9 @@ public class LeapSystem implements EventHandlerSystem {
             // Get the hand's sphere radius and palm position
             System.out.println("Hand sphere radius: " + hand.sphereRadius()
                     + " mm, palm position: " + hand.palmPosition());
+            */
 
+            /*
             // Cheap dirty way to base an action off a change :D
             Hand oldHand = oldFrame.hands().get(0);
             float y = hand.palmPosition().getY();
@@ -115,15 +130,74 @@ public class LeapSystem implements EventHandlerSystem {
                 event.prepare("Whatisthisidonteven", -1f, 1f);
                 CoreRegistry.get(LocalPlayer.class).getEntity().send(event);
             }
+            */
 
             // Get the hand's normal vector and direction
             Vector normal = hand.palmNormal();
             Vector direction = hand.direction();
 
             // Calculate the hand's pitch, roll, and yaw angles
-            System.out.println("Hand pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
-                    + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
-                    + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees\n");
+            //System.out.println("Hand pitch: " + Math.toDegrees(direction.pitch()) + " degrees, "
+            //        + "roll: " + Math.toDegrees(normal.roll()) + " degrees, "
+            //        + "yaw: " + Math.toDegrees(direction.yaw()) + " degrees\n");
+
+
+            // Use the hand's location relative to the controller to direct player's direction and speed
+            float x = hand.palmPosition().getX();
+            float y = hand.palmPosition().getY();
+            float z = hand.palmPosition().getZ();
+            float absX = Math.abs(x);
+            float absZ = Math.abs(z);
+
+            System.out.println("X = " + x + ", Y = " + y + ", Z = " + z);
+
+            // TODO: Make half width/depth enough to reach max walk speed
+            // TODO: Use runFactor when more than half *depth* (no pure run-strafing?), but scale it up to max depth
+            // TODO: Use pitch/roll/yaw to change camera direction (impersonate mouse?)
+
+            // Left / right
+            if (absX < MOTION_SPACE_WIDTH_MAX && absX > MOTION_SPACE_WIDTH_MIN) {
+                float xVelocity = (absX - MOTION_SPACE_WIDTH_MIN) / (MOTION_SPACE_WIDTH_MAX - MOTION_SPACE_WIDTH_MIN);
+                BindAxisEvent event = new StrafeMovementAxis();
+                if (x > 0) {
+                    xVelocity *= -1;
+                }
+                // The first and last variable here aren't really needed - first is unused and third gets reset
+                System.out.println("xVelocity is" + xVelocity);
+                event.prepare("leap:stopsideways", xVelocity, 1f);
+                playerEntity.send(event);
+            } else {
+                BindAxisEvent event = new StrafeMovementAxis();
+                System.out.println("Stopping x velocity");
+                event.prepare("leap:stopsideways", 0f, 1f);
+                playerEntity.send(event);
+            }
+
+            // Jump
+            if (y > MOTION_SPACE_HEIGHT_MIN && y < MOTION_SPACE_HEIGHT_MAX) {
+                System.out.println("Tank: Load the jump program");
+                CharacterMovementComponent characterMovement = playerEntity.getComponent(CharacterMovementComponent.class);
+                characterMovement.jump = true;
+            }
+
+            // Forwards / backwards
+            if (absZ < MOTION_SPACE_DEPTH_MAX && absZ > MOTION_SPACE_DEPTH_MIN) {
+                float zVelocity = (absZ - MOTION_SPACE_DEPTH_MIN) / (MOTION_SPACE_DEPTH_MAX - MOTION_SPACE_DEPTH_MIN);
+                BindAxisEvent event = new ForwardsMovementAxis();
+                if (z > 0) {
+                    zVelocity *= -1;
+                }
+                // The first and last variable here aren't really needed - first is unused and third gets reset
+                System.out.println("zVelocity is" + zVelocity);
+                event.prepare("leap:stopforwardbackwards", zVelocity, 1f);
+                playerEntity.send(event);
+            } else {
+                BindAxisEvent event = new ForwardsMovementAxis();
+                System.out.println("Stopping z velocity");
+                event.prepare("leap:stopforwardbackwards", 0f, 1f);
+                playerEntity.send(event);
+            }
+
         }
 
         PerformanceMonitor.endActivity();
