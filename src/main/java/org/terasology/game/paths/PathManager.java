fc2bdd9db2d7ebfbe49e1b7194b7895e38d51d8b
Immortius <immortius@gmail.com>
2013-05-26 09:43:17 +1000

    Improved detection of install path Removed creation of folders in PathManager constructor. Added additional fallback when determining Windows home path.

--- src/main/java/org/terasology/game/paths/PathManager.java
+++ src/main/java/org/terasology/game/paths/PathManager.java
@@ -18,16 +18,25 @@ package org.terasology.game.paths;
 
 import com.google.common.collect.ImmutableList;
 import org.lwjgl.LWJGLUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.game.paths.windows.SavedGamesPathFinder;
 
 import javax.swing.*;
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLDecoder;
 import java.util.List;
 
 /**
  * @author Immortius
  */
 public final class PathManager {
+    public static final String TERASOLOGY_FOLDER_NAME = "Terasology";
+    public static final String TERASOLOGY_HIDDEN_FOLDER_NAME = "/.terasology";
+
     private static final String WORLD_DIR = "worlds";
     private static final String LOG_DIR = "logs";
     private static final String MOD_DIR = "mods";
@@ -48,9 +57,20 @@ public final class PathManager {
     private File nativesPath;
 
     private PathManager() {
-        installPath = new File("").getAbsoluteFile();
+        try {
+            URL path = PathManager.class.getProtectionDomain().getCodeSource().getLocation();
+            File codeLocation = new File(path.toURI());
+            if (codeLocation.isFile()) {
+                installPath = codeLocation.getParentFile();
+            }
+        } catch (URISyntaxException e) {
+            // Can't use logger, because logger not set up when PathManager is used.
+            System.out.println("Failed to conver to uri");
+        }
+        if (installPath == null) {
+            installPath = new File("").getAbsoluteFile();
+        }
         homePath = installPath;
-        updateDirs();
     }
 
     public static PathManager getInstance() {
@@ -68,23 +88,26 @@ public final class PathManager {
     public void useDefaultHomePath() {
         switch (LWJGLUtil.getPlatform()) {
             case LWJGLUtil.PLATFORM_LINUX:
-                homePath = new File("~/.terasology");
+                homePath = new File("~" + TERASOLOGY_HIDDEN_FOLDER_NAME);
                 break;
             case LWJGLUtil.PLATFORM_MACOSX:
-                homePath = new File(System.getProperty("user.home") + "/Library/Application Support/" + "Terasology");
+                homePath = new File(System.getProperty("user.home") + "/Library/Application Support/" + TERASOLOGY_FOLDER_NAME);
                 break;
             case LWJGLUtil.PLATFORM_WINDOWS:
                 String savedGamesPath = SavedGamesPathFinder.findSavedGamesPath();
+                if (savedGamesPath == null) {
+                    savedGamesPath = SavedGamesPathFinder.findDocumentsPath();
+                }
                 File rawPath;
                 if (savedGamesPath != null) {
                     rawPath = new File(savedGamesPath);
                 } else {
                     rawPath = new JFileChooser().getFileSystemView().getDefaultDirectory();
                 }
-                homePath = new File(rawPath, "Terasology");
+                homePath = new File(rawPath, TERASOLOGY_FOLDER_NAME);
                 break;
             default:
-                homePath = new File(System.getProperty("user.home") + "/.terasology");
+                homePath = new File(System.getProperty("user.home") + TERASOLOGY_HIDDEN_FOLDER_NAME);
         }
         updateDirs();
     }
