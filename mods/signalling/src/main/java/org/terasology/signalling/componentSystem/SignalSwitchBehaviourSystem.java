ca40d2f8a82f4a027e4640556f118153aeee65dc
MarcinSc <marcins78@gmail.com>
2013-06-03 02:21:07 +0100

    Added transformer (regulated signal emitter).

--- mods/signalling/src/main/java/org/terasology/signalling/componentSystem/SignalSwitchBehaviourSystem.java
+++ mods/signalling/src/main/java/org/terasology/signalling/componentSystem/SignalSwitchBehaviourSystem.java
@@ -3,6 +3,7 @@ package org.terasology.signalling.componentSystem;
 import org.terasology.entitySystem.*;
 import org.terasology.entitySystem.event.ChangedComponentEvent;
 import org.terasology.signalling.components.SignalConsumerComponent;
+import org.terasology.signalling.components.SignalProducerComponent;
 import org.terasology.world.BlockEntityRegistry;
 import org.terasology.world.WorldProvider;
 import org.terasology.world.block.BlockComponent;
@@ -10,6 +11,7 @@ import org.terasology.world.block.Block;
 import org.terasology.world.block.management.BlockManager;
 import org.terasology.components.world.LocationComponent;
 import org.terasology.math.Vector3i;
+import org.terasology.events.ActivateEvent;
 
 import javax.vecmath.Vector3f;
 
@@ -31,8 +33,23 @@ public class SignalSwitchBehaviourSystem implements EventHandlerSystem {
 
     }
 
+    @ReceiveEvent(components = {BlockComponent.class, LocationComponent.class, SignalProducerComponent.class}) 
+    public void producerActivated(ActivateEvent event, EntityRef entity){
+        SignalProducerComponent producerComponent = entity.getComponent(SignalProducerComponent.class);
+        Vector3i blockLocation = new Vector3i(entity.getComponent(LocationComponent.class).getWorldPosition());
+        Block blockAtLocation = worldProvider.getBlock(blockLocation);
+        String entityPrefab = blockAtLocation.getEntityPrefab();
+        if (entityPrefab.equals("signalling:SignalTransformer")) {
+            int result = producerComponent.signalStrength+1;
+            if (result == 11)
+                result = 0;
+            producerComponent.signalStrength = result;
+            entity.saveComponent(producerComponent);
+        }
+    }
+
     @ReceiveEvent(components = {SignalConsumerComponent.class})
-    public void signalSwitched(ChangedComponentEvent event, EntityRef entity) {
+    public void consumerModified(ChangedComponentEvent event, EntityRef entity) {
         if (entity.hasComponent(BlockComponent.class) && entity.hasComponent(LocationComponent.class)) {
             SignalConsumerComponent consumerComponent = entity.getComponent(SignalConsumerComponent.class);
             Vector3i blockLocation = new Vector3i(entity.getComponent(LocationComponent.class).getWorldPosition());
