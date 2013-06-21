c2d44bb9cc4173661472c73b53ab1f2d93b84bc7
MarcinSc <marcins78@gmail.com>
2013-06-21 11:51:29 +0100

    Now cables connect only to sides they can actually connect to, not just blocks.

--- mods/signalling/src/main/java/org/terasology/signalling/blockFamily/SignalCableBlockFamilyFactory.java
+++ mods/signalling/src/main/java/org/terasology/signalling/blockFamily/SignalCableBlockFamilyFactory.java
@@ -2,6 +2,7 @@ package org.terasology.signalling.blockFamily;
 
 import org.terasology.entitySystem.EntityRef;
 import org.terasology.math.Direction;
+import org.terasology.math.DirectionsUtil;
 import org.terasology.math.Vector3i;
 import org.terasology.signalling.components.SignalConductorComponent;
 import org.terasology.signalling.components.SignalConsumerComponent;
@@ -24,12 +25,36 @@ public class SignalCableBlockFamilyFactory extends ConnectToSixSidesFamilyFactor
                     public boolean isConnectingTo(Vector3i blockLocation, Direction connectDirection, WorldProvider worldProvider, BlockEntityRegistry blockEntityRegistry) {
                         Vector3i neighborLocation = new Vector3i(blockLocation);
                         neighborLocation.add(connectDirection.getVector3i());
+                        final EntityRef blockEntity = blockEntityRegistry.getBlockEntityAt(blockLocation);
+
+                        final SignalConductorComponent conductorComponent = blockEntity.getComponent(SignalConductorComponent.class);
+                        byte cableConnections = conductorComponent.connectionSides;
+                        // Cable cannot be connected to that side
+                        if (!DirectionsUtil.hasDirection(cableConnections, connectDirection))
+                            return false;
+
                         EntityRef neighborEntity = blockEntityRegistry.getBlockEntityAt(neighborLocation);
-                        return (neighborEntity != null &&
-                                (neighborEntity.hasComponent(SignalConductorComponent.class)
-                                        || neighborEntity.hasComponent(SignalProducerComponent.class)
-                                || neighborEntity.hasComponent(SignalConsumerComponent.class)));
+                        return blockEntity != null && neighborEntity != null &&
+                                connectsToNeighbor(connectDirection, neighborEntity);
                     }
                 });
+    }
+
+    private static boolean connectsToNeighbor(Direction connectDirection, EntityRef neighborEntity) {
+        final Direction oppositeDirection = connectDirection.reverse();
+
+        final SignalConductorComponent neighborConductorComponent = neighborEntity.getComponent(SignalConductorComponent.class);
+        if (neighborConductorComponent != null && DirectionsUtil.hasDirection(neighborConductorComponent.connectionSides, oppositeDirection))
+            return true;
+
+        final SignalConsumerComponent neighborConsumerComponent = neighborEntity.getComponent(SignalConsumerComponent.class);
+        if (neighborConsumerComponent != null && DirectionsUtil.hasDirection(neighborConsumerComponent.connectionSides, oppositeDirection))
+            return true;
+
+        final SignalProducerComponent neighborProducerComponent = neighborEntity.getComponent(SignalProducerComponent.class);
+        if (neighborProducerComponent != null && DirectionsUtil.hasDirection(neighborProducerComponent.connectionSides, oppositeDirection))
+            return true;
+
+        return false;
     }
 }
