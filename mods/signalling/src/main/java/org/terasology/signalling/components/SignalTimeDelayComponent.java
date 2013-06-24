01c87c87c657b7aaeb50ac51fdbcbcc912611c63
MarcinSc <marcins78@gmail.com>
2013-06-24 18:28:30 +0100

    Signal on and signal off delay timer gates.

---
+++ mods/signalling/src/main/java/org/terasology/signalling/components/SignalTimeDelayComponent.java
@@ -0,0 +1,7 @@
+package org.terasology.signalling.components;
+
+import org.terasology.entitySystem.Component;
+
+public class SignalTimeDelayComponent implements Component {
+    public long delaySetting;
+}
