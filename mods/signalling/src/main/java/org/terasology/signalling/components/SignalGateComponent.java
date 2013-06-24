96e7bb0dbeed057067566115391000746f11b40c
MarcinSc <marcins78@gmail.com>
2013-06-24 16:06:53 +0100

    Gates can now be rotated and are placed based on placementSide.

---
+++ mods/signalling/src/main/java/org/terasology/signalling/components/SignalGateComponent.java
@@ -0,0 +1,6 @@
+package org.terasology.signalling.components;
+
+import org.terasology.entitySystem.Component;
+
+public class SignalGateComponent implements Component {
+}
---
+++ src/main/java/org/terasology/world/block/family/OneCrucialSideFamilyFactory.java
@@ -0,0 +1,31 @@
+package org.terasology.world.block.family;
+
+import com.google.gson.JsonObject;
+import org.terasology.asset.AssetUri;
+import org.terasology.math.*;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockUri;
+import org.terasology.world.block.loader.BlockDefinition;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+@RegisterBlockFamilyFactory("oneCrucialSide")
+public class OneCrucialSideFamilyFactory implements BlockFamilyFactory {
+    @Override
+    public BlockFamily createBlockFamily(BlockBuilderHelper blockBuilder, AssetUri blockDefUri, BlockDefinition blockDefinition, JsonObject blockDefJson) {
+        Map<Side, Block> blocksBySide = new EnumMap<Side, Block>(Side.class);
+        final Block frontBlock = blockBuilder.constructSimpleBlock(blockDefUri, blockDefinition);
+        blocksBySide.put(Side.FRONT, frontBlock);
+        blocksBySide.put(Side.LEFT, blockBuilder.constructTransformedBlock(blockDefUri, blockDefinition, Rotation.rotate(Yaw.CLOCKWISE_90)));
+        blocksBySide.put(Side.BACK, blockBuilder.constructTransformedBlock(blockDefUri, blockDefinition, Rotation.rotate(Yaw.CLOCKWISE_180)));
+        blocksBySide.put(Side.RIGHT, blockBuilder.constructTransformedBlock(blockDefUri, blockDefinition, Rotation.rotate(Yaw.CLOCKWISE_270)));
+        blocksBySide.put(Side.TOP, blockBuilder.constructTransformedBlock(blockDefUri, blockDefinition, Rotation.rotate(Pitch.CLOCKWISE_90)));
+        blocksBySide.put(Side.BOTTOM, blockBuilder.constructTransformedBlock(blockDefUri, blockDefinition, Rotation.rotate(Pitch.CLOCKWISE_270)));
+        return new OneCrucialSideFamily(new BlockUri(blockDefUri.getPackage(), blockDefUri.getAssetName()), blockDefinition.categories, frontBlock, blocksBySide);
+    }
+
+    public static void main(String[]  args) {
+        System.out.println(Rotation.rotate(Pitch.CLOCKWISE_270).rotate(Side.FRONT));
+    }
+}
