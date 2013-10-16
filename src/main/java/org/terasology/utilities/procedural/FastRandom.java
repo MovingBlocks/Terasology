102b5dfbb0db956ff66e45c31fc1e05ba5013d66
Immortius <immortius@gmail.com>
2013-10-17 00:32:06 +1100

    Fix to randomCharacterString().

--- src/main/java/org/terasology/utilities/procedural/FastRandom.java
+++ src/main/java/org/terasology/utilities/procedural/FastRandom.java
@@ -153,7 +153,7 @@ public class FastRandom {
     public String randomCharacterString(int length) {
         char[] randomChars = new char[length];
         for (int i = 0; i < length; i++) {
-            randomChars[i] = VALID_CHARS[randomInt(VALID_CHARS.length)];
+            randomChars[i] = VALID_CHARS[randomIntAbs(VALID_CHARS.length)];
         }
         return new String(randomChars);
     }
