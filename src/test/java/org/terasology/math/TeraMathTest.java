0c2358dd2c7d9e03b4733c456e6906f00f523033
Marcos Vives Del Sol <socram8888@gmail.com>
2013-10-18 22:09:12 +0200

    Improve TeraMath.pow and add tests to check its behaviour

--- src/test/java/org/terasology/math/TeraMathTest.java
+++ src/test/java/org/terasology/math/TeraMathTest.java
@@ -17,16 +17,63 @@ package org.terasology.math;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import org.junit.Assert;
 
 /**
  * @author Immortius
  */
 public class TeraMathTest {
 
+	private static final double MAX_DOUBLE_ERROR = 0.00001;
+
     @Test
     public void getEdgeRegion() {
         Region3i region = Region3i.createFromMinAndSize(new Vector3i(16,0,16), new Vector3i(16,128,16));
-        assertEquals(Region3i.createFromMinMax(new Vector3i(16, 0, 16), new Vector3i(16, 127, 31)), TeraMath.getEdgeRegion(region, Side.LEFT));
+        Assert.assertEquals(Region3i.createFromMinMax(new Vector3i(16, 0, 16), new Vector3i(16, 127, 31)), TeraMath.getEdgeRegion(region, Side.LEFT));
     }
+
+	// This is a slow yet simple implementation of a basic power function
+	private int naivePow(int base, int exp) {
+		if (base == 0) {
+			return 0;
+		}
+		if (exp < 0) {
+			base = 1 / base;
+			exp = -exp;
+		}
+		int result = 1;
+		while (exp-- > 0) {
+			result *= base;
+		}
+		return result;
+	}
+
+	@Test
+	public void powDouble() {
+		for (int exp = -5; exp <= 5; exp++) {
+			for (double base = -10.0; base <= 10.0; base += 0.2f) {
+				checkError(Math.pow(base, exp) / TeraMath.pow(base, exp));
+			}
+		}
+	}
+
+	@Test
+	public void powInt() {
+		// Only from -2 because most negative exponents return a 0
+		for (int exp = -2; exp <= 8; exp++) {
+			for (int base = -8; base <= 8; base++) {
+				int naive = naivePow(base, exp);
+				Assert.assertEquals(TeraMath.pow(base, exp), naive);
+				Assert.assertEquals(TeraMath.pow((long) base, exp), naive);
+			}
+		}
+	}
+
+	private void checkError(double error) {
+		if (error < 0.0) {
+			error = 1.0 / error;
+		}
+		error -= 1.0;
+		Assert.assertTrue(error <= MAX_DOUBLE_ERROR);
+	}
 }
