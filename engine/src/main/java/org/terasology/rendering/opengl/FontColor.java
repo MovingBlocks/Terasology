157954fc28a662e4e38490f5ed44f6420f558568
Martin Steiger <git@martin-steiger.de>
2014-01-08 20:56:56 +0100

    Implements text color manipulation characters
    Fixes #808

---
+++ engine/src/main/java/org/terasology/rendering/opengl/FontColor.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.rendering.opengl;
+
+import org.newdawn.slick.Color;
+
+/**
+ * Defines a set of special characters that manipulate the font color of a rendered text string.
+ * Use {@link #toChar(int, int, int)}# to get such a char and append it to the text string at the desired position.
+ * Use {@link #getReset()} to reset the color back to the default.
+ * <br/> 
+ * <b>Note:</b> The resolution is only 4 bit per channel (not 8 as usual). 
+ * @author Martin Steiger
+ */
+public final class FontColor {
+    private static final char FIRST_COLOR = 0xE000; // Unicode 6 specs: "Private Use Area: U+E000 -- U+F8FF"
+    private static final char LAST_COLOR  = 0xEFFF; 
+
+    private static final char RESET_COLOR  = 0xF000;
+    
+    private FontColor() {
+        // avoid instantiation
+    }
+
+    /**
+     * @param ch the character to test
+     * @return true for all color chars and the reset char
+     */
+    public static boolean isValid(char ch) {
+        return (ch == RESET_COLOR) || (ch >= FIRST_COLOR && ch <= LAST_COLOR);
+    }
+    
+    /**
+     * @param ch the character to convert
+     * @return the actual color
+     */
+    public static Color toColor(char ch) {
+        
+        int rgb = ch - FIRST_COLOR;
+        int r = (rgb >> 8) & 0xF;
+        int g = (rgb >> 4) & 0xF;
+        int b = (rgb >> 0) & 0xF;
+        
+        return new Color(r << 4, g << 4, b << 4);
+    }
+    
+    /**
+     * Stores the color with (4 + 4 + 4) = 12 bit accuracy as Unicode char
+     * @param r red in [0..255]
+     * @param g green in [0..255]
+     * @param b blue in [0..255]
+     * @return the unicode char
+     */
+    public static char toChar(int r, int g, int b) {
+        int rr = (r >> 4);
+        int rg = (g >> 4);
+        int rb = (b >> 4);
+        
+        int ch = (rr << 8) | (rg << 4) | (rb << 0);
+        return (char) (FIRST_COLOR + ch);
+    }
+
+    /**
+     * @return the color reset char
+     */
+    public static char getReset() {
+        return RESET_COLOR;
+    }
+}
