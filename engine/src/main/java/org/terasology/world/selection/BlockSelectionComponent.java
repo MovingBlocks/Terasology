89992a6c5ef84627a525867468c3d83160c5ded3
mkienenb <mkienenb@gmail.com>
2014-01-09 09:35:31 -0500

    Update BlockSelectionComponent documentation.

--- engine/src/main/java/org/terasology/world/selection/BlockSelectionComponent.java
+++ engine/src/main/java/org/terasology/world/selection/BlockSelectionComponent.java
@@ -22,18 +22,39 @@ import org.terasology.math.Vector3i;
 import org.terasology.rendering.assets.texture.Texture;
 
 /**
+ * @author mkienenb
+ * 
+ * Use this component to track and draw a region of selected blocks.
+ *
+ * @author synopia
+ * 
+ * One example use is with the LocalPlayerBlockSelectionByItemSystem.
  * Add this component to any item entity, to make the item to a selection item. When using such items, a temporary
  * selection is placed in the world. First use sets the starting point, second use finishes the selection and
  * a ApplyBlockSelectionEvent is fired to the player using the selection item.
  * <p/>
- * TODO add customizing properties to this component, like selection color.
- *
- * @author synopia
  */
 @API
 public class BlockSelectionComponent implements Component {
+    /**
+     * Starting point for the block selection.   Used to re-create the currentSelection region when the ending point is changed.
+     */
     public Vector3i startPosition;
+    
+    /**
+     * Selected block region.   Starts as null, then is set to a single block indicated by the start position
+     * when the starting point is set, then represents the region between the starting
+     * and ending points after the ending point is set.
+     */
     public Region3i currentSelection;
-    public Texture texture;
+    
+    /**
+     * If true, block selection will be drawn
+     */
     public boolean shouldRender;
+    
+    /**
+     * Texture used to indicate the selected blocks when drawing block selection.  Defaults to "engine:selection" if not specified.
+     */
+    public Texture texture;
 }
