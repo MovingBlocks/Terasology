e43943ecfb23b26ed6ea9a252a451b29079efb6f
MarcinSc <marcins78@gmail.com>
2014-01-10 21:53:05 +0000

    Growing Oak trees are the only ones generated.

--- modules/Core/src/main/java/org/terasology/core/logic/generators/DefaultGenerators.java
+++ modules/Core/src/main/java/org/terasology/core/logic/generators/DefaultGenerators.java
@@ -16,11 +16,8 @@
 package org.terasology.core.logic.generators;
 
 import com.google.common.collect.ImmutableMap;
+import org.terasology.core.world.generator.chunkGenerators.*;
 import org.terasology.world.WorldBiomeProvider;
-import org.terasology.core.world.generator.chunkGenerators.ForestGenerator;
-import org.terasology.core.world.generator.chunkGenerators.TreeGenerator;
-import org.terasology.core.world.generator.chunkGenerators.TreeGeneratorCactus;
-import org.terasology.core.world.generator.chunkGenerators.TreeGeneratorLSystem;
 import org.terasology.engine.CoreRegistry;
 import org.terasology.math.LSystemRule;
 import org.terasology.world.block.BlockManager;
@@ -33,56 +30,23 @@ public abstract class DefaultGenerators {
 
         BlockManager blockManager = CoreRegistry.get(BlockManager.class);
 
-        Map<Character, LSystemRule> rules = ImmutableMap.<Character, LSystemRule>builder()
-                .put('A', new LSystemRule("[&FFBFA]////[&BFFFA]////[&FBFFA]", 1.0f))
-                .put('B', new LSystemRule("[&FFFA]////[&FFFA]////[&FFFA]", 0.8f)).build();
-        TreeGenerator oakTree = new TreeGeneratorLSystem("FFFFFFA", rules, 4, (float) Math.toRadians(30)).setLeafType(blockManager.getBlock("core:GreenLeaf"))
-                .setBarkType(blockManager.getBlock("core:OakTrunk")).setGenerationProbability(0.08f);
-
-        // Pine
-        rules = ImmutableMap.<Character, LSystemRule>builder()
-                .put('A', new LSystemRule("[&FFFFFA]////[&FFFFFA]////[&FFFFFA]", 1.0f)).build();
-        TreeGenerator pineTree = new TreeGeneratorLSystem("FFFFAFFFFFFFAFFFFA", rules, 4, (float) Math.toRadians(35)).setLeafType(blockManager.getBlock("core:DarkLeaf"))
-                .setBarkType(blockManager.getBlock("core:PineTrunk")).setGenerationProbability(0.05f);
-
-        // Birk
-        rules = ImmutableMap.<Character, LSystemRule>builder()
-                .put('A', new LSystemRule("[&FFFAFFF]////[&FFAFFF]////[&FFFAFFF]", 1.0f))
-                .put('B', new LSystemRule("[&FAF]////[&FAF]////[&FAF]", 0.8f)).build();
-        TreeGenerator birkTree = new TreeGeneratorLSystem("FFFFAFFFFBFFFFAFFFFBFFFFAFFFFBFF", rules, 4, (float) Math.toRadians(35))
-                .setLeafType(blockManager.getBlock("core:DarkLeaf"))
-                .setBarkType(blockManager.getBlock("core:BirkTrunk")).setGenerationProbability(0.02f);
-
-        // Oak variation tree
-        rules = ImmutableMap.<Character, LSystemRule>builder()
-                .put('A', new LSystemRule("[&FFBFA]////[&BFFFA]////[&FBFFAFFA]", 1.0f))
-                .put('B', new LSystemRule("[&FFFAFFFF]////[&FFFAFFF]////[&FFFAFFAA]", 0.8f)).build();
-        TreeGenerator oakVariationTree = new TreeGeneratorLSystem("FFFFFFA", rules, 4, (float) Math.toRadians(35)).setLeafType(blockManager.getBlock("core:GreenLeaf"))
-                .setBarkType(blockManager.getBlock("core:OakTrunk")).setGenerationProbability(0.08f);
-
-        // A red tree
-        rules = ImmutableMap.<Character, LSystemRule>builder()
-                .put('A', new LSystemRule("[&FFAFF]////[&FFAFF]////[&FFAFF]", 1.0f)).build();
-        TreeGenerator redTree = new TreeGeneratorLSystem("FFFFFAFAFAF", rules, 4, (float) Math.toRadians(40)).setLeafType(blockManager.getBlock("core:RedLeaf"))
-                .setBarkType(blockManager.getBlock("core:OakTrunk")).setGenerationProbability(0.05f);
-
         // Cactus
         TreeGenerator cactus = new TreeGeneratorCactus().setTrunkType(blockManager.getBlock("core:Cactus")).setGenerationProbability(0.05f);
 
         // Oak
-        //TreeGenerator oakTree = new SeedTreeGenerator().setBlock(blockManager.getBlock("core:OakSaplingGenerated")).setGenerationProbability(0.08f);
+        TreeGenerator oakTree = new SeedTreeGenerator().setBlock(blockManager.getBlock("core:OakSaplingGenerated")).setGenerationProbability(0.08f);
 
         // Add the trees to the generator lists
         mngr.addTreeGenerator(WorldBiomeProvider.Biome.MOUNTAINS, oakTree);
-        mngr.addTreeGenerator(WorldBiomeProvider.Biome.MOUNTAINS, pineTree);
+        //mngr.addTreeGenerator(WorldBiomeProvider.Biome.MOUNTAINS, pineTree);
 
         mngr.addTreeGenerator(WorldBiomeProvider.Biome.FOREST, oakTree);
-        mngr.addTreeGenerator(WorldBiomeProvider.Biome.FOREST, pineTree);
-        mngr.addTreeGenerator(WorldBiomeProvider.Biome.FOREST, oakVariationTree);
+        //mngr.addTreeGenerator(WorldBiomeProvider.Biome.FOREST, pineTree);
+        //mngr.addTreeGenerator(WorldBiomeProvider.Biome.FOREST, oakVariationTree);
 
-        mngr.addTreeGenerator(WorldBiomeProvider.Biome.SNOW, birkTree);
+        //mngr.addTreeGenerator(WorldBiomeProvider.Biome.SNOW, birkTree);
 
-        mngr.addTreeGenerator(WorldBiomeProvider.Biome.PLAINS, redTree);
+        //mngr.addTreeGenerator(WorldBiomeProvider.Biome.PLAINS, redTree);
         mngr.addTreeGenerator(WorldBiomeProvider.Biome.PLAINS, oakTree);
 
         mngr.addTreeGenerator(WorldBiomeProvider.Biome.DESERT, cactus);
