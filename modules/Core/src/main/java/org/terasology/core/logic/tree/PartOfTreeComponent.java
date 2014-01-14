f1c77fdcd389047268895d88ec54f77c719224de
MarcinSc <marcins78@gmail.com>
2014-01-14 12:58:54 +0000

    Moving growing trees to separate module.

--- modules/Core/src/main/java/org/terasology/core/logic/tree/PartOfTreeComponent.java
+++
@@ -1,9 +0,0 @@
-package org.terasology.core.logic.tree;
-
-import org.terasology.entitySystem.Component;
-
-/**
- * @author Marcin Sciesinski <marcins78@gmail.com>
- */
-public class PartOfTreeComponent implements Component {
-}
