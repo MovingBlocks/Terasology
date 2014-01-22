346d3f7b6ac9a87709b8f2302e6ba762104da10e
Immortius <immortius@gmail.com>
2014-01-22 21:04:03 +1100

    Fixed security to allow YourKit profiling.

--- engine/src/main/java/org/terasology/engine/TerasologyEngine.java
+++ engine/src/main/java/org/terasology/engine/TerasologyEngine.java
@@ -581,6 +581,7 @@ public class TerasologyEngine implements GameEngine {
         moduleSecurityManager.addAPIPackage("gnu.trove.stack.array");
         moduleSecurityManager.addAPIPackage("gnu.trove.strategy");
         moduleSecurityManager.addAPIPackage("javax.vecmath");
+        moduleSecurityManager.addAPIPackage("com.yourkit.runtime");
 
         moduleSecurityManager.addAPIClass(Joiner.class);
         moduleSecurityManager.addAPIClass(IOException.class);
