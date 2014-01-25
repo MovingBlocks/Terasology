38e8b84c5def1e9468fb78c90c368f61b4471a92
Martin Steiger <git@martin-steiger.de>
2014-01-25 12:22:07 +0100

    Moved Profiler to src/dev

--- engine-tests/src/test/java/org/terasology/ProfiledEnvironment.java
+++ engine-tests/src/test/java/org/terasology/ProfiledEnvironment.java
@@ -20,7 +20,7 @@ import java.io.IOException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.registry.CoreRegistry;
+import org.terasology.benchmark.Profiler;
 
 /**
  * Profiles a given Terasology environment setup
@@ -38,6 +38,7 @@ public class ProfiledEnvironment extends Environment {
         this.delegate = delegate;
     }
 
+    @Override
     protected void reset() throws Exception {
         Object total = Profiler.start();
         
@@ -46,84 +47,98 @@ public class ProfiledEnvironment extends Environment {
         logger.info("Total time: " + Profiler.getAsStringAndStop(total));
     }
 
+    @Override
     protected void activateAllModules() {
         Object id = Profiler.start();
         delegate.activateAllModules();
         logger.info("Activate all modules: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void loadPrefabs() {
         Object id = Profiler.start();
         delegate.loadPrefabs();
         logger.info("loadPrefabs: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupComponentManager() {
         Object id = Profiler.start();
         delegate.setupComponentManager();
         logger.info("ComponentManager: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupPathManager() throws IOException {
         Object id = Profiler.start();
         delegate.setupPathManager();
         logger.info("PathManager: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupModuleManager() {
         Object id = Profiler.start();
         delegate.setupModuleManager();
         logger.info("ModuleManager: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupDisplay() {
         Object id = Profiler.start();
         delegate.setupDisplay();
         logger.info("Display: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupConfig() {
         Object id = Profiler.start();
         delegate.setupConfig();
         logger.info("Config: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupAudio() {
         Object id = Profiler.start();
         delegate.setupAudio();
         logger.info("Audio: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupAssetManager() {
         Object id = Profiler.start();
         delegate.setupAssetManager();
         logger.info("Asset: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupBlockManager() {
         Object id = Profiler.start();
         delegate.setupBlockManager();
         logger.info("BlockManager: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupCollisionManager() {
         Object id = Profiler.start();
         delegate.setupCollisionManager();
         logger.info("CollisionManager: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupEntitySystem() {
         Object id = Profiler.start();
         delegate.setupEntitySystem();
         logger.info("EntitySystem: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupNetwork() {
         Object id = Profiler.start();
         delegate.setupNetwork();
         logger.info("NetworkManager: " + Profiler.getAsStringAndStop(id));
     }
 
+    @Override
     protected void setupStorageManager() {
         Object id = Profiler.start();
         delegate.setupStorageManager();
@@ -134,6 +149,7 @@ public class ProfiledEnvironment extends Environment {
      * Cleans up all resources (similar to Autocloseable)
      * @throws Exception if something goes wrong
      */
+    @Override
     public void close() throws Exception {
         delegate.close();
     }
--- engine/src/dev/java/org/terasology/benchmark/Profiler.java
+++ engine/src/dev/java/org/terasology/benchmark/Profiler.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.terasology;
+package org.terasology.benchmark;
 
 import java.util.Map;
 
