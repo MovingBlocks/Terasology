808b0163de22098d86ac3198d75dae18f67316e1
MarcinSc <marcins78@gmail.com>
2014-01-26 20:28:22 +0800

    Added additional events to query and cancel delayed actions.

---
+++ engine/src/main/java/org/terasology/logic/delay/CancelDelayedActionEvent.java
@@ -0,0 +1,9 @@
+package org.terasology.logic.delay;
+
+import org.terasology.entitySystem.event.Event;
+
+/**
+ * @author Marcin Sciesinski <marcins78@gmail.com>
+ */
+public class CancelDelayedActionEvent implements Event {
+}
