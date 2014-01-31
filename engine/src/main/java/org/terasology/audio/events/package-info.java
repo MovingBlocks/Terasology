fed84234d8c0444ce875dce33073391dc601f49a
MarcinSc <marcins78@gmail.com>
2014-01-31 17:59:30 +0000

    Removed wrong comment.

--- engine/src/main/java/org/terasology/audio/events/package-info.java
+++ engine/src/main/java/org/terasology/audio/events/package-info.java
@@ -14,6 +14,19 @@
  * limitations under the License.
  */
 
+/**
+ * This package provides a low-level system for describing classes and fields, with support for construction and field access. Essentially it is a simplified reflection
+ * framework.
+ * <p/>
+ * To support this functionality, a copy-strategy library is used to provide copying support for types. This is used instead of cloning because
+ * <ol>
+ *     <li>Not all types support cloning, including types outside of our control</li>
+ *     <li>Cloning doesn't allow for possible performance improvements though runtime generated code</li>
+ *     <li>Cloning is generally a poorly implemented feature of Java - copy constructors and methods are preferred</li>
+ * </ol>
+ * <p/>
+ * Additionally, ReflectFactory is used to provide support for construction and field access, to allow for alternate implementations.
+ */
 @API package org.terasology.audio.events;
 
 import org.terasology.engine.API;
