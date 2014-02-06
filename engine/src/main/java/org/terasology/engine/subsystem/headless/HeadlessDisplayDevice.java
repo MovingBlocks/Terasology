6002db4efcb91515afd3c9e35fd69a702592374f
mkienenb <mkienenb@gmail.com>
2014-02-05 21:40:34 -0500

    Working headless support

---
+++ engine/src/main/java/org/terasology/engine/subsystem/headless/HeadlessDisplayDevice.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.engine.subsystem.headless;
+
+import org.terasology.engine.subsystem.DisplayDevice;
+
+public class HeadlessDisplayDevice implements DisplayDevice {
+
+    public HeadlessDisplayDevice() {
+    }
+
+    @Override
+    public boolean isHeadless() {
+        return true;
+    }
+
+    @Override
+    public boolean isActive() {
+
+        return true;
+    }
+
+    @Override
+    public boolean isCloseRequested() {
+        return false;
+    }
+
+    @Override
+    public void setFullscreen(boolean state) {
+    }
+
+    @Override
+    public void processMessages() {
+    }
+
+    @Override
+    public void prepareToRender() {
+    }
+}
