178be816cf8a79a71f2498e1285c28eed0f03062
Benjamin Glatzel <benjamin.glatzel@me.com>
2014-02-09 20:25:56 +0100

    Halved samples used for the screen space lightshaft effect while retaining almost the same quality (tweaked and limited sample step size).

--- engine/src/main/java/org/terasology/rendering/shader/ShaderParametersLightShaft.java
+++ engine/src/main/java/org/terasology/rendering/shader/ShaderParametersLightShaft.java
@@ -34,13 +34,13 @@ import javax.vecmath.Vector4f;
 public class ShaderParametersLightShaft extends ShaderParametersBase {
 
     @EditorRange(min = 0.0f, max = 10.0f)
-    private float density = 1.5f;
+    private float density = 1.0f;
     @EditorRange(min = 0.0f, max = 0.01f)
     private float exposure = 0.0075f;
     @EditorRange(min = 0.0f, max = 10.0f)
     private float weight = 8.0f;
     @EditorRange(min = 0.0f, max = 0.99f)
-    private float decay = 0.9f;
+    private float decay = 0.95f;
 
     @Override
     public void applyParameters(Material program) {
