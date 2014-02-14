16f38fbe30501f95c492750cbb67a196ba8747b6
Immortius <immortius@gmail.com>
2014-02-14 16:33:49 +1100

    Fixed getHUDElement methods. Fixed ordering of saved games.

--- engine/src/main/java/org/terasology/rendering/nui/layers/hud/HUDScreenLayer.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/hud/HUDScreenLayer.java
@@ -74,7 +74,7 @@ public class HUDScreenLayer extends CoreScreenLayer {
     }
 
     public ControlWidget getHUDElement(String uri) {
-        return getHUDElement(new AssetUri(uri));
+        return getHUDElement(new AssetUri(AssetType.UI_ELEMENT, uri));
     }
 
     public ControlWidget getHUDElement(AssetUri uri) {
@@ -86,7 +86,7 @@ public class HUDScreenLayer extends CoreScreenLayer {
     }
 
     public <T extends ControlWidget> T getHUDElement(String uri, Class<T> type) {
-        return getHUDElement(new AssetUri(uri), type);
+        return getHUDElement(new AssetUri(AssetType.UI_ELEMENT,uri), type);
     }
 
     public <T extends ControlWidget> T getHUDElement(AssetUri uri, Class<T> type) {
