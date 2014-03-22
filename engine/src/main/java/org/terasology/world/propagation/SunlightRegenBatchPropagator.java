56cafe73085fabe1e26562e116c535708e02365d
Immortius <immortius@gmail.com>
2014-03-22 16:06:20 +1100

    Fixed an issue causing sunlight not to propagate into water correctly during light merge.

--- engine/src/main/java/org/terasology/world/propagation/SunlightRegenBatchPropagator.java
+++ engine/src/main/java/org/terasology/world/propagation/SunlightRegenBatchPropagator.java
@@ -238,7 +238,7 @@ public class SunlightRegenBatchPropagator implements BatchPropagator {
                 } else {
                     int initialDepth = Math.max(adjDepths[depthIndex], ChunkConstants.SUNLIGHT_REGEN_THRESHOLD - start);
                     byte strength = (byte) Math.min(ChunkConstants.MAX_SUNLIGHT, start + initialDepth - ChunkConstants.SUNLIGHT_REGEN_THRESHOLD + 1);
-                    for (int i = initialDepth; i < depth[depthIndex]; ++i) {
+                    for (int i = initialDepth; i <= depth[depthIndex]; ++i) {
                         sunlightPropagator.propagateFrom(toChunk.chunkToWorldPosition(x, ChunkConstants.SIZE_Y - i - 1, z), strength);
                         if (strength < ChunkConstants.MAX_SUNLIGHT) {
                             strength++;
