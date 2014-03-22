217880225d4a27068e9a18ff44f3d112be63648e
Martin Steiger <git@martin-steiger.de>
2014-03-22 12:16:15 +0100

    Upgraded CircularBuffer to List
    * Added set() method
    * Added remove(int) method

--- engine/src/main/java/org/terasology/utilities/collection/CircularBuffer.java
+++ engine/src/main/java/org/terasology/utilities/collection/CircularBuffer.java
@@ -16,13 +16,17 @@
 
 package org.terasology.utilities.collection;
 
+import java.util.AbstractList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
+ * A circular/cyclic/ring buffer. Adding elements is allowed only at the end of the buffer. 
+ * Removing elements can be done through {@link #popFirst()} and {@link #popLast()} or {@link #remove(int)}.
  * @author Immortius
  */
-public final class CircularBuffer<T> implements Iterable<T> {
-    private T[] buffer;
+public final class CircularBuffer<T> extends AbstractList<T> {
+    private final T[] buffer;
     private int startIndex;
     private int occupancy;
 
@@ -34,30 +38,77 @@ public final class CircularBuffer<T> implements Iterable<T> {
         return new CircularBuffer<>(length);
     }
 
+    @Override
     public T get(int index) {
-        if (index >= occupancy) {
+        if (index < 0 || index >= occupancy) {
             throw new IndexOutOfBoundsException();
         }
         return buffer[calculateIndex(index)];
     }
 
+    @Override
+    public T set(int index, T element) {
+        if (index < 0 || index >= occupancy) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        int bufIndex = calculateIndex(index);
+        T prev = buffer[bufIndex];
+        buffer[bufIndex] = element;
+        return prev;
+    }
+
+    /**
+     * @return the last element in the buffer
+     */
     public T getLast() {
         return get(occupancy - 1);
     }
 
-    public void add(T item) {
+    @Override
+    public boolean add(T item) {
         buffer[(startIndex + occupancy) % buffer.length] = item;
         if (occupancy < buffer.length) {
             occupancy++;
         } else {
             startIndex = (startIndex + 1) % buffer.length;
         }
+        return true;
     }
 
+    @Override
+    public T remove(int idx) {
+        if (idx < 0 || idx >= occupancy) {
+            throw new IndexOutOfBoundsException();
+        }
+        
+        T old = buffer[calculateIndex(idx)];
+
+        // shift all elements that are on the right side of element by one
+        for (int i = idx; i < occupancy - 1; i++) {
+            int thisIdx = calculateIndex(i);
+            int nextIdx = calculateIndex(i + 1);
+
+            buffer[thisIdx] = buffer[nextIdx];
+        }
+
+        buffer[calculateIndex(occupancy - 1)] = null;
+        occupancy--;
+        
+        return old;
+    }
+    
+    /**
+     * @return the first element in the buffer
+     */
     public T getFirst() {
         return buffer[startIndex];
     }
 
+    /**
+     * Removes the first element from the buffer
+     * @return the first element
+     */
     public T popFirst() {
         T result = buffer[startIndex];
         buffer[startIndex] = null;
@@ -66,14 +117,31 @@ public final class CircularBuffer<T> implements Iterable<T> {
         return result;
     }
 
+    /**
+     * Removes the last element from the buffer
+     * @return the last element
+     */
+    public T popLast() {
+        int index = calculateIndex(occupancy - 1);
+        T result = buffer[index];
+        buffer[index] = null;
+        occupancy--;
+        return result;
+    }
+
+    @Override
     public boolean isEmpty() {
         return size() == 0;
     }
 
+    @Override
     public int size() {
         return occupancy;
     }
 
+    /**
+     * @return the maximum size of the buffer
+     */
     public int capacity() {
         return buffer.length;
     }
@@ -83,18 +151,20 @@ public final class CircularBuffer<T> implements Iterable<T> {
         return (relativeIndex + startIndex) % buffer.length;
     }
 
-
     @Override
     public Iterator<T> iterator() {
         return new BufferIterator();
     }
 
+    @Override
     public void clear() {
         occupancy = 0;
+        startIndex = 0;
     }
 
     private class BufferIterator implements Iterator<T> {
-        int index;
+        private int index;
+        private int prevIndex = -1;
 
         @Override
         public boolean hasNext() {
@@ -103,12 +173,23 @@ public final class CircularBuffer<T> implements Iterable<T> {
 
         @Override
         public T next() {
+            if (index >= occupancy) {
+                throw new NoSuchElementException();
+            }
+            
+            prevIndex = index;
             return get(index++);
         }
 
         @Override
         public void remove() {
-            throw new UnsupportedOperationException();
+            if (prevIndex >= 0) {
+                CircularBuffer.this.remove(prevIndex);
+                index = prevIndex;
+                prevIndex = -1;
+            } else {
+                throw new IllegalStateException();
+            }
         }
     }
 }
