457f1e11963789b7ec43fb353ad9b7a4233d49e4
MarcinSc <marcins78@gmail.com>
2014-04-01 13:58:58 +0100

    Block structure support feature. Blocks destroyed due to lack of support now drop in a single stack, rather than each stack separately.

--- engine/src/main/java/org/terasology/world/block/entity/BlockSupportRequiredSystem.java
+++ engine/src/main/java/org/terasology/world/block/entity/BlockSupportRequiredSystem.java
@@ -15,12 +15,16 @@
  */
 package org.terasology.world.block.entity;
 
+import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.prefab.PrefabManager;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.logic.health.DestroyEvent;
-import org.terasology.logic.health.EngineDamageTypes;
+import org.terasology.logic.inventory.InventoryComponent;
+import org.terasology.logic.inventory.InventoryManager;
+import org.terasology.logic.inventory.PickupBuilder;
 import org.terasology.math.Side;
 import org.terasology.math.Vector3i;
 import org.terasology.registry.In;
@@ -30,8 +34,8 @@ import org.terasology.world.WorldProvider;
 import org.terasology.world.block.Block;
 import org.terasology.world.block.BlockComponent;
 import org.terasology.world.block.BlockManager;
+import org.terasology.world.block.BlockSupportRequiredComponent;
 import org.terasology.world.block.family.AttachedToSurfaceFamily;
-import org.terasology.world.block.family.BlockFamily;
 
 /**
  * @author Marcin Sciesinski <marcins78@gmail.com>
@@ -42,35 +46,104 @@ public class BlockSupportRequiredSystem extends BaseComponentSystem {
     private WorldProvider worldProvider;
     @In
     private BlockEntityRegistry blockEntityRegistry;
+    @In
+    private EntityManager entityManager;
+    @In
+    private InventoryManager inventoryManager;
+    @In
+    private PrefabManager prefabManager;
+
+    private boolean midDestruction;
+    private EntityRef gatheringEntity;
 
     @ReceiveEvent(components = {BlockComponent.class})
-    public void checkForAttachments(OnChangedBlock event, EntityRef entity) {
+    public void checkForSupportRemoved(OnChangedBlock event, EntityRef entity) {
         if (event.getNewType() == BlockManager.getAir()) {
-            for (Side side : Side.values()) {
-                Vector3i attachedBlockPosition = side.getAdjacentPos(event.getBlockPosition());
-                final Block attachedBlock = worldProvider.getBlock(attachedBlockPosition);
-                final BlockFamily blockFamily = attachedBlock.getBlockFamily();
-                if (blockFamily instanceof AttachedToSurfaceFamily) {
-                    AttachedToSurfaceFamily attachmentFamily = (AttachedToSurfaceFamily) blockFamily;
-                    final Side sideAttachedTo = attachmentFamily.getSideAttachedTo(attachedBlock);
-                    if (sideAttachedTo == side) {
-                        // Block it was attached to was removed
-                        blockEntityRegistry.getBlockEntityAt(attachedBlockPosition).send(new DestroyEvent(EntityRef.NULL, EntityRef.NULL, EngineDamageTypes.DIRECT.get()));
+            boolean initialEvent = !midDestruction;
+
+            if (initialEvent) {
+                midDestruction = true;
+                gatheringEntity = entityManager.create();
+                gatheringEntity.addComponent(new InventoryComponent(20));
+            }
+            try {
+                for (Side side : Side.values()) {
+                    validateSupportForBlockOnSide(event.getBlockPosition(), side);
+                }
+
+                if (initialEvent) {
+                    PickupBuilder pickupBuilder = new PickupBuilder(entityManager);
+                    for (int i = 0; i < 20; i++) {
+                        EntityRef item = inventoryManager.getItemInSlot(gatheringEntity, i);
+                        if (item.exists()) {
+                            pickupBuilder.createPickupFor(item, event.getBlockPosition().toVector3f(), 60, true);
+                        }
                     }
+                }
+            } finally {
+                if (initialEvent) {
+                    midDestruction = false;
+                    gatheringEntity.destroy();
                 }
             }
         }
     }
 
-
-    @ReceiveEvent(components = {BlockComponent.class})
-    public void checkForSupportRemoved(OnChangedBlock event, EntityRef entity) {
-        if (event.getNewType() == BlockManager.getAir()) {
-            final Vector3i blockPosition = Side.TOP.getAdjacentPos(event.getBlockPosition());
-            final Block block = worldProvider.getBlock(blockPosition);
-            if (block.isSupportRequired()) {
-                blockEntityRegistry.getBlockEntityAt(blockPosition).send(new DestroyEvent(EntityRef.NULL, EntityRef.NULL, EngineDamageTypes.DIRECT.get()));
+    private void validateSupportForBlockOnSide(Vector3i replacedBlockPosition, Side side) {
+        final Vector3i blockPosition = side.getAdjacentPos(replacedBlockPosition);
+        boolean destroyBlock = false;
+        Block blockAtPosition = worldProvider.getBlock(blockPosition);
+        if (side == Side.TOP && blockAtPosition.isSupportRequired()) {
+            destroyBlock = true;
+        } else if (blockAtPosition.getBlockFamily() instanceof AttachedToSurfaceFamily
+                && ((AttachedToSurfaceFamily) blockAtPosition.getBlockFamily()).getSideAttachedTo(blockAtPosition) == side) {
+            destroyBlock = true;
+        } else {
+            EntityRef blockEntity = blockEntityRegistry.getExistingBlockEntityAt(blockPosition);
+            if (blockEntity.exists()) {
+                BlockSupportRequiredComponent supportComponent = blockEntity.getComponent(BlockSupportRequiredComponent.class);
+                if (supportComponent != null) {
+                    if (side.isHorizontal() && supportComponent.sideAllowed
+                            && !hasSupport(blockPosition, supportComponent)) {
+                        destroyBlock = true;
+                    } else if (!side.isHorizontal() && (supportComponent.topAllowed || supportComponent.bottomAllowed)
+                            && !hasSupport(blockPosition, supportComponent)) {
+                        destroyBlock = true;
+                    }
+                }
+            } else {
+                BlockSupportRequiredComponent supportComponent = blockAtPosition.getEntity().getComponent(BlockSupportRequiredComponent.class);
+                if (supportComponent != null) {
+                    if (side.isHorizontal() && supportComponent.sideAllowed
+                            && !hasSupport(blockPosition, supportComponent)) {
+                        destroyBlock = true;
+                    } else if (!side.isHorizontal() && (supportComponent.topAllowed || supportComponent.bottomAllowed)
+                            && !hasSupport(blockPosition, supportComponent)) {
+                        destroyBlock = true;
+                    }
+                }
             }
         }
+        if (destroyBlock) {
+            blockEntityRegistry.getBlockEntityAt(blockPosition).send(new DestroyEvent(gatheringEntity, EntityRef.NULL, prefabManager.getPrefab("engine:supportRemovedDamage")));
+        }
+    }
+
+    private boolean hasSupport(Vector3i blockPosition, BlockSupportRequiredComponent supportComponent) {
+        if (supportComponent.bottomAllowed && hasBlockOnSide(blockPosition, Side.BOTTOM)) {
+            return true;
+        }
+        if (supportComponent.topAllowed && hasBlockOnSide(blockPosition, Side.TOP)) {
+            return true;
+        }
+        if (supportComponent.sideAllowed && (hasBlockOnSide(blockPosition, Side.LEFT) || hasBlockOnSide(blockPosition, Side.RIGHT)
+                || hasBlockOnSide(blockPosition, Side.FRONT) || hasBlockOnSide(blockPosition, Side.BACK))) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean hasBlockOnSide(Vector3i blockPosition, Side side) {
+        return worldProvider.getBlock(side.getAdjacentPos(blockPosition)) != BlockManager.getAir();
     }
 }
