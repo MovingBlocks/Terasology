b46df877ed15779ceae09552b7ba33b418d3402c
Martin Steiger <git@martin-steiger.de>
2014-04-27 11:56:18 +0200

    Moved CrashReporter to engine
    This way, other facades can also use it.

--- engine/src/main/java/org/terasology/utilities/CrashReporter.java
+++ engine/src/main/java/org/terasology/utilities/CrashReporter.java
@@ -1,5 +1,20 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-package org.terasology.engine;
+package org.terasology.utilities;
 
 import java.awt.Component;
 import java.awt.Cursor;
@@ -60,7 +75,7 @@ import com.google.common.base.Joiner;
  * Errors are reported to {@link System#err}
  * @author Martin Steiger
  */
-public class CrashReporter {
+public final class CrashReporter {
 
     /**
      * Username Terasology
@@ -70,6 +85,10 @@ public class CrashReporter {
 
     private static final String REPORT_ISSUE_LINK = "https://github.com/MovingBlocks/Terasology/issues/new";
     private static final String JOIN_IRC_LINK = "https://webchat.freenode.net/?channels=terasology";
+
+    private CrashReporter() {
+        // don't create any instances
+    }
     
     public static void report(final Throwable t) {
 
@@ -85,13 +104,13 @@ public class CrashReporter {
                     try {
                         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
                     } catch (Exception e) {
-                        // ignore
+                        e.printStackTrace(System.err);
                     }
                     showModalDialog(t, logFileContent);
                     try {
                         UIManager.setLookAndFeel(oldLaF);
                     } catch (Exception e) {
-                        // ignore
+                        e.printStackTrace(System.err);
                     }
                 }
             });
--- facades/PC/src/main/java/org/terasology/engine/Terasology.java
+++ facades/PC/src/main/java/org/terasology/engine/Terasology.java
@@ -32,6 +32,7 @@ import org.terasology.engine.subsystem.lwjgl.LwjglAudio;
 import org.terasology.engine.subsystem.lwjgl.LwjglGraphics;
 import org.terasology.engine.subsystem.lwjgl.LwjglInput;
 import org.terasology.engine.subsystem.lwjgl.LwjglTimer;
+import org.terasology.utilities.CrashReporter;
 
 import com.google.common.collect.Lists;
 
