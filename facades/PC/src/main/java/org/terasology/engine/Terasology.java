3dc1acd41aec49e398699b48a41867bd4c1a80cf
Emanuele D'Arrigo <manu3d@gmail.com>
2014-05-15 22:42:28 +0200

    Capitalized and shortened static final fields, removed SubsystemList field, now handling many print usage request flags (i.e. -help, /?), added class-level javadoc.

--- facades/PC/src/main/java/org/terasology/engine/Terasology.java
+++ facades/PC/src/main/java/org/terasology/engine/Terasology.java
@@ -37,65 +37,43 @@ import java.nio.file.Paths;
 import java.util.Collection;
 
 /**
- * Main method for launching Terasology
+ * Class providing the main() method for launching Terasology
  *
  * @author Benjamin Glatzel <benjamin.glatzel@me.com>
  * @author Kireev   Anton   <adeon.k87@gmail.com>
+ *
+ * This class can be used to start Terasology via command line and to customize
+ * its launch characteristics. Default locations to store logs and game saves
+ * can be overriden by using the current directory or a specified one as the
+ * home directory. Furthermore, Terasology can be launched headless, to save
+ * resources while acting as a server.
+ *
+ * To print out the command-line help and see usage examples:
+ *
+ *      terasology -help    or    terasology /?
+ *
+ * In case of crashes Terasology logs available information in <logpath>/Terasology.log
  */
 public final class Terasology {
 
-    private static final String PrintUsage_ARG            = "-help";
-    private static final String UseCurrentDirAsHome_ARG   = "-homedir";
-    private static final String UseSpecifiedDirAsHome_ARG = "-homedir=";
-    private static final String StartHeadlessEngine_ARG   = "-headless";
+    private static final String[] PRINT_USAGE_FLAGS = {"--help", "-help", "/help", "-h", "/h",  "/?"};
+    private static final String USE_CURRENT_DIR_AS_HOME = "-homedir";
+    private static final String USE_SPECIFIED_DIR_AS_HOME = "-homedir=";
+    private static final String START_HEADLESS = "-headless";
 
     private static boolean isHeadless = false;
-    private static Collection<EngineSubsystem> subsystemList;
 
     private Terasology() {
     }
 
-    private static void printUsageAndExit() {
-
-        String usage =
-            "Usage:\n" +
-            "\n" +
-            "    terasology [" + PrintUsage_ARG + "] [" + UseCurrentDirAsHome_ARG + "|" + UseSpecifiedDirAsHome_ARG + "<path>] [" + StartHeadlessEngine_ARG + "]\n" +
-            "\n" +
-            "By default Terasology saves data such as game saves and logs into subfolders of a platform-specific \"home directory\".\n" +
-            "Optionally, the user can override the default by using one of the following launch arguments:\n" +
-            "\n" +
-            "    " + UseCurrentDirAsHome_ARG + "           Use the current directory as the home directory.\n" +
-            "    " + UseSpecifiedDirAsHome_ARG + "<path> Use the specified directory as the home directory.\n" +
-            "\n" +
-            "It is also possible to start Terasology in headless mode (no graphics), i.e. to act as a server.\n" +
-            "For this purpose use the " + StartHeadlessEngine_ARG + " launch argument.\n" +
-            "\n" +
-            "Examples:\n" +
-            "\n" +
-            "    Use the current directory as the home directory:\n" +
-            "    terasology " + UseCurrentDirAsHome_ARG + "\n" +
-            "\n" +
-            "    Use \"myPath\" as the home directory:\n" +
-            "    terasology " + UseSpecifiedDirAsHome_ARG + "myPath" + "\n" +
-            "\n" +
-            "    Start terasology in headless mode (no graphics):\n" +
-            "    terasology " + StartHeadlessEngine_ARG + "\n" +
-            "\n" +
-            "    Don't start Terasology, just print this help:\n" +
-            "    terasology " + PrintUsage_ARG + "\n" +
-            "\n";
-
-        System.out.println(usage);
-        System.exit(0);
-    }
-
     public static void main(String[] args) {
+
+        handlePrintUsageRequest(args);
+
         try {
             handleLaunchArguments(args);
-            fillSubsystemList();
 
-            TerasologyEngine engine = new TerasologyEngine(subsystemList);
+            TerasologyEngine engine = new TerasologyEngine(createSubsystemList());
             try {
                 engine.init();
                 if (isHeadless) {
@@ -112,6 +90,7 @@ public final class Terasology {
                     e.printStackTrace();
                 }
             }
+
         } catch (RuntimeException | IOException e) {
             if (!isHeadless) {
                 logException(e);
@@ -120,18 +99,76 @@ public final class Terasology {
         System.exit(0);
     }
 
+    private static void handlePrintUsageRequest(String[] args) {
+        for (String arg : args) {
+            for (String usageArg : PRINT_USAGE_FLAGS) {
+                if (usageArg.equals(arg.toLowerCase())) {
+                    printUsageAndExit();
+                }
+            }
+        }
+    }
+
+    private static void printUsageAndExit() {
+
+        String usage =
+
+            "Usage:\n" +
+            "\n" +
+            "    terasology [" + joinUsageFlags("|") + "] [" + USE_CURRENT_DIR_AS_HOME + "|" + USE_SPECIFIED_DIR_AS_HOME + "<path>] [" + START_HEADLESS + "]\n" +
+            "\n" +
+            "By default Terasology saves data such as game saves and logs into subfolders of a platform-specific \"home directory\".\n" +
+            "Optionally, the user can override the default by using one of the following launch arguments:\n" +
+            "\n" +
+            "    " + USE_CURRENT_DIR_AS_HOME + "           Use the current directory as the home directory.\n" +
+            "    " + USE_SPECIFIED_DIR_AS_HOME + "<path> Use the specified directory as the home directory.\n" +
+            "\n" +
+            "It is also possible to start Terasology in headless mode (no graphics), i.e. to act as a server.\n" +
+            "For this purpose use the " + USE_SPECIFIED_DIR_AS_HOME + " launch argument.\n" +
+            "\n" +
+            "Examples:\n" +
+            "\n" +
+            "    Use the current directory as the home directory:\n" +
+            "    terasology " + USE_CURRENT_DIR_AS_HOME + "\n" +
+            "\n" +
+            "    Use \"myPath\" as the home directory:\n" +
+            "    terasology " + USE_SPECIFIED_DIR_AS_HOME + "myPath" + "\n" +
+            "\n" +
+            "    Start terasology in headless mode (no graphics):\n" +
+            "    terasology " + START_HEADLESS + "\n" +
+            "\n" +
+            "    Don't start Terasology, just print this help:\n" +
+            "    terasology " + PRINT_USAGE_FLAGS[1] + "\n" +
+            "\n";
+
+        System.out.println(usage);
+        System.exit(0);
+    }
+
+    /**
+     * Joins the items in PRINT_USAGE_FLAGS into a single string
+     * using the separator provided. Example:
+     *
+     *      {"-help", "-h", "/?"}  becomes  "-help|-h|/?"
+     */
+    private static String joinUsageFlags(String separator) {
+        String joinedFlags = PRINT_USAGE_FLAGS[0];
+        for (int index = 1; index < PRINT_USAGE_FLAGS.length; index++) {
+            joinedFlags += separator + PRINT_USAGE_FLAGS[index];
+        }
+        return joinedFlags;
+    }
+
     private static void handleLaunchArguments(String[] args) throws IOException {
 
         Path homePath = null;
 
         for (String arg : args) {
-            if(arg.equals(PrintUsage_ARG)) {
-                printUsageAndExit();
-            } else if (arg.startsWith(UseSpecifiedDirAsHome_ARG)) {
-                homePath = Paths.get(arg.substring(UseSpecifiedDirAsHome_ARG.length()));
-            } else if (arg.equals(UseCurrentDirAsHome_ARG)) {
+            if (arg.startsWith(USE_SPECIFIED_DIR_AS_HOME)) {
+                homePath = Paths.get(arg.substring(USE_SPECIFIED_DIR_AS_HOME.length()));
+            } else if (arg.equals(USE_CURRENT_DIR_AS_HOME)) {
                 homePath = Paths.get("");
-            } else if (arg.equals(StartHeadlessEngine_ARG)) {
+            } else if (arg.equals(START_HEADLESS)) {
                 isHeadless = true;
             }
         }
@@ -143,11 +180,11 @@ public final class Terasology {
         }
     }
 
-    private static void fillSubsystemList() {
+    private static Collection<EngineSubsystem> createSubsystemList() {
         if (isHeadless) {
-            subsystemList = Lists.newArrayList(new HeadlessGraphics(), new HeadlessTimer(), new HeadlessAudio(), new HeadlessInput());
+            return Lists.newArrayList(new HeadlessGraphics(), new HeadlessTimer(), new HeadlessAudio(), new HeadlessInput());
         } else {
-            subsystemList = Lists.<EngineSubsystem>newArrayList(new LwjglGraphics(), new LwjglTimer(), new LwjglAudio(), new LwjglInput());
+            return Lists.<EngineSubsystem>newArrayList(new LwjglGraphics(), new LwjglTimer(), new LwjglAudio(), new LwjglInput());
         }
     }
 
