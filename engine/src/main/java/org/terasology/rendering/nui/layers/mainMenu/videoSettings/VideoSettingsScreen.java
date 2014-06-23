fbeac6bc019bd960e62efbe822a22c97a3ab2d5f
Member1221 <TheisCHR@live.dk>
2014-06-23 17:52:04 +0200

    Fixed some issues.

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/VideoSettingsScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/VideoSettingsScreen.java
@@ -13,43 +13,50 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.rendering.nui.layers.mainMenu.videoSettings;
+    package org.terasology.rendering.nui.layers.mainMenu.videoSettings;
 
-import com.google.common.collect.Lists;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.terasology.config.BindsConfig;
-import org.terasology.config.Config;
-import org.terasology.engine.GameEngine;
-import org.terasology.engine.subsystem.lwjgl.LwjglGraphics;
-import org.terasology.input.BindableButton;
-import org.terasology.logic.console.Console;
-import org.terasology.registry.CoreRegistry;
-import org.terasology.registry.In;
-import org.terasology.rendering.ShaderManager;
-import org.terasology.rendering.nui.CoreScreenLayer;
-import org.terasology.rendering.nui.UIWidget;
-import org.terasology.rendering.nui.WidgetUtil;
-import org.terasology.rendering.nui.databinding.BindHelper;
-import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
-import org.terasology.rendering.nui.itemRendering.StringTextRenderer;
-import org.terasology.rendering.nui.widgets.*;
-import org.terasology.rendering.world.ViewDistance;
+    import com.google.common.collect.Lists;
+    import org.slf4j.Logger;
+    import org.slf4j.LoggerFactory;
+    import org.terasology.asset.AssetType;
+    import org.terasology.asset.AssetUri;
+    import org.terasology.asset.Assets;
+    import org.terasology.config.BindsConfig;
+    import org.terasology.config.Config;
+    import org.terasology.engine.GameEngine;
+    import org.terasology.engine.subsystem.lwjgl.LwjglGraphics;
+    import org.terasology.input.BindableButton;
+    import org.terasology.logic.console.Console;
+    import org.terasology.registry.CoreRegistry;
+    import org.terasology.registry.In;
+    import org.terasology.rendering.ShaderManager;
+    import org.terasology.rendering.nui.CoreScreenLayer;
+    import org.terasology.rendering.nui.UIWidget;
+    import org.terasology.rendering.nui.WidgetUtil;
+    import org.terasology.rendering.nui.asset.UIData;
+    import org.terasology.rendering.nui.asset.UIElement;
+    import org.terasology.rendering.nui.databinding.BindHelper;
+    import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+    import org.terasology.rendering.nui.itemRendering.StringTextRenderer;
+    import org.terasology.rendering.nui.layers.mainMenu.inputSettings.InputSettingsScreen;
+    import org.terasology.rendering.nui.widgets.*;
+    import org.terasology.rendering.world.ViewDistance;
 
-import java.util.Arrays;
+    import java.util.Arrays;
 
-/**
- * @author Immortius
- */
-public class VideoSettingsScreen extends CoreScreenLayer {
-    private static final Logger logger = LoggerFactory.getLogger(VideoSettingsScreen.class);
+    /**
+     * @author Immortius
+     */
+    public class VideoSettingsScreen extends CoreScreenLayer {
+        private static final Logger logger = LoggerFactory.getLogger(VideoSettingsScreen.class);
+        private static final AssetUri INPUT_SCREEN_URI = new AssetUri(AssetType.UI_ELEMENT, "engine:inputScreen");
 
-    @In
-    private GameEngine engine;
+        @In
+        private GameEngine engine;
 
 
-    @In
-    private Config config;
+        @In
+        private Config config;
 
     public VideoSettingsScreen() {
     }
@@ -134,6 +141,7 @@ public class VideoSettingsScreen extends CoreScreenLayer {
         WidgetUtil.tryBindCheckbox(this, "outline", BindHelper.bindBeanProperty("outline", config.getRendering(), Boolean.TYPE));
         WidgetUtil.tryBindCheckbox(this, "vsync", BindHelper.bindBeanProperty("vSync", config.getRendering(), Boolean.TYPE));
         WidgetUtil.tryBindCheckbox(this, "eyeAdaptation", BindHelper.bindBeanProperty("eyeAdaptation", config.getRendering(), Boolean.TYPE));
+        WidgetUtil.tryBindCheckbox(this, "fullscreen", BindHelper.bindBeanProperty("fullscreen", config.getRendering(), Boolean.TYPE));
         WidgetUtil.trySubscribe(this, "fovReset", new ActivateEventListener() {
 
             @Override
@@ -143,6 +151,23 @@ public class VideoSettingsScreen extends CoreScreenLayer {
 
             }
         });
+
+        CoreScreenLayer inputScreen = new InputSettingsScreen();
+        inputScreen.setSkin(getSkin());
+        UIData inputScreenData = new UIData(inputScreen);
+        Assets.generateAsset(INPUT_SCREEN_URI, inputScreenData, UIElement.class);
+
+        //region WIP Feature
+        /*
+        WidgetUtil.trySubscribe(this, "advancedVideo", new ActivateEventListener() {
+            @Override
+            public void onActivated(UIWidget button) {
+                getManager().pushScreen("engine:advancedVideoMenuScreen");
+            }
+        });
+        */
+        //endregion
+
         WidgetUtil.trySubscribe(this, "close", new ActivateEventListener() {
             @Override
             public void onActivated(UIWidget button) {
