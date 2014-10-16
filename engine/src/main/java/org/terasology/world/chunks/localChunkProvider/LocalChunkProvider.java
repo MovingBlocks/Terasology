027191454744c16a2454b80267a24c84ef4d167c
Florian Köberle <florian@fkoeberle.de>
2014-10-16 11:21:54 +0200

    Don't announce the chunk provider disposal, when there are still threads creating chunks.
    Otherwise the chunk monitor can end up with references to chunks,
    after the chunk provider disposal is done.

--- engine/src/main/java/org/terasology/world/chunks/localChunkProvider/LocalChunkProvider.java
+++ engine/src/main/java/org/terasology/world/chunks/localChunkProvider/LocalChunkProvider.java
@@ -484,7 +484,6 @@ public class LocalChunkProvider implements ChunkProvider, GeneratingChunkProvide
 
     @Override
     public void dispose() {
-        ChunkMonitor.fireChunkProviderDisposed(this);
         pipeline.shutdown();
         unloadRequestTaskMaster.shutdown(new ChunkUnloadRequest(), true);
         lightMerger.shutdown();
@@ -496,7 +495,11 @@ public class LocalChunkProvider implements ChunkProvider, GeneratingChunkProvide
             store.save();
         }
         nearCache.clear();
-
+        /*
+         * The chunk monitor needs to clear chunk references, so it's important
+         * that no new chunk get created
+         */
+        ChunkMonitor.fireChunkProviderDisposed(this);
     }
 
     @Override
