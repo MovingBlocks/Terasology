6e01bcd7e488f9a8ff23d7a36ae3e8da650b16d5
Paul Fritsche <synopia@gmx.net>
2014-10-22 23:36:30 +0200

    fixes #1294

--- engine/src/main/java/org/terasology/utilities/concurrency/TaskMaster.java
+++ engine/src/main/java/org/terasology/utilities/concurrency/TaskMaster.java
@@ -23,7 +23,12 @@ import org.slf4j.LoggerFactory;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import java.util.Comparator;
-import java.util.concurrent.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author Immortius
