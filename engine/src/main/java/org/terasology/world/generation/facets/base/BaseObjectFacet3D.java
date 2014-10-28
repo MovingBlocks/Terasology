55987f7205cb5a237c2d1cf5a2f21609cc785c11
MarcinSc <marcins78@gmail.com>
2014-10-28 12:05:54 -0700

    Base classes for ObjectFacet3D, similar to ObjectFacet2D.

---
+++ engine/src/main/java/org/terasology/world/generation/facets/base/BaseObjectFacet3D.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.world.generation.facets.base;
+
+import com.google.common.base.Preconditions;
+import org.terasology.math.Region3i;
+import org.terasology.math.Vector3i;
+import org.terasology.world.generation.Border3D;
+
+import java.lang.reflect.Array;
+
+public abstract class BaseObjectFacet3D<T> extends BaseFacet3D implements ObjectFacet3D<T> {
+
+    private T[] data;
+
+    public BaseObjectFacet3D(Region3i targetRegion, Border3D border, Class<T> objectType) {
+        super(targetRegion, border);
+        Vector3i size = getRelativeRegion().size();
+        this.data = (T[]) Array.newInstance(objectType, size.x * size.y * size.z);
+    }
+
+    @Override
+    public T get(int x, int y, int z) {
+        return data[getRelativeIndex(x, y, z)];
+    }
+
+    @Override
+    public T get(Vector3i pos) {
+        return get(pos.x, pos.y, pos.z);
+    }
+
+    @Override
+    public T getWorld(int x, int y, int z) {
+        return data[getWorldIndex(x, y, z)];
+    }
+
+    @Override
+    public T getWorld(Vector3i pos) {
+        return getWorld(pos.x, pos.y, pos.z);
+    }
+
+    @Override
+    public T[] getInternal() {
+        return data;
+    }
+
+    @Override
+    public void set(int x, int y, int z, T value) {
+        data[getRelativeIndex(x, y, z)] = value;
+    }
+
+    @Override
+    public void set(Vector3i pos, T value) {
+        set(pos.x, pos.y, pos.z, value);
+    }
+
+    @Override
+    public void setWorld(int x, int y, int z, T value) {
+        data[getWorldIndex(x, y, z)] = value;
+    }
+
+    @Override
+    public void setWorld(Vector3i pos, T value) {
+        setWorld(pos.x, pos.y, pos.z, value);
+    }
+
+    @Override
+    public void set(T[] newData) {
+        Preconditions.checkArgument(newData.length == data.length);
+        System.arraycopy(newData, 0, data, 0, newData.length);
+    }
+}
---
+++ engine/src/main/java/org/terasology/world/generation/facets/base/ObjectFacet3D.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2014 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.world.generation.facets.base;
+
+import org.terasology.math.Vector3i;
+import org.terasology.world.generation.WorldFacet3D;
+
+/**
+ * @author Immortius
+ */
+public interface ObjectFacet3D<T> extends WorldFacet3D {
+
+    T get(int x, int y, int z);
+
+    T get(Vector3i pos);
+
+    T getWorld(int x, int y, int z);
+
+    T getWorld(Vector3i pos);
+
+    T[] getInternal();
+
+    void set(int x, int y, int z, T value);
+
+    void set(Vector3i pos, T value);
+
+    void setWorld(int x, int y, int z, T value);
+
+    void setWorld(Vector3i pos, T value);
+
+    void set(T[] data);
+}
