ce1262e6d817e8b4909bcb4581ff85c1c1f1d7a8
Florian Köberle <florian@fkoeberle.de>
2015-03-15 14:05:40 +0100

    Remove the placeBlock command as it does not work in multiplayer.
    Reasoning: The command is not worth fixing for multiplayer as it is
    useless. Blocks can be placed via right click. The bad thing about the
    command is that it appears to be working but in multiplayer too, but
    the blocks exist only on client side only.
    
    Interesting for the future might be a command for placing multiple
    blocks at once at a certain location but as it is the command is
    useless.

--- engine/src/main/java/org/terasology/world/block/entity/BlockCommands.java
+++ engine/src/main/java/org/terasology/world/block/entity/BlockCommands.java
@@ -34,12 +34,9 @@ import org.terasology.logic.console.commandSystem.annotations.Sender;
 import org.terasology.logic.inventory.InventoryManager;
 import org.terasology.logic.permission.PermissionManager;
 import org.terasology.logic.players.LocalPlayer;
-import org.terasology.math.Vector3i;
-import org.terasology.math.geom.Vector3f;
 import org.terasology.network.ClientComponent;
 import org.terasology.registry.In;
 import org.terasology.registry.Share;
-import org.terasology.rendering.cameras.Camera;
 import org.terasology.rendering.world.WorldRenderer;
 import org.terasology.world.WorldProvider;
 import org.terasology.world.block.BlockManager;
@@ -85,43 +82,6 @@ public class BlockCommands extends BaseComponentSystem {
     @Override
     public void initialise() {
         blockItemFactory = new BlockItemFactory(entityManager);
-    }
-
-    // TODO: Fix this up for multiplayer (cannot at the moment due to the use of camera), also apply required permission
-    @Command(shortDescription = "Places a block in front of the player", helpText = "Places the specified block in front of the player. " +
-            "The block is set directly into the world and might override existing blocks. After placement the block can be destroyed like any regular placed block.")
-    public String placeBlock(@CommandParam("blockName") String blockName) {
-        Camera camera = renderer.getActiveCamera();
-        Vector3f spawnPos = camera.getPosition();
-        Vector3f offset = camera.getViewingDirection();
-        offset.scale(3);
-        spawnPos.add(offset);
-
-        BlockFamily blockFamily;
-
-        List<BlockUri> matchingUris = blockManager.resolveAllBlockFamilyUri(blockName);
-        if (matchingUris.size() == 1) {
-            blockFamily = blockManager.getBlockFamily(matchingUris.get(0));
-
-        } else if (matchingUris.isEmpty()) {
-            throw new IllegalArgumentException("No block found for '" + blockName + "'");
-        } else {
-            StringBuilder builder = new StringBuilder();
-            builder.append("Non-unique block name, possible matches: ");
-            Joiner.on(", ").appendTo(builder, matchingUris);
-            return builder.toString();
-        }
-
-        if (world != null) {
-            world.setBlock(new Vector3i((int) spawnPos.x, (int) spawnPos.y, (int) spawnPos.z), blockFamily.getArchetypeBlock());
-
-            StringBuilder builder = new StringBuilder();
-            builder.append(blockFamily.getArchetypeBlock());
-            builder.append(" block placed at position (");
-            builder.append((int) spawnPos.x).append((int) spawnPos.y).append((int) spawnPos.z).append(")");
-            return builder.toString();
-        }
-        throw new IllegalArgumentException("Sorry, something went wrong!");
     }
 
     @Command(shortDescription = "Lists all available items (prefabs)",
