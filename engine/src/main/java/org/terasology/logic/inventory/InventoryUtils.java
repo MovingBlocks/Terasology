c1a8cfea7da18c6fdbc3df2ad910ac0260ae53a3
Florian Köberle <florian@fkoeberle.de>
2015-03-29 12:42:37 +0200

    Fix duplicate entwork ids at items get splitted that caused invisible items.

--- engine/src/main/java/org/terasology/logic/inventory/InventoryUtils.java
+++ engine/src/main/java/org/terasology/logic/inventory/InventoryUtils.java
@@ -16,13 +16,11 @@
 package org.terasology.logic.inventory;
 
 import org.terasology.entitySystem.Component;
-import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.inventory.events.BeforeItemPutInInventory;
 import org.terasology.logic.inventory.events.BeforeItemRemovedFromInventory;
 import org.terasology.logic.inventory.events.InventorySlotChangedEvent;
 import org.terasology.logic.inventory.events.InventorySlotStackSizeChangedEvent;
-import org.terasology.registry.CoreRegistry;
 
 import java.util.HashMap;
 import java.util.HashSet;
@@ -358,7 +356,7 @@ public final class InventoryUtils {
         EntityRef itemTo = getItemAt(to, slotTo);
 
         if (!itemTo.exists()) {
-            EntityRef fromCopy = CoreRegistry.get(EntityManager.class).copy(itemFrom);
+            EntityRef fromCopy = itemFrom.copy();
 
             ItemComponent copyItem = fromCopy.getComponent(ItemComponent.class);
             copyItem.stackCount = (byte) amount;
