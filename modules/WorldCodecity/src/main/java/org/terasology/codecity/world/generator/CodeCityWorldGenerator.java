4e623a99f44407b0159236703512a03c055021ed
Ciclop <yayofelipe@gmail.com>
2015-04-26 14:43:45 -0300

    CodeCity: Agregando modulo base para la generacion de mapas estilo codecity.

---
+++ modules/WorldCodecity/src/main/java/org/terasology/codecity/world/generator/CodeCityWorldGenerator.java
@@ -0,0 +1,19 @@
+package org.terasology.codecity.world.generator;
+
+import org.terasology.engine.SimpleUri;
+import org.terasology.world.generation.BaseFacetedWorldGenerator;
+import org.terasology.world.generation.WorldBuilder;
+import org.terasology.world.generator.RegisterWorldGenerator;
+
+
+@RegisterWorldGenerator(id = "codecity", displayName = "CodeCity3", description = "Generates the world using a CodeCity structure")
+public class CodeCityWorldGenerator extends BaseFacetedWorldGenerator {
+    public CodeCityWorldGenerator(SimpleUri uri) {
+        super(uri);
+    }
+
+    @Override
+    protected WorldBuilder createWorld(long seed) {
+        return new WorldBuilder(seed);
+    }
+}
