190def408b71054b13cdd158cbe3690a84707b14
emoreno-94 <edward_moreno_94@hotmail.com>
2015-05-08 15:17:34 -0300

    Agregado archivo de prueba In.java

---
+++ engine/PruebasCheckStyle/In.java
@@ -0,0 +1,13 @@
+public class In {
+	public static void main(String[] args) {
+		if (true);
+		if (true || false);
+		if (true || false || false);
+		if (true || false || false || false);
+		if (true || false || false || false || false );
+		if (true || false || false || false || false || false);
+		if (true || false || false || false || false || false || false);
+		if (true || false || false || false || false || false || false || false);
+		if (true || false || false || false || false || false || false || false || false);
+	}
+}
