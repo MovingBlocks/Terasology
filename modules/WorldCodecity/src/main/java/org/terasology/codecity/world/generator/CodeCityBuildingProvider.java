762fc0dda03a332fb13d0ff59711fa179c80b13a
Alejandro Carrasco <alstrat@alstrat-HP-340-G1.(none)>
2015-05-12 13:02:36 -0300

    The package height have been fidex.

--- modules/WorldCodecity/src/main/java/org/terasology/codecity/world/generator/CodeCityBuildingProvider.java
+++ modules/WorldCodecity/src/main/java/org/terasology/codecity/world/generator/CodeCityBuildingProvider.java
@@ -45,27 +45,65 @@ public class CodeCityBuildingProvider implements FacetProvider {
     private final CodeMapFactory factory = new CodeMapFactory(scale);
 	
 	public CodeCityBuildingProvider() {
-        CodeClass c = new CodeClass(10, 1500);
-        CodeClass c2 = new CodeClass(50, 150);
-        CodePackage p3 = new CodePackage();
-        CodePackage p1 = new CodePackage();
-        CodePackage p2 = new CodePackage();
-        p1.addCodeContent(c2);
-        p1.addCodeContent(c2);
-        p1.addCodeContent(c2);
-        p1.addCodeContent(c2);
-        p2.addCodeContent(c);
-        p3.addCodeContent(p1);
-        p3.addCodeContent(p2);
-
+	    CodePackage facet = new CodePackage();
+        CodePackage generator = new CodePackage();
+        CodePackage map = new CodePackage();
+        CodePackage structure = new CodePackage();
+        CodePackage scale = new CodePackage();
+        CodePackage terasology = new CodePackage();
+        
+        CodeClass fac = new CodeClass(1, 18);
+        facet.addCodeContent(fac);
+        
+        CodeClass bProv = new CodeClass(3, 122);
+        CodeClass bRast = new CodeClass(1, 54);
+        CodeClass gProv = new CodeClass(0, 37);
+        CodeClass gRast = new CodeClass(1, 34);
+        CodeClass wGen = new CodeClass(0, 24);
+        generator.addCodeContent(bProv);
+        generator.addCodeContent(bRast);
+        generator.addCodeContent(gProv);
+        generator.addCodeContent(gRast);
+        generator.addCodeContent(wGen);
+        terasology.addCodeContent(generator);
+ 
+        CodeClass cMap = new CodeClass(0, 83);
+        CodeClass cMapF = new CodeClass(1,101);
+        CodeClass cMapH = new CodeClass(3,147);
+        CodeClass cMapN = new CodeClass(0,57);
+        CodeClass cMapC = new CodeClass(0,36);
+        CodeClass cMapCC = new CodeClass(1,34);
+        CodeClass cMapCP = new CodeClass(1,43);
+        CodeClass cMapO = new CodeClass(4,67);
+        map.addCodeContent(cMap);
+        map.addCodeContent(cMapF);
+        map.addCodeContent(cMapH);
+        map.addCodeContent(cMapN);
+        map.addCodeContent(cMapC);
+        map.addCodeContent(cMapCC);
+        map.addCodeContent(cMapCP);
+        map.addCodeContent(cMapO);
+        terasology.addCodeContent(map);
+ 
+        CodeClass cClas = new CodeClass(2, 45);
+        CodeClass cPac = new CodeClass(1,34);
+        CodeClass cRep = new CodeClass(0,17);
+        structure.addCodeContent(cClas);
+        structure.addCodeContent(cPac);
+        structure.addCodeContent(cRep);
+        terasology.addCodeContent(structure);
+ 
+        CodeClass cSca = new CodeClass(0,28);
+        CodeClass cLin = new CodeClass(0,16);
+        CodeClass cSqu = new CodeClass(0,21);
+        scale.addCodeContent(cSca);
+        scale.addCodeContent(cLin);
+        scale.addCodeContent(cSqu);
+        structure.addCodeContent(scale);
+ 
         List<DrawableCode> code = new ArrayList<DrawableCode>();
-        code.add(p1.getDrawableCode());
-        code.add(p2.getDrawableCode());
-        code.add(p2.getDrawableCode());
-        code.add(p2.getDrawableCode());
-        code.add(p2.getDrawableCode());
-        code.add(p3.getDrawableCode());
-
+        code.add(terasology.getDrawableCode());
+ 
         codeMap = factory.generateMap(code);
 	}
 
@@ -104,7 +142,7 @@ public class CodeCityBuildingProvider implements FacetProvider {
                 facet.setWorld(x, y, z);
             
             if (obj.isOrigin())
-                processMap(facet, region, obj.getObject().getSubmap(scale, factory), new Vector2i(x+1, y+1), z+1);
+                processMap(facet, region, obj.getObject().getSubmap(scale, factory), new Vector2i(x+1, y+1), z);
         }
     }
 }
