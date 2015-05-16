1fe675954e33cb411631e300ecd1092386630d87
sking32 <el.ariel.cl@gmail.com>
2015-05-16 15:41:00 -0300

    Create buildInputString method and fix jar's path

--- modules/PMDColoring/src/main/java/PMDCommand.java
+++ modules/PMDColoring/src/main/java/PMDCommand.java
@@ -8,9 +8,9 @@ import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.logic.console.Console;
 import org.terasology.logic.console.commandSystem.annotations.Command;
 import org.terasology.logic.console.commandSystem.annotations.CommandParam;
-import org.terasology.logic.console.commands.ThreadPMDExecution;
 import org.terasology.logic.permission.PermissionManager;
 import org.terasology.registry.In;
+
 
 @RegisterSystem
 public class PMDCommand extends BaseComponentSystem{
@@ -22,42 +22,16 @@ public class PMDCommand extends BaseComponentSystem{
     {
     	if (rules == null) rules = "basic";
     	if (outPutType == null) outPutType = "text";
-    	String inputString = "java -classpath .\\engine\\libs\\pmd\\lib\\* net.sourceforge.pmd.PMD -d "+ sourcePath+" -f "+outPutType+" -R rulesets/java/"+ rules+".xml";
-    	//String inputString = "C:\\Users\\Manuel\\Desktop\\pmd-bin-5.3.1\\bin\\pmd.bat -d "+ sourcePath+" -f text -R rulesets/java/"+ rules+".xml";
-    	/*
-    	System.out.println(inputString);
-    	Process process;
-		process = Runtime.getRuntime().exec(inputString);
-		InputStream is = process.getInputStream();
-		 InputStreamReader isr = new InputStreamReader(is);
-		 BufferedReader br = new BufferedReader(isr);
-			
-		 StringBuilder sb = new StringBuilder();
-		 String line;
-		 while ((line = br.readLine()) != null) 
-		 {
-			 console.addMessage(line);	
-			 System.out.println(line);
-		 }
-		 */
-		 /* 
-    	PrintStream out = System.out;
-    	System.setOut(new PrintStream(new ConsoleOutputStream(console)));
-    	String[] args = {"-d", "C:\\Users\\Manuel\\Documents\\Terasology-develop\\modules\\Core\\src\\main\\java\\org\\terasology\\core\\world\\generator\\facetProviders\\PositionFilters.java", "-f", "text", "-R" ,"rulesets/java/basic.xml"};
-			PMD.main(args);
-			System.setOut(out);
-		*/ 
-		
+    	String inputString = buildInputString(sourcePath, outPutType, rules);
+    	
     	Thread t = new Thread(new ThreadPMDExecution(inputString,console));
-		 t.start();
-		 
-    	/*
-		 String line = "-d C:\\Users\\Manuel\\workspace\\Tarea2Teoria -f text -R rulesets/java/basic.xml";
-	    	String []args1 = line.split(" ");
-			 PMD.main(args1);
-			 */
-		 return "Esperando por resultados del analisis...";
+		t.start();
+		
+		return "Esperando por resultados del analisis...";
     }
+	private String buildInputString(String sourcePath, String outPutType, String rules) {
+		return "java -classpath .\\modules\\PMDColoring\\libs\\pmd\\lib\\* net.sourceforge.pmd.PMD -d "+ sourcePath+" -f "+outPutType+" -R rulesets/java/"+ rules+".xml";
+	}
 }
 
 class ThreadPMDExecution implements Runnable
