fd56e8023551c99b7811823336b635b9412df7fa
Florian Köberle <florian@fkoeberle.de>
2015-08-16 19:06:38 +0200

    Added the possiblity to create entities that represent floating texts.

---
+++ engine/src/main/java/org/terasology/rendering/logic/FloatingTextRenderer.java
@@ -0,0 +1,179 @@
+package org.terasology.rendering.logic;
+
+import com.google.api.client.util.Maps;
+import org.terasology.assets.management.AssetManager;
+import org.terasology.entitySystem.entity.EntityManager;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.entity.lifecycleEvents.BeforeDeactivateComponent;
+import org.terasology.entitySystem.entity.lifecycleEvents.OnChangedComponent;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.entitySystem.systems.RenderSystem;
+import org.terasology.logic.location.LocationComponent;
+import org.terasology.math.geom.Vector3f;
+import org.terasology.registry.In;
+import org.terasology.rendering.assets.font.Font;
+import org.terasology.rendering.assets.font.FontMeshBuilder;
+import org.terasology.rendering.assets.material.Material;
+import org.terasology.rendering.assets.mesh.Mesh;
+import org.terasology.rendering.cameras.Camera;
+import org.terasology.rendering.nui.Color;
+import org.terasology.rendering.nui.HorizontalAlign;
+import org.terasology.rendering.opengl.OpenGLUtil;
+import org.terasology.world.WorldProvider;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import static org.lwjgl.opengl.GL11.GL_DEPTH_TEST;
+import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glPopMatrix;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glScaled;
+import static org.lwjgl.opengl.GL11.glTranslated;
+
+
+@RegisterSystem(RegisterMode.CLIENT)
+public class FloatingTextRenderer extends BaseComponentSystem implements  RenderSystem {
+
+    private static final int PIXEL_PER_METER = 100;
+
+    private static final float METER_PER_PIXEL= 1.0f / PIXEL_PER_METER;
+
+    @In
+    private EntityManager entityManager;
+
+    @In
+    private AssetManager assetManager;
+
+    @In
+    private WorldProvider worldProvider;
+
+    @In
+    private Camera camera;
+
+    private Font font;
+    private Material underlineMaterial;
+    private Map<EntityRef, Map<Material, Mesh>> entityMeshCache = Maps.newHashMap();
+
+    @Override
+    public void initialise() {
+        this.font = assetManager.getAsset("engine:default", Font.class).get();
+        this.underlineMaterial = assetManager.getAsset("engine:UIUnderline", Material.class).get();
+    }
+
+    private void render(Iterable<EntityRef> floatingTextEntities) {
+        glDisable(GL_DEPTH_TEST);
+
+        Vector3f cameraPosition = camera.getPosition();
+
+        for (EntityRef entity : floatingTextEntities) {
+            LocationComponent location = entity.getComponent(LocationComponent.class);
+            if (location == null) {
+                continue;
+            }
+
+            Vector3f worldPos = location.getWorldPosition();
+            if (!worldProvider.isBlockRelevant(worldPos)) {
+                continue;
+            }
+
+            FloatingTextComponent floatingText = entity.getComponent(FloatingTextComponent.class);
+
+            String text = floatingText.text;
+            Color baseColor = floatingText.textColor;
+            Color shadowColor = floatingText.textShadowColor;
+            boolean underline = false;
+            int textWidth = font.getWidth(text);
+
+            FontMeshBuilder meshBuilder = new FontMeshBuilder(underlineMaterial);
+
+            Map<Material, Mesh> meshMap = entityMeshCache.get(entity);
+            if (meshMap == null) {
+                meshMap = meshBuilder
+                        .createTextMesh(font, Arrays.asList(text), textWidth, HorizontalAlign.CENTER, baseColor,
+                                shadowColor, underline);
+                entityMeshCache.put(entity, meshMap);
+            }
+            glPushMatrix();
+
+
+            glTranslated(worldPos.x - cameraPosition.x, worldPos.y - cameraPosition.y, worldPos.z - cameraPosition.z);
+            OpenGLUtil.applyBillboardOrientation();
+            glScaled(METER_PER_PIXEL, -METER_PER_PIXEL, METER_PER_PIXEL);
+            glTranslated(-(textWidth/2.0), 0.0, 0.0);
+            for (Map.Entry<Material, Mesh> meshMapEntry : meshMap.entrySet()) {
+                Mesh mesh = meshMapEntry.getValue();
+                Material material = meshMapEntry.getKey();
+                material.enable();
+                material.bindTextures();
+                material.setFloat4("croppingBoundaries", Float.MIN_VALUE, Float.MAX_VALUE,
+                        Float.MIN_VALUE, Float.MAX_VALUE);
+                material.setFloat2("offset", 0.0f, 0.0f);
+                material.setFloat("alpha", 1.0f);
+                mesh.render();
+            }
+
+            glPopMatrix();
+        }
+        glEnable(GL_DEPTH_TEST);
+    }
+
+    private void diposeMeshMap(Map<Material, Mesh> meshMap) {
+        for (Map.Entry<Material, Mesh> meshMapEntry : meshMap.entrySet()) {
+            Mesh mesh = meshMapEntry.getValue();
+            mesh.dispose();
+            // Note: Material belongs to font and must not be disposed
+        }
+    }
+
+    @Override
+    public void renderOpaque() {
+    }
+
+    @Override
+    public void renderAlphaBlend() {
+        render(entityManager.getEntitiesWith(FloatingTextComponent.class, LocationComponent.class));
+    }
+
+    @Override
+    public void renderFirstPerson() {
+    }
+
+    @Override
+    public void renderOverlay() {
+    }
+
+    @Override
+    public void renderShadows() {
+    }
+
+    @ReceiveEvent(components = {FloatingTextComponent.class })
+    public void onDisplayNameChange(OnChangedComponent event, EntityRef entity) {
+        disposeCachedMeshOfEntity(entity);
+    }
+
+
+    @ReceiveEvent(components = {FloatingTextComponent.class })
+    public void onNameTagOwnerRemoved(BeforeDeactivateComponent event, EntityRef entity) {
+        disposeCachedMeshOfEntity(entity);
+    }
+
+    private void disposeCachedMeshOfEntity(EntityRef entity) {
+        Map<Material, Mesh> meshMap = entityMeshCache.remove(entity);
+        if (meshMap != null) {
+            diposeMeshMap(meshMap);
+        }
+    }
+
+    @Override
+    public void shutdown() {
+        for (Map<Material, Mesh> meshMap: entityMeshCache.values()) {
+            diposeMeshMap(meshMap);
+        }
+        entityMeshCache.clear();
+    }
+}
