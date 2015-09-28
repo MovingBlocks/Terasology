24dd610dd13f2c84b825886adbf0577901c22711
Martin Steiger <git@martin-steiger.de>
2015-09-28 18:47:44 +0200

    Add unit test for facet provider chain update

--- engine-tests/src/test/java/org/terasology/world/generation/WorldBuilderTest.java
+++ engine-tests/src/test/java/org/terasology/world/generation/WorldBuilderTest.java
@@ -24,6 +24,8 @@ import org.terasology.world.generation.facets.base.BaseFacet3D;
 import org.terasology.world.generator.plugin.WorldGeneratorPluginLibrary;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 public class WorldBuilderTest {
 
@@ -91,7 +93,37 @@ public class WorldBuilderTest {
         assertEquals(Region3i.createFromMinAndSize(new Vector3i(-4, -1, -4), new Vector3i(9, 3, 9)), facet2.getWorldRegion());
     }
 
+
+    @Test
+    public void testUpdating() {
+        WorldBuilder worldBuilder = new WorldBuilder(context.get(WorldGeneratorPluginLibrary.class));
+        worldBuilder.setSeed(12);
+        worldBuilder.addProvider(new Facet1Provider());
+        worldBuilder.addProvider(new Facet2Provider());
+        worldBuilder.addProvider(new Facet3Provider());
+        worldBuilder.addProvider(new Facet4Provider());
+        worldBuilder.addProvider(new FacetUpdater());
+
+        Region3i regionToGenerate = Region3i.createFromCenterExtents(new Vector3i(), 1);
+
+        World world;
+        Region regionData;
+
+        // try checking updated facet
+        world = worldBuilder.build();
+        regionData = world.getWorldData(regionToGenerate);
+        assertTrue(regionData.getFacet(Facet1.class).updated);
+        assertTrue(regionData.getFacet(Facet4.class).updated);
+
+        // try checking generated facet
+        world = worldBuilder.build();
+        regionData = world.getWorldData(regionToGenerate);
+        assertNotNull(regionData.getFacet(Facet3.class));
+        assertTrue(regionData.getFacet(Facet4.class).updated);
+    }
+
     public static class Facet1 extends BaseFacet3D {
+        public boolean updated;
         public Facet1(Region3i targetRegion, Border3D border) {
             super(targetRegion, border);
         }
@@ -110,18 +142,16 @@ public class WorldBuilderTest {
     }
 
     public static class Facet4 extends BaseFacet3D {
+        public boolean updated;
         public Facet4(Region3i targetRegion, Border3D border) {
             super(targetRegion, border);
+
         }
     }
 
     @Produces(Facet1.class)
     @Requires(@Facet(value = Facet2.class, border = @FacetBorder(sides = 2)))
     public static class Facet1Provider implements FacetProvider {
-        @Override
-        public void setSeed(long seed) {
-
-        }
 
         @Override
         public void process(GeneratingRegion region) {
@@ -132,10 +162,6 @@ public class WorldBuilderTest {
 
     @Produces(Facet2.class)
     public static class Facet2Provider implements FacetProvider {
-        @Override
-        public void setSeed(long seed) {
-
-        }
 
         @Override
         public void process(GeneratingRegion region) {
@@ -147,10 +173,6 @@ public class WorldBuilderTest {
     @Produces(Facet3.class)
     @Requires(@Facet(value = Facet1.class, border = @FacetBorder(sides = 1)))
     public static class Facet3Provider implements FacetProvider {
-        @Override
-        public void setSeed(long seed) {
-
-        }
 
         @Override
         public void process(GeneratingRegion region) {
@@ -162,15 +184,27 @@ public class WorldBuilderTest {
     @Produces(Facet4.class)
     @Requires(@Facet(value = Facet2.class, border = @FacetBorder(sides = 3)))
     public static class Facet4Provider implements FacetProvider {
-        @Override
-        public void setSeed(long seed) {
-
-        }
 
         @Override
         public void process(GeneratingRegion region) {
             Facet4 facet = new Facet4(region.getRegion(), region.getBorderForFacet(Facet4.class));
             region.setRegionFacet(Facet4.class, facet);
+        }
+    }
+
+    @Requires(@Facet(Facet2.class))
+    @Produces(Facet3.class)
+    @Updates({@Facet(Facet1.class), @Facet(Facet4.class)})
+    public static class FacetUpdater implements FacetProvider {
+
+        @Override
+        public void process(GeneratingRegion region) {
+            Facet3 facet = new Facet3(region.getRegion(), region.getBorderForFacet(Facet3.class));
+            Facet1 facet1 = region.getRegionFacet(Facet1.class);
+            Facet4 facet4 = region.getRegionFacet(Facet4.class);
+            facet1.updated = true;
+            facet4.updated = true;
+            region.setRegionFacet(Facet3.class, facet);
         }
     }
 }
