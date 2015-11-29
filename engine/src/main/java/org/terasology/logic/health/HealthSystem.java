f01d326b3526cdd3d7ac0adf265ddfb3245ddeb2
Netopya <netopya@netopyaplanet.com>
2015-11-29 15:03:47 -0500

    Extracted Healing Methods
    This refactor reduces long method issues and improves readability

--- engine/src/main/java/org/terasology/logic/health/HealthSystem.java
+++ engine/src/main/java/org/terasology/logic/health/HealthSystem.java
@@ -63,17 +63,26 @@ public class HealthSystem extends BaseComponentSystem implements UpdateSubscribe
             }
 
             int healAmount = 0;
-            while (time.getGameTimeInMs() >= health.nextRegenTick) {
-                healAmount++;
-                health.nextRegenTick = health.nextRegenTick + (long) (1000 / health.regenRate);
-            }
+            healAmount = regenerateHealth(health, healAmount);
 
-            if (healAmount > 0) {
-                checkHeal(entity, healAmount, entity, health);
-                entity.saveComponent(health);
-            }
+            checkHealed(entity, health, healAmount);
         }
     }
+
+	private int regenerateHealth(HealthComponent health, int healAmount) {
+		while (time.getGameTimeInMs() >= health.nextRegenTick) {
+		    healAmount++;
+		    health.nextRegenTick = health.nextRegenTick + (long) (1000 / health.regenRate);
+		}
+		return healAmount;
+	}
+
+	private void checkHealed(EntityRef entity, HealthComponent health, int healAmount) {
+		if (healAmount > 0) {
+		    checkHeal(entity, healAmount, entity, health);
+		    entity.saveComponent(health);
+		}
+	}
 
     private void checkHeal(EntityRef entity, int healAmount, EntityRef instigator) {
         checkHeal(entity, healAmount, instigator, null);
