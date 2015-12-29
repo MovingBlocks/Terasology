8d1a7bc3acc5409ef9ec849cf294b885c26a819f
Martin Steiger <git@martin-steiger.de>
2015-12-29 11:44:13 +0100

    Working implemention of binding buttons to controller inputs

---
+++ engine/src/main/java/org/terasology/input/ControllerId.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.terasology.input;
+
+public interface ControllerId {
+
+    int NONE = -1;
+    int ZERO = 0;
+    int ONE = 1;
+    int TWO = 2;
+    int THREE = 3;
+    int FOUR = 4;
+    int FIVE = 5;
+    int SIX = 6;
+    int SEVEN = 7;
+    int EIGHT = 8;
+    int NINE = 9;
+    int TEN = 10;
+    int ELEVEN = 11;
+}
