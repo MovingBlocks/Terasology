ed0d508769e44d17f85e212ea34bbe239fb5c868
Malo Jaffré <jaffre.malo@gmail.com>
2016-01-11 18:24:25 +0100

    Refactor ControllerInput to cleanup code.
    Transform ControllerId(interface of constants ) into a "static" class.

--- engine/src/main/java/org/terasology/input/ControllerId.java
+++ engine/src/main/java/org/terasology/input/ControllerId.java
@@ -16,30 +16,33 @@
 
 package org.terasology.input;
 
-public interface ControllerId {
+public final class ControllerId {
 
-    int NONE = -1;
+    public static final int NONE = -1;
 
-    int ZERO = 0;
-    int ONE = 1;
-    int TWO = 2;
-    int THREE = 3;
-    int FOUR = 4;
-    int FIVE = 5;
-    int SIX = 6;
-    int SEVEN = 7;
-    int EIGHT = 8;
-    int NINE = 9;
-    int TEN = 10;
-    int ELEVEN = 11;
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int SEVEN = 7;
+    public static final int EIGHT = 8;
+    public static final int NINE = 9;
+    public static final int TEN = 10;
+    public static final int ELEVEN = 11;
 
-    int X_AXIS = 100;
-    int Y_AXIS = 200;
-    int Z_AXIS = 300;
+    public static final int X_AXIS = 100;
+    public static final int Y_AXIS = 200;
+    public static final int Z_AXIS = 300;
 
-    int RX_AXIS = 500;
-    int RY_AXIS = 600;
+    public static final int RX_AXIS = 500;
+    public static final int RY_AXIS = 600;
 
-    int POVX_AXIS = 800;
-    int POVY_AXIS = 900;
+    public static final int POVX_AXIS = 800;
+    public static final int POVY_AXIS = 900;
+
+    private ControllerId() {
+    }
 }
