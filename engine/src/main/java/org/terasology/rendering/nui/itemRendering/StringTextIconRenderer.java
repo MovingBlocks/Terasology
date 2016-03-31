379346ddf6bf19c1f467aed5568506668ca7a4e2
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2016-03-31 23:16:22 +0300

    Enable text wrapping by default and fix font drawing

--- engine/src/main/java/org/terasology/rendering/nui/itemRendering/StringTextIconRenderer.java
+++ engine/src/main/java/org/terasology/rendering/nui/itemRendering/StringTextIconRenderer.java
@@ -37,7 +37,7 @@ public abstract class StringTextIconRenderer<T> extends AbstractItemRenderer<T> 
     private final int marginRight;
 
     protected StringTextIconRenderer() {
-        this(true, 5, 5, 5, 10);
+        this(false, 5, 5, 5, 10);
     }
 
     protected StringTextIconRenderer(boolean wrap, int marginTop, int marginBottom, int marginLeft, int marginRight) {
@@ -84,7 +84,7 @@ public abstract class StringTextIconRenderer<T> extends AbstractItemRenderer<T> 
                     }
                     sb.setLength(sb.length() - 1);
                 }
-                canvas.drawText(shortText);
+                canvas.drawText(shortText, Rect2i.createFromMinAndSize(iconWidth, 0, canvas.getRegion().width() - iconWidth, canvas.getRegion().height()));
             }
         }
     }
