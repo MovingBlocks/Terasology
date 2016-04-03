456416a9b6a5b732f956f872ab8d317ca1acfd4d
viveret <viveret.amant@gmail.com>
2016-04-03 12:50:00 -0700

    Expanded interfaces and classes, added interpolators

---
+++ engine/src/main/java/org/terasology/rendering/nui/animation/CycleInterpolator.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering.nui.animation;
+
+/*
+ * Cycle interpolator, interpolates between start and end a float
+ * amount of times.
+ */
+public class CycleInterpolator extends BaseInterpolator {
+    private float factor;
+
+    public CycleInterpolator() {
+        super();
+        factor = 1;
+    }
+
+    public CycleInterpolator(float cycles) {
+        super();
+        this.factor = cycles;
+    }
+
+    public float getInterpolation(float v) {
+        return (float) Math.sin(2 * Math.PI * factor * v) * (end - start) + start;
+    }
+}
