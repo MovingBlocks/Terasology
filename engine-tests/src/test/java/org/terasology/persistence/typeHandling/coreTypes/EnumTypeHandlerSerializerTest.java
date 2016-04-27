5a731e1f9b4043dce093cc40a4bb734c47a3cc06
Martin Steiger <git@martin-steiger.de>
2016-04-27 20:59:16 +0200

    Add resilience to Serializer against malformed field data

--- engine-tests/src/test/java/org/terasology/persistence/typeHandling/coreTypes/EnumTypeHandlerSerializerTest.java
+++ engine-tests/src/test/java/org/terasology/persistence/typeHandling/coreTypes/EnumTypeHandlerSerializerTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.persistence.typeSerialization.typeHandlers.core;
+package org.terasology.persistence.typeHandling.coreTypes;
 
 import org.junit.Test;
 import org.terasology.persistence.typeHandling.DeserializationContext;
