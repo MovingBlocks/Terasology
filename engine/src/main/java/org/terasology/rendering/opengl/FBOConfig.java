04e9d7b2cd3b43f4ffb02e4f9eb2c77cfdfd4cad
Taha Doğan Güneş <tdgunes@gmail.com>
2016-08-04 23:35:50 +0300

    Yet another quick-fix for Preconditions

--- engine/src/main/java/org/terasology/rendering/opengl/FBOConfig.java
+++ engine/src/main/java/org/terasology/rendering/opengl/FBOConfig.java
@@ -15,7 +15,7 @@
  */
 package org.terasology.rendering.opengl;
 
-import com.google.api.client.repackaged.com.google.common.base.Preconditions;
+import com.google.common.base.Preconditions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
