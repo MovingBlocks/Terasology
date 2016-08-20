979de7ea6db80925697f8a7def52b3614762acbe
Taha Doğan Güneş <tdgunes@gmail.com>
2016-08-20 09:52:32 +0300

    Add desiredStateChanges to InitialPostProcessingNode

--- engine/src/main/java/org/terasology/rendering/dag/nodes/InitialPostProcessingNode.java
+++ engine/src/main/java/org/terasology/rendering/dag/nodes/InitialPostProcessingNode.java
@@ -19,9 +19,10 @@ import org.terasology.assets.ResourceUrn;
 import org.terasology.config.Config;
 import org.terasology.monitoring.PerformanceMonitor;
 import org.terasology.registry.In;
-import org.terasology.rendering.assets.material.Material;
 import org.terasology.rendering.dag.AbstractNode;
-import static org.terasology.rendering.opengl.DefaultDynamicFBOs.READ_ONLY_GBUFFER;
+import org.terasology.rendering.dag.stateChanges.BindFBO;
+import org.terasology.rendering.dag.stateChanges.EnableMaterial;
+import org.terasology.rendering.dag.stateChanges.SetViewportSizeOf;
 import org.terasology.rendering.opengl.FBO;
 import org.terasology.rendering.opengl.FBOConfig;
 import static org.terasology.rendering.opengl.ScalingFactors.FULL_SCALE;
@@ -30,8 +31,6 @@ import org.terasology.rendering.world.WorldRenderer;
 import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
 import static org.lwjgl.opengl.GL11.glClear;
-import static org.terasology.rendering.opengl.OpenGLUtils.bindDisplay;
-import static org.terasology.rendering.opengl.OpenGLUtils.setViewportToSizeOf;
 import static org.terasology.rendering.opengl.OpenGLUtils.renderFullscreenQuad;
 
 /**
@@ -49,15 +48,14 @@ public class InitialPostProcessingNode extends AbstractNode {
     @In
     private WorldRenderer worldRenderer;
 
-    private FBO scenePrePost;
-
-    private Material initialPost;
-
     @Override
     public void initialise() {
-        initialPost = worldRenderer.getMaterial("engine:prog.prePost"); // TODO: rename shader to scenePrePost
         requiresFBO(new FBOConfig(SCENE_PRE_POST, FULL_SCALE, FBO.Type.HDR), displayResolutionDependentFBOs);
 
+        addDesiredStateChange(new EnableMaterial("engine:prog.prePost")); // TODO: rename shader to scenePrePost
+        // TODO: verify what the inputs are
+        addDesiredStateChange(new BindFBO(SCENE_PRE_POST, displayResolutionDependentFBOs)); // TODO: see if we could write this straight into sceneOpaque
+        addDesiredStateChange(new SetViewportSizeOf(SCENE_PRE_POST, displayResolutionDependentFBOs));
     }
 
     /**
@@ -68,19 +66,9 @@ public class InitialPostProcessingNode extends AbstractNode {
     public void process() {
         // Initial Post-Processing: chromatic aberration, light shafts, 1/8th resolution bloom, vignette
         PerformanceMonitor.startActivity("rendering/initialPostProcessing");
-        scenePrePost = displayResolutionDependentFBOs.get(SCENE_PRE_POST);
-        initialPost.enable();
 
-        // TODO: verify what the inputs are
-        scenePrePost.bind(); // TODO: see if we could write this straight into sceneOpaque
-
-        setViewportToSizeOf(scenePrePost);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // TODO: verify this is necessary
-
         renderFullscreenQuad();
-
-        bindDisplay();     // TODO: verify this is necessary
-        setViewportToSizeOf(READ_ONLY_GBUFFER); // TODO: verify this is necessary
 
         PerformanceMonitor.endActivity();
     }
