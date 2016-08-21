15d4b305727389e1e7d9dee9161722832986912b
Taha Doğan Güneş <tdgunes@gmail.com>
2016-08-21 14:58:02 +0300

    Unwrap preRenderSetupSimpleBlendMaterials in SimpleBlendMaterialsNode

--- engine/src/main/java/org/terasology/rendering/dag/nodes/SimpleBlendMaterialsNode.java
+++ engine/src/main/java/org/terasology/rendering/dag/nodes/SimpleBlendMaterialsNode.java
@@ -45,6 +45,19 @@ public class SimpleBlendMaterialsNode extends AbstractNode {
         addDesiredStateChange(new BindFBO(READ_ONLY_GBUFFER.getName(), displayResolutionDependentFBOs)); // TODO: might be removed, verify it
         // TODO: review - might be redundant to setRenderBufferMask(sceneOpaque) again at the end of the process() method
 
+        /**
+         * Sets the state for the rendering of objects or portions of objects having some degree of transparency.
+         * <p>
+         * Generally speaking objects drawn with this state will have their color blended with the background
+         * color, depending on their opacity. I.e. a 25% opaque foreground object will provide 25% of its
+         * color while the background will provide the remaining 75%. The sum of the two RGBA vectors gets
+         * written onto the output buffer.
+         * <p>
+         * Important note: this method disables writing to the Depth Buffer. This is why filters relying on
+         * depth information (i.e. DoF) have problems with transparent objects: the depth of their pixels is
+         * found to be that of the background. This is an unresolved (unresolv-able?) issue that would only
+         * be reversed, not eliminated, by re-enabling writing to the Depth Buffer.
+         */
         addDesiredStateChange(new EnableBlending());
         addDesiredStateChange(new DisableDepthMask());
     }
@@ -52,35 +65,19 @@ public class SimpleBlendMaterialsNode extends AbstractNode {
     @Override
     public void process() {
         PerformanceMonitor.startActivity("rendering/simpleBlendMaterials");
-        preRenderSetupSimpleBlendMaterials();
-
-        for (RenderSystem renderer : componentSystemManager.iterateRenderSubscribers()) {
-            renderer.renderAlphaBlend();
-        }
-
-        PerformanceMonitor.endActivity();
-    }
-
-    /**
-     * Sets the state for the rendering of objects or portions of objects having some degree of transparency.
-     * <p>
-     * Generally speaking objects drawn with this state will have their color blended with the background
-     * color, depending on their opacity. I.e. a 25% opaque foreground object will provide 25% of its
-     * color while the background will provide the remaining 75%. The sum of the two RGBA vectors gets
-     * written onto the output buffer.
-     * <p>
-     * Important note: this method disables writing to the Depth Buffer. This is why filters relying on
-     * depth information (i.e. DoF) have problems with transparent objects: the depth of their pixels is
-     * found to be that of the background. This is an unresolved (unresolv-able?) issue that would only
-     * be reversed, not eliminated, by re-enabling writing to the Depth Buffer.
-     */
-    private void preRenderSetupSimpleBlendMaterials() {
         READ_ONLY_GBUFFER.setRenderBufferMask(true, true, true);
+        // TODO: make a glBlendFunc StateChange and eliminate this line
         GL11.glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); // (*)
 
         // (*) In this context SRC is Foreground. This effectively says:
         // Resulting RGB = ForegroundRGB * ForegroundAlpha + BackgroundRGB * (1 - ForegroundAlpha)
         // Which might still look complicated, but it's actually the most typical alpha-driven composite.
         // A neat tool to play with this settings can be found here: http://www.andersriggelsen.dk/glblendfunc.php
+
+        for (RenderSystem renderer : componentSystemManager.iterateRenderSubscribers()) {
+            renderer.renderAlphaBlend();
+        }
+
+        PerformanceMonitor.endActivity();
     }
 }
