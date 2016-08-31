5321286b53912962e22af2146776c1c90acfb1e4
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2016-08-31 13:06:39 +0300

    Also update ContextMenuUtilsTest

--- engine-tests/src/test/java/org/terasology/rendering/nui/editor/ContextMenuUtilsTest.java
+++ engine-tests/src/test/java/org/terasology/rendering/nui/editor/ContextMenuUtilsTest.java
@@ -90,6 +90,6 @@ public class ContextMenuUtilsTest extends TerasologyTestingEnvironment {
     }
 
     private Class getNodeType(JsonTree node) {
-        return NUIEditorNodeUtils.getNodeInfo(node, context.get(NUIManager.class));
+        return NUIEditorNodeUtils.getNodeInfo(node, context.get(NUIManager.class)).getNodeClass();
     }
 }
