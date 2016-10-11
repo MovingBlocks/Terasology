dc394e84b6df7ff8ec23b2ee2d55a3420f53100d
manu3d <manu3d@gmail.com>
2016-10-11 02:04:22 +0200

    # Trimmed SkyBandsNode - removed a number of unnecessary lines of code / moved all rendering code into process(), in sight of splitting this node into two instances of the same node class, probably a blur node class. / changed way the engine:blur material is acquired

--- engine/src/main/java/org/terasology/rendering/dag/nodes/SkyBandsNode.java
+++ engine/src/main/java/org/terasology/rendering/dag/nodes/SkyBandsNode.java
@@ -16,23 +16,22 @@
 package org.terasology.rendering.dag.nodes;
 
 import org.terasology.assets.ResourceUrn;
+import org.terasology.config.Config;
 import org.terasology.config.RenderingConfig;
 import org.terasology.monitoring.PerformanceMonitor;
 import org.terasology.registry.In;
 import org.terasology.rendering.assets.material.Material;
-import org.terasology.rendering.cameras.Camera;
-import org.terasology.rendering.dag.WireframeCapableNode;
+import org.terasology.rendering.dag.ConditionDependentNode;
 import static org.terasology.rendering.opengl.DefaultDynamicFBOs.READ_ONLY_GBUFFER;
+
+import org.terasology.rendering.dag.stateChanges.EnableMaterial;
 import org.terasology.rendering.opengl.FBO;
 import org.terasology.rendering.opengl.FBOConfig;
 import static org.terasology.rendering.opengl.ScalingFactors.ONE_16TH_SCALE;
 import static org.terasology.rendering.opengl.ScalingFactors.ONE_32TH_SCALE;
 import org.terasology.rendering.opengl.fbms.DisplayResolutionDependentFBOs;
-import org.terasology.rendering.world.WorldRenderer;
-import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
-import static org.lwjgl.opengl.GL11.glClear;
-import static org.terasology.rendering.opengl.OpenGLUtils.bindDisplay;
+import org.terasology.utilities.Assets;
+
 import static org.terasology.rendering.opengl.OpenGLUtils.setViewportToSizeOf;
 import static org.terasology.rendering.opengl.OpenGLUtils.renderFullscreenQuad;
 
@@ -40,76 +39,62 @@ import static org.terasology.rendering.opengl.OpenGLUtils.renderFullscreenQuad;
  * TODO: Diagram of this node
  * TODO: Separate this node into multiple SkyBandNode's
  */
-public class SkyBandsNode extends WireframeCapableNode {
+public class SkyBandsNode extends ConditionDependentNode {
     public static final ResourceUrn SKY_BAND_0 = new ResourceUrn("engine:sceneSkyBand0");
     public static final ResourceUrn SKY_BAND_1 = new ResourceUrn("engine:sceneSkyBand1");
 
     @In
-    private WorldRenderer worldRenderer;
+    private DisplayResolutionDependentFBOs displayResolutionDependentFBOs;
 
     @In
-    private DisplayResolutionDependentFBOs displayResolutionDependentFBOs;
+    private Config config;
 
     private RenderingConfig renderingConfig;
     private Material blurShader;
 
     private FBO sceneSkyBand0;
     private FBO sceneSkyBand1;
-    private Camera playerCamera;
 
 
     @Override
     public void initialise() {
-        super.initialise();
 
         requiresFBO(new FBOConfig(SKY_BAND_0, ONE_16TH_SCALE, FBO.Type.DEFAULT), displayResolutionDependentFBOs);
         requiresFBO(new FBOConfig(SKY_BAND_1, ONE_32TH_SCALE, FBO.Type.DEFAULT), displayResolutionDependentFBOs);
 
         renderingConfig = config.getRendering();
-        blurShader = worldRenderer.getMaterial("engine:prog.blur");
-        playerCamera = worldRenderer.getActiveCamera();
+        renderingConfig.subscribe(RenderingConfig.INSCATTERING, this);
+        requiresCondition(() -> renderingConfig.isInscattering());
+
+        addDesiredStateChange(new EnableMaterial("engine:prog.blur"));
+        blurShader = Assets.getMaterial("engine:prog.blur").orElseThrow(() ->
+                new RuntimeException("Failed to resolve required asset: '" + "engine:prog.blur" + "'"));
     }
 
     @Override
     public void process() {
         PerformanceMonitor.startActivity("rendering/skyBands");
 
-        READ_ONLY_GBUFFER.setRenderBufferMask(true, true, true);
-        if (renderingConfig.isInscattering()) {
-            sceneSkyBand0 = displayResolutionDependentFBOs.get(SKY_BAND_0);
-            sceneSkyBand1 = displayResolutionDependentFBOs.get(SKY_BAND_1);
+        blurShader.setFloat("radius", 8.0f, true);
 
-            generateSkyBand(sceneSkyBand0);
-            generateSkyBand(sceneSkyBand1);
-        }
+        sceneSkyBand0 = displayResolutionDependentFBOs.get(SKY_BAND_0);
+        blurShader.setFloat2("texelSize", 1.0f / sceneSkyBand0.width(), 1.0f / sceneSkyBand0.height(), true);
+        READ_ONLY_GBUFFER.bindTexture();
+        sceneSkyBand0.bind();
+        setViewportToSizeOf(sceneSkyBand0);
+        renderFullscreenQuad();
+
+        sceneSkyBand1 = displayResolutionDependentFBOs.get(SKY_BAND_1);
+        blurShader.setFloat2("texelSize", 1.0f / sceneSkyBand1.width(), 1.0f / sceneSkyBand1.height(), true);
+        sceneSkyBand0.bindTexture();
+        sceneSkyBand1.bind();
+        setViewportToSizeOf(sceneSkyBand1);
+        renderFullscreenQuad();
 
         READ_ONLY_GBUFFER.bind();
-
-        playerCamera.lookThrough();
+        setViewportToSizeOf(READ_ONLY_GBUFFER); // TODO: verify this is necessary
 
         PerformanceMonitor.endActivity();
     }
 
-    private void generateSkyBand(FBO skyBand) {
-        blurShader.enable();
-        blurShader.setFloat("radius", 8.0f, true);
-        blurShader.setFloat2("texelSize", 1.0f / skyBand.width(), 1.0f / skyBand.height(), true);
-
-        if (skyBand == sceneSkyBand0) {
-            READ_ONLY_GBUFFER.bindTexture();
-        } else {
-            sceneSkyBand0.bindTexture();
-        }
-
-        skyBand.bind();
-        skyBand.setRenderBufferMask(true, false, false);
-
-        setViewportToSizeOf(skyBand);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // TODO: verify this is necessary
-
-        renderFullscreenQuad();
-
-        bindDisplay();     // TODO: verify this is necessary
-        setViewportToSizeOf(READ_ONLY_GBUFFER); // TODO: verify this is necessary
-    }
 }
