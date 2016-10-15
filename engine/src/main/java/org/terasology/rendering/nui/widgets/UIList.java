MovingBlocks/Terasology

commit 8c07c4e14f38da5450bb64a6909196335676da26

Use interaction region to enable/disable widgets

engine/src/main/java/org/terasology/rendering/nui/widgets/UIList.java
@@ -17,6 +17,7 @@ package org.terasology.rendering.nui.widgets;
 
 import com.google.common.collect.Lists;
 import org.terasology.input.MouseInput;
+import org.terasology.math.Border;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.math.geom.Vector2i;
 import org.terasology.rendering.nui.BaseInteractionListener;
@@ -34,7 +35,8 @@ import java.util.List;
 import java.util.Objects;
 
 /**
- *
+ * A list widget.
+ * @param <T> the list element type
  */
 public class UIList<T> extends CoreWidget {
 
@@ -59,12 +61,17 @@ public class UIList<T> extends CoreWidget {
     public void onDraw(Canvas canvas) {
         updateItemListeners();
 
+        boolean enabled = isEnabled();
+        Border margin = canvas.getCurrentStyle().getMargin();
+
         canvas.setPart("item");
         int yOffset = 0;
         for (int i = 0; i < list.get().size(); ++i) {
             T item = list.get().get(i);
+            Vector2i preferredSize = margin.grow(itemRenderer.getPreferredSize(item, canvas));
+            Rect2i itemRegion = Rect2i.createFromMinAndSize(0, yOffset, canvas.size().x, preferredSize.y);
             ItemInteractionListener listener = itemListeners.get(i);
-            if (this.isEnabled()) {
+            if (enabled) {
                 if (Objects.equals(item, selection.get())) {
                     canvas.setMode(ACTIVE_MODE);
                 } else if (listener.isMouseOver()) {
@@ -72,17 +79,13 @@ public class UIList<T> extends CoreWidget {
                 } else {
                     canvas.setMode(DEFAULT_MODE);
                 }
+                canvas.addInteractionRegion(listener, itemRenderer.getTooltip(item), itemRegion);
             } else {
                 canvas.setMode(DISABLED_MODE);
             }
 
-
-            Vector2i preferredSize = canvas.getCurrentStyle().getMargin().grow(itemRenderer.getPreferredSize(item, canvas));
-            Rect2i itemRegion = Rect2i.createFromMinAndSize(0, yOffset, canvas.size().x, preferredSize.y);
             canvas.drawBackground(itemRegion);
-
-            itemRenderer.draw(item, canvas, canvas.getCurrentStyle().getMargin().shrink(itemRegion));
-            canvas.addInteractionRegion(listener, itemRenderer.getTooltip(item), itemRegion);
+            itemRenderer.draw(item, canvas, margin.shrink(itemRegion));
 
             yOffset += preferredSize.getY();
         }
@@ -202,7 +205,7 @@ public class UIList<T> extends CoreWidget {
 
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled() && event.getMouseButton() == MouseInput.MOUSE_LEFT && isSelectable()) {
+            if (event.getMouseButton() == MouseInput.MOUSE_LEFT && isSelectable()) {
                 select(index);
                 return true;
             }
@@ -211,7 +214,7 @@ public class UIList<T> extends CoreWidget {
 
         @Override
         public boolean onMouseDoubleClick(NUIMouseDoubleClickEvent event) {
-            if (isEnabled() && event.getMouseButton() == MouseInput.MOUSE_LEFT && isSelectable()) {
+            if (event.getMouseButton() == MouseInput.MOUSE_LEFT && isSelectable()) {
                 activate(index);
                 return true;
             }

engine/src/main/java/org/terasology/rendering/nui/widgets/UIBox.java
@@ -16,13 +16,18 @@
 package org.terasology.rendering.nui.widgets;
 
 import org.terasology.math.geom.Vector2i;
-import org.terasology.rendering.nui.*;
+import org.terasology.rendering.nui.AbstractWidget;
+import org.terasology.rendering.nui.Canvas;
+import org.terasology.rendering.nui.CoreWidget;
+import org.terasology.rendering.nui.LayoutConfig;
+import org.terasology.rendering.nui.UIWidget;
 
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Iterator;
 
 /**
+ * A simple element that just renders a box-style background.
  */
 public class UIBox extends CoreWidget {
 

engine/src/main/java/org/terasology/rendering/nui/widgets/UICheckbox.java
@@ -21,16 +21,15 @@ import org.terasology.rendering.nui.BaseInteractionListener;
 import org.terasology.rendering.nui.Canvas;
 import org.terasology.rendering.nui.CoreWidget;
 import org.terasology.rendering.nui.InteractionListener;
-import org.terasology.rendering.nui.LayoutConfig;
 import org.terasology.rendering.nui.databinding.Binding;
 import org.terasology.rendering.nui.databinding.DefaultBinding;
 import org.terasology.rendering.nui.events.NUIMouseClickEvent;
 
 /**
+ * A check-box. Hovering is supported.
  */
 public class UICheckbox extends CoreWidget {
     public static final String HOVER_ACTIVE_MODE = "hover-active";
-
 
     private Binding<Boolean> active = new DefaultBinding<>(false);
 
@@ -38,7 +37,7 @@ public class UICheckbox extends CoreWidget {
 
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled() && event.getMouseButton() == MouseInput.MOUSE_LEFT) {
+            if (event.getMouseButton() == MouseInput.MOUSE_LEFT) {
                 active.set(!active.get());
                 return true;
             }
@@ -56,7 +55,9 @@ public class UICheckbox extends CoreWidget {
 
     @Override
     public void onDraw(Canvas canvas) {
-        canvas.addInteractionRegion(interactionListener);
+        if (isEnabled()) {
+            canvas.addInteractionRegion(interactionListener);
+        }
     }
 
     @Override

engine/src/main/java/org/terasology/rendering/nui/widgets/UIDoubleSlider.java
@@ -19,7 +19,12 @@ import org.terasology.input.MouseInput;
 import org.terasology.math.TeraMath;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.math.geom.Vector2i;
-import org.terasology.rendering.nui.*;
+import org.terasology.rendering.nui.BaseInteractionListener;
+import org.terasology.rendering.nui.Canvas;
+import org.terasology.rendering.nui.CoreWidget;
+import org.terasology.rendering.nui.InteractionListener;
+import org.terasology.rendering.nui.LayoutConfig;
+import org.terasology.rendering.nui.SubRegion;
 import org.terasology.rendering.nui.databinding.Binding;
 import org.terasology.rendering.nui.databinding.DefaultBinding;
 import org.terasology.rendering.nui.events.NUIMouseClickEvent;
@@ -50,7 +55,7 @@ public class UIDoubleSlider extends CoreWidget {
 
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled() && event.getMouseButton() == MouseInput.MOUSE_LEFT) {
+            if (event.getMouseButton() == MouseInput.MOUSE_LEFT) {
                 active = true;
                 offset.set(event.getRelativeMousePosition());
                 offset.x -= pixelOffsetFor(getValueLeft(), sliderWidth);
@@ -66,7 +71,7 @@ public class UIDoubleSlider extends CoreWidget {
 
         @Override
         public void onMouseDrag(NUIMouseDragEvent event) {
-            if (isEnabled() && sliderWidth > 0) {
+            if (sliderWidth > 0) {
                 Vector2i pos = event.getRelativeMousePosition();
                 int maxSlot = TeraMath.floorToInt(getRange() / getIncrement());
                 int slotWidth = sliderWidth / maxSlot;
@@ -82,7 +87,7 @@ public class UIDoubleSlider extends CoreWidget {
 
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled() && event.getMouseButton() == MouseInput.MOUSE_LEFT) {
+            if (event.getMouseButton() == MouseInput.MOUSE_LEFT) {
                 active = true;
                 offset.set(event.getRelativeMousePosition());
                 offset.x -= pixelOffsetFor(getValueRight(), sliderWidth);
@@ -98,7 +103,7 @@ public class UIDoubleSlider extends CoreWidget {
 
         @Override
         public void onMouseDrag(NUIMouseDragEvent event) {
-            if (isEnabled() && sliderWidth > 0) {
+            if (sliderWidth > 0) {
                 Vector2i pos = event.getRelativeMousePosition();
                 int maxSlot = TeraMath.floorToInt(getRange() / getIncrement());
                 int slotWidth = sliderWidth / maxSlot;
@@ -141,7 +146,9 @@ public class UIDoubleSlider extends CoreWidget {
         try (SubRegion ignored = canvas.subRegion(tickerRegion, false)) {
             canvas.drawBackground();
             canvas.drawText(display);
-            canvas.addInteractionRegion(tickerListener);
+            if (isEnabled()) {
+                canvas.addInteractionRegion(tickerListener);
+            }
         }
     }
 

engine/src/main/java/org/terasology/rendering/nui/widgets/UIDropdown.java
@@ -20,7 +20,11 @@ import org.terasology.math.Border;
 import org.terasology.math.geom.Rect2i;
 import org.terasology.math.geom.Vector2i;
 import org.terasology.rendering.assets.font.Font;
-import org.terasology.rendering.nui.*;
+import org.terasology.rendering.nui.BaseInteractionListener;
+import org.terasology.rendering.nui.Canvas;
+import org.terasology.rendering.nui.CoreWidget;
+import org.terasology.rendering.nui.InteractionListener;
+import org.terasology.rendering.nui.SubRegion;
 import org.terasology.rendering.nui.databinding.Binding;
 import org.terasology.rendering.nui.databinding.DefaultBinding;
 import org.terasology.rendering.nui.events.NUIMouseClickEvent;
@@ -31,6 +35,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * A dropdown widget.
+ * @param <T> the list element type
  */
 public class UIDropdown<T> extends CoreWidget {
     private static final String LIST = "list";
@@ -44,17 +50,14 @@ public class UIDropdown<T> extends CoreWidget {
     private InteractionListener mainListener = new BaseInteractionListener() {
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled()) {
-                opened = !opened;
-                optionListeners.clear();
-                if (opened) {
-                    for (int i = 0; i < getOptions().size(); ++i) {
-                        optionListeners.add(new ItemListener(i));
-                    }
+            opened = !opened;
+            optionListeners.clear();
+            if (opened) {
+                for (int i = 0; i < getOptions().size(); ++i) {
+                    optionListeners.add(new ItemListener(i));
                 }
-                return true;
             }
-            return false;
+            return true;
         }
     };
 
@@ -80,7 +83,9 @@ public class UIDropdown<T> extends CoreWidget {
             }
         }
 
-        if (opened) {
+        if (!isEnabled()) {
+            // do not open and do not add an interaction region
+        } else if (opened) {
             canvas.setPart(LIST);
             canvas.setDrawOnTop(true);
             Font font = canvas.getCurrentStyle().getFont();

engine/src/main/java/org/terasology/rendering/nui/widgets/UIDropdownScrollable.java
@@ -35,6 +35,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * A scrollable dropdown widget.
+ * @param <T> the list element type
  */
 public class UIDropdownScrollable<T> extends UIDropdown<T> {
     private static final String LIST = "list";
@@ -51,27 +53,21 @@ public class UIDropdownScrollable<T> extends UIDropdown<T> {
     private InteractionListener mainListener = new BaseInteractionListener() {
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled()) {
-                opened = !opened;
-                optionListeners.clear();
-                if (opened) {
-                    for (int i = 0; i < getOptions().size(); ++i) {
-                        optionListeners.add(new ItemListener(i));
-                    }
+            opened = !opened;
+            optionListeners.clear();
+            if (opened) {
+                for (int i = 0; i < getOptions().size(); ++i) {
+                    optionListeners.add(new ItemListener(i));
                 }
-                return true;
             }
-            return false;
+            return true;
         }
 
         @Override
         public boolean onMouseWheel(NUIMouseWheelEvent event) {
-            if (isEnabled()) {
-                int scrollMultiplier = 0 - verticalBar.getRange() / getOptions().size();
-                verticalBar.setValue(verticalBar.getValue() + event.getWheelTurns() * scrollMultiplier);
-                return true;
-            }
-            return false;
+            int scrollMultiplier = 0 - verticalBar.getRange() / getOptions().size();
+            verticalBar.setValue(verticalBar.getValue() + event.getWheelTurns() * scrollMultiplier);
+            return true;
         }
     };
 
@@ -96,7 +92,9 @@ public class UIDropdownScrollable<T> extends UIDropdown<T> {
             }
         }
 
-        if (opened) {
+        if (!isEnabled()) {
+            // do not open and do not add an interaction region
+        } else if (opened) {
             canvas.setPart(LIST);
             canvas.setDrawOnTop(true);
             Font font = canvas.getCurrentStyle().getFont();

engine/src/main/java/org/terasology/rendering/nui/widgets/UILabel.java
@@ -27,6 +27,7 @@ import org.terasology.rendering.nui.databinding.DefaultBinding;
 import java.util.List;
 
 /**
+ * A (multi-line) label widget.
  */
 public class UILabel extends CoreWidget {
 
@@ -69,8 +70,9 @@ public class UILabel extends CoreWidget {
 
     @Override
     public String getMode() {
-        if (isEnabled())
+        if (isEnabled()) {
             return DEFAULT_MODE;
+        }
         return DISABLED_MODE;
     }
 

engine/src/main/java/org/terasology/rendering/nui/widgets/UISlider.java
@@ -43,7 +43,7 @@ public class UISlider extends CoreWidget {
 
         @Override
         public boolean onMouseClick(NUIMouseClickEvent event) {
-            if (isEnabled() && event.getMouseButton() == MouseInput.MOUSE_LEFT) {
+            if (event.getMouseButton() == MouseInput.MOUSE_LEFT) {
                 active = true;
                 offset.set(event.getRelativeMousePosition());
                 offset.x -= pixelOffsetFor(getValue(), sliderWidth);
@@ -59,7 +59,7 @@ public class UISlider extends CoreWidget {
 
         @Override
         public void onMouseDrag(NUIMouseDragEvent event) {
-            if (isEnabled() && sliderWidth > 0) {
+            if (sliderWidth > 0) {
                 Vector2i pos = event.getRelativeMousePosition();
                 int maxSlot = TeraMath.floorToInt(getRange() / getIncrement());
                 int slotWidth = sliderWidth / maxSlot;
@@ -124,7 +124,9 @@ public class UISlider extends CoreWidget {
         try (SubRegion ignored = canvas.subRegion(tickerRegion, false)) {
             canvas.drawBackground();
             canvas.drawText(display);
-            canvas.addInteractionRegion(tickerListener);
+            if (isEnabled()) {
+                canvas.addInteractionRegion(tickerListener);
+            }
         }
     }
 

commit 1745adec09ec6dfdd492b13bc7e96d2c38e259be

Fixes issue #2508, excess scrolling in module list
This was a regression introduced by 8c07c4e14.  The list items were rendering
without the correct margins but the margins were still being included when
calculating the size of the scrollable area, resulting in excess space at the
end of the list.

engine/src/main/java/org/terasology/rendering/nui/widgets/UIList.java
@@ -61,11 +61,11 @@ public class UIList<T> extends CoreWidget {
     @Override
     public void onDraw(Canvas canvas) {
         updateItemListeners();
+        canvas.setPart("item");
 
         boolean enabled = isEnabled();
         Border margin = canvas.getCurrentStyle().getMargin();
 
-        canvas.setPart("item");
         int yOffset = 0;
         for (int i = 0; i < list.get().size(); ++i) {
             T item = list.get().get(i);