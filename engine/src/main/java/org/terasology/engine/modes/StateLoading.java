266def3b8ce86a666630cf1ca11f945dc4d3b560
Max <maximborsch@gmail.com>
2017-01-18 22:17:40 -0500

    Added postWorldGeneration event / WorldGenerationSubscriberSystem

--- engine/src/main/java/org/terasology/engine/modes/StateLoading.java
+++ engine/src/main/java/org/terasology/engine/modes/StateLoading.java
@@ -44,6 +44,7 @@ import org.terasology.engine.modes.loadProcesses.JoinServer;
 import org.terasology.engine.modes.loadProcesses.LoadEntities;
 import org.terasology.engine.modes.loadProcesses.LoadPrefabs;
 import org.terasology.engine.modes.loadProcesses.PostBeginSystems;
+import org.terasology.engine.modes.loadProcesses.PostWorldGenerationSystems;
 import org.terasology.engine.modes.loadProcesses.PreBeginSystems;
 import org.terasology.engine.modes.loadProcesses.PrepareWorld;
 import org.terasology.engine.modes.loadProcesses.ProcessBlockPrefabs;
@@ -167,6 +168,7 @@ public class StateLoading implements GameState {
         loadProcesses.add(new SetupRemotePlayer(context));
         loadProcesses.add(new AwaitCharacterSpawn(context));
         loadProcesses.add(new PrepareWorld(context));
+        loadProcesses.add(new PostWorldGenerationSystems(context));
     }
 
     private void initHost() {
@@ -209,6 +211,7 @@ public class StateLoading implements GameState {
             loadProcesses.add(new AwaitCharacterSpawn(context));
         }
         loadProcesses.add(new PrepareWorld(context));
+        loadProcesses.add(new PostWorldGenerationSystems(context));
     }
 
     private void popStep() {
