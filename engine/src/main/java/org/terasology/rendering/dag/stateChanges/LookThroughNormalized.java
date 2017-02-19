dfd2d4e8d2c311afcc39b5e41e9f10863568491d
manu3d <manu3d@gmail.com>
2017-02-19 15:59:34 +0100

    # Added LookThrough state changes + added LookThrough and LookThroughNormalized state changes, and 3 associated task classes * The new state changes and their tasks, at the point of this commit, are not in use yet

---
+++ engine/src/main/java/org/terasology/rendering/dag/stateChanges/LookThroughNormalized.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering.dag.stateChanges;
+
+import com.google.common.base.Objects;
+import org.terasology.rendering.cameras.Camera;
+import org.terasology.rendering.dag.RenderPipelineTask;
+import org.terasology.rendering.dag.StateChange;
+import org.terasology.rendering.dag.tasks.LookThroughDefaultCameraTask;
+import org.terasology.rendering.dag.tasks.LookThroughNormalizedTask;
+
+/**
+ * TODO
+ */
+public class LookThroughNormalized implements StateChange {
+
+    private static LookThroughNormalized defaultInstance = new LookThroughNormalized();
+
+    private Camera camera;
+    private RenderPipelineTask task;
+
+    public LookThroughNormalized(Camera camera) {
+        this.camera = camera;
+    }
+
+    private LookThroughNormalized() {
+
+    }
+
+    @Override
+    public RenderPipelineTask generateTask() {
+        if (task == null) {
+            if (camera != null) {
+                task = new LookThroughNormalizedTask(camera);
+            } else {
+                task = new LookThroughDefaultCameraTask();
+            }
+        }
+        return task;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(camera);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return (obj instanceof LookThroughNormalized) && camera == ((LookThroughNormalized) obj).camera;
+    }
+
+    @Override
+    public StateChange getDefaultInstance() {
+        return defaultInstance;
+    }
+
+    @Override
+    public boolean isTheDefaultInstance() {
+        return this.equals(defaultInstance);
+    }
+}
---
+++ engine/src/main/java/org/terasology/rendering/dag/tasks/LookThroughDefaultCameraTask.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2016 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering.dag.tasks;
+
+import org.terasology.rendering.dag.RenderPipelineTask;
+
+import static org.lwjgl.opengl.GL11.*;
+
+/**
+ * TODO
+ */
+public class LookThroughDefaultCameraTask implements RenderPipelineTask {
+
+    @Override
+    public void execute() {
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+
+        glMatrixMode(GL_PROJECTION);
+        glLoadIdentity();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%30s: %s", this.getClass().getSimpleName(), "default opengl camera");
+
+    }
+}
