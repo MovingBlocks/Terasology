MovingBlocks/Terasology

commit c6247eb344c3bc01c002aeed98995c605be458c2

Use multiple stacks to give blocks. This closes #2607

engine/src/main/java/org/terasology/world/block/entity/BlockCommands.java
@@ -327,17 +327,21 @@ public class BlockCommands extends BaseComponentSystem {
         if (quantity < 1) {
             return "Here, have these zero (0) blocks just like you wanted";
         }
+        //continue giving blocks until there are no more blocks to give
+        //TODO reference maxStackSize instead of explicitely subtracting 99
+        for(int quantityLeft = quantity; quantityLeft > 0; quantityLeft -= 99) {
+            EntityRef item = blockItemFactory.newInstance(blockFamily, quantityLeft > 99 ? 99 : quantityLeft);
+            if (!item.exists()) {
+                throw new IllegalArgumentException("Unknown block or item");
+            }
 
-        EntityRef item = blockItemFactory.newInstance(blockFamily, quantity);
-        if (!item.exists()) {
-            throw new IllegalArgumentException("Unknown block or item");
-        }
-        EntityRef playerEntity = client.getComponent(ClientComponent.class).character;
+            EntityRef playerEntity = client.getComponent(ClientComponent.class).character;
 
-        GiveItemEvent giveItemEvent = new GiveItemEvent(playerEntity);
-        item.send(giveItemEvent);
-        if (!giveItemEvent.isHandled()) {
-            item.destroy();
+            GiveItemEvent giveItemEvent = new GiveItemEvent(playerEntity);
+            item.send(giveItemEvent);
+            if (!giveItemEvent.isHandled()) {
+                item.destroy();
+            }
         }
 
         return "You received " + quantity + " blocks of " + blockFamily.getDisplayName();

commit f9f02ab2f6337cca593f95785769b8703a5b9276

Fix give lag for high block quantites, and not filling partial stacks
Fix the last part of #2835, where giveBlock would lag when given high values
due to it not breaking out of the loop (caused by #2678).

Also fixes an issue where giveBlock would not fill up partial stacks in the
inventory, if the requested value would put the stack at higher than 99.

giveBlock now reports the correct number of blocks actually given, rather than
just repeating the number requested.

engine/src/main/java/org/terasology/world/block/entity/BlockCommands.java
@@ -347,20 +347,22 @@ public class BlockCommands extends BaseComponentSystem {
         if (quantity < 1) {
             return "Here, have these zero (0) blocks just like you wanted";
         }
-        //continue giving blocks until there are no more blocks to give
-        //TODO reference maxStackSize instead of explicitly subtracting 99 and introduce an upper bound? 10 million lags ..
-        for (int quantityLeft = quantity; quantityLeft > 0; quantityLeft -= 99) {
-            EntityRef item = blockItemFactory.newInstance(blockFamily, quantityLeft > 99 ? 99 : quantityLeft);
+
+        EntityRef playerEntity = client.getComponent(ClientComponent.class).character;
+
+        for (int quantityLeft = quantity; quantityLeft > 0; quantityLeft--) {
+            EntityRef item = blockItemFactory.newInstance(blockFamily, 1);
             if (!item.exists()) {
                 throw new IllegalArgumentException("Unknown block or item");
             }
 
-            EntityRef playerEntity = client.getComponent(ClientComponent.class).character;
-
             GiveItemEvent giveItemEvent = new GiveItemEvent(playerEntity);
             item.send(giveItemEvent);
+
             if (!giveItemEvent.isHandled()) {
                 item.destroy();
+                quantity -= quantityLeft;
+                break;
             }
         }
 