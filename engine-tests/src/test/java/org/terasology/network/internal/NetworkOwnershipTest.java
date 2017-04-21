9dad84085948039b7fdb7c6506968e933abb0f52
kaen <kaencastszk@gmail.com>
2017-04-21 15:24:50 -0700

    use statecontext setter in network system tests

--- engine-tests/src/test/java/org/terasology/network/internal/NetworkOwnershipTest.java
+++ engine-tests/src/test/java/org/terasology/network/internal/NetworkOwnershipTest.java
@@ -57,6 +57,7 @@ public class NetworkOwnershipTest extends TerasologyTestingEnvironment {
         context.put(ModuleManager.class, moduleManager);
         EngineTime mockTime = mock(EngineTime.class);
         networkSystem = new NetworkSystemImpl(mockTime, context);
+        networkSystem.setStateContext(context);
         context.put(NetworkSystem.class, networkSystem);
 
         EntitySystemSetupUtil.addReflectionBasedLibraries(context);
