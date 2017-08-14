914a0e237b40e3e6f2b92c086d37c6e67049b655
kaen <kaencastszk@gmail.com>
2017-08-13 21:08:17 -0700

    add some basic documentation

--- engine/src/main/java/org/terasology/utilities/concurrency/Task.java
+++ engine/src/main/java/org/terasology/utilities/concurrency/Task.java
@@ -17,12 +17,27 @@
 package org.terasology.utilities.concurrency;
 
 /**
+ * Task interface for use with {@link TaskMaster}. Typically, this interface is implemented by a BaseTask class
+ * specific to your TaskMaster client code. For example, a pathfinder might create a PathfindingBaseTask, and
+ * extend that to make a PathfindingMapAnalysisTask.
+ * <p>
+ * The {@link #run()} method is called (generally on a separate task processing thread) when the task manager is
+ * ready to execute the task.
+ *
+ * @see TaskMaster
  */
 public interface Task {
 
     String getName();
 
+    /**
+     * Called when the Task is ready to be executed
+     */
     void run();
 
+    /**
+     * Return true if the {@link TaskProcessor} should shut down after performing this task
+     * @return
+     */
     boolean isTerminateSignal();
 }
