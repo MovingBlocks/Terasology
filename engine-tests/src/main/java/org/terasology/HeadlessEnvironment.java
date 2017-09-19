d832477a827db7243f80bc037727cfbf3285fee2
Michael Pollind <polli104@mail.chapman.edu>
2017-09-19 10:43:26 -0700

    fixed Headless enviroment

--- engine-tests/src/main/java/org/terasology/HeadlessEnvironment.java
+++ engine-tests/src/main/java/org/terasology/HeadlessEnvironment.java
@@ -216,7 +216,7 @@ public class HeadlessEnvironment extends Environment {
         assetTypeManager.registerCoreAssetType(StaticSound.class, NullSound::new, "sounds");
         assetTypeManager.registerCoreAssetType(StreamingSound.class, NullStreamingSound::new, "music");
 
-        BlockFamilyRegistryImpl blockFamilyFactoryRegistry = new BlockFamilyRegistryImpl(context);
+        BlockFamilyRegistryImpl blockFamilyFactoryRegistry = new BlockFamilyRegistryImpl();
         blockFamilyFactoryRegistry.setBlockFamily("horizontal", HorizontalBlockFamily.class);
         blockFamilyFactoryRegistry.setBlockFamily("alignToSurface", AttachedToSurfaceFamily.class);
         assetTypeManager.registerCoreFormat(BlockFamilyDefinition.class,
