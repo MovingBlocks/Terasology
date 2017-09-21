d8f5ac67e83ef9fc2a5f95c4e29d682138f9d7fa
Michael Pollind <mpollind@gmail.com>
2017-09-20 19:45:29 -0700

    fixed imports

--- engine/src/main/java/org/terasology/world/block/family/MultiSections.java
+++ engine/src/main/java/org/terasology/world/block/family/MultiSections.java
@@ -15,7 +15,11 @@
  */
 package org.terasology.world.block.family;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
