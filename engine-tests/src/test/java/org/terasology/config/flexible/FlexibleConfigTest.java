61f6265b4ab2994698048fa3fd5b4e2ea1d07f5e
Arav Singhal <singhal.arav@gmail.com>
2018-01-15 22:51:02 +0530

    Add FlexibleConfig load and save tests

--- engine-tests/src/test/java/org/terasology/config/flexible/FlexibleConfigTest.java
+++ engine-tests/src/test/java/org/terasology/config/flexible/FlexibleConfigTest.java
@@ -21,7 +21,14 @@ import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import org.terasology.engine.SimpleUri;
 
-import static org.junit.Assert.*;
+import java.io.Reader;
+import java.io.StringReader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(Enclosed.class)
 public class FlexibleConfigTest {
@@ -142,5 +149,109 @@ public class FlexibleConfigTest {
 
             assertFalse(config.remove(id));
         }
+    }
+
+    public static class JsonStorage {
+        private static final String CONFIG_AS_JSON = "{\n" +
+                "  \"engine-tests:TestSetting3\": {\n" +
+                "    \"a\": 31,\n" +
+                "    \"b\": \"string\",\n" +
+                "    \"c\": \"A2\"\n" +
+                "  },\n" +
+                "  \"engine-tests:TestSetting1\": \"A3\",\n" +
+                "  \"engine-tests:TestSetting2\": 21.0\n" +
+                "}";
+
+        private static final TestEnum TESTENUM_SETTING_VALUE = TestEnum.A3;
+        private static final double DOUBLE_SETTING_VALUE = 21.0;
+        private static final TestClass TESTCLASS_SETTING_VALUE = new TestClass();
+
+        private FlexibleConfig config;
+
+        private SettingImpl<TestEnum> testEnumSetting;
+        private SettingImpl<Double> doubleSetting;
+        private SettingImpl<TestClass> testClassSetting;
+
+        private void setupSettings() {
+            testEnumSetting = new SettingImpl<>(TestEnum.class, new SimpleUri("engine-tests:TestSetting1"), TestEnum.A1);
+            config.add(testEnumSetting);
+
+            doubleSetting = new SettingImpl<>(Double.class, new SimpleUri("engine-tests:TestSetting2"), 30.0);
+            config.add(doubleSetting);
+
+            testClassSetting = new SettingImpl<>(TestClass.class, new SimpleUri("engine-tests:TestSetting3"), null);
+            config.add(testClassSetting);
+        }
+
+        @Before
+        public void setUp() throws Exception {
+            config = new FlexibleConfigImpl();
+        }
+
+        @Test
+        public void testSave() throws Exception {
+            setupSettings();
+
+            testEnumSetting.setValue(TESTENUM_SETTING_VALUE);
+            doubleSetting.setValue(DOUBLE_SETTING_VALUE);
+            testClassSetting.setValue(TESTCLASS_SETTING_VALUE);
+
+            Writer writer = new StringWriter();
+            config.save(writer);
+
+            assertEquals(CONFIG_AS_JSON, writer.toString());
+        }
+
+        @Test
+        public void testLoad() throws Exception {
+            Reader reader = new StringReader(CONFIG_AS_JSON);
+            config.load(reader);
+
+            setupSettings();
+
+            assertEquals(TESTENUM_SETTING_VALUE, testEnumSetting.getValue());
+            assertEquals(DOUBLE_SETTING_VALUE, doubleSetting.getValue(), 0.00000001);
+            assertEquals(TESTCLASS_SETTING_VALUE, testClassSetting.getValue());
+        }
+
+        private enum TestEnum {
+            A1(1), A2(2), A3(3);
+
+            private int i;
+
+            TestEnum(int i) {
+                this.i = i;
+            }
+
+            public int getI() {
+                return i;
+            }
+        }
+
+        private static class TestClass {
+            int a;
+            String b;
+
+            TestEnum c;
+
+            public TestClass() {
+                a = 31;
+                b = "string";
+                c = TestEnum.A2;
+            }
+
+            @Override
+            public boolean equals(Object o) {
+                if (this == o) return true;
+                if (o == null || getClass() != o.getClass()) return false;
+
+                TestClass testClass = (TestClass) o;
+
+                if (a != testClass.a) return false;
+                if (b != null ? !b.equals(testClass.b) : testClass.b != null) return false;
+                return c == testClass.c;
+            }
+        }
+
     }
 }
