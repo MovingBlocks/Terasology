86a8f63d1622597dd58f2ad0b2b1efad7b11cd43
oniatus <oniatus@googlemail.com>
2018-01-19 00:28:44 +0100

    Add tests for block addition and activation events

--- engine-tests/src/test/java/org/terasology/world/chunks/localChunkProvider/LocalChunkProviderTest.java
+++ engine-tests/src/test/java/org/terasology/world/chunks/localChunkProvider/LocalChunkProviderTest.java
@@ -15,17 +15,26 @@
  */
 package org.terasology.world.chunks.localChunkProvider;
 
+import gnu.trove.list.TIntList;
+import gnu.trove.list.array.TIntArrayList;
 import gnu.trove.map.hash.TShortObjectHashMap;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InOrder;
 import org.terasology.entitySystem.Component;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.entity.EntityStore;
+import org.terasology.entitySystem.event.Event;
 import org.terasology.entitySystem.prefab.Prefab;
 import org.terasology.math.geom.Vector3i;
 import org.terasology.persistence.ChunkStore;
+import org.terasology.world.BlockEntityRegistry;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.block.OnActivatedBlocks;
+import org.terasology.world.block.OnAddedBlocks;
 import org.terasology.world.chunks.Chunk;
 import org.terasology.world.chunks.event.OnChunkGenerated;
 import org.terasology.world.chunks.event.OnChunkLoaded;
@@ -34,22 +43,34 @@ import org.terasology.world.chunks.internal.ReadyChunkInfo;
 import java.util.Collections;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.atLeastOnce;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class LocalChunkProviderTest {
 
     private LocalChunkProvider chunkProvider;
     private ChunkFinalizer chunkFinalizer;
     private EntityManager entityManager;
+    private BlockManager blockManager;
+    private BlockEntityRegistry blockEntityRegistry;
 
     @Before
     public void setUp() throws Exception {
         entityManager = mock(EntityManager.class);
         chunkFinalizer = mock(ChunkFinalizer.class);
+        blockManager = mock(BlockManager.class);
+        blockEntityRegistry = mock(BlockEntityRegistry.class);
         chunkProvider = new LocalChunkProvider(null,
-                entityManager, null, null, null, chunkFinalizer, null);
+                entityManager, null, blockManager, null, chunkFinalizer, null);
+        chunkProvider.setBlockEntityRegistry(blockEntityRegistry);
     }
 
     @Test
@@ -125,6 +146,66 @@ public class LocalChunkProviderTest {
         chunkProvider.completeUpdate();
 
         verify(chunkStore).restoreEntities();
+    }
+
+    @Test
+    public void testCompleteUpdateSendsBlockAddedEvents() throws Exception {
+        final EntityRef worldEntity = mock(EntityRef.class);
+        chunkProvider.setWorldEntity(worldEntity);
+        final Chunk chunk = mock(Chunk.class);
+        when(chunk.getPosition()).thenReturn(new Vector3i(0, 0, 0));
+        final TShortObjectHashMap<TIntList> blockPositionMapppings = new TShortObjectHashMap<>();
+        final short blockId = 42;
+        final EntityRef blockEntity = mock(EntityRef.class);
+        final Block block = new Block();
+        block.setEntity(blockEntity);
+        when(blockManager.getBlock(eq(blockId))).thenReturn(block);
+        final TIntArrayList positions = new TIntArrayList();
+        final Vector3i position = new Vector3i(1, 2, 3);
+        positions.add(position.x);
+        positions.add(position.y);
+        positions.add(position.z);
+        blockPositionMapppings.put(blockId, positions);
+        final ReadyChunkInfo readyChunkInfo = new ReadyChunkInfo(chunk, blockPositionMapppings, mock(ChunkStore.class), Collections.emptyList());
+        when(chunkFinalizer.completeFinalization()).thenReturn(readyChunkInfo);
+
+        chunkProvider.completeUpdate();
+
+        final ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
+        verify(blockEntity, atLeastOnce()).send(eventArgumentCaptor.capture());
+        final Event event = eventArgumentCaptor.getAllValues().get(0);
+        assertThat(event, instanceOf(OnAddedBlocks.class));
+        assertThat(((OnAddedBlocks) event).getBlockPositions(), hasItem(position));
+    }
+
+    @Test
+    public void testCompleteUpdateSendsBlockActivatedEvents() throws Exception {
+        final EntityRef worldEntity = mock(EntityRef.class);
+        chunkProvider.setWorldEntity(worldEntity);
+        final Chunk chunk = mock(Chunk.class);
+        when(chunk.getPosition()).thenReturn(new Vector3i(0, 0, 0));
+        final TShortObjectHashMap<TIntList> blockPositionMapppings = new TShortObjectHashMap<>();
+        final short blockId = 42;
+        final EntityRef blockEntity = mock(EntityRef.class);
+        final Block block = new Block();
+        block.setEntity(blockEntity);
+        when(blockManager.getBlock(eq(blockId))).thenReturn(block);
+        final TIntArrayList positions = new TIntArrayList();
+        final Vector3i position = new Vector3i(1, 2, 3);
+        positions.add(position.x);
+        positions.add(position.y);
+        positions.add(position.z);
+        blockPositionMapppings.put(blockId, positions);
+        final ReadyChunkInfo readyChunkInfo = new ReadyChunkInfo(chunk, blockPositionMapppings, mock(ChunkStore.class), Collections.emptyList());
+        when(chunkFinalizer.completeFinalization()).thenReturn(readyChunkInfo);
+
+        chunkProvider.completeUpdate();
+
+        final ArgumentCaptor<Event> eventArgumentCaptor = ArgumentCaptor.forClass(Event.class);
+        verify(blockEntity, atLeastOnce()).send(eventArgumentCaptor.capture());
+        final Event event = eventArgumentCaptor.getAllValues().get(1);
+        assertThat(event, instanceOf(OnActivatedBlocks.class));
+        assertThat(((OnActivatedBlocks) event).getBlockPositions(), hasItem(position));
     }
 
     private static class ChunkProviderTestComponent implements Component {
