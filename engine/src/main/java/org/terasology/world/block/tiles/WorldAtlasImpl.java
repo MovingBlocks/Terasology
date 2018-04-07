65843b025ab152d42a2baaac1719aefb37cbdc9c
4denthusiast <andrew_kr@btinternet.com>
2018-04-07 19:13:26 +0100

    Remove debugging from previous commit.
    Debugging code was left in by mistake.

--- engine/src/main/java/org/terasology/world/block/tiles/WorldAtlasImpl.java
+++ engine/src/main/java/org/terasology/world/block/tiles/WorldAtlasImpl.java
@@ -158,7 +158,6 @@ public class WorldAtlasImpl implements WorldAtlas {
     }
 
     private int indexTile(ResourceUrn uri) {
-        logger.info("Adding tile: "+uri.toString());
         if (tiles.size() == MAX_TILES) {
             logger.error("Maximum tiles exceeded");
             return 0;
