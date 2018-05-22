d46622bfb1443082801029d9c4f869014b0286fa
Quinn Roberts <quinn@jelly-snakes.com>
2018-05-22 15:03:35 +1000

    Add a template for making a new System

---
+++ fileTemplates/System.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+
+@RegisterSystem
+public class ${NAME} extends BaseComponentSystem {
+    
+}
