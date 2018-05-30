e6443e8514cf6270fe7cca0ac7464a4481dfbc31
TheFlash98 <mastermind.sarthak@gmail.com>
2018-05-31 00:15:51 +0530

    Loads world generators and world names on universe setup screen

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/UniverseSetupScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/UniverseSetupScreen.java
@@ -15,17 +15,125 @@
  */
 package org.terasology.rendering.nui.layers.mainMenu;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
 import org.terasology.assets.ResourceUrn;
+import org.terasology.config.Config;
+import org.terasology.config.ModuleConfig;
+import org.terasology.context.Context;
+import org.terasology.engine.module.ModuleManager;
+import org.terasology.module.DependencyInfo;
+import org.terasology.module.Module;
+import org.terasology.naming.Name;
+import org.terasology.registry.In;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.WidgetUtil;
+import org.terasology.rendering.nui.databinding.Binding;
+import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+import org.terasology.rendering.nui.itemRendering.StringTextRenderer;
+import org.terasology.rendering.nui.widgets.UIDropdownScrollable;
+import org.terasology.world.generator.internal.WorldGeneratorInfo;
+import org.terasology.world.generator.internal.WorldGeneratorManager;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class UniverseSetupScreen extends CoreScreenLayer {
 
     public static final ResourceUrn ASSET_URI = new ResourceUrn("engine:universeSetupScreen");
+    private String selectedWorld = "";
+
+    @In
+    private WorldGeneratorManager worldGeneratorManager;
+
+    @In
+    private ModuleManager moduleManager;
+
+    @In
+    private Config config;
+
+    @In
+    private Context context;
+
+    HashMap<String, WorldGeneratorInfo> worlds = new HashMap<String, WorldGeneratorInfo>();
+    int worldNumber = 0;
 
     @Override
-    public void initialise(){
+    public void initialise() {
 
+        ModuleConfig moduleConfig = config.getDefaultModSelection();
+
+
+        final UIDropdownScrollable<WorldGeneratorInfo> worldGenerator = find("worldGenerators", UIDropdownScrollable.class);
+        if (worldGenerator != null) {
+            worldGenerator.bindOptions(new ReadOnlyBinding<List<WorldGeneratorInfo>>() {
+                @Override
+                public List<WorldGeneratorInfo> get() {
+                    // grab all the module names and their dependencies
+                    // This grabs modules from `config.getDefaultModSelection()` which is updated in AdvancedGameSetupScreen
+                    Set<Name> enabledModuleNames = getAllEnabledModuleNames().stream().collect(Collectors.toSet());
+                    List<WorldGeneratorInfo> result = Lists.newArrayList();
+                    for (WorldGeneratorInfo option : worldGeneratorManager.getWorldGenerators()) {
+                        if (enabledModuleNames.contains(option.getUri().getModuleName())) {
+                            result.add(option);
+                        }
+                    }
+
+                    return result;
+                }
+            });
+            worldGenerator.setVisibleOptions(3);
+            worldGenerator.bindSelection(new Binding<WorldGeneratorInfo>() {
+                @Override
+                public WorldGeneratorInfo get() {
+                    // get the default generator from the config. This is likely to have a user triggered selection.
+                    WorldGeneratorInfo info = worldGeneratorManager.getWorldGeneratorInfo(config.getWorldGeneration().getDefaultGenerator());
+                    if (info != null && getAllEnabledModuleNames().contains(info.getUri().getModuleName())) {
+                        return info;
+                    }
+
+                    // just use the first available generator
+                    for (WorldGeneratorInfo worldGenInfo : worldGeneratorManager.getWorldGenerators()) {
+                        if (getAllEnabledModuleNames().contains(worldGenInfo.getUri().getModuleName())) {
+                            set(worldGenInfo);
+                            return worldGenInfo;
+                        }
+                    }
+
+                    return null;
+                }
+
+                @Override
+                public void set(WorldGeneratorInfo value) {
+                    if (value != null) {
+                        config.getWorldGeneration().setDefaultGenerator(value.getUri());
+                    }
+                }
+            });
+            worldGenerator.setOptionRenderer(new StringTextRenderer<WorldGeneratorInfo>() {
+                @Override
+                public String getString(WorldGeneratorInfo value) {
+                    if (value != null) {
+                        return value.getDisplayName();
+                    }
+                    return "";
+                }
+            });
+        }
+        final UIDropdownScrollable worldsDropdown = find("worlds", UIDropdownScrollable.class);
+        worldsDropdown.bindSelection(new Binding<String>() {
+            @Override
+            public String get() {
+                return selectedWorld;
+            }
+
+            @Override
+            public void set(String value) {
+                selectedWorld = value;
+            }
+        });
 
         WidgetUtil.trySubscribe(this, "close", button ->
                 triggerBackAnimation()
@@ -33,17 +141,47 @@ public class UniverseSetupScreen extends CoreScreenLayer {
 
         WorldSetupScreen worldSetupScreen = getManager().createScreen(WorldSetupScreen.ASSET_URI, WorldSetupScreen.class);
         WidgetUtil.trySubscribe(this, "worldConfig", button ->
-            triggerForwardAnimation(worldSetupScreen)
-        );
-
-        WidgetUtil.trySubscribe(this, "addGenerator", button ->
                 triggerForwardAnimation(worldSetupScreen)
         );
+
+        WidgetUtil.trySubscribe(this, "addGenerator", button -> {
+            addNewWorld(worldGenerator.getSelection());
+            List<String> worldOptions = Lists.newArrayList(worlds.keySet());
+            worldsDropdown.setOptions(worldOptions);
+            //triggerForwardAnimation(worldSetupScreen);
+        });
 
         WorldPreGenerationScreen worldPreGenerationScreen = getManager().createScreen(WorldPreGenerationScreen.ASSET_URI, WorldPreGenerationScreen.class);
         WidgetUtil.trySubscribe(this, "continue", button ->
                 triggerForwardAnimation(worldPreGenerationScreen)
-         );
+        );
+    }
+
+    private Set<Name> getAllEnabledModuleNames() {
+        Set<Name> enabledModules = Sets.newHashSet();
+        for (Name moduleName : config.getDefaultModSelection().listModules()) {
+            enabledModules.add(moduleName);
+            recursivelyAddModuleDependencies(enabledModules, moduleName);
+        }
+
+        return enabledModules;
+    }
+
+
+    private void recursivelyAddModuleDependencies(Set<Name> modules, Name moduleName) {
+        Module module = moduleManager.getRegistry().getLatestModuleVersion(moduleName);
+        if (module != null) {
+            for (DependencyInfo dependencyInfo : module.getMetadata().getDependencies()) {
+                modules.add(dependencyInfo.getId());
+                recursivelyAddModuleDependencies(modules, dependencyInfo.getId());
+            }
+        }
+    }
+
+    private void addNewWorld(WorldGeneratorInfo worldGeneratorInfo) {
+        selectedWorld = worldGeneratorInfo.getDisplayName() + '-' + worldNumber;
+        worlds.put(selectedWorld, worldGeneratorInfo);
+        worldNumber++;
     }
 }
 
