739b05a2a8e835492944f07290b6d957a0ff8070
Arav Singhal <singhal.arav@gmail.com>
2018-06-18 22:12:30 +0530

    Add GsonTypeHandlerAdapterFactory

---
+++ engine/src/main/java/org/terasology/persistence/typeHandling/gson/GsonTypeHandlerAdapterFactory.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.persistence.typeHandling.gson;
+
+import com.google.gson.Gson;
+import com.google.gson.TypeAdapter;
+import com.google.gson.TypeAdapterFactory;
+import com.google.gson.reflect.TypeToken;
+import org.terasology.persistence.typeHandling.TypeHandler;
+import org.terasology.persistence.typeHandling.TypeSerializationLibrary;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GsonTypeHandlerAdapterFactory implements TypeAdapterFactory {
+    private Map<Class<?>, TypeHandler<?>> typeHandlerMap;
+
+    public GsonTypeHandlerAdapterFactory() {
+        typeHandlerMap = new HashMap<>();
+    }
+
+    public <T> void addTypeHandler(Class<T> type, TypeHandler<T> typeHandler) {
+        typeHandlerMap.put(type, typeHandler);
+    }
+
+    public boolean containsTypeHandlerFor(Class<?> type) {
+        return typeHandlerMap.containsKey(type);
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public <T> TypeAdapter<T> create(Gson gson, TypeToken<T> type) {
+        Class<? super T> rawType = type.getRawType();
+
+        if (!containsTypeHandlerFor(rawType)) {
+            return null;
+        }
+
+        return new GsonTypeHandlerAdapter<>((TypeHandler<T>) typeHandlerMap.get(rawType), gson, type);
+    }
+}
