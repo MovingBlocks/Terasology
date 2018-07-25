2c09ea6cb309633157fdb5889ad15708bdf86741
TheFlash98 <mastermind.sarthak@gmail.com>
2018-07-26 01:16:23 +0530

    Remove redundant comments

--- engine/src/main/java/org/terasology/config/UniverseConfig.java
+++ engine/src/main/java/org/terasology/config/UniverseConfig.java
@@ -16,18 +16,9 @@
 package org.terasology.config;
 
 import com.google.common.collect.Lists;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonDeserializationContext;
-import com.google.gson.JsonDeserializer;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParseException;
-import com.google.gson.JsonSerializationContext;
-import com.google.gson.JsonSerializer;
 import org.terasology.engine.TerasologyConstants;
 import org.terasology.world.internal.WorldInfo;
 
-import java.lang.reflect.Type;
 import java.util.List;
 
 public class UniverseConfig {
@@ -55,39 +46,4 @@ public class UniverseConfig {
         universeSeed = seed;
     }
 
-//    static class Handler implements JsonSerializer<UniverseConfig>, JsonDeserializer<UniverseConfig> {
-//
-//        private static final String WORLDS = "worlds";
-//        private static final String SPAWN_WORLD = "spawnWorld";
-//        private static final String UNIVERSE_SEED = "universeSeed";
-//
-//        @Override
-//        public UniverseConfig deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
-//            UniverseConfig result = new UniverseConfig();
-//            JsonObject jsonObject = json.getAsJsonObject();
-//            if (jsonObject.has(WORLDS)) {
-//                JsonArray worldsArray = jsonObject.getAsJsonArray(WORLDS);
-//                for (JsonElement jsonElement : worldsArray) {
-//                    WorldInfo worldInfo = context.deserialize(jsonElement, WorldInfo.class);
-//                    result.addWorldManager(worldInfo);
-//                }
-//            }
-//            if (jsonObject.has(SPAWN_WORLD)) {
-//                result.spawnWorld = context.deserialize(jsonObject.getAsJsonObject(SPAWN_WORLD), WorldInfo.class);
-//            }
-//            if (jsonObject.has(UNIVERSE_SEED)) {
-//                result.universeSeed = context.deserialize(jsonObject.getAsJsonObject(UNIVERSE_SEED), String.class);
-//            }
-//            return result;
-//        }
-//
-//        @Override
-//        public JsonElement serialize(UniverseConfig src, Type typeOfBindsConfig, JsonSerializationContext context) {
-//            JsonObject result = new JsonObject();
-//            result.add(WORLDS, context.serialize(src.worlds));
-//            result.add(SPAWN_WORLD, context.serialize(src.spawnWorld));
-//            result.add(UNIVERSE_SEED, context.serialize(src.universeSeed));
-//            return result;
-//        }
-//    }
 }
