777e984424996d598d39f88e1f41b6291764075e
theobisproject <theobisproject@gmail.com>
2018-08-09 18:45:47 +0200

    Remove not used import

--- engine/src/main/java/org/terasology/world/propagation/StandardBatchPropagator.java
+++ engine/src/main/java/org/terasology/world/propagation/StandardBatchPropagator.java
@@ -28,7 +28,6 @@ import org.terasology.world.chunks.LitChunk;
 import java.util.Arrays;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Stream;
 
 /**
  * Batch propagator that works on a set of changed blocks
