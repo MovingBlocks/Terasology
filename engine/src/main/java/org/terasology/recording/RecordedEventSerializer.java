acbb08120cba991ad8ad5165019a5625ac8213c0
Arav Singhal <singhal.arav@gmail.com>
2018-11-05 23:23:30 -0600

    Use GsonSerializer in RecordedEventSerializer

--- engine/src/main/java/org/terasology/recording/RecordedEventSerializer.java
+++ engine/src/main/java/org/terasology/recording/RecordedEventSerializer.java
@@ -15,36 +15,23 @@
  */
 package org.terasology.recording;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.entitySystem.entity.EntityManager;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.entity.internal.EngineEntityManager;
-import org.terasology.input.ButtonState;
-import org.terasology.input.Keyboard;
-import org.terasology.input.MouseInput;
-import org.terasology.input.events.MouseAxisEvent;
-import org.terasology.logic.characters.MovementMode;
 import org.terasology.module.ModuleEnvironment;
+import org.terasology.persistence.serializers.GsonSerializer;
 import org.terasology.persistence.typeHandling.PersistedData;
 import org.terasology.persistence.typeHandling.TypeHandler;
 import org.terasology.persistence.typeHandling.TypeSerializationLibrary;
-import org.terasology.persistence.typeHandling.coreTypes.EnumTypeHandler;
 import org.terasology.persistence.typeHandling.extensionTypes.EntityRefTypeHandler;
-import org.terasology.persistence.typeHandling.gson.GsonPersistedData;
-import org.terasology.persistence.typeHandling.gson.GsonPersistedDataSerializer;
 import org.terasology.reflection.TypeInfo;
 import org.terasology.reflection.copy.CopyStrategyLibrary;
 import org.terasology.reflection.reflect.ReflectionReflectFactory;
 import org.terasology.utilities.ReflectionUtil;
 
-import java.io.Reader;
-import java.io.FileReader;
-import java.io.Writer;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -57,6 +44,7 @@ class RecordedEventSerializer {
 
     private static final Logger logger = LoggerFactory.getLogger(RecordedEventSerializer.class);
 
+    private GsonSerializer gsonSerializer = new GsonSerializer();
     private TypeHandler<List<RecordedEvent>> recordedEventListTypeHandler;
 
     public RecordedEventSerializer(EntityManager entityManager, ModuleEnvironment moduleEnvironment) {
@@ -65,11 +53,6 @@ class RecordedEventSerializer {
 
         TypeSerializationLibrary typeSerializationLibrary = TypeSerializationLibrary.createDefaultLibrary(reflectFactory, copyStrategyLibrary);
         typeSerializationLibrary.addTypeHandler(EntityRef.class, new EntityRefTypeHandler((EngineEntityManager) entityManager));
-        typeSerializationLibrary.addTypeHandler(MouseAxisEvent.MouseAxis.class, new EnumTypeHandler<>(MouseAxisEvent.MouseAxis.class));
-        typeSerializationLibrary.addTypeHandler(ButtonState.class, new EnumTypeHandler<>(ButtonState.class));
-        typeSerializationLibrary.addTypeHandler(Keyboard.Key.class, new EnumTypeHandler<>(Keyboard.Key.class));
-        typeSerializationLibrary.addTypeHandler(MouseInput.class, new EnumTypeHandler<>(MouseInput.class));
-        typeSerializationLibrary.addTypeHandler(MovementMode.class, new EnumTypeHandler<>(MovementMode.class));
 
         ClassLoader[] classLoaders = ReflectionUtil.getComprehensiveEngineClassLoaders(moduleEnvironment);
 
@@ -84,12 +67,8 @@ class RecordedEventSerializer {
      * @param filePath path where the data should be saved.
      */
     public void serializeRecordedEvents(List<RecordedEvent> events, String filePath) {
-        GsonPersistedDataSerializer serializationContext = new GsonPersistedDataSerializer();
-        GsonPersistedData data = (GsonPersistedData) recordedEventListTypeHandler.serialize(events, serializationContext);
-
-        try (Writer writer = new FileWriter(filePath)) {
-            Gson gson = new GsonBuilder().create();
-            gson.toJson(data.getElement(), writer);
+        try {
+            gsonSerializer.writeJson(events, recordedEventListTypeHandler, filePath);
         } catch (IOException e) {
             logger.error("Error while serializing recorded events", e);
         }
@@ -103,10 +82,8 @@ class RecordedEventSerializer {
     public List<RecordedEvent> deserializeRecordedEvents(String filePath) {
         List<RecordedEvent> events = new ArrayList<>();
 
-        try (Reader reader = new FileReader(filePath)) {
-            Gson gson = new GsonBuilder().create();
-            JsonElement jsonElement = gson.fromJson(reader, JsonElement.class);
-            PersistedData persistedData = new GsonPersistedData(jsonElement);
+        try {
+            PersistedData persistedData = gsonSerializer.persistedDatafromJson(new File(filePath));
 
             Optional<List<RecordedEvent>> recordedEvents = recordedEventListTypeHandler.deserialize(persistedData);
             recordedEvents.ifPresent(events::addAll);
