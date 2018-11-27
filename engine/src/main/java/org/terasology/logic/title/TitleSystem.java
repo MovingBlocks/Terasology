c399942277f5eda268b775cf2e181eda2793f5db
iHDeveloper <hamzadevelop@gmail.com>
2018-11-27 20:42:38 +0300

    Title: Add /title command

---
+++ engine/src/main/java/org/terasology/logic/title/TitleSystem.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.logic.title;
+
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterMode;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.console.Console;
+import org.terasology.logic.console.commandSystem.annotations.Command;
+import org.terasology.registry.In;
+
+@RegisterSystem(RegisterMode.ALWAYS)
+public class TitleSystem extends BaseComponentSystem {
+
+    @In
+    private Console console;
+
+    @Command(shortDescription = "To test the title")
+    public void title() {
+        // TODO Execute the title command
+        // TODO title <title:subtitle> <text> <fadeIn(30)> <stay(30)> <fadeOut(30)>
+    }
+}
