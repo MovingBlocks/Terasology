be540bda2e2f65e12fdd13d41bab4ec25a28fb85
iHDeveloper <hamzadevelop@gmail.com>
2018-12-04 19:31:12 +0300

    Title: Rebuild the system

--- engine/src/main/java/org/terasology/logic/title/TitleSystem.java
+++ engine/src/main/java/org/terasology/logic/title/TitleSystem.java
@@ -18,7 +18,8 @@ package org.terasology.logic.title;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.assets.ResourceUrn;
-import org.terasology.context.Context;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterMode;
 import org.terasology.entitySystem.systems.RegisterSystem;
@@ -26,18 +27,22 @@ import org.terasology.logic.console.Console;
 import org.terasology.logic.console.CoreMessageType;
 import org.terasology.logic.console.commandSystem.annotations.Command;
 import org.terasology.logic.console.commandSystem.annotations.CommandParam;
+import org.terasology.logic.delay.DelayManager;
+import org.terasology.logic.delay.DelayedActionTriggeredEvent;
+import org.terasology.logic.players.LocalPlayer;
 import org.terasology.registry.In;
+import org.terasology.registry.Share;
 import org.terasology.rendering.nui.NUIManager;
 
+@Share(Title.class)
 @RegisterSystem(RegisterMode.ALWAYS)
-public class TitleSystem extends BaseComponentSystem implements Runnable {
+public class TitleSystem extends BaseComponentSystem implements Title {
 
     private static final Logger logger = LoggerFactory.getLogger(TitleSystem.class);
 
     private static final ResourceUrn UI_URL = new ResourceUrn("engine:title");
 
-    @In
-    private Context context;
+    private static final String DELAY_ACTION_ID = "TITLE";
 
     @In
     private Console console;
@@ -45,51 +50,25 @@ public class TitleSystem extends BaseComponentSystem implements Runnable {
     @In
     private NUIManager nuiManager;
 
+    @In
+    private DelayManager delayManager;
+
+    @In
+    private LocalPlayer localPlayer;
+
     private TitleScreen titleScreen;
-    private Thread thread;
-    private float currentStay;
-    private boolean alive;
     private String lastTitle = "";
     private String lastSubtitle = "";
+    private long lastStay;
 
     @Override
     public void initialise() {
-        thread = new Thread(this);
-        thread.setName("TITLE-STAY");
-        thread.start();
-        alive = true;
         logger.info("Initialised the title system!");
-        context.put(TitleSystem.class, this);
     }
 
     @Override
     public void shutdown() {
-        alive = false;
-        thread.interrupt();
         logger.info("Successfully! shut down the title system!");
-        context.put(TitleSystem.class, null);
-    }
-
-    @Override
-    public void run() {
-        while (alive) {
-            if (currentStay <= 0) {
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException ex) { } // Ignore because this may happen because of #shutdown() which it doesn't matter
-                continue;
-            }
-            currentStay -= 1f;
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException ex) { } // Ignore because this may happen because of #shutdown() which it doesn't matter
-            if (currentStay <= 0 && (titleScreen != null)) {
-                currentStay = 0;
-                hide();
-                continue;
-            }
-        }
-        currentStay = 0; // Make sure to make it 0 even if the title got hidden
     }
 
     @Command(
@@ -107,16 +86,17 @@ public class TitleSystem extends BaseComponentSystem implements Runnable {
         }
         if (value != null) {
             if (type.equalsIgnoreCase("title")) {
-                show(value, lastSubtitle, currentStay);
+                show(value, lastSubtitle, lastStay);
                 lastTitle = value;
                 console.addMessage("Done! The current title value is " + value);
             } else if (type.equalsIgnoreCase("subtitle")) {
-                show(lastTitle, value, currentStay);
+                show(lastTitle, value, lastStay);
                 lastSubtitle = value;
                 console.addMessage("Done! The current subtitle value is " + value);
             } else if (type.equalsIgnoreCase("stay")) {
                 try {
-                    float stay = Float.parseFloat(value);
+                    long stay = Long.parseLong(value);
+                    lastStay = stay;
                     show(lastTitle, lastSubtitle, stay);
                     console.addMessage("Done! The current stay value is " + stay);
                 } catch (NumberFormatException ex) {
@@ -128,32 +108,47 @@ public class TitleSystem extends BaseComponentSystem implements Runnable {
         }
     }
 
+    @ReceiveEvent(netFilter = RegisterMode.CLIENT)
+    public void onDelayTriggered(DelayedActionTriggeredEvent event, EntityRef entity) {
+        if (event.getActionId().equals(DELAY_ACTION_ID)) {
+            hide();
+        }
+    }
+
     /**
      * Show a title screen to the player.
      *
      * @param title will be shown in bigger font
      * @param subtitle will be shown in small font
-     * @param stay how much do you wanna from it to stay for the player ( in milliseconds )
+     * @param stay how much do you wanna from it to stay for the player
      */
-    public void show(String title, String subtitle, float stay) {
+    public void show(String title, String subtitle, long stay) {
         build();
-        currentStay = stay;
         titleScreen.setTitle(title);
         titleScreen.setSubtitle(subtitle);
         titleScreen.update();
+        clear();
+        delayManager.addDelayedAction(localPlayer.getClientEntity(), DELAY_ACTION_ID, stay);
     }
 
     /**
      * Reset the title screen
      */
     public void hide() {
+        clear();
         build();
         titleScreen.setTitle("");
         titleScreen.setSubtitle("");
         titleScreen.update();
         nuiManager.removeOverlay(UI_URL);
         titleScreen = null;
-        currentStay = 0;
+    }
+
+    private void clear() {
+        EntityRef entity = localPlayer.getClientEntity();
+        if (delayManager.hasDelayedAction(entity, DELAY_ACTION_ID)) {
+            delayManager.cancelDelayedAction(entity, DELAY_ACTION_ID);
+        }
     }
 
     private void build() {
