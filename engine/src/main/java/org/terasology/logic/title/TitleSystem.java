3bf393196e6df89a13d0bc5ae8e3913947177b5f
iHDeveloper <hamzadevelop@gmail.com>
2018-12-05 17:31:58 +0300

    Title: Apply checkstyle

--- engine/src/main/java/org/terasology/logic/title/TitleSystem.java
+++ engine/src/main/java/org/terasology/logic/title/TitleSystem.java
@@ -15,8 +15,6 @@
  */
 package org.terasology.logic.title;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
