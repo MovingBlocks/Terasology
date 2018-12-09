236348c7feed2bebe3d4b34e1a6eacbfb085f6fc
iHDeveloper <hamzadevelop@gmail.com>
2018-12-09 20:48:50 +0300

    Title: Add javadoc

--- engine/src/main/java/org/terasology/logic/title/Title.java
+++ engine/src/main/java/org/terasology/logic/title/Title.java
@@ -16,6 +16,20 @@
 package org.terasology.logic.title;
 
 public interface Title {
+
+    /**
+     * Shows a title with subtitle in the middle screen
+     * and gives you a option to make it disappear after some time
+     * of showing it.
+     *
+     * @param title The title that will show in the middle of the screen
+     * @param subtitle The subtitle that will show under the title with small size
+     * @param stay How much do you want to that title to stay
+     */
     void show(String title, String subtitle, long stay);
+
+    /**
+     * Hide the title from the screen
+     */
     void hide();
 }
