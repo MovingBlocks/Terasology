e2745e2dda6f1d94d8871b5f243c580a636e538b
Quinn Roberts <quinn@jelly-snakes.com>
2018-12-20 16:14:07 +1100

    Reformat tests to use spaces not tabs

--- engine-tests/src/test/java/org/terasology/rendering/FontColorTest.java
+++ engine-tests/src/test/java/org/terasology/rendering/FontColorTest.java
@@ -16,40 +16,40 @@
 
 package org.terasology.rendering;
 
+import org.junit.Test;
+
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
-
 public class FontColorTest {
-		
+
     @Test
     public void testResetColor() {
         char resetColor = 0xF000;
         assertTrue(FontColor.isValid(resetColor));
     }
-	
+
     @Test
-	public void testFirstColor() {
+    public void testFirstColor() {
         char firstColor = 0xE000;
         assertTrue(FontColor.isValid(firstColor));
     }
-	
-	@Test
-	public void testLastColor() {
+
+    @Test
+    public void testLastColor() {
         char lastColor = 0xEFFF;
         assertTrue(FontColor.isValid(lastColor));
     }
-	
-	@Test
-	public void testBetweenColor() {
+
+    @Test
+    public void testBetweenColor() {
         char betweenColor = 0xEB8F;
         assertTrue(FontColor.isValid(betweenColor));
     }
-	
-	@Test
-	public void testInvalidColor() {
+
+    @Test
+    public void testInvalidColor() {
         char invalidColor = 0xA10F;
         assertFalse(FontColor.isValid(invalidColor));
-    }			
+    }
 }
