040fb875fe0ca779caae672ba774f65ddbac2880
dave2s <xskace12@vutbr.cz>
2019-01-27 11:43:12 +0100

    separate vignette fragment shader draft

--- engine/src/main/resources/assets/shaders/vignette_frag.glsl
+++ engine/src/main/resources/assets/shaders/vignette_frag.glsl
@@ -1,19 +1,25 @@
+#ifdef VIGNETTE
 uniform sampler2D texScene;
 
 uniform sampler2D texVignette;
 uniform vec3 inLiquidTint;
+uniform vec3 tint = vec3(0.937,0.126,0.016);
+uniform bool isTint = true;
 
 void main(){
 	vec4 color = texture2D(texScene, gl_TexCoord[0].xy);
-
-	    float vig = texture2D(texVignette, gl_TexCoord[0].xy).x;
-
-	    if (!swimming) {
-	        color.rgb *= vig;
-	    } else {
-	        color.rgb *= vig * vig * vig;
-	        color.rgb *= inLiquidTint;
-	    }
-	    gl_FragData[0].rgba = color.rgba;
-
+    float vig = texture2D(texVignette, gl_TexCoord[0].xy).x;    
+    if (!swimming) {
+		if(isTint){
+			color.rgb *=  vec3(vig)+vec3(1-vig)*tint.rgb;      
+        }
+        else{
+        	color.rgb *= vig;
+    	}
+    } else {
+        color.rgb *= vig * vig * vig;
+        color.rgb *= inLiquidTint;
+    }
+    gl_FragData[0].rgba = color.rgba;
 }
+#endif
