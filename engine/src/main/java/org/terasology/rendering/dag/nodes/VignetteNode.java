ac0d9877978b99f46304debf87742e44d7e81762
dave2s <xskace12@vutbr.cz>
2019-01-30 19:32:18 +0100

    Vignette read from finalPostprocess write to Final Buffer.

--- engine/src/main/java/org/terasology/rendering/dag/nodes/VignetteNode.java
+++ engine/src/main/java/org/terasology/rendering/dag/nodes/VignetteNode.java
@@ -20,6 +20,7 @@ import org.terasology.config.Config;
 import org.terasology.config.RenderingConfig;
 import org.terasology.context.Context;
 import org.terasology.engine.SimpleUri;
+import org.terasology.math.geom.Vector3f;
 import org.terasology.monitoring.PerformanceMonitor;
 import org.terasology.rendering.assets.material.Material;
 import org.terasology.rendering.cameras.SubmersibleCamera;
@@ -36,7 +37,9 @@ import org.terasology.world.WorldProvider;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 
-import static org.terasology.rendering.dag.nodes.LightShaftsNode.LIGHT_SHAFTS_FBO_URI;
+import static org.terasology.rendering.opengl.fbms.DisplayResolutionDependentFBOs.FINAL_BUFFER;
+import static org.terasology.rendering.dag.nodes.FinalPostProcessingNode.POST_FBO_URI;
+
 import static org.terasology.rendering.dag.stateChanges.SetInputTextureFromFbo.FboTexturesTypes.ColorTexture;
 import static org.terasology.rendering.opengl.OpenGLUtils.renderFullscreenQuad;
 import static org.terasology.rendering.opengl.ScalingFactors.FULL_SCALE;
@@ -46,11 +49,11 @@ import static org.terasology.rendering.opengl.ScalingFactors.ONE_8TH_SCALE;
  * An instance of this node adds vignette onto the rendering achieved so far, stored in the gbuffer.
  * It should provide ability to use various vignette textures and tinting.
  * 1 Channeled transparency texture is used atm.
- * Stores the result into the InitialPostProcessingNode.VIGNETTE_FBO_URI, to be used at a later stage.
+ * Stores the result into the VignetteNode.VIGNETTE_FBO_URI, to be used at a later stage.
  * Requirements: https://github.com/MovingBlocks/Terasology/issues/3040
  */
 public class VignetteNode extends AbstractNode implements PropertyChangeListener {
-    static final SimpleUri VIGNETTE_FBO_URI = new SimpleUri("engine:fbo.vignette");
+    public static final SimpleUri VIGNETTE_FBO_URI = new SimpleUri("engine:fbo.vignette");
     private static final ResourceUrn VIGNETTE_MATERIAL_URN = new ResourceUrn("engine:prog.vignette");
 
     private RenderingConfig renderingConfig;
@@ -61,6 +64,9 @@ public class VignetteNode extends AbstractNode implements PropertyChangeListener
     private Material vignetteMaterial;
 
     private boolean vignetteIsEnabled;
+    // TODO: figure where from to set this variable
+    //private Vector3f tint = new Vector3f(1.0f,1.0f,1.0f);
+    private Vector3f tint = new Vector3f(0.937f,0.126f,0.016f);
 
     private StateChange setVignetteInputTexture;
 
@@ -74,38 +80,28 @@ public class VignetteNode extends AbstractNode implements PropertyChangeListener
 
         DisplayResolutionDependentFBOs displayResolutionDependentFBOs = context.get(DisplayResolutionDependentFBOs.class);
         // TODO: see if we could write this straight into a GBUFFER
-        FBO vignetteFbo = requiresFBO(new FBOConfig(VIGNETTE_FBO_URI, FULL_SCALE, FBO.Type.HDR), displayResolutionDependentFBOs);
-        addDesiredStateChange(new BindFbo(vignetteFbo));
-        addDesiredStateChange(new SetViewportToSizeOf(vignetteFbo));
+       // FBO vignetteFbo = requiresFBO(new FBOConfig(VIGNETTE_FBO_URI, FULL_SCALE, FBO.Type.DEFAULT), displayResolutionDependentFBOs);
+        FBO finalBuffer = displayResolutionDependentFBOs.get(FINAL_BUFFER);
+        addDesiredStateChange(new BindFbo(finalBuffer));
+        addDesiredStateChange(new SetViewportToSizeOf(finalBuffer));
 
         addDesiredStateChange(new EnableMaterial(VIGNETTE_MATERIAL_URN));
 
         vignetteMaterial = getMaterial(VIGNETTE_MATERIAL_URN);
 
         renderingConfig = context.get(Config.class).getRendering();
-//        bloomIsEnabled = renderingConfig.isBloom();
-//        renderingConfig.subscribe(RenderingConfig.BLOOM, this);
-//        lightShaftsAreEnabled = renderingConfig.isLightShafts();
-//        renderingConfig.subscribe(RenderingConfig.LIGHT_SHAFTS, this);
         vignetteIsEnabled = renderingConfig.isVignette();
         renderingConfig.subscribe(RenderingConfig.VIGNETTE, this);
 
-        // TODO: Temporary hack for now.
-//        FBOConfig one8thScaleBloomConfig = new FBOConfig(BloomBlurNode.ONE_8TH_SCALE_FBO_URI, ONE_8TH_SCALE, FBO.Type.DEFAULT);
-//        FBO one8thBloomFbo = requiresFBO(one8thScaleBloomConfig, displayResolutionDependentFBOs);
-
         int textureSlot = 0;
-        addDesiredStateChange(new SetInputTextureFromFbo(textureSlot++, displayResolutionDependentFBOs.getGBufferPair().getLastUpdatedFbo(), ColorTexture, displayResolutionDependentFBOs, VIGNETTE_MATERIAL_URN, "texScene"));
-        addDesiredStateChange(new SetInputTexture2D(textureSlot++, "engine:vignette", VIGNETTE_MATERIAL_URN, "texVignette"));
-//        setBloomInputTexture = new SetInputTextureFromFbo(textureSlot++, one8thBloomFbo, ColorTexture, displayResolutionDependentFBOs, VIGNETTE_MATERIAL_URN, "texBloom");
-//        setLightShaftsInputTexture = new SetInputTextureFromFbo(textureSlot, LIGHT_SHAFTS_FBO_URI, ColorTexture, displayResolutionDependentFBOs, VIGNETTE_MATERIAL_URN, "texLightShafts");
+        //addDesiredStateChange(new SetInputTextureFromFbo(textureSlot++, displayResolutionDependentFBOs.getGBufferPair().getLastUpdatedFbo(), ColorTexture, displayResolutionDependentFBOs, VIGNETTE_MATERIAL_URN, "texScene"));
+        addDesiredStateChange(new SetInputTextureFromFbo(textureSlot++, POST_FBO_URI , ColorTexture, displayResolutionDependentFBOs, VIGNETTE_MATERIAL_URN, "texScene"));
 
-//        if (bloomIsEnabled) {
-//            addDesiredStateChange(setBloomInputTexture);
-//        }
-//        if (lightShaftsAreEnabled) {
-//            addDesiredStateChange(setLightShaftsInputTexture);
-//        }
+        setVignetteInputTexture = new SetInputTexture2D(textureSlot++, "engine:vignette", VIGNETTE_MATERIAL_URN, "texVignette");
+
+        if(vignetteIsEnabled) {
+            addDesiredStateChange(setVignetteInputTexture);
+        }
     }
 
     /**
@@ -122,6 +118,7 @@ public class VignetteNode extends AbstractNode implements PropertyChangeListener
         // Shader Parameters
 
         vignetteMaterial.setFloat3("inLiquidTint", worldProvider.getBlock(activeCamera.getPosition()).getTint(), true);
+        vignetteMaterial.setFloat3("tint", tint);
 
         // Actual Node Processing
 
@@ -136,7 +133,7 @@ public class VignetteNode extends AbstractNode implements PropertyChangeListener
 
         switch (propertyName) {
             case RenderingConfig.VIGNETTE:
-                vignetteIsEnabled = renderingConfig.isBloom();
+                vignetteIsEnabled = renderingConfig.isVignette();
                 if (vignetteIsEnabled) {
                     addDesiredStateChange(setVignetteInputTexture);
                 } else {
