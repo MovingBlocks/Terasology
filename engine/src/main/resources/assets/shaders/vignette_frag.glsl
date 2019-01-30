431f6a09f67deece2c6a93b52fb4055604d66f59
dave2s <xskace12@vutbr.cz>
2019-01-30 16:12:03 +0100

    vignette tinting shader

--- engine/src/main/resources/assets/shaders/vignette_frag.glsl
+++ engine/src/main/resources/assets/shaders/vignette_frag.glsl
@@ -3,19 +3,14 @@ uniform sampler2D texScene;
 
 uniform sampler2D texVignette;
 uniform vec3 inLiquidTint;
-uniform vec3 tint = vec3(0.937,0.126,0.016);
-uniform bool isTint = true;
+uniform vec3 tint = vec3(1.0,1.0,1.0);
 
 void main(){
 	vec4 color = texture2D(texScene, gl_TexCoord[0].xy);
     float vig = texture2D(texVignette, gl_TexCoord[0].xy).x;    
     if (!swimming) {
-		if(isTint){
-			color.rgb *=  vec3(vig)+vec3(1-vig)*tint.rgb;      
-        }
-        else{
-        	color.rgb *= vig;
-    	}
+			color.rgb *= vec3(vig)+(1-vig)*tint.rgb;      
+        	//color.rgb *= vig;
     } else {
         color.rgb *= vig * vig * vig;
         color.rgb *= inLiquidTint;
