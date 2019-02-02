895d8344423ab14a1a45885b2500f4499beefdfc
dave2s <xskace12@vutbr.cz>
2019-02-02 14:07:30 +0100

    fix shader for vignette off -fixes black screen for vignette off -other nodes do this the same way

--- engine/src/main/resources/assets/shaders/vignette_frag.glsl
+++ engine/src/main/resources/assets/shaders/vignette_frag.glsl
@@ -1,12 +1,15 @@
-#ifdef VIGNETTE
 uniform sampler2D texScene;
 
+#ifdef VIGNETTE
 uniform sampler2D texVignette;
 uniform vec3 inLiquidTint;
 uniform vec3 tint = vec3(1.0,1.0,1.0);
+#endif
 
 void main(){
 	vec4 color = texture2D(texScene, gl_TexCoord[0].xy);
+
+#ifdef VIGNETTE
     float vig = texture2D(texVignette, gl_TexCoord[0].xy).x;    
     if (!swimming) {
 			color.rgb *= vec3(vig)+(1-vig)*tint.rgb;      
@@ -15,6 +18,6 @@ void main(){
         color.rgb *= vig * vig * vig;
         color.rgb *= inLiquidTint;
     }
+#endif    
     gl_FragData[0].rgba = color.rgba;
 }
-#endif
