4deebacb6ff84f20fea5d98ad24cab5482c98c8b
SHAUNAK DIXIT <shaunak.thapar@gmail.com>
2019-02-15 01:37:05 +0530

    added a new screen in InGame Menu

--- engine/src/main/java/org/terasology/rendering/nui/layers/ingame/ExtraMenuScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/ingame/ExtraMenuScreen.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 MovingBlocks
+ * Copyright 2019 MovingBlocks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,19 +15,20 @@
  */
 package org.terasology.rendering.nui.layers.ingame;
 
-import org.terasology.engine.GameEngine;
+import org.terasology.crashreporter.CrashReporter;
+import org.terasology.engine.LoggingContext;
 import org.terasology.engine.Time;
-import org.terasology.engine.modes.StateMainMenu;
 import org.terasology.network.NetworkMode;
 import org.terasology.network.NetworkSystem;
-import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.WidgetUtil;
+import org.terasology.telemetry.TelemetryScreen;
 
 /**
+ * Handles the "Extras" button from the game's pause menu screen.
  */
-public class PauseMenu extends CoreScreenLayer {
+public class ExtraMenuScreen extends CoreScreenLayer {
 
     @In
     private Time time;
@@ -37,12 +38,10 @@ public class PauseMenu extends CoreScreenLayer {
 
     @Override
     public void initialise() {
-        WidgetUtil.trySubscribe(this, "close", widget -> getManager().closeScreen(PauseMenu.this));
-        WidgetUtil.trySubscribe(this, "extra", widget -> getManager().pushScreen("extraMenuScreen"));
-        WidgetUtil.trySubscribe(this, "settings", widget -> getManager().pushScreen("settingsMenuScreen"));
-        WidgetUtil.trySubscribe(this, "mainMenu", widget -> CoreRegistry.get(GameEngine.class).changeState(new StateMainMenu()));
-        WidgetUtil.trySubscribe(this, "exit", widget -> CoreRegistry.get(GameEngine.class).shutdown());
-
+        WidgetUtil.trySubscribe(this, "devTools", widget -> getManager().pushScreen("devToolsMenuScreen"));
+        WidgetUtil.trySubscribe(this, "crashReporter", widget -> CrashReporter.report(new Throwable("There is no error."), LoggingContext.getLoggingPath(), CrashReporter.MODE.ISSUE_REPORTER));
+        WidgetUtil.trySubscribe(this, "telemetry", button -> triggerForwardAnimation(TelemetryScreen.ASSET_URI));
+        WidgetUtil.trySubscribe(this, "close", widget -> getManager().closeScreen(ExtraMenuScreen.this));
     }
 
     @Override
