83fb3deb83fd196eb91f0f73f555ce4cc670851f
TheHxn <hassaan.ali.lel@gmail.com>
2019-04-03 15:31:03 +0500

    -Organized server and client side stats. -Added compass navigation (debug).

--- engine/src/main/java/org/terasology/rendering/nui/layers/ingame/metrics/DebugOverlay.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/ingame/metrics/DebugOverlay.java
@@ -37,7 +37,6 @@ import org.terasology.world.biomes.BiomeManager;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryType;
-import java.lang.management.MemoryUsage;
 
 import java.util.Locale;
 
