b7e409a6dfa846dcd20eb6ce8a2d26d875581fb8
Dave2S <davidskacel@gmail.com>
2019-05-17 13:48:57 +0200

    mend

--- engine/src/main/java/org/terasology/rendering/dag/nodes/FinalPostProcessingNode.java
+++ engine/src/main/java/org/terasology/rendering/dag/nodes/FinalPostProcessingNode.java
@@ -24,8 +24,7 @@ import org.terasology.monitoring.PerformanceMonitor;
 import org.terasology.rendering.assets.material.Material;
 import org.terasology.rendering.assets.texture.TextureUtil;
 import org.terasology.rendering.cameras.Camera;
-import org.terasology.rendering.dag.gsoc.EdgeConnection;
-import org.terasology.rendering.dag.gsoc.NewAbstractNode;
+import org.terasology.rendering.dag.AbstractNode;
 import org.terasology.rendering.dag.StateChange;
 import org.terasology.rendering.dag.stateChanges.BindFbo;
 import org.terasology.rendering.dag.stateChanges.EnableMaterial;
@@ -60,7 +59,7 @@ import static org.terasology.rendering.opengl.fbms.DisplayResolutionDependentFBO
  * the content of a number of technical buffers rather than the final, post-processed rendering
  * of the scene.
  */
-public class FinalPostProcessingNode extends NewAbstractNode implements PropertyChangeListener {
+public class FinalPostProcessingNode extends AbstractNode implements PropertyChangeListener {
     private static final ResourceUrn POST_MATERIAL_URN = new ResourceUrn("engine:prog.post");
 
     private WorldRenderer worldRenderer;
@@ -88,8 +87,8 @@ public class FinalPostProcessingNode extends NewAbstractNode implements Property
 
     private final int noiseTextureSize = 1024;
 
-    public FinalPostProcessingNode(String nodeUri, Context context, EdgeConnection in, EdgeConnection out) {
-        super(nodeUri, context, in, out);
+    public FinalPostProcessingNode(String nodeUri, Context context) {
+        super(nodeUri, context);
 
         worldRenderer = context.get(WorldRenderer.class);
         activeCamera = worldRenderer.getActiveCamera();
