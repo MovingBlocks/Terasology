0aa3543c8be632294381548b0cce3a2b87eb84fa
Dave2S <davidskacel@gmail.com>
2019-06-13 14:10:13 +0200

    deapify DAG packages

--- engine/src/main/java/org/terasology/rendering/dag/gsoc/package-info.java
+++ engine/src/main/java/org/terasology/rendering/dag/gsoc/package-info.java
@@ -14,6 +14,6 @@
  * limitations under the License.
  */
 
-@API package org.terasology.rendering.dag.gsoc;
+// package org.terasology.rendering.dag.gsoc;
 
-import org.terasology.module.sandbox.API;
+// import org.terasology.module.sandbox.API;
