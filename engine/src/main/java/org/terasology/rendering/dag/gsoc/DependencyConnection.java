e82817233d12b2fed93b3ee57b5898a9dd5d3df9
Dave2S <davidskacel@gmail.com>
2019-06-13 14:09:22 +0200

    Dep.Con. to DependencyConnection<T>, FboConnection extends DC<FBO>, DC has T data; FboConnection: -- ~get/setData()~, only inherits now, ++ overrides getConnectionName() returns FBO.append(id) DependencyConnection: -- SimpleUri connectedNode ++ DependencyConnection connectedConnection + associated getter/setter ++ reconnectInputConnectionToOutput(); ++ T getData(), setData(T data),

--- engine/src/main/java/org/terasology/rendering/dag/gsoc/DependencyConnection.java
+++ engine/src/main/java/org/terasology/rendering/dag/gsoc/DependencyConnection.java
@@ -21,18 +21,28 @@ import org.terasology.engine.SimpleUri;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
-public abstract class DependencyConnection {
+public abstract class DependencyConnection<T> {
 
     private String connectionName;
     private Type connectionType;
     private SimpleUri parentNode;
-    private SimpleUri connectedNode;
+    private DependencyConnection connectedConnection;
+    private T data;
 
     DependencyConnection(String name, Type type, SimpleUri parentNode) {
         this.connectionName = name;
         this.connectionType = type;
         this.parentNode = parentNode;
-        connectedNode = null;
+        // connectedNode = null;
+        connectedConnection = null;
+    }
+
+    public T getData() {
+        return this.data;
+    }
+
+    protected void setData(T data) {
+        this.data = data;
     }
 
     public enum Type { // Might differentiate by name only
@@ -51,25 +61,24 @@ public abstract class DependencyConnection {
     }
 
     /**
-     * Getter for a SimpleUri name of DependencyConnection.connectedNode. The idea is that when connecting a toNode's input to another
-     * fromNode's output, you query the OUTPUT connection for its connectedNode.
-     * TODO Read TODO for setConnectedNode
+     * Getter for a DependencyConnection connection of connected node. The idea is that when connecting a toNode's input to another
+     * fromNode's output, you query the OUTPUT connection for its connectedNode. And vice-versa when you want to e.g. remove a node and you need
+     * to know whether it's connected to some dependencies.
      * @return A node name if this Connection has been connected to another node's connection. Null if the output
      * has not yet been connected.
      */
     @Nullable
-    public SimpleUri getConnectedNode() {
-        return this.connectedNode;
+    public DependencyConnection getConnectedConnection() {
+        return this.connectedConnection;
     }
 
     /**
-     * Setter for a SimpleUri name of DependencyConnection.connectedNode. The idea is that when connecting a toNode's input to another
-     * fromNode's output, you set the OUTPUT connection's connectedNode to the toNode's SimpleUri name.
-     * TODO Whether to set this for input connections which do not origin in it's parent node remains a question.
-     * @param connectedNode A SimpleUri name of connected node.
+     * Setter for a DependencyConnection connection of DependencyConnection.connectedConnection. The idea is that when connecting a toNode's input to another
+     * fromNode's output, you set the OUTPUT connection's connectedConnection to the toNode's DependencyConnection which you connect.
+     * @param connectedConnection A DependenyConnection connection of connected node.
      */
-    protected void setConnectedNode(SimpleUri connectedNode) {
-        this.connectedNode = connectedNode;
+    protected void setConnectedConnection(DependencyConnection connectedConnection) {
+        this.connectedConnection= connectedConnection;
     }
 
     protected void setParentNode(SimpleUri parentNode) {
@@ -84,11 +93,23 @@ public abstract class DependencyConnection {
         return this.connectionType;
     }
 
+    /**
+     * Removes current link for both ends of the connection, set's connected node to connectToConnection and gets its data
+     * @param connectToConnection
+     */
+    public void reconnectInputConnectionToOutput(DependencyConnection<T> connectToConnection) {
+        this.connectedConnection.connectedConnection = null;
+        this.connectedConnection = connectToConnection;
+        this.setData(connectToConnection.getData());
+    }
+
+    // String getConnectionName(int number);
+
     @Override
     public String toString() {
-        return (this.connectionType == Type.OUTPUT) ?
-                String.format("Output:%s([Parent]%s->%s)", this.connectionName, this.parentNode, this.connectedNode) :
-                String.format("Input:%s([Parent]%s<-%s)", this.connectionName, this.parentNode, this.connectedNode);
+        return (connectionType == Type.OUTPUT) ?
+                String.format("Output:%s([Parent]%s->%s)", connectionName, parentNode, connectedConnection.getName()) :
+                String.format("Input:%s([Parent]%s<-%s)", connectionName, parentNode, connectedConnection.getName());
     }
 
 }
