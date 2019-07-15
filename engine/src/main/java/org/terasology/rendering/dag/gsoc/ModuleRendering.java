3286aeea654aeed6610ab1fcb464e8405d14c244
Dave2S <davidskacel@gmail.com>
2019-07-15 15:33:25 +0200

    Update ModuleRendering to be extended by BasicRenderingModule and add createBufferPair() and generateWithDimensions()

--- engine/src/main/java/org/terasology/rendering/dag/gsoc/ModuleRendering.java
+++ engine/src/main/java/org/terasology/rendering/dag/gsoc/ModuleRendering.java
@@ -15,17 +15,24 @@
  */
 package org.terasology.rendering.dag.gsoc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.context.Context;
+import org.terasology.engine.SimpleUri;
 import org.terasology.engine.module.ModuleManager;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
 import org.terasology.entitySystem.systems.RegisterSystem;
 import org.terasology.naming.Name;
 import org.terasology.registry.In;
 import org.terasology.rendering.dag.RenderGraph;
+import org.terasology.rendering.opengl.FBO;
+import org.terasology.rendering.opengl.FboConfig;
+import org.terasology.rendering.opengl.ScalingFactors;
 import org.terasology.rendering.world.WorldRenderer;
 
 @RegisterSystem
 public abstract class ModuleRendering extends BaseComponentSystem {
+    protected static final Logger logger = LoggerFactory.getLogger(ModuleRendering.class);
 
     @In
     protected Context context;
@@ -34,25 +41,58 @@ public abstract class ModuleRendering extends BaseComponentSystem {
     protected WorldRenderer worldRenderer;
 
     // @Override
-    public void initialise() {
+    public void initialise(Class clazz) {
         super.initialise();
+        setProvidingModule(clazz);
         renderGraph = context.get(RenderGraph.class);
         worldRenderer = context.get(WorldRenderer.class);
-
-        /* Class implementingClass;
-        ModuleManager moduleManager = context.get(ModuleManager.class);
-        try {
-            implementingClass = Class.forName(this.getClass().getName());
-        } catch (Exception e) {
-            throw new RuntimeException("Error in getting name of the class implementing ModuleRenderingSystem: " + e);
-        }
-        providingModule =  moduleManager.getEnvironment().getModuleProviding(implementingClass);
-         */
     }
 
     protected void setProvidingModule(Class implementingClass) {
         ModuleManager moduleManager = context.get(ModuleManager.class);
         this.providingModule = moduleManager.getEnvironment().getModuleProviding(implementingClass);
+    }
+
+    protected BufferPair createBufferPair(String primaryBufferName, String secondaryBufferName,
+                                          ScalingFactors primaryBufferScale, ScalingFactors secondaryBufferScale,
+                                          FBO.Type primaryBufferType, FBO.Type secondaryBufferType, FBO.Dimensions scale) {
+
+        FBO buffer1 = generateWithDimensions(new FboConfig(new SimpleUri(providingModule + ":fbo." + primaryBufferName), primaryBufferScale, primaryBufferType)
+                .useDepthBuffer().useNormalBuffer().useLightBuffer().useStencilBuffer(), scale);
+        FBO buffer2 = generateWithDimensions(new FboConfig(new SimpleUri(providingModule + ":fbo." + secondaryBufferName), secondaryBufferScale, secondaryBufferType)
+                .useDepthBuffer().useNormalBuffer().useLightBuffer().useStencilBuffer(), scale);
+        return new BufferPair(buffer1, buffer2);
+    }
+
+    /**
+     * Generates and returns an FBO as characterized by the FboConfig and the dimensions arguments.
+     *
+     * Notice that if the name of the FBO being generated matches the name of an FBO already stored
+     * by the manager, the latter will be overwritten. However, the GPU-side Frame Buffer associated
+     * with the overwritten FBO is not disposed by this method.
+     *
+     * As such, this method should be used only after the relevant checks are made and any
+     * pre-existing FBO with the same name as the new one is appropriately disposed.
+     *
+     * This method produces errors in the log in case the FBO generation process results in
+     * FBO.Status.INCOMPLETE or FBO.Status.UNEXPECTED.
+     *
+     * @param fboConfig an FboConfig object providing FBO configuration details.
+     * @param dimensions an FBO.Dimensions instance providing the dimensions of the FBO.
+     * @return an FBO instance
+     */
+    protected FBO generateWithDimensions(FboConfig fboConfig, FBO.Dimensions dimensions) {
+        fboConfig.setDimensions(dimensions);
+        FBO fbo = FBO.create(fboConfig);
+
+        // At this stage it's unclear what should be done in this circumstances as I (manu3d) do not know what
+        // the effects of using an incomplete FrameBuffer are. Throw an exception? Live with visual artifacts?
+        if (fbo.getStatus() == FBO.Status.INCOMPLETE) {
+            logger.error("FBO " + fboConfig.getName() + " is incomplete. Look earlier in the log for details.");
+        } else if (fbo.getStatus() == FBO.Status.UNEXPECTED) {
+            logger.error("FBO " + fboConfig.getName() + " has generated an unexpected status code. Look earlier in the log for details.");
+        }
+        return fbo;
     }
 
 }
