abeb22e72ecbfe8a4ee10ea770f17373420b3885
Dave2S <davidskacel@gmail.com>
2019-07-22 21:13:11 +0200

    DummyNode draft

---
+++ engine/src/main/java/org/terasology/rendering/dag/gsoc/DummyNode.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering.dag.gsoc;
+
+import org.terasology.context.Context;
+
+public class DummyNode extends NewAbstractNode {
+
+
+    public DummyNode(String nodeId, String nodeAka, Context context) {
+        super(nodeId, nodeAka, context);
+    }
+
+    public DummyNode(String nodeId, Context context) {
+        super(nodeId, nodeId, context);
+    }
+
+    @Override
+    public void setDependencies(Context context) {
+
+    }
+
+    @Override
+    public void process() {
+
+    }
+}
