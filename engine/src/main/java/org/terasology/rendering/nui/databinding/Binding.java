27c37305ef6758098192c6254eb18f837af48312
Arav Singhal <singhal.arav@gmail.com>
2019-07-27 10:09:53 +0530

    Add Binding.makeChildBinding and InteriorImmutableBinding

--- engine/src/main/java/org/terasology/rendering/nui/databinding/Binding.java
+++ engine/src/main/java/org/terasology/rendering/nui/databinding/Binding.java
@@ -22,4 +22,17 @@ public interface Binding<T> {
     T get();
 
     void set(T value);
+
+    /**
+     * Makes {@code binding} a child of this {@link Binding}. Child bindings are bound
+     * to the same value as the original binding ({@code binding}), but also handle
+     * propagation of changes in their value to parent bindings if needed.
+     *
+     * @param binding The {@link Binding} that must be made a child.
+     * @param <C> The type of value bound by {@code binding}.
+     * @return The child {@link Binding} that is bound to the same value as {@code binding}.
+     */
+    default <C> Binding<C> makeChildBinding(Binding<C> binding) {
+        return binding;
+    }
 }
