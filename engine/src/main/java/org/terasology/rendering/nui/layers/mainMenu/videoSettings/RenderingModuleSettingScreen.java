91a364261c4a79c02da421da6292793c5fe3dd21
Dave2S <davidskacel@gmail.com>
2019-08-06 20:56:17 +0200

    introduce renderingModuleScreen.ui and .java for configuration of rendering modules' (for now) ordering

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/RenderingModuleSettingScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/RenderingModuleSettingScreen.java
@@ -19,28 +19,34 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.context.Context;
-import org.terasology.engine.module.ModuleManager;
-import org.terasology.naming.Name;
+import org.terasology.engine.module.RenderingModuleManager;
+import org.terasology.module.ModuleEnvironment;
 import org.terasology.registry.In;
+import org.terasology.rendering.dag.RenderingModuleRegistry;
 import org.terasology.rendering.dag.gsoc.ModuleRendering;
 import org.terasology.rendering.nui.CoreScreenLayer;
+import org.terasology.rendering.nui.WidgetUtil;
+import org.terasology.rendering.nui.widgets.UIButton;
+import org.terasology.rendering.nui.widgets.UIText;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.*;
 
 public class RenderingModuleSettingScreen extends CoreScreenLayer {
     public static final ResourceUrn ASSET_URI = new ResourceUrn("engine:renderingModuleSettingScreen");
 
     private static final Logger logger = LoggerFactory.getLogger(RenderingModuleSettingScreen.class);
 
-    private List<ModuleRendering> renderingModules = new ArrayList<>();
-    private List<ModuleRendering> orderedRenderingModules = new ArrayList<>();
+    private List<ModuleRendering> orderedModuleRenderingInstances = new ArrayList<>();
 
     @In
     private Context context;
-    private ModuleManager moduleManager;
+    private Context subContext;
+    private ModuleEnvironment moduleEnvironment;
+
+    private RenderingModuleRegistry renderingModuleRegistry;
+
+    private UIText renderingModuleInfo;
+    private UIButton recalculateOrder;
 
     public RenderingModuleSettingScreen() {
 
@@ -48,50 +54,49 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer {
 
     @Override
     public void onOpened() {
-        renderingModules = fetchRenderingModules();
-        orderedRenderingModules = calculateModuleOrder(renderingModules);
+        //This must be here not in constructor. subContext is set post-creation
+        moduleEnvironment = subContext.get(ModuleEnvironment.class);
+
+        renderingModuleRegistry = context.get(RenderingModuleManager.class).getRegistry();
+
+        renderingModuleRegistry.updateRenderingModulesOrder(moduleEnvironment, subContext);
+        orderedModuleRenderingInstances = renderingModuleRegistry.getOrderedRenderingModules();
+
+        renderingModuleInfo = find("modulesInfo", UIText.class);
+        recalculateOrder = find("update", UIButton.class);
+
+        if (recalculateOrder != null) {
+            StringBuilder infoText = new StringBuilder("");
+            int[] idx = {1};
+            orderedModuleRenderingInstances.forEach(
+                    (module)-> infoText.append(String.format("%d. %s (Priority: %d)\n",
+                                idx[0]++, module.getProvidingModule(), module.getInitPriority()))
+            );
+            renderingModuleInfo.setText(infoText.toString());
+
+            recalculateOrder.subscribe(button -> {
+                renderingModuleRegistry.updateRenderingModulesOrder(moduleEnvironment, subContext);
+                orderedModuleRenderingInstances = renderingModuleRegistry.getOrderedRenderingModules();
+                StringBuilder infoTextLambda = new StringBuilder("");
+                idx[0] = 1;
+                orderedModuleRenderingInstances.forEach(
+                        (module)-> infoTextLambda.append(String.format("%d. %s (Priority: %d)\n",
+                                    idx[0]++, module.getProvidingModule(), module.getInitPriority()))
+                );
+                renderingModuleInfo.setText(infoTextLambda.toString());
+            });
+        }
+
+        WidgetUtil.trySubscribe(this, "return", button ->
+                triggerBackAnimation());
     }
 
     @Override
     public void initialise() {
-        moduleManager = context.get(ModuleManager.class);
+
     }
 
-    public List<ModuleRendering> getRenderingModules() {
-        return renderingModules;
-    }
-
-    public List<ModuleRendering> getOrderedRenderingModules() {
-        return orderedRenderingModules;
-    }
-
-    public void setRenderingModulePriority(ModuleRendering module, int initializationPriority) {
-        module.setInitPriority(initializationPriority);
-    }
-
-    public int getRenderingModulePriority(ModuleRendering module) {
-        return module.getInitPriority();
-    }
-
-    public Name getRenderingModuleId(ModuleRendering module) {
-        return module.getProvidingModule();
-    }
-
-    private List<ModuleRendering> calculateModuleOrder(List<ModuleRendering> activeRenderingModules) {
-        // Sorted copy of activeRenderingModules
-        orderedRenderingModules = activeRenderingModules.stream()
-                .sorted(Comparator.comparing(ModuleRendering::getInitPriority)) // sort ascending by initializationPriority attribute
-                .collect(Collectors.toList()); //convert stream to List again
-        return orderedRenderingModules;
-    }
-
-    // TODO the getSubtypesOf approach might in the future be changed for Annotations @RenderingModule when the ModuleRendering.java
-    // TODO gets deprecated by being merged into module management itself. Another option is for renderingModules to register themselves in the
-    // TODO module management (at this stage equivalently register themselves in the ModuleRendering upon instantiaion (pre ECS system init) )
-    private List<ModuleRendering> fetchRenderingModules() {
-            List<ModuleRendering> renderingModuleBaseClasses = new ArrayList<>();
-        moduleManager.getEnvironment().getSubtypesOf(ModuleRendering.class)
-                .forEach((renderingClass)->renderingModuleBaseClasses.add(context.get(renderingClass)));
-        return renderingModuleBaseClasses;
+    public void setSubContext(Context subContext) {
+        this.subContext = subContext;
     }
 }
