2acbecdc1aff3cd6749254b5a22fe5cf51dc9b93
Dave2S <davidskacel@gmail.com>
2019-08-09 02:11:05 +0200

    RenderingModuleSettingScreen.java + renderingModuleSettingScreen.ui, update functionality. Patchup Return button problem, add Slider and dropdown for module selection and initPriority edit. Setting slider updates the info text field, hitting Update recalculates order of modules based on shown priority. Switching modules in dropdown sets slider to actual priority

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/RenderingModuleSettingScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/RenderingModuleSettingScreen.java
@@ -20,18 +20,24 @@ import org.slf4j.LoggerFactory;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.context.Context;
 import org.terasology.engine.module.rendering.RenderingModuleManager;
+import org.terasology.math.geom.Vector2i;
 import org.terasology.module.ModuleEnvironment;
+import org.terasology.naming.Name;
 import org.terasology.registry.In;
 import org.terasology.engine.module.rendering.RenderingModuleRegistry;
 import org.terasology.rendering.dag.gsoc.ModuleRendering;
+import org.terasology.rendering.nui.Canvas;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.WidgetUtil;
-import org.terasology.rendering.nui.widgets.UIButton;
-import org.terasology.rendering.nui.widgets.UIText;
+import org.terasology.rendering.nui.databinding.Binding;
+import org.terasology.rendering.nui.itemRendering.AbstractItemRenderer;
+import org.terasology.rendering.nui.itemRendering.StringTextRenderer;
+import org.terasology.rendering.nui.layers.mainMenu.StartPlayingScreen;
+import org.terasology.rendering.nui.widgets.*;
 
 import java.util.*;
 
-public class RenderingModuleSettingScreen extends CoreScreenLayer {
+public class RenderingModuleSettingScreen extends CoreScreenLayer implements UISliderOnChangeTriggeredListener {
     public static final ResourceUrn ASSET_URI = new ResourceUrn("engine:renderingModuleSettingScreen");
 
     private static final Logger logger = LoggerFactory.getLogger(RenderingModuleSettingScreen.class);
@@ -47,14 +53,15 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer {
 
     private UIText renderingModuleInfo;
     private UIButton recalculateOrder;
+    private UISlider initPrioritySlider;
+    private UIDropdownScrollable<ModuleRendering> moduleList;
 
     public RenderingModuleSettingScreen() {
+//This must be here not in constructor. subContext is set post-creation
 
     }
 
-    @Override
-    public void onOpened() {
-        //This must be here not in constructor. subContext is set post-creation
+    public void postInit() {
         moduleEnvironment = subContext.get(ModuleEnvironment.class);
 
         renderingModuleRegistry = context.get(RenderingModuleManager.class).getRegistry();
@@ -65,30 +72,97 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer {
         renderingModuleInfo = find("modulesInfo", UIText.class);
         recalculateOrder = find("update", UIButton.class);
 
-        if (recalculateOrder != null) {
-            StringBuilder infoText = new StringBuilder("");
-            int[] idx = {1};
-            orderedModuleRenderingInstances.forEach(
-                    (module)-> infoText.append(String.format("%d. %s (Priority: %d)\n",
-                                idx[0]++, module.getProvidingModule(), module.getInitPriority()))
-            );
-            renderingModuleInfo.setText(infoText.toString());
+        // List<Name> orderedModuleNames = new ArrayList<>();
+        // orderedModuleRenderingInstances.forEach(module->orderedModuleNames.add(module.getProvidingModule()));
 
+        initPrioritySlider = find("moduleInitPrioritySlider", UISlider.class);
+        if (initPrioritySlider != null) {
+            initPrioritySlider.setValue(2f);
+            initPrioritySlider.setUiSliderOnChangeTriggeredListener(this);
+        }
+
+        moduleList = find("moduleNameList", UIDropdownScrollable.class);
+        if (moduleList != null) {
+            moduleList.bindSelection(new Binding<ModuleRendering>() {
+                ModuleRendering selected;
+                @Override
+                public ModuleRendering get() {
+                    return selected;
+                }
+
+                @Override
+                public void set(ModuleRendering value) {
+                    if (initPrioritySlider != null) {
+                        initPrioritySlider.setValue(value.getInitPriority());
+                    }
+                    selected = value;
+                }
+            });
+
+            moduleList.setOptions(orderedModuleRenderingInstances);
+            moduleList.setVisibleOptions(5);
+            moduleList.setSelection(orderedModuleRenderingInstances.get(0));
+            initPrioritySlider.setValue(moduleList.getSelection().getInitPriority());
+            moduleList.setOptionRenderer(new StringTextRenderer<ModuleRendering>() {
+                @Override
+                public String getString(ModuleRendering value) {
+                    if (value != null) {
+                        return value.getProvidingModule().toString();
+                    }
+                    return "";
+                }
+                @Override
+                public void draw(ModuleRendering value, Canvas canvas) {
+                    canvas.drawText(getString(value), canvas.getRegion());
+                }
+            });
+        }
+
+        if (recalculateOrder != null) {
+            updateRenderingModuleInfo();
             recalculateOrder.subscribe(button -> {
                 renderingModuleRegistry.updateRenderingModulesOrder(moduleEnvironment, subContext);
                 orderedModuleRenderingInstances = renderingModuleRegistry.getOrderedRenderingModules();
                 StringBuilder infoTextLambda = new StringBuilder("");
-                idx[0] = 1;
+                int[] idx = {1};
                 orderedModuleRenderingInstances.forEach(
                         (module)-> infoTextLambda.append(String.format("%d. %s (Priority: %d)\n",
-                                    idx[0]++, module.getProvidingModule(), module.getInitPriority()))
+                                idx[0]++, module.getProvidingModule(), module.getInitPriority()))
                 );
                 renderingModuleInfo.setText(infoTextLambda.toString());
             });
         }
 
-        WidgetUtil.trySubscribe(this, "return", button ->
-                triggerBackAnimation());
+        // TODO returns one more screen every time...gradually
+        WidgetUtil.trySubscribe(this, "return", widget ->
+                getManager().pushScreen(StartPlayingScreen.ASSET_URI));
+
+        // Update slider if module selection changes
+//        if (initPrioritySlider != null && moduleList != null) {
+//            moduleList. (this, "moduleNameList", widget->initPrioritySlider.setValue(moduleList.getSelection().getInitPriority()));
+//        }
+    }
+
+    @Override
+    public void onOpened() {
+        super.onOpened();
+
+    }
+
+    private void updateRenderingModuleInfo() {
+        StringBuilder infoText = new StringBuilder("");
+        int[] idx = {1};
+        orderedModuleRenderingInstances.forEach(
+                (module)-> infoText.append(String.format("%d. %s (Priority: %d)\n",
+                        idx[0]++, module.getProvidingModule(), module.getInitPriority()))
+        );
+        renderingModuleInfo.setText(infoText.toString());
+    }
+
+    @Override
+    public void onSliderValueChanged(float val) {
+        moduleList.getSelection().setInitPriority((int) val);
+        updateRenderingModuleInfo();
     }
 
     @Override
