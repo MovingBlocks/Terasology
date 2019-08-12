2b3ad0a30dcac1bfba8937388b1cf2576d1eb836
Dave2S <davidskacel@gmail.com>
2019-08-12 19:50:37 +0200

    update renderingModuleSettingScreen ui and java with Enable/Disable button for rendering classes, change layout for settings on the right to be in rows

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/RenderingModuleSettingScreen.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/videoSettings/RenderingModuleSettingScreen.java
@@ -20,6 +20,7 @@ import org.slf4j.LoggerFactory;
 import org.terasology.assets.ResourceUrn;
 import org.terasology.context.Context;
 import org.terasology.engine.module.rendering.RenderingModuleManager;
+import org.terasology.i18n.TranslationSystem;
 import org.terasology.math.geom.Vector2i;
 import org.terasology.module.ModuleEnvironment;
 import org.terasology.naming.Name;
@@ -30,6 +31,7 @@ import org.terasology.rendering.nui.Canvas;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.WidgetUtil;
 import org.terasology.rendering.nui.databinding.Binding;
+import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.itemRendering.AbstractItemRenderer;
 import org.terasology.rendering.nui.itemRendering.StringTextRenderer;
 import org.terasology.rendering.nui.layers.mainMenu.StartPlayingScreen;
@@ -49,12 +51,16 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer implements UIS
     private Context subContext;
     private ModuleEnvironment moduleEnvironment;
 
+    @In
+    private TranslationSystem translationSystem;
+
     private RenderingModuleRegistry renderingModuleRegistry;
 
     private UIText renderingModuleInfo;
     private UIButton recalculateOrder;
     private UISlider initPrioritySlider;
     private UIDropdownScrollable<ModuleRendering> moduleList;
+    private UIButton setEnabledRenderingClassButton;
 
     public RenderingModuleSettingScreen() {
 //This must be here not in constructor. subContext is set post-creation
@@ -71,6 +77,7 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer implements UIS
 
         renderingModuleInfo = find("modulesInfo", UIText.class);
         recalculateOrder = find("update", UIButton.class);
+        setEnabledRenderingClassButton = find ("setEnabledRenderingClassButton", UIButton.class);
 
         // List<Name> orderedModuleNames = new ArrayList<>();
         // orderedModuleRenderingInstances.forEach(module->orderedModuleNames.add(module.getProvidingModule()));
@@ -126,15 +133,26 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer implements UIS
             recalculateOrder.subscribe(button -> {
                 renderingModuleRegistry.updateRenderingModulesOrder(moduleEnvironment, subContext);
                 orderedModuleRenderingInstances = renderingModuleRegistry.getOrderedRenderingModules();
-                StringBuilder infoTextLambda = new StringBuilder("");
-                int[] idx = {1};
-                orderedModuleRenderingInstances.forEach(
-                        (module)-> infoTextLambda.append(String.format("%d. %s (Priority: %d)\n",
-                                idx[0]++, module.getProvidingModule(), module.getInitPriority()))
-                );
-                renderingModuleInfo.setText(infoTextLambda.toString());
+                updateRenderingModuleInfo();
             });
         }
+
+        if (setEnabledRenderingClassButton != null && moduleList != null) {
+            setEnabledRenderingClassButton.bindText(new ReadOnlyBinding<String>() {
+                @Override
+                public String get() {
+                    return (moduleList.getSelection().isEnabled())
+                            ? translationSystem.translate("${engine:menu#disable-rendering-class}")
+                            : translationSystem.translate("${engine:menu#enable-rendering-class}");
+                }
+            });
+
+            setEnabledRenderingClassButton.subscribe(button -> {
+                moduleList.getSelection().toggleEnabled();
+                updateRenderingModuleInfo();
+            });
+        }
+
 
         // TODO returns one more screen every time...gradually
         WidgetUtil.trySubscribe(this, "return", widget ->
@@ -153,12 +171,21 @@ public class RenderingModuleSettingScreen extends CoreScreenLayer implements UIS
     }
 
     private void updateRenderingModuleInfo() {
-        StringBuilder infoText = new StringBuilder("");
+        StringBuilder infoText = new StringBuilder("Rendering Modules\n").append("=================\n");
         int[] idx = {1};
-        orderedModuleRenderingInstances.forEach(
-                (module)-> infoText.append(String.format("%d. %s - in %s module (Priority: %d)\n",
-                        idx[0]++, module.getClass().getSimpleName(), module.getProvidingModule(), module.getInitPriority()))
-        );
+        if (!orderedModuleRenderingInstances.isEmpty()) {
+            orderedModuleRenderingInstances.forEach(
+                    (module) -> infoText.append(
+                            String.format("%d. %s - in %s module (Priority: %d, Enabled: %s)\n",
+                                    idx[0]++,
+                                    module.getClass().getSimpleName(),
+                                    module.getProvidingModule(),
+                                    module.getInitPriority(),
+                                    String.valueOf(module.isEnabled())
+                            )
+                    )
+            );
+        }
         renderingModuleInfo.setText(infoText.toString());
     }
 
