46ea3fa955645edc4ccda420ea0d1a2750d65f7e
Arthur Casals <arthur.casals@gmail.com>
2019-08-17 09:08:26 -0300

    Added javadoc for GroupMindComponent

--- engine/src/main/java/org/terasology/logic/behavior/GroupMindComponent.java
+++ engine/src/main/java/org/terasology/logic/behavior/GroupMindComponent.java
@@ -21,9 +21,24 @@ import org.terasology.entitySystem.entity.EntityRef;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * This component  complements GroupTagComponent
+ * in cases where there's the need of an unison
+ * behavior by all entities belonging to a group.
+ * @see GroupTagComponent
+ */
 public class GroupMindComponent implements Component {
-
+    /**
+     * The unique group identifier with which the component will be associated
+     */
     public String groupLabel;
+    /**
+     * The name of the behavior tree used by the group (trees from other modules
+     * can be used as long as they are listed as dependencies).
+     */
     public String behavior;
+    /**
+     * The identifiers for each of the entities belonging to the group.
+     */
     public Set<EntityRef> groupMembers = new HashSet<>();
 }
