234c59cf42272ec42b8eb264410aa4e7f42659e2
Arav Singhal <singhal.arav@gmail.com>
2019-08-20 10:16:32 +0530

    Add LabeledNumberRowLayoutBuilder, Vector2fWidgetFactory

---
+++ engine/src/main/java/org/terasology/rendering/nui/widgets/types/math/Vector2fWidgetFactory.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering.nui.widgets.types.math;
+
+import org.terasology.math.geom.Vector2f;
+import org.terasology.reflection.TypeInfo;
+import org.terasology.rendering.nui.UIWidget;
+import org.terasology.rendering.nui.databinding.Binding;
+import org.terasology.rendering.nui.widgets.types.RegisterTypeWidgetFactory;
+import org.terasology.rendering.nui.widgets.types.TypeWidgetFactory;
+import org.terasology.rendering.nui.widgets.types.TypeWidgetLibrary;
+
+import java.util.Optional;
+
+@RegisterTypeWidgetFactory
+public class Vector2fWidgetFactory implements TypeWidgetFactory {
+    @Override
+    public <T> Optional<UIWidget> create(Binding<T> binding, TypeInfo<T> type, TypeWidgetLibrary library) {
+        if (!Vector2f.class.equals(type.getRawType())) {
+            return Optional.empty();
+        }
+
+        if (binding.get() == null) {
+            binding.set((T) Vector2f.zero());
+        }
+
+        LabeledNumberRowLayoutBuilder<Float> builder = new LabeledNumberRowLayoutBuilder<>(float.class, library)
+            .addField("x", binding)
+            .addField("y", binding);
+
+        return Optional.of(builder.build());
+    }
+}
---
+++ engine/src/main/java/org/terasology/rendering/nui/widgets/types/testScreens/MathTypeWidgetTestScreen.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.rendering.nui.widgets.types.testScreens;
+
+import org.terasology.math.geom.Vector2f;
+
+public class MathTypeWidgetTestScreen extends TypeWidgetTestScreen {
+    @Override
+    protected void addWidgets() {
+        newBinding(Vector2f.class);
+    }
+}
