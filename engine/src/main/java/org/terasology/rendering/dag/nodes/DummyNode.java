d5ac761cd1c5e07619b2999347b3d0c491abee98
Dave2S <davidskacel@gmail.com>
2019-08-24 05:33:11 +0200

    move dummy node concept class to nodes package

--- engine/src/main/java/org/terasology/rendering/dag/nodes/DummyNode.java
+++ engine/src/main/java/org/terasology/rendering/dag/nodes/DummyNode.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.rendering.dag.gsoc;
+package org.terasology.rendering.dag.nodes;
 
 import org.terasology.context.Context;
 import org.terasology.naming.Name;
