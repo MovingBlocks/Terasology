93f874a1429ba1a5b470283d585fdffbf6dff267
Niruandaleth <jd.rueckert@googlemail.com>
2020-04-26 20:34:49 +0200

    chore: add licensing header and java doc

--- engine/src/main/java/org/terasology/logic/common/RetainComponentsComponent.java
+++ engine/src/main/java/org/terasology/logic/common/RetainComponentsComponent.java
@@ -1,11 +1,31 @@
+/*
+ * Copyright 2020 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.terasology.logic.common;
 
 import com.google.api.client.util.Sets;
 import org.terasology.entitySystem.Component;
 
-import java.util.Collections;
 import java.util.Set;
 
+/**
+ * This component is intended to list component classes that are supposed to be retained when placing a block.
+ *
+ * If a block entity has a component that is not part of its prefab, retaining this component results in
+ * the block entity still having this component afterwards. If not retained, it is likely to be removed instead.
+ */
 public class RetainComponentsComponent implements Component {
     public Set<Class<? extends Component>> components = Sets.newHashSet();
 }
