42251ac000ef8a22c09d5f25808c393a5543e7a1
Michael Pollind <mpollind@gmail.com>
2020-05-04 18:43:41 -0700

    moved back into math package

--- engine/src/main/java/org/terasology/math/SpiralIterable.java
+++ engine/src/main/java/org/terasology/math/SpiralIterable.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.terasology.math.iterator;
+package org.terasology.math;
 
 import com.google.common.base.Preconditions;
 import org.joml.Vector2i;
