dd36fee7fd33e075074af993cdc791ea2fe59933
Michael <polli104@mail.chapman.edu>
2020-05-10 16:33:59 -0700

    added test cases for PerspectiveCameraTest and OrthogonalCameraTest

--- engine-tests/src/test/java/org/terasology/rendering/cameras/OrthogonalCameraTest.java
+++ engine-tests/src/test/java/org/terasology/rendering/cameras/OrthogonalCameraTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 MovingBlocks
+ * Copyright 2020 MovingBlocks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,7 +16,6 @@
 package org.terasology.rendering.cameras;
 
 import org.joml.Matrix4f;
-import org.joml.Quaternionf;
 import org.junit.Before;
 import org.junit.Test;
 import org.terasology.config.Config;
@@ -32,8 +31,7 @@ import org.terasology.world.WorldProvider;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class PerspectiveCameraTest {
-
+public class OrthogonalCameraTest {
 
     @Before
     public void setUp() {
@@ -44,40 +42,36 @@ public class PerspectiveCameraTest {
     }
 
     @Test
-    public void testPerspectiveCamera() {
+    public void testOrthogonalCameraTest() {
         WorldProvider provider = mock(WorldProvider.class);
         DisplayDevice device = mock(DisplayDevice.class);
         when(device.getDisplayWidth()).thenReturn(1280);
         when(device.getDisplayHeight()).thenReturn(720);
         RenderingConfig config = new RenderingConfig();
 
-
-        PerspectiveCamera camera = new PerspectiveCamera(provider, config, device);
+        OrthographicCamera camera = new OrthographicCamera(10,3,2,8);
         camera.setOrientation(new Quat4f(45, .001f, .001f));
         camera.updateMatrices();
 
-        Matrix4f viewMatrix = new Matrix4f(-5.253E-1f, 0.000E+0f, 8.509E-1f, 0.000E+0f,
-            8.509E-4f, 1.000E+0f, 5.253E-4f, 0.000E+0f,
-            -8.509E-1f, 1.000E-3f, -5.253E-1f, 0.000E+0f,
-            -0.000E+0f, -0.000E+0f, -0.000E+0f, 1.000E+0f);
-        Matrix4f projectionMatrix = new Matrix4f(5.0273395f, 0.0f, 0.0f, 0.0f
-            , 0.0f, 8.937492f, 0.0f, 0.0f
-            , 0.0f, 0.0f, -1.00004f, -0.20000401f
-            , 0.0f, 0.0f, -1.0f, 0.0f);
-        Matrix4f normViewMatrix = new Matrix4f(-.525322f, 0.0f, 0.8509036f, -0.0f
-            , 8.509035E-4f, 0.9999995f, 5.25322E-4f, -0.0f,
-            -0.85090315f, 0.001f, -0.5253218f, -0.0f,
-            0.0f, 0.0f, 0.0f, 1.0f);
+        Matrix4f viewMatrix = new Matrix4f(-5.253E-1f,  8.509E-4f, 8.509E-1f,  0.000E+0f,
+            0.000E+0f,  1.000E+0f,  1.000E-3f,  0.000E+0f,
+            -8.509E-1f,  5.253E-4f, -5.253E-1f,  0.000E+0f,
+            -0.000E+0f, -0.000E+0f, -0.000E+0f,  1.000E+0f);
+        Matrix4f projectionMatrix = new Matrix4f(-0.2857143f,0.0f,0.0f,1.8571428f,
+            0.0f,-0.33333334f,0.0f,1.6666666f,
+            0.0f,0.0f,-0.001f,-0.0f,
+            0.0f,0.0f,0.0f,1.0f);
+        Matrix4f normViewMatrix = new Matrix4f(-5.253E-1f, 8.509E-4f, 8.509E-1f,  0.000E+0f,
+            0.000E+0f, 1.000E+0f,  1.000E-3f,  0.000E+0f,
+            -8.509E-1f, 5.253E-4f, -5.253E-1f,  0.000E+0f,
+            -0.000E+0f,-0.000E+0f, -0.000E+0f,  1.000E+0f);
 
-        Matrix4f reflectedNormViewMatrix =  new Matrix4f( -5.253E-1f, 0.000E+0f, 8.509E-1f,   0.000E+0f,
-            8.509E-4f, -1.000E+0f, 5.253E-4f,  0.000E+0f,
-            -8.509E-1f,  -1.000E-3f, -5.253E-1f,  0.000E+0f,
-            0.000E+0f,  0.000E+0f,  0.000E+0f,  1.000E+0f);
-
-        TeraAssert.assertEquals(viewMatrix, camera.getViewMatrix(), 0.001f);
-        TeraAssert.assertEquals(projectionMatrix, camera.getProjectionMatrix(), 0.001f);
-        TeraAssert.assertEquals(normViewMatrix, camera.getNormViewMatrix(), 0.001f);
+        TeraAssert.assertEquals(viewMatrix,camera.getViewMatrix(), 0.001f);
+        TeraAssert.assertEquals(projectionMatrix,camera.getProjectionMatrix(), 0.001f);
+        TeraAssert.assertEquals(normViewMatrix,camera.getNormViewMatrix(),0.001f);
         camera.setReflected(true);
-        TeraAssert.assertEquals(reflectedNormViewMatrix, camera.getNormViewMatrix(), 0.001f);
+        TeraAssert.assertEquals(new Matrix4f(),camera.getNormViewMatrix(),0.001f);
+
     }
+
 }
