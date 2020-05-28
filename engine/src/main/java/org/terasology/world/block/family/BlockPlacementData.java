b398b66c7fbe0d99dc04cd16b6e2d46c27c8650d
Kevin Blaszczyk <OrcHound@gmail.com>
2020-05-28 19:26:26 +0200

    Improve Javadoc in BlockPlacementData

--- engine/src/main/java/org/terasology/world/block/family/BlockPlacementData.java
+++ engine/src/main/java/org/terasology/world/block/family/BlockPlacementData.java
@@ -30,23 +30,40 @@ import org.terasology.math.Side;
  */
 public class BlockPlacementData {
 
+    /**
+     * The block position, at which the block is supposed to be placed at
+     */
     public final Vector3ic blockPosition;
+
+    /**
+     * The block side, this block is being attached to, e.g. Top if the block is being placed on the ground
+     */
     public final Side attachmentSide;
+
+    /**
+     * The player's viewing direction
+     */
     public final Vector3fc viewingDirection;
+
+    /**
+     * The position on the block surface that the user aimed at when placing the block. A vector in the range (0..1, 0..1)
+     */
     public final Vector2fc relativeAttachmentPosition;
 
     /**
-     * @param blockPosition     The block position, at which the block is going to be placed at.
+     * @param blockPosition     The block position, at which the block is supposed to be placed at
      * @param attachmentSide    The side of the block which this block is being attached to, e.g. Top if the block is being placed on the ground
+     * @param viewingDirection  The players viewing direction
      */
     public BlockPlacementData(Vector3ic blockPosition, Side attachmentSide, Vector3fc viewingDirection) {
         this(blockPosition, attachmentSide, viewingDirection, new Vector2f());
     }
 
     /**
-     * @param blockPosition     The block position, at which the block is going to be placed at.
+     * @param blockPosition     The block position, at which the block is supposed to be placed at
      * @param attachmentSide    The side of the block which this block is being attached to, e.g. Top if the block is being placed on the ground
-     * @param relativeAttachmentPosition The position on the block surface that the user aimed at when placing the block. A value between (0, 0) and (1, 1).
+     * @param viewingDirection  The players viewing direction
+     * @param relativeAttachmentPosition The position on the block surface that the user aimed at when placing the block. A vector in the range (0..1, 0..1)
      */
     public BlockPlacementData(Vector3ic blockPosition, Side attachmentSide, Vector3fc viewingDirection, Vector2fc relativeAttachmentPosition) {
         this.blockPosition = new Vector3i(blockPosition);
