0f17d2e0283c9df59742e4c5f968ceabc068e2c4
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-05-28 16:41:30 -0700

    fix(concurrency): make TaskProcessor threads daemon-threads by default
    so they do not prevent the application from closing

--- engine/src/main/java/org/terasology/utilities/concurrency/TaskProcessor.java
+++ engine/src/main/java/org/terasology/utilities/concurrency/TaskProcessor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2013 MovingBlocks
+ * Copyright 2020 MovingBlocks
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *     https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -43,6 +43,7 @@ final class TaskProcessor<T extends Task> implements Runnable {
         boolean running = true;
         Thread.currentThread().setPriority(Thread.MIN_PRIORITY);
         Thread.currentThread().setName(name);
+        Thread.currentThread().setDaemon(true);
         while (running) {
             try {
                 T task = queue.take();
