a813fc29e82adb7cbfe1445ed244ffd244310e85
DarkWeird <foxflameless@gmail.com>
2020-05-31 20:18:41 +0300

    feat(DAG_overlay): implements basic dag graph visualization

--- engine/src/main/java/org/terasology/rendering/nui/layers/ingame/RenderGraphOverlay.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/ingame/RenderGraphOverlay.java
@@ -24,13 +24,8 @@ import org.terasology.assets.ResourceUrn;
 import org.terasology.input.MouseInput;
 import org.terasology.registry.In;
 import org.terasology.rendering.dag.Node;
-import org.terasology.rendering.dag.RenderGraph;
-import org.terasology.rendering.dag.StateChange;
 import org.terasology.rendering.nui.CoreScreenLayer;
 import org.terasology.rendering.nui.WidgetUtil;
-import org.terasology.rendering.nui.itemRendering.StringTextRenderer;
-import org.terasology.rendering.nui.widgets.UITreeView;
-import org.terasology.rendering.nui.widgets.treeView.GenericTree;
 import org.terasology.rendering.world.WorldRenderer;
 
 /**
@@ -45,57 +40,57 @@ public class RenderGraphOverlay extends CoreScreenLayer {
     @In
     private WorldRenderer worldRenderer;
 
-    private UITreeView<RenderNodeInfo> renderGraphTreeView;
-
     @Override
     public void initialise() {
         WidgetUtil.trySubscribe(this, "close", widget -> getManager().closeScreen(RenderGraphOverlay.this));
-        renderGraphTreeView = find("renderGraphTreeView", UITreeView.class);
-        renderGraphTreeView.subscribeNodeClick((event, treeNode) -> {
-            if (event.getMouseButton() == MouseInput.MOUSE_LEFT) {
-                RenderNodeInfo renderNodeInfo = (RenderNodeInfo) treeNode.getValue();
-                Node renderNode = renderNodeInfo.getNode();
-                String uriString = renderNode.getUri().toString();
-                logger.info(uriString);
-            }
-        });
-
-        renderGraphTreeView.setItemRenderer(new StringTextRenderer<RenderNodeInfo>() {
-            @Override
-            public String getString(RenderNodeInfo renderNodeInfo) {
-                Node renderNode = renderNodeInfo.getNode();
-                if (null == renderNode) {
-                    return "ROOT";
-                }
-                String uriString = renderNode.getUri().toString();
-                if (!renderNode.isEnabled()) {
-                    return uriString + " -- DISABLED";
-                }
-
-
-                StringBuilder sb = new StringBuilder();
-                sb.append(uriString);
-                sb.append('\n');
-                sb.append('\n');
-
-                // printing out individual desired state changes
-                for (StateChange desiredStateChange : renderNode.getDesiredStateChanges()) {
-                    sb.append(desiredStateChange.toString());
-                    sb.append('\n');
-                }
-                sb.append('\n');
-
-                sb.append(renderNode);
-                sb.append(": process()");
-                sb.append('\n');
-                return sb.toString();
-            }
-        });
-
-        RenderGraph renderGraph = worldRenderer.getRenderGraph();
-        RenderNodeInfoTree renderGraphTreeModel = new RenderNodeInfoTree(renderGraph);
-        renderGraphTreeView.setModel(renderGraphTreeModel);
+        find("graph", GraphRenderer.class).setRenderGraph(worldRenderer.getRenderGraph());
+//        renderGraphTreeView = find("renderGraphTreeView", UITreeView.class);
+//        renderGraphTreeView.subscribeNodeClick((event, treeNode) -> {
+//            if (event.getMouseButton() == MouseInput.MOUSE_LEFT) {
+//                RenderNodeInfo renderNodeInfo = (RenderNodeInfo) treeNode.getValue();
+//                Node renderNode = renderNodeInfo.getNode();
+//                String uriString = renderNode.getUri().toString();
+//                logger.info(uriString);
+//            }
+//        });
+//
+//        renderGraphTreeView.setItemRenderer(new StringTextRenderer<RenderNodeInfo>() {
+//            @Override
+//            public String getString(RenderNodeInfo renderNodeInfo) {
+//                Node renderNode = renderNodeInfo.getNode();
+//                if (null == renderNode) {
+//                    return "ROOT";
+//                }
+//                String uriString = renderNode.getUri().toString();
+//                if (!renderNode.isEnabled()) {
+//                    return uriString + " -- DISABLED";
+//                }
+//
+//
+//                StringBuilder sb = new StringBuilder();
+//                sb.append(uriString);
+//                sb.append('\n');
+//                sb.append('\n');
+//
+//                // printing out individual desired state changes
+//                for (StateChange desiredStateChange : renderNode.getDesiredStateChanges()) {
+//                    sb.append(desiredStateChange.toString());
+//                    sb.append('\n');
+//                }
+//                sb.append('\n');
+//
+//                sb.append(renderNode);
+//                sb.append(": process()");
+//                sb.append('\n');
+//                return sb.toString();
+//            }
+//        });
+//
+//        RenderGraph renderGraph = worldRenderer.getRenderGraph();
+//        RenderNodeInfoTree renderGraphTreeModel = new RenderNodeInfoTree(renderGraph);
+//        renderGraphTreeView.setModel(renderGraphTreeModel);
     }
+
 
     private static class RenderNodeInfo {
         private Node node;
@@ -120,36 +115,6 @@ public class RenderGraphOverlay extends CoreScreenLayer {
             }
             return node.getUri().toString();
         }
-    }
-
-    private static class RenderNodeInfoTree extends GenericTree<RenderNodeInfo> {
-        public RenderNodeInfoTree(RenderGraph renderGraph) {
-            super(new RenderNodeInfo());
-
-            List<Node> nodes = renderGraph.getStartingNodes();
-            for (Node node : nodes) {
-                RenderNodeInfoTree child = new RenderNodeInfoTree(renderGraph, new RenderNodeInfo(node));
-                addChild(child);
-            }
-        }
-
-        private RenderNodeInfoTree(RenderGraph renderGraph, RenderNodeInfo renderNodeInfo) {
-            super(renderNodeInfo);
-            Node node = renderNodeInfo.getNode();
-            if (null == node) {
-                throw new NullPointerException("RenderNodeInfo.getNode()");
-            }
-            Set<Node> childNodeList = renderGraph.getOutgoingNodesForNode(node);
-            for (Node childNode : childNodeList) {
-                RenderNodeInfoTree child = new RenderNodeInfoTree(renderGraph, new RenderNodeInfo(childNode));
-                addChild(child);
-            }
-        }
-    }
-
-    @Override
-    public void update(float delta) {
-        renderGraphTreeView.update(delta);
     }
 
     @Override
