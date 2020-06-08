08a952baaff93cefa4e7e773b6cdbf0c80173d25
Kevin Turner <83819+keturn@users.noreply.github.com>
2020-06-08 12:56:19 -0700

    fix(StateLoading): show localized messageOnLoad

--- engine/src/main/java/org/terasology/engine/modes/StateLoading.java
+++ engine/src/main/java/org/terasology/engine/modes/StateLoading.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.engine.modes;
 
@@ -251,7 +238,7 @@ public class StateLoading implements GameState {
                 }
             } catch (Exception e) {
                 logger.error("Error while loading {}", current, e);
-                String errorMessage = String.format("Failed to load game. There was an error during %s.",
+                String errorMessage = String.format("Failed to load game. There was an error during \"%s\".",
                         current == null ? "the last part" : current.getMessage());
                 gameEngine.changeState(new StateMainMenu(errorMessage));
                 CrashReporter.report(e, LoggingContext.getLoggingPath());
