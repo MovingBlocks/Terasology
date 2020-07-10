f4e103ec88c4090e5658e5876a853536e9a4f2ba
Tobias Nett <skaldarnar@googlemail.com>
2020-07-11 00:49:51 +0200

    use NumberedGameName to determine next game name

--- engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/savedGames/GameProvider.java
+++ engine/src/main/java/org/terasology/rendering/nui/layers/mainMenu/savedGames/GameProvider.java
@@ -31,6 +31,7 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.SortedMap;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -123,30 +124,56 @@ public final class GameProvider {
     }
 
     /**
-     * Generates the game name based on the game number of the last saved game
+     * Retrieve the next game name based on the game number of the last saved game.
+     * <p>
+     * This will append a game number or increment a given name number if the name prefix is already present as saved
+     * game.
+     *
+     * <pre>
+     *     1. "Game"    -> "Game"
+     *     2. "Game"    -> "Game 1"
+     *     3. "Game 1"  -> "Game 2"
+     * </pre>
+     * <p>
+     * When incrementing the number the currently highest number is incremented by one (i.e., "gaps" are not filled).
+     *
+     * <pre>
+     *     1. "Gooey 3"     -> "Gooey 3"
+     *     2. "Gooey"       -> "Gooey 4"
+     * </pre>
      *
      * @param gameName will to use as game prefix, if saves contains this game name
-     * @return next game name with number. Example: "Game" -> "Game 1", "Game 1 -> Game 2"
+     * @return next game name with number, or the given name if unique
      */
     public static String getNextGameName(String gameName) {
-        int gameNumber = 1;
-        String name = gameName;
-        for (GameInfo info : GameProvider.getSavedGames()) {
-            if (info.getManifest().getTitle().startsWith(name)) {
-                String remainder = info.getManifest().getTitle().substring(name.length());
-                if (remainder.trim().isEmpty()) {
-                    remainder = name.replaceAll(".*?(\\d+)$", "$1");
-                    name = name.replaceAll("(\\d+)$", "");
-                }
-                try {
-                    gameNumber = Math.max(gameNumber, Integer.parseInt(remainder) + 1);
-                } catch (NumberFormatException e) {
-                    logger.trace("Could not parse {} as integer (not an error)", remainder, e);
-                }
-                return name.trim() + " " + gameNumber;
-            }
+        final NumberedGameName requestedName = NumberedGameName.fromString(gameName);
+
+        final Map<String, List<NumberedGameName>> savedGames = GameProvider.getSavedGames().stream()
+                .map(savedGame -> savedGame.getManifest().getTitle())
+                .map(NumberedGameName::fromString)
+                .collect(Collectors.groupingBy(numberedGameName -> numberedGameName.namePrefix));
+
+        if (savedGames.containsKey(requestedName.namePrefix)) {
+            final int nextNumber = highestGameNumber(savedGames.get(requestedName.namePrefix)) + 1;
+            return new NumberedGameName(requestedName.namePrefix, Optional.of(nextNumber)).toString();
+        } else {
+            return requestedName.toString();
         }
-        return name;
+    }
+
+    /**
+     * Find the highest game number in the list of numbered game names.
+     *
+     * @param names a list of numbered game names
+     * @return the highest number associated with the a game name, or 0 if none was found
+     */
+    private static int highestGameNumber(final List<NumberedGameName> names) {
+        return names.stream()
+                .map(n -> n.number)
+                .filter(Optional::isPresent)
+                .mapToInt(Optional::get)
+                .max()
+                .orElse(0);
     }
 
 }
