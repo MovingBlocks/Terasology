dcdaadab3f63d07506bc1cfa225f3a06024a5479
Michael <polli104@mail.chapman.edu>
2020-07-14 12:04:54 -0700

    feat(JOML): Migrate PhysicsEngine#awakenArea

--- engine/src/main/java/org/terasology/math/VecMath.java
+++ engine/src/main/java/org/terasology/math/VecMath.java
@@ -16,6 +16,7 @@
 
 package org.terasology.math;
 
+import org.joml.Vector3fc;
 import org.terasology.math.geom.Quat4f;
 import org.terasology.math.geom.Vector3f;
 
@@ -34,6 +35,10 @@ public final class VecMath {
 
     public static javax.vecmath.Vector3f to(Vector3f v) {
         return new javax.vecmath.Vector3f(v.x, v.y, v.z);
+    }
+
+    public static javax.vecmath.Vector3f to(Vector3fc v) {
+        return new javax.vecmath.Vector3f(v.x(), v.y(), v.z());
     }
 
     public static Quat4f from(javax.vecmath.Quat4f v) {
