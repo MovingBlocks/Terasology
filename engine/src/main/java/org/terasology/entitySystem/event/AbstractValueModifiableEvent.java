3ddfff104e6a858460ba414d417bde9a6c1f5faf
Tobias Nett <skaldarnar@googlemail.com>
2020-07-20 22:16:51 +0200

    fix(AbstractValueModifiableEvent): simplify calculation of result value, document guarantees

--- engine/src/main/java/org/terasology/entitySystem/event/AbstractValueModifiableEvent.java
+++ engine/src/main/java/org/terasology/entitySystem/event/AbstractValueModifiableEvent.java
@@ -21,11 +21,11 @@ import gnu.trove.list.array.TFloatArrayList;
 
 /**
  * A generic event for getting a value for a property.
- *
- * @see AbstractValueModifiableEvent
+ * <p>
+ * The result value is guaranteed to be greater or equal to zero.
  */
 public abstract class AbstractValueModifiableEvent implements Event {
-    private float baseValue;
+    private final float baseValue;
 
     private TFloatList modifiers = new TFloatArrayList();
     private TFloatList multipliers = new TFloatArrayList();
@@ -47,38 +47,33 @@ public abstract class AbstractValueModifiableEvent implements Event {
         modifiers.add(amount);
     }
 
+    /**
+     * @deprecated please use {@link #postAdd(float)} instead.
+     */
+    @Deprecated
     public void addPostMultiply(float amount) {
+        postAdd(amount);
+    }
+
+    public void postAdd(float amount) {
         postModifiers.add(amount);
     }
 
     /**
      * Calculates the result value from the base value and given modifiers and multipliers.
-     * 
-     * The value is calculated base on the following formula:
+     * <p>
+     * The value is calculated based on the following formula:
      * <pre>
-     * result = max(0, <baseValue> + Σ <modifier>) * Π <multiplier> + Σ <postModifier>
+     * result = max(0, <baseValue> + Σ <modifier> * Π <multiplier> + Σ <postModifier>)
      * </pre>
      *
-     * <emph>Note that the value range of result is not restricted!</emph>
+     * <emph>The result value is guaranteed to be non-negative!</emph>
      */
     public float getResultValue() {
-        float result = baseValue;
-        TFloatIterator modifierIter = modifiers.iterator();
-        while (modifierIter.hasNext()) {
-            result += modifierIter.next();
-        }
-        result = Math.max(0, result);
-
-        TFloatIterator multiplierIter = multipliers.iterator();
-        while (multiplierIter.hasNext()) {
-            result *= multiplierIter.next();
-        }
-
-        final TFloatIterator postModifierIter = postModifiers.iterator();
-        while (postModifierIter.hasNext()) {
-            result += postModifierIter.next();
-        }
-        return result;
+        //TODO(skaldarnar): Based on a discussion in https://github.com/MovingBlocks/Terasology/pull/4063 we may want
+        // to lift the guarantee/restriction that the result value needs to be non-negative. Systems are still free to
+        // apply this restriction if needed.
+        return Math.max(0, baseValue + modifiers.sum() * product(multipliers) + postModifiers.sum());
     }
 
     public TFloatList getModifiers() {
@@ -103,5 +98,22 @@ public abstract class AbstractValueModifiableEvent implements Event {
 
     public void setPostModifiers(TFloatList postModifiers) {
         this.postModifiers = postModifiers;
+    }
+
+    /**
+     * Calculate the product of all values in the given list.
+     * <p>
+     * A static helper dual to {@code TFloatList#sum()}.
+     *
+     * @param multipliers the list of multipliers to calculate the product of.
+     * @return the product of all values. 1 if the list is empty.
+     */
+    private static float product(TFloatList multipliers) {
+        float multiplierProduct = 1f;
+        TFloatIterator multiplierIter = multipliers.iterator();
+        while (multiplierIter.hasNext()) {
+            multiplierProduct *= multiplierIter.next();
+        }
+        return multiplierProduct;
     }
 }
