77c509e244cb3cf4332e597203339b8e5eaa53f5
Tobias Nett <skaldarnar@googlemail.com>
2020-07-29 22:54:10 +0200

    refactor(FlowLayout): Use same code path for drawing and calculating size

--- engine/src/main/java/org/terasology/rendering/nui/layouts/FlowLayout.java
+++ engine/src/main/java/org/terasology/rendering/nui/layouts/FlowLayout.java
@@ -35,18 +35,6 @@ public class FlowLayout extends CoreLayout<LayoutHint> {
     private List<UIWidget> contents = Lists.newArrayList();
 
     /**
-     * Whether the directional flow of this layout goes from left-to-right and right-to-left.
-     * <p>
-     * The children are laid out from left-to-right by default (false), aligned at the left border of the canvas. If
-     * this toggle is explicitly enabled (true) the children are laid out right-to-left, aligned at the right border of
-     * the canvas.
-     * <p>
-     * This toggle can be set programmatically or in {@code .ui} files that use the Flow layout.
-     */
-    @LayoutConfig
-    private boolean rightToLeftAlign = false;
-
-    /**
      * The vertical spacing between adjacent widgets, in pixels
      */
     @LayoutConfig
@@ -75,49 +63,64 @@ public class FlowLayout extends CoreLayout<LayoutHint> {
 
     @Override
     public void onDraw(Canvas canvas) {
-        int filledWidth = getInitializedWidgetWidth(canvas.size().x, 0);
-        int filledHeight = 0;
-        int heightOffset = 0;
-        for (UIWidget widget : contents) {
-            Vector2i size = canvas.calculatePreferredSize(widget);
-            if (rightToLeftAlign) {
-                filledWidth -= size.x;
-            }
-            if (filledWidth != getInitializedWidgetWidth(canvas.size().x, size.x) &&
-                    (filledWidth + size.x > canvas.size().x || filledWidth < 0)) {
-                heightOffset += filledHeight + verticalSpacing;
-                filledWidth = getInitializedWidgetWidth(canvas.size().x, size.x);
-                filledHeight = 0;
-            }
-            canvas.drawWidget(widget, Rect2i.createFromMinAndSize(filledWidth, heightOffset, size.x, size.y));
-            filledWidth += ((rightToLeftAlign) ? -horizontalSpacing : size.x + horizontalSpacing);
-            filledHeight = Math.max(filledHeight, size.y);
-        }
-    }
-
-    private int getInitializedWidgetWidth(int canvasSizeX, int widgetSizeX) {
-        return (rightToLeftAlign) ? canvasSizeX - widgetSizeX : 0;
+        layout(canvas, canvas.size(), true);
     }
 
     @Override
     public Vector2i getPreferredContentSize(Canvas canvas, Vector2i sizeHint) {
+        return layout(canvas, sizeHint, false);
+    }
+
+    /**
+     * Applies the flow layout to its children, arranging them in a directional flow.
+     * <p>
+     * The layout algorithm will wrap elements to a new line if adding them in the same line would exceed the bounding
+     * size. The first element of a row is placed regardless of its size, and thus may exceed the bounding size. The
+     * Flow layout attempts to stay within the preferred width, but will add new lines until all widgets have been laid
+     * out.
+     * <p>
+     * If the preferred bounding size is wider than the preferred size of the widest child the Flow layout guarantees to
+     * fit into the preferred width. There is no guarantee for the actual height.
+     *
+     * @param canvas the canvas to draw to and/or calculate sizes for
+     * @param boundingSize the boundary for this layout, may be a canvas size or a size hint
+     * @param draw whether to actually draw the widgets to the canvas
+     * @return the computed bounding box size for when arranging the widget
+     */
+    private Vector2i layout(Canvas canvas, Vector2i boundingSize, boolean draw) {
+        // current bounding box for the widgets already laid out
         Vector2i result = new Vector2i();
-        int filledWidth = 0;
-        int filledHeight = 0;
+        // current "cursor" position, always where the next widget is to be placed?
+        int widthOffset = 0;
+        // current "cursor" position, always where the next widget is to be placed
+        int heightOffset = 0;
+        // local maximum for row height
+        int rowHeight = 0;
+
         for (UIWidget widget : contents) {
             Vector2i size = canvas.calculatePreferredSize(widget);
-            if (filledWidth != 0 && filledWidth + size.x > sizeHint.x) {
-                result.x = Math.max(result.x, filledWidth);
-                result.y += filledHeight + verticalSpacing;
-                filledWidth = size.x + horizontalSpacing;
-                filledHeight = size.y;
+
+            if (widthOffset != 0 && widthOffset + horizontalSpacing + size.x <= boundingSize.x) {
+                // place widget in the current row
+                widthOffset += horizontalSpacing;
             } else {
-                filledWidth += size.x + horizontalSpacing;
-                filledHeight = Math.max(filledHeight, size.y);
+                // we need to wrap the row
+                result.x = Math.max(result.x, widthOffset);
+                result.y += rowHeight + verticalSpacing;
+                heightOffset = result.y;
+                widthOffset = 0;
+                rowHeight = 0;
             }
+
+            if (draw) {
+                canvas.drawWidget(widget, Rect2i.createFromMinAndSize(widthOffset, heightOffset, size.x, size.y));
+            }
+            widthOffset += size.x;
+            rowHeight = Math.max(rowHeight, size.y);
         }
-        result.x = Math.max(result.x, filledWidth);
-        result.y += filledHeight;
+
+        result.x = Math.max(result.x, widthOffset);
+        result.y += rowHeight;
 
         return result;
     }
@@ -170,34 +173,5 @@ public class FlowLayout extends CoreLayout<LayoutHint> {
     public FlowLayout setVerticalSpacing(int spacing) {
         this.verticalSpacing = spacing;
         return this;
-    }
-
-    /**
-     * Whether the directional flow of this layout goes from left-to-right and right-to-left.
-     * <p>
-     * If false, the children are laid out from left-to-right and aligned at the left border of the canvas. If true, the
-     * children are laid out right-to-left and aligned at the right border of the canvas.
-     * <p>
-     * This toggle can be set programmatically or in {@code .ui} files that use the Flow layout.
-     *
-     * @return whether the children are laid out right-to-left and aligned to the right
-     */
-    public boolean isRightToLeftAlign() {
-        return rightToLeftAlign;
-    }
-
-    /**
-     * Set whether the directional flow of this layout goes from left-to-right and right-to-left.
-     * <p>
-     * The children are laid out from left-to-right by default (false), aligned at the left border of the canvas. If
-     * this toggle is explicitly enabled (true) the children are laid out right-to-left, aligned at the right border of
-     * the canvas.
-     * <p>
-     * This toggle can be set programmatically or in {@code .ui} files that use the Flow layout.
-     *
-     * @param rightToLeftAlign  whether the children are laid out right-to-left and aligned to the right
-     */
-    public void setRightToLeftAlign(boolean rightToLeftAlign) {
-        this.rightToLeftAlign = rightToLeftAlign;
     }
 }
