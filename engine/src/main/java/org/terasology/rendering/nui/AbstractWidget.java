b7b468a1b956001aba2046aedac571cce9f02842
Dmitry D. (Ankhell) <2011Hellangel@gmail.com>
2020-08-15 23:49:52 +0300

    Added new prop "overrideChildEnabledProp" to AbstractWidget to avoid overriding of child enabled property during initialization by UIBox, modified UIFormat to process enabled field after Layout content initialization, disabled Identity Service Login features in UI

--- engine/src/main/java/org/terasology/rendering/nui/AbstractWidget.java
+++ engine/src/main/java/org/terasology/rendering/nui/AbstractWidget.java
@@ -42,6 +42,9 @@ public abstract class AbstractWidget implements UIWidget {
     private UISkin skin;
 
     @LayoutConfig
+    private boolean overrideChildEnabledProp = true;
+
+    @LayoutConfig
     private Binding<String> family = new DefaultBinding<>();
 
     @LayoutConfig
@@ -68,6 +71,14 @@ public abstract class AbstractWidget implements UIWidget {
 
     public AbstractWidget(String id) {
         this.id = id;
+    }
+
+    public boolean isOverrideChildEnabledProp() {
+        return overrideChildEnabledProp;
+    }
+
+    public void setOverrideChildEnabledProp(boolean overrideChildEnabledProp) {
+        this.overrideChildEnabledProp = overrideChildEnabledProp;
     }
 
     @Override
@@ -169,12 +180,13 @@ public abstract class AbstractWidget implements UIWidget {
         this.enabled.set(enabled);
 
         for (UIWidget child : this) {
-            if (child instanceof AbstractWidget) {
-                AbstractWidget widget = (AbstractWidget) child;
-                widget.setEnabled(this.isEnabled());
+            if (overrideChildEnabledProp) {
+                if (child instanceof AbstractWidget) {
+                    AbstractWidget widget = (AbstractWidget) child;
+                    widget.setEnabled(this.isEnabled());
+                }
             }
         }
-
     }
 
     public void bindEnabled(Binding<Boolean> binding) {
