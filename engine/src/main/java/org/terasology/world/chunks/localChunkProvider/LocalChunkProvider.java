256adff499cf1844ace1b77dcc09438677a6e78d
DarkWeird <foxflameless@gmail.com>
2020-08-19 21:01:17 +0300

    chore: Copyright, remove unused constructor parameter

--- engine/src/main/java/org/terasology/world/chunks/localChunkProvider/LocalChunkProvider.java
+++ engine/src/main/java/org/terasology/world/chunks/localChunkProvider/LocalChunkProvider.java
@@ -1,19 +1,5 @@
-/*
- * Copyright 2018 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.world.chunks.localChunkProvider;
 
 import com.google.common.collect.Lists;
@@ -112,14 +98,13 @@ public class LocalChunkProvider implements GeneratingChunkProvider {
                 extraDataManager,
                 new LightMergingChunkFinalizer(),
                 LightMergingChunkFinalizer::new,
-                new ConcurrentMapChunkCache(),
-                null);
+                new ConcurrentMapChunkCache());
     }
 
     LocalChunkProvider(StorageManager storageManager, EntityManager entityManager, WorldGenerator generator,
                        BlockManager blockManager, ExtraBlockDataManager extraDataManager,
                        ChunkFinalizer chunkFinalizer, Supplier<ChunkFinalizer> chunkFinalizerSupplier,
-                       ChunkCache chunkCache, RelevanceSystem relevanceSystem) {
+                       ChunkCache chunkCache) {
         this.storageManager = storageManager;
         this.entityManager = entityManager;
         this.generator = generator;
