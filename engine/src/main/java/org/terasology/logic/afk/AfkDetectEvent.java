d453bd392a24d726dbde26e7be667c17ac997786
iHDeveloper <20463031+iHDeveloper@users.noreply.github.com>
2020-11-11 22:16:18 +0300

    feat: Add AFK state for players in multiplayer (#3571)
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com

---
+++ engine/src/main/java/org/terasology/logic/afk/AfkDetectEvent.java
@@ -0,0 +1,10 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+package org.terasology.logic.afk;
+
+import org.terasology.network.BroadcastEvent;
+import org.terasology.network.NetworkEvent;
+
+@BroadcastEvent
+public class AfkDetectEvent extends NetworkEvent {
+}
