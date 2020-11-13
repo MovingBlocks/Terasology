f06b5e2b451f1f3466014319887fad7e64d478a5
DarkWeird <foxflameless@gmail.com>
2020-11-13 20:10:57 +0300

    fix(skeletal-mesh-scaling): fix invisible for big models - add scaling for skeletal-meshes

--- engine/src/main/java/org/terasology/rendering/logic/SkeletonRenderer.java
+++ engine/src/main/java/org/terasology/rendering/logic/SkeletonRenderer.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.rendering.logic;
 
@@ -254,6 +241,15 @@ public class SkeletonRenderer extends BaseComponentSystem implements RenderSyste
             float worldScale = location.getWorldScale();
 
             aabb = aabb.transform(worldRot, worldPos, worldScale);
+
+
+            //Scale bounding box for skeletalMesh.
+            Vector3f scale = skeletalMesh.scale;
+
+            Vector3f aabbCenter = aabb.getCenter();
+            Vector3f scaledExtents = aabb.getExtents().mul(scale.x(), scale.y(), scale.z());
+            aabb = AABB.createCenterExtent(aabbCenter, scaledExtents);
+
             if (!worldRenderer.getActiveCamera().hasInSight(aabb)) {
                 continue;
             }
