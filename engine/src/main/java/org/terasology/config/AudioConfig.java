e40abd68d6605df4cdc1dbe75d9e2c60a9d5d8a2
DarkWeird <foxflameless@gmail.com>
2020-11-17 14:36:44 +0300

    feat(flexibleconfig-ui): add testScreens to settings.

--- engine/src/main/java/org/terasology/config/AudioConfig.java
+++ engine/src/main/java/org/terasology/config/AudioConfig.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2017 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.config;
 
 import org.terasology.config.flexible.AutoConfig;
@@ -21,6 +8,7 @@ import org.terasology.config.flexible.constraints.NumberRangeConstraint;
 
 import static org.terasology.config.flexible.SettingArgument.constraint;
 import static org.terasology.config.flexible.SettingArgument.defaultValue;
+import static org.terasology.config.flexible.SettingArgument.name;
 import static org.terasology.config.flexible.SettingArgument.type;
 
 public class AudioConfig extends AutoConfig {
@@ -40,11 +28,13 @@ public class AudioConfig extends AutoConfig {
             );
 
     public final Setting<Float> musicVolume =
-        setting(
-            type(Float.class),
-            defaultValue(1.0f),
-            constraint(new NumberRangeConstraint<>(0.0f, 1.0f, true, true))
-        );
+            setting(
+                    type(Float.class),
+                    defaultValue(1.0f),
+                    name("${engine:menu#music-volume}"),
+                    constraint(new NumberRangeConstraint<>(0.0f, 1.0f, true, true))
+            );
+
 
     // TODO: Convert into Setting -- no uses yet
     private boolean disableSound;
