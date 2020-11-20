df2122a8ff97eea33e3e3e0bb95afc62866f178e
DarkWeird <foxflameless@gmail.com>
2020-11-20 17:11:05 +0300

    docs(TypeHandlerLibrary): some license headers and docs

--- subsystems/TypeHandlerLibrary/src/main/java/org/terasology/reflection/reflect/ConstructorLibrary.java
+++ subsystems/TypeHandlerLibrary/src/main/java/org/terasology/reflection/reflect/ConstructorLibrary.java
@@ -1,21 +1,5 @@
-/*
- * Copyright 2018 MovingBlocks
- * Copyright (C) 2011 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Based on Gson v2.6.2 com.google.gson.internal.ConstructorConstructor
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.reflection.reflect;
 
 import org.terasology.persistence.typeHandling.InstanceCreator;
