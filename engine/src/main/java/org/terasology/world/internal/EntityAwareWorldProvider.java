3dcc0b9638b966d9febc02c5e67bc9454d6a4573
jdrueckert <jd.rueckert@googlemail.com>
2020-12-08 23:07:00 +0100

    feat: use `iterableInPlace` for removing pos
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- engine/src/main/java/org/terasology/world/internal/EntityAwareWorldProvider.java
+++ engine/src/main/java/org/terasology/world/internal/EntityAwareWorldProvider.java
@@ -496,7 +496,7 @@ public class EntityAwareWorldProvider extends AbstractWorldProviderDecorator imp
     @ReceiveEvent(components = {BlockRegionComponent.class})
     public void onBlockRegionDeactivated(BeforeDeactivateComponent event, EntityRef entity) {
         BlockRegion oldRegion = blockRegions.get(entity);
-        for (org.joml.Vector3i pos : BlockRegions.iterable(oldRegion)) {
+        for (org.joml.Vector3i pos : BlockRegions.iterableInPlace(oldRegion)) {
             blockRegionLookup.remove(pos);
         }
         blockRegions.remove(entity);
