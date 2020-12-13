728e1a9fc001a77fed692d78716822cd92f57dda
DUMONT Joanne <joanne.dumont@imt-atlantique.net>
2020-12-13 15:54:52 +0100

    Modification BlockRegion intersection methods + tests

--- engine-tests/src/test/java/org/terasology/math/BlockRegionTest.java
+++ engine-tests/src/test/java/org/terasology/math/BlockRegionTest.java
@@ -17,6 +17,13 @@
 package org.terasology.math;
 
 import com.google.common.collect.Sets;
+
+import org.joml.AABBf;
+import org.joml.AABBi;
+import org.joml.LineSegmentf;
+import org.joml.Rayf;
+import org.joml.Spheref;
+import org.joml.Vector2f;
 import org.joml.Vector3i;
 import org.joml.Vector3ic;
 import org.junit.jupiter.api.Test;
@@ -174,5 +181,126 @@ public class BlockRegionTest {
 
         assertEquals(min, region.getMin(new Vector3i()));
         assertEquals(max, region.getMax(new Vector3i()));
+    }
+    
+    @Test
+    public void testContainsPoint() {
+        BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+        
+        assertTrue(a.containsPoint(1.0f, 1.0f, 1.0f));
+        
+        assertTrue(a.containsPoint(1.2f, 0f, 0f));
+        assertTrue(a.containsPoint(1.2f, 0f, 1.2f));
+        assertFalse(a.containsPoint(1.2f, 0f, -1.2f));
+        
+        assertTrue(a.containsPoint(0f, 1.2f, 0f));
+        assertTrue(a.containsPoint(0f, 1.2f, 1.2f));
+        assertFalse(a.containsPoint(0f, 1.2f, -1.2f));
+        
+        assertTrue(a.containsPoint(1.2f, 1.2f, 0f));
+        assertTrue(a.containsPoint(1.2f, 1.2f, 1.2f));
+        assertFalse(a.containsPoint(1.2f, 1.2f, -1.2f));
+        
+        assertFalse(a.containsPoint(-1.2f, 0f, 0f));
+        assertFalse(a.containsPoint(-1.2f, 0f, 1.2f));
+        assertFalse(a.containsPoint(-1.2f, 0f, -1.2f));
+        
+        assertFalse(a.containsPoint(0f, -1.2f, 0f));
+        assertFalse(a.containsPoint(0f, -1.2f, 1.2f));
+        assertFalse(a.containsPoint(0f, -1.2f, -1.2f));
+        
+        assertFalse(a.containsPoint(-1.2f, 1.2f, 0f));
+        assertFalse(a.containsPoint(-1.2f, 1.2f, 1.2f));
+        assertFalse(a.containsPoint(-1.2f, 1.2f, -1.2f));
+        
+        assertFalse(a.containsPoint(1.2f, -1.2f, 0f));
+        assertFalse(a.containsPoint(1.2f, -1.2f, 1.2f));
+        assertFalse(a.containsPoint(1.2f, -1.2f, -1.2f));
+        
+        assertFalse(a.containsPoint(-1.2f, -1.2f, 0f));
+        assertFalse(a.containsPoint(-1.2f, -1.2f, 1.2f));
+        assertFalse(a.containsPoint(-1.2f, -1.2f, -1.2f));
+        
+    }
+    
+    @Test
+    public void testIntersectionPlane() {
+    	BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+    	assertTrue(a.intersectsPlane(1, 1, 1, 1));
+        assertFalse(a.intersectsPlane(1, 1, 1, 2));
+    }
+    
+    @Test
+    public void testIntersectionBlockRegion() {
+    	BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+    	BlockRegion b = new BlockRegion(2, 2, 2, 4, 4, 4);
+    	BlockRegion c = new BlockRegion(3, 3, 3, 4, 4, 4);
+    	
+    	assertTrue(a.intersectsBlockRegion(b));
+    	assertFalse(a.intersectsBlockRegion(c));
+    }
+
+@Test
+    public void testIntersectionAABB() {
+    	BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+    	
+    	AABBi aabbi1 = new AABBi(1, 1, 1, 2, 2, 2);
+    	AABBi aabbi2 = new AABBi(2, 2, 2, 3, 3, 3);
+    	assertTrue(a.intersectsAABB(aabbi1));
+    	assertFalse(a.intersectsAABB(aabbi2));
+    	
+    	AABBf aabbf1 = new AABBf(1.2f, 1.5f, 1.2f, 2, 2, 2);
+    	AABBf aabbf2 = new AABBf(2, 2, 2, 3, 3, 3);
+    	assertTrue(a.intersectsAABB(aabbf1));
+    	assertFalse(a.intersectsAABB(aabbf2));
+    	
+    }
+    
+    @Test
+    public void testIntersectionSphere() {
+    	BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+    	Spheref s1 = new Spheref(0, 0, 1, 2);
+    	Spheref s2 = new Spheref(3, 3, 3, 1);
+    	
+    	assertTrue(a.intersectsSphere(s1));
+    	assertTrue(a.intersectsSphere(2, 2, 2, 1));
+    	assertFalse(a.intersectsSphere(s2));
+    	assertFalse(a.intersectsSphere(2, 2, 2, 0.25f));
+    }
+    
+    @Test
+    public void testIntersectionRay() {
+    	BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+    	Rayf r1 = new Rayf(0, 0, 3, 1, 1, -2);
+    	Rayf r2 = new Rayf(0, 2, 2, 1, 0, 0);
+    	
+    	assertTrue(a.intersectsRay(r1));
+    	assertFalse(a.intersectsRay(r2));
+    	assertTrue(a.intersectsRay(1.2f, 0, 0, 1, 0, 0));
+    	assertFalse(a.intersectsRay(0, 0, 3, 1, 1, -1));
+    }
+    
+    @Test void testIntersectionLineSegment() {
+    	BlockRegion a = new BlockRegion(0, 0, 0, 1, 1, 1);
+    	
+    	//no intersection
+    	assertEquals(a.intersectLineSegment(3f, 3f, 3f, 2f, 3f, 3f, new Vector2f()), -1);
+    	LineSegmentf l1 = new LineSegmentf(3f, 2f, 3f, 2f, 3f, 2f);
+    	assertEquals(a.intersectLineSegment(l1, new Vector2f()), -1);
+    	
+    	//one intersection
+    	assertEquals(a.intersectLineSegment(1.2f, 1.2f, 1.2f, 1.6f, 1.6f, 1.6f, new Vector2f()), 1);
+    	LineSegmentf l2 = new LineSegmentf(-0.6f, 0f, 0f, -0.2f, 1.2f, 0f);
+    	assertEquals(a.intersectLineSegment(l2, new Vector2f()), 1);
+    	
+    	//two intersections
+    	assertEquals(a.intersectLineSegment(1.2f, 1.2f, 2f, -0.6f, 0f, -0.2f, new Vector2f()), 2);
+    	LineSegmentf l3 = new LineSegmentf(2f, 2f, 2f, -0.6f, -2f, 0f);
+    	assertEquals(a.intersectLineSegment(l3, new Vector2f()), 2);
+    	
+    	//segment inside the BlocRegion
+    	assertEquals(a.intersectLineSegment(0f, 1f, 1.2f, 1f, -0.2f, 0.2f, new Vector2f()), 3);
+    	LineSegmentf l4 = new LineSegmentf(1f, 1f, 1.2f, -0.2f, 0f, 1f);
+    	assertEquals(a.intersectLineSegment(l4, new Vector2f()), 3);
     }
 }
