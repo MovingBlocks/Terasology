c469d17d3baf308b05ecf248a054c076edbb9578
DarkWeird <foxflameless@gmail.com>
2020-12-15 15:48:10 +0300

    feature(TypeHandlerLibary): add Reader and Writer for Protobuf and Gson

---
+++ engine/src/main/java/org/terasology/persistence/typeHandling/protobuf/ProtobufDataWriter.java
@@ -0,0 +1,22 @@
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.persistence.typeHandling.protobuf;
+
+import com.google.protobuf.CodedOutputStream;
+import org.terasology.persistence.serializers.PersistedDataWriter;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ProtobufDataWriter implements PersistedDataWriter<ProtobufPersistedData> {
+    @Override
+    public byte[] writeBytes(ProtobufPersistedData data) {
+        return data.getValue().toByteArray();
+    }
+
+    @Override
+    public void writeTo(ProtobufPersistedData data, OutputStream outputStream) throws IOException {
+        data.getValue().writeTo(CodedOutputStream.newInstance(outputStream));
+    }
+}
