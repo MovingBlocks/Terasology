f7f711dfb22f38c0883a9dc1c09994fdcda2bd87
DarkWeird <foxflameless@gmail.com>
2020-12-18 10:46:25 +0300

    fix(extract-gsonserializer): fix api/impl for TypeHandlerLibrary

--- subsystems/TypeHandlerLibrary/build.gradle.kts
+++ subsystems/TypeHandlerLibrary/build.gradle.kts
@@ -10,10 +10,10 @@ apply(from = "$rootDir/config/gradle/common.gradle")
 
 dependencies {
     implementation("org.slf4j:slf4j-api:1.7.21")
-    implementation("net.sf.trove4j:trove4j:3.0.3")
+    api("net.sf.trove4j:trove4j:3.0.3")
+    api("org.terasology.nui:nui-reflect:1.3.1")
 
     implementation("org.reflections:reflections:0.9.10")
-    implementation("org.terasology.nui:nui-reflect:1.3.1")
     implementation("org.terasology:gestalt-module:5.1.5")
     implementation("org.terasology:gestalt-asset-core:5.1.5")
 
