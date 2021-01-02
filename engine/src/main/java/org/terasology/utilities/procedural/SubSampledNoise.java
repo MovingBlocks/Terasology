5d59a99ec6c2702b3cdb691b9f0c92dca882f55d
Tobias Nett <skaldarnar@googlemail.com>
2021-01-02 11:49:55 +0100

    chore: remove old implementation based on TeraMath types

--- engine/src/main/java/org/terasology/utilities/procedural/SubSampledNoise.java
+++ engine/src/main/java/org/terasology/utilities/procedural/SubSampledNoise.java
@@ -76,10 +76,7 @@ public class SubSampledNoise extends AbstractNoise {
      */
     @Deprecated
     public float[] noise(Rect2i region) {
-        Rect2i fullRegion = determineRequiredRegion(region);
-        float[] keyData = getKeyValues(fullRegion);
-        float[] fullData = mapExpand(keyData, fullRegion);
-        return getSubset(fullData, fullRegion, region);
+        return noise(new BlockArea(region.minX(), region.minY(), region.maxX(), region.maxY()));
     }
 
     public float[] noise(BlockAreac area) {
@@ -95,19 +92,6 @@ public class SubSampledNoise extends AbstractNoise {
             Vector2i offset = new Vector2i(subRegion.minX() - fullRegion.minX(), subRegion.minY() - fullRegion.minY());
             for (int y = 0; y < subRegion.getSizeY(); ++y) {
                 System.arraycopy(fullData, offset.x() + fullRegion.getSizeX() * (y + offset.y()), result, subRegion.getSizeX() * y, subRegion.getSizeX());
-            }
-            return result;
-        } else {
-            return fullData;
-        }
-    }
-
-    private float[] getSubset(float[] fullData, Rect2i fullRegion, Rect2i subRegion) {
-        if (subRegion.sizeX() != fullRegion.sizeX() || subRegion.sizeY() != fullRegion.sizeY()) {
-            float[] result = new float[subRegion.sizeX() * subRegion.sizeY()];
-            Vector2i offset = new Vector2i(subRegion.minX() - fullRegion.minX(), subRegion.minY() - fullRegion.minY());
-            for (int y = 0; y < subRegion.sizeY(); ++y) {
-                System.arraycopy(fullData, offset.x() + fullRegion.sizeX() * (y + offset.y()), result, subRegion.sizeX() * y, subRegion.sizeX());
             }
             return result;
         } else {
@@ -136,44 +120,6 @@ public class SubSampledNoise extends AbstractNoise {
         return fullData;
     }
 
-    private float[] mapExpand(float[] keyData, Rect2i fullRegion) {
-        float[] fullData = new float[fullRegion.sizeX() * fullRegion.sizeY()];
-        int samplesX = fullRegion.sizeX() / sampleRate + 1;
-        int samplesY = fullRegion.sizeY() / sampleRate + 1;
-        for (int y = 0; y < samplesY - 1; y++) {
-            for (int x = 0; x < samplesX - 1; x++) {
-                float q11 = keyData[x + y * samplesX];
-                float q21 = keyData[x + 1 + y * samplesX];
-                float q12 = keyData[x + (y + 1) * samplesX];
-                float q22 = keyData[(x + 1) + (y + 1) * samplesX];
-                for (int innerY = 0; innerY < sampleRate; ++innerY) {
-                    for (int innerX = 0; innerX < sampleRate; ++innerX) {
-                        fullData[x * sampleRate + innerX + fullRegion.sizeX() * (y * sampleRate + innerY)] =
-                                TeraMath.biLerp(q11, q21, q12, q22, (float) innerX / sampleRate, (float) innerY / sampleRate);
-                    }
-                }
-            }
-        }
-        return fullData;
-    }
-
-    @Deprecated
-    private float[] getKeyValues(Rect2i fullRegion) {
-        int xDim = fullRegion.sizeX() / sampleRate + 1;
-        int yDim = fullRegion.sizeY() / sampleRate + 1;
-        float[] fullData = new float[xDim * yDim];
-        for (int y = 0; y < yDim; y++) {
-            for (int x = 0; x < xDim; x++) {
-                int actualX = x * sampleRate + fullRegion.minX();
-                int actualY = y * sampleRate + fullRegion.minY();
-                fullData[x + y * xDim] = source.noise(zoom.x * actualX, zoom.y * actualY);
-            }
-        }
-
-        return fullData;
-    }
-
-
     private float[] getKeyValues(BlockAreac fullRegion) {
         int xDim = fullRegion.getSizeX() / sampleRate + 1;
         int yDim = fullRegion.getSizeY() / sampleRate + 1;
@@ -187,14 +133,6 @@ public class SubSampledNoise extends AbstractNoise {
         }
 
         return fullData;
-    }
-
-    private Rect2i determineRequiredRegion(Rect2i region) {
-        int newMinX = region.minX() - IntMath.mod(region.minX(), sampleRate);
-        int newMinY = region.minY() - IntMath.mod(region.minY(), sampleRate);
-        int newMaxX = region.maxX() + 4 - IntMath.mod(region.maxX(), sampleRate) - 1;
-        int newMaxY = region.maxY() + 4 - IntMath.mod(region.maxY(), sampleRate) - 1;
-        return Rect2i.createFromMinAndMax(newMinX, newMinY, newMaxX, newMaxY);
     }
 
     private BlockArea determineRequiredRegion(BlockAreac region) {
