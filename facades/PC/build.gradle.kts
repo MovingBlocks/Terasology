f383cb13f6c9ecf972c1e3532e24c1aadcb5b08c
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-01-02 16:00:43 -0800

    chore (build): the dependency on modules is for local dev builds

--- facades/PC/build.gradle.kts
+++ facades/PC/build.gradle.kts
@@ -1,4 +1,4 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 
 // The PC facade is responsible for the primary distribution - a plain Java application runnable on PCs
@@ -93,6 +93,9 @@ dependencies {
     // TODO: Consider whether we can move the CR dependency back here from the engine, where it is referenced from the main menu
     implementation(group = "org.terasology.crashreporter", name = "cr-terasology", version = "4.1.0")
 
+    // Make sure any local module builds are up-to-date and have their dependencies by declaring
+    // a runtime dependency on whatever the `:modules` subproject declares.
+    // This won't add anything if there are no modules checked out.
     runtimeOnly(platform(project(":modules")))
 }
 
