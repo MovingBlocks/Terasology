dad99697624fdfb6fb5b4b3ca0eae759cbae2880
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-02-07 11:15:50 -0800

    chore(entitySystem): add more info to NPE in addPrefab

--- engine/src/main/java/org/terasology/entitySystem/entity/EntityBuilder.java
+++ engine/src/main/java/org/terasology/entitySystem/entity/EntityBuilder.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2013 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.entitySystem.entity;
 
 import com.google.common.collect.Maps;
@@ -89,7 +76,14 @@ public class EntityBuilder implements MutableComponentContainer {
     public void addPrefab(Prefab prefab) {
         if (prefab != null) {
             for (Component component : prefab.iterateComponents()) {
-                addComponent(entityManager.getComponentLibrary().copy(component));
+                Component componentCopy = entityManager.getComponentLibrary().copy(component);
+                try {
+                    addComponent(componentCopy);
+                } catch (NullPointerException e) {
+                    throw new NullPointerException(
+                            String.format("Component %s not registered (in prefab %s)", component, prefab)
+                    );
+                }
             }
             addComponent(new EntityInfoComponent(prefab, prefab.isPersisted(), prefab.isAlwaysRelevant()));
         } else {
