22c82c5ea062caa56c8831c0f22f14a0364ba997
jdrueckert <jd.rueckert@googlemail.com>
2021-02-07 21:37:44 +0100

    chore(rendering): clean up imports and update copyrights (#4487)

--- engine/src/main/java/org/terasology/rendering/nui/internal/TerasologyCanvasRenderer.java
+++ engine/src/main/java/org/terasology/rendering/nui/internal/TerasologyCanvasRenderer.java
@@ -1,25 +1,12 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.rendering.nui.internal;
 
 import org.joml.Quaternionfc;
-import org.terasology.joml.geom.Rectanglei;
 import org.joml.Vector2ic;
 import org.joml.Vector3fc;
 import org.terasology.assets.ResourceUrn;
+import org.terasology.joml.geom.Rectanglei;
 import org.terasology.nui.canvas.CanvasRenderer;
 import org.terasology.rendering.assets.material.Material;
 import org.terasology.rendering.assets.mesh.Mesh;
