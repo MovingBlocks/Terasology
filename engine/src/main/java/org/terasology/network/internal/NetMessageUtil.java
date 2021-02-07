75936a55f5a52c20e1a2370ea60937360b08209f
Tobias Nett <skaldarnar@googlemail.com>
2021-02-07 14:05:28 +0100

    refactor(JOML)!: migrate `network` fully to JOML (#4484)
    * chore(network): remove unused import from LocalClient
    * chore(network)!: remove unused method from NetMessageUtil
    * refactor(network): migrate NetEntityRefTypeHandler to JOML

--- engine/src/main/java/org/terasology/network/internal/NetMessageUtil.java
+++ engine/src/main/java/org/terasology/network/internal/NetMessageUtil.java
@@ -1,25 +1,11 @@
-/*
- * Copyright 2013 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.network.internal;
 
 import com.google.protobuf.ByteString;
 import org.joml.Vector3ic;
 import org.terasology.identity.PublicIdentityCertificate;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.protobuf.NetData;
 
 import java.math.BigInteger;
@@ -34,10 +20,6 @@ public final class NetMessageUtil {
 
     public static org.joml.Vector3i convert(NetData.Vector3iData data) {
         return new org.joml.Vector3i(data.getX(), data.getY(), data.getZ());
-    }
-
-    public static NetData.Vector3iData convert(Vector3i data) {
-        return NetData.Vector3iData.newBuilder().setX(data.x).setY(data.y).setZ(data.z).build();
     }
 
     public static NetData.Vector3iData convert(Vector3ic data) {
