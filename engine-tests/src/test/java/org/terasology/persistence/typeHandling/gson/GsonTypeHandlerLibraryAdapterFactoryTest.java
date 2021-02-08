173c5edd4bdf3a1ed39057bcdb77b1cb3420b8a5
Josephine Rueckert <jd.rueckert@googlemail.com>
2021-02-08 21:23:16 +0100

    feat(JOML): drop remaining references to Rect2i and Rect2f
    * remove unused imports
    * update copyright

--- engine-tests/src/test/java/org/terasology/persistence/typeHandling/gson/GsonTypeHandlerLibraryAdapterFactoryTest.java
+++ engine-tests/src/test/java/org/terasology/persistence/typeHandling/gson/GsonTypeHandlerLibraryAdapterFactoryTest.java
@@ -1,4 +1,4 @@
-// Copyright 2020 The Terasology Foundation
+// Copyright 2021 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.persistence.typeHandling.gson;
 
@@ -32,7 +32,7 @@ public class GsonTypeHandlerLibraryAdapterFactoryTest {
     );
 
     private static final String OBJECT_JSON = "{\"color\":[222,173,190,239],\"vector4fs\":[[0.0,0.0,0.0,0.0]," +
-            "[1.0,1.0,1.0,1.0]],\"rect2iMap\":{\"someRect\":{\"min\":[-3,-3],\"max\":[7,7]}},\"i\":-912559}";
+            "[1.0,1.0,1.0,1.0]],\"rectangleiMap\":{\"someRect\":{\"min\":[-3,-3],\"max\":[7,7]}},\"i\":-912559}";
 
     private final Reflections reflections = new Reflections(getClass().getClassLoader());
 
@@ -60,18 +60,18 @@ public class GsonTypeHandlerLibraryAdapterFactoryTest {
     private static class TestClass {
         private final Color color;
         private final Set<Vector4f> vector4fs;
-        private final Map<String, Rectanglei> rect2iMap;
+        private final Map<String, Rectanglei> rectangleiMap;
 
         // Will not be serialized
         private final Map<Integer, Integer> intMap;
 
         private final int i;
 
-        private TestClass(Color color, Set<Vector4f> vector4fs, Map<String, Rectanglei> rect2iMap,
+        private TestClass(Color color, Set<Vector4f> vector4fs, Map<String, Rectanglei> rectangleiMap,
                           Map<Integer, Integer> intMap, int i) {
             this.color = color;
             this.vector4fs = vector4fs;
-            this.rect2iMap = rect2iMap;
+            this.rectangleiMap = rectangleiMap;
             this.intMap = intMap;
             this.i = i;
         }
@@ -84,7 +84,7 @@ public class GsonTypeHandlerLibraryAdapterFactoryTest {
             return i == testClass.i &&
                     Objects.equals(color, testClass.color) &&
                     Objects.equals(vector4fs, testClass.vector4fs) &&
-                    Objects.equals(rect2iMap, testClass.rect2iMap);
+                    Objects.equals(rectangleiMap, testClass.rectangleiMap);
         }
     }
 }
