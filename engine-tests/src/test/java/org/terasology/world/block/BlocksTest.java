ff5c55e477e42484ab08982a8af1dcd086446806
Tobias Nett <skaldarnar@googlemail.com>
2021-02-14 21:32:32 +0100

    test(block): add parameterized tests for Blocks#toBlockPos

--- engine-tests/src/test/java/org/terasology/world/block/BlocksTest.java
+++ engine-tests/src/test/java/org/terasology/world/block/BlocksTest.java
@@ -5,16 +5,33 @@ package org.terasology.world.block;
 
 import org.joml.Vector3f;
 import org.joml.Vector3i;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BlocksTest {
 
-    @Test
-    public void testToBlockPos() {
-        assertEquals(Blocks.toBlockPos(new Vector3f(.2f, .2f, .2f), new Vector3i()), new Vector3i(0, 0, 0));
-        assertEquals(Blocks.toBlockPos(new Vector3f(-.2f, -.2f, -.2f), new Vector3i()), new Vector3i(0, 0, 0));
-        assertEquals(Blocks.toBlockPos(new Vector3f(.56f, 0, 0), new Vector3i()), new Vector3i(1, 0, 0));
+    static Stream<Arguments> coordinates() {
+        return Stream.of(
+                Arguments.of(new Vector3f(.2f, .2f, .2f), new Vector3i(0, 0, 0)),
+                Arguments.of(new Vector3f(.49f, .49f, .49f), new Vector3i(0, 0, 0)),
+                Arguments.of(new Vector3f(.5f, .5f, .5f), new Vector3i(1, 1, 1)),
+                Arguments.of(new Vector3f(1, 1, 1), new Vector3i(1, 1, 1)),
+                Arguments.of(new Vector3f(-.1f, -.1f, -.1f), new Vector3i(0, 0, 0)),
+                Arguments.of(new Vector3f(-.5f, -.5f, -.5f), new Vector3i(0, 0, 0)),
+                Arguments.of(new Vector3f(-.51f, -.51f, -.51f), new Vector3i(-1, -1, -1))
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("coordinates")
+    @DisplayName("toBlockPos should round half-up towards positive infinity")
+    public void testToBlockPos(Vector3f worldPos, Vector3i expectedBlockPos) {
+        assertEquals(expectedBlockPos, Blocks.toBlockPos(worldPos));
     }
 }
