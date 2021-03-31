87cd39bd907d61633de3edd72c391884dad09d3f
Benjamin Amos <benjamin.amos11@gmail.com>
2021-03-31 19:55:55 +0100

    Added clarifying comment to UISkinWithUrn.

--- engine/src/main/java/org/terasology/engine/rendering/nui/skin/UISkinWithUrn.java
+++ engine/src/main/java/org/terasology/engine/rendering/nui/skin/UISkinWithUrn.java
@@ -10,6 +10,12 @@ import org.terasology.nui.skin.UIStyleFamily;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * [NOTE] Sub-classing UISkin into UISkinWithUrn is a work-around in order to associate a ResourceUrn with a UISkin.
+ * When a UISkin is deserialised (via UISkinTypeHandler), then it is copied into a new UISkinWithUrn instance to associate it with the urn it was deserialised with.
+ *
+ * (2021-03-31) It is not clear whether this feature is used anywhere (in the Omega module space) right now.
+ */
 public class UISkinWithUrn extends UISkin {
     private final ResourceUrn urn;
 
