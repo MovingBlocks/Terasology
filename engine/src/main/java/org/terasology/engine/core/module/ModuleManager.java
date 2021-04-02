3986c4dd9c14bc0deebdc11aa7db9a39dda9a769
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-04-01 22:28:18 -0700

    refactor(ModuleManager): restore some organization from the develop branch

--- engine/src/main/java/org/terasology/engine/core/module/ModuleManager.java
+++ engine/src/main/java/org/terasology/engine/core/module/ModuleManager.java
@@ -3,24 +3,22 @@
 package org.terasology.engine.core.module;
 
 import com.google.common.collect.Sets;
-import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.engine.config.Config;
 import org.terasology.engine.config.SystemConfig;
 import org.terasology.engine.core.TerasologyConstants;
 import org.terasology.engine.core.paths.PathManager;
-import org.terasology.gestalt.i18n.I18nMap;
 import org.terasology.gestalt.module.Module;
 import org.terasology.gestalt.module.ModuleEnvironment;
 import org.terasology.gestalt.module.ModuleFactory;
 import org.terasology.gestalt.module.ModuleMetadata;
 import org.terasology.gestalt.module.ModuleMetadataJsonAdapter;
+import org.terasology.gestalt.module.ModuleMetadataLoader;
 import org.terasology.gestalt.module.ModulePathScanner;
 import org.terasology.gestalt.module.ModuleRegistry;
 import org.terasology.gestalt.module.TableModuleRegistry;
 import org.terasology.gestalt.module.dependencyresolution.DependencyInfo;
-import org.terasology.gestalt.module.resources.ClasspathFileSource;
 import org.terasology.gestalt.module.sandbox.APIScanner;
 import org.terasology.gestalt.module.sandbox.ModuleSecurityManager;
 import org.terasology.gestalt.module.sandbox.ModuleSecurityPolicy;
@@ -30,17 +28,20 @@ import org.terasology.gestalt.module.sandbox.WarnOnlyProviderFactory;
 import org.terasology.gestalt.naming.Name;
 import org.terasology.gestalt.naming.Version;
 
+import java.io.File;
 import java.lang.reflect.ReflectPermission;
 import java.nio.file.Path;
 import java.security.Policy;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class ModuleManager {
     private static final Logger logger = LoggerFactory.getLogger(ModuleManager.class);
-    private final ModuleFactory moduleFactory;
+
     private final StandardPermissionProviderFactory permissionProviderFactory = new StandardPermissionProviderFactory();
     private final PermissionProviderFactory wrappingPermissionProviderFactory =
             new WarnOnlyProviderFactory(permissionProviderFactory);
@@ -48,45 +49,83 @@ public class ModuleManager {
     private final ModuleRegistry registry;
     private ModuleEnvironment environment;
     private final ModuleMetadataJsonAdapter metadataReader;
+    private final ModuleFactory moduleFactory;
     private final ModuleInstallManager installManager;
+    private final Module engineModule;
 
     public ModuleManager(String masterServerAddress) {
         this(masterServerAddress, Collections.emptyList());
     }
 
     public ModuleManager(String masterServerAddress, List<Class<?>> classesOnClasspathsToAddToEngine) {
-        metadataReader = new ModuleMetadataJsonAdapter();
-        for (ModuleExtension ext : StandardModuleExtension.values()) {
-            metadataReader.registerExtension(ext.getKey(), ext.getValueType());
-        }
-        for (ModuleExtension ext : ExtraDataModuleExtension.values()) {
-            metadataReader.registerExtension(ext.getKey(), ext.getValueType());
-        }
-        moduleFactory = new ModuleFactory();
-        moduleFactory.getModuleMetadataLoaderMap().put(TerasologyConstants.MODULE_INFO_FILENAME.toString(),
-                metadataReader);
-        moduleFactory.getModuleMetadataLoaderMap().put("engine-module.txt", metadataReader);
+        PathManager pathManager = PathManager.getInstance();  // get early so if it needs to initialize, it does it now
 
-        Module engineModule = moduleFactory.createPackageModule("org.terasology.engine");
-        Module nuiModule = createSyntheticPackageModule(engineModule, "nui", "0.0.0", "org.terasology.nui");
+        metadataReader = newMetadataReader();
+
+        moduleFactory = new ModuleFactory();
+        Map<String, ModuleMetadataLoader> mmlm = moduleFactory.getModuleMetadataLoaderMap();
+        mmlm.put(TerasologyConstants.MODULE_INFO_FILENAME.toString(), metadataReader);
+        mmlm.put("engine-module.txt", metadataReader);  // FIXME: this should be *only* for engine-module.
+        // FIXME: …or maybe engine-module doesn't need a special filename anymore?
 
         registry = new TableModuleRegistry();
-        registry.add(engineModule);
 
-        ModulePathScanner scanner = new ModulePathScanner(moduleFactory);
-        scanner.scan(registry, PathManager.getInstance().getModulePaths().stream().map(Path::toFile).collect(Collectors.toSet()));
+        engineModule = loadEngineModule(classesOnClasspathsToAddToEngine);
 
+        loadModulesFromApplicationPath(pathManager);
+
+        ensureModulesDependOnEngine();
+
+        setupSandbox();
+        loadEnvironment(Sets.newHashSet(engineModule), true);
+        installManager = new ModuleInstallManager(this, masterServerAddress);
+    }
+
+    /**
+     * I wondered why this is important, and found MovingBlocks/Terasology#1450.
+     * It's not a worry that the engine module wouldn't be loaded without it.
+     * It's about ordering: some things run in an order derived from the dependency
+     * tree, and we want to make sure engine is at the root of it.
+     */
+    private void ensureModulesDependOnEngine() {
         DependencyInfo engineDep = new DependencyInfo();
         engineDep.setId(engineModule.getId());
         engineDep.setMinVersion(engineModule.getVersion());
         engineDep.setMaxVersion(engineModule.getVersion().getNextPatchVersion());
 
-        registry.stream().filter(mod -> mod != engineModule).forEach(mod -> mod.getMetadata().getDependencies().add(engineDep));
-        registry.add(nuiModule);
+        HashSet<Name> engineModules = Sets.newHashSet(engineModule.getId());
+        engineModules.addAll(engineModule.getMetadata().getDependencies().stream().map(DependencyInfo::getId).collect(Collectors.toList()));
 
-        setupSandbox();
-        loadEnvironment(Sets.newHashSet(engineModule, nuiModule), true);
-        installManager = new ModuleInstallManager(this, masterServerAddress);
+        registry.stream()
+                .filter(mod -> !engineModules.contains(mod.getId()))
+                .forEach(mod -> mod.getMetadata().getDependencies().add(engineDep));
+    }
+
+    private void loadModulesFromApplicationPath(PathManager pathManager) {
+        ModulePathScanner scanner = new ModulePathScanner(moduleFactory);
+        List<File> paths = pathManager.getModulePaths().stream().map(Path::toFile).collect(Collectors.toList());
+        scanner.scan(registry, paths);
+    }
+
+    private Module loadEngineModule(List<Class<?>> classesOnClasspathsToAddToEngine) {
+        // FIXME: is `classes…toAddToEngine` gone? Did we ever use it in the first place?
+        Module engine = moduleFactory.createPackageModule("org.terasology.engine");
+        // TODO: document why nui is a module when other libraries are not
+        Module nui = createSyntheticPackageModule("nui", "3.0.0" /* FIXME */, "org.terasology.nui");
+
+        DependencyInfo nuiDependency = new DependencyInfo();
+        nuiDependency.setId(nui.getId());
+        nuiDependency.setMinVersion(nui.getVersion());
+        nuiDependency.setMaxVersion(nui.getVersion().getNextPatchVersion());
+        engine.getMetadata().getDependencies().add(nuiDependency);
+
+        registry.add(nui);
+        registry.add(engine);
+
+        // FIXME: why doesn't gestalt-v7 need this?
+        //  enrichReflectionsWithSubsystems(engineModule);
+
+        return engine;
     }
 
     public ModuleManager(Config config) {
@@ -97,21 +136,24 @@ public class ModuleManager {
         this(config.getNetwork().getMasterServer(), classesOnClasspathsToAddToEngine);
     }
 
-    @NotNull
-    private Module createSyntheticPackageModule(Module engineModule, String id, String version, String packageName) {
+    private ModuleMetadataJsonAdapter newMetadataReader() {
+        final ModuleMetadataJsonAdapter metadataJsonAdapter = new ModuleMetadataJsonAdapter();
+        for (ModuleExtension ext : StandardModuleExtension.values()) {
+            metadataJsonAdapter.registerExtension(ext.getKey(), ext.getValueType());
+        }
+        for (ModuleExtension ext : ExtraDataModuleExtension.values()) {
+            metadataJsonAdapter.registerExtension(ext.getKey(), ext.getValueType());
+        }
+        return metadataJsonAdapter;
+    }
+
+    private Module createSyntheticPackageModule(String id, String version, String packageName) {
         ModuleMetadata metadata = new ModuleMetadata(
                 new Name(id),
                 new Version(version));
-        metadata.setDisplayName(new I18nMap(id));
-        Module module = moduleFactory.createPackageModule(
+        return moduleFactory.createPackageModule(
                 metadata,
                 packageName);
-        DependencyInfo moduleDep = new DependencyInfo();
-        moduleDep.setId(module.getId());
-        moduleDep.setMinVersion(module.getVersion());
-        moduleDep.setMaxVersion(module.getVersion().getNextPatchVersion());
-        engineModule.getMetadata().getDependencies().add(moduleDep);
-        return module;
     }
 
     private void setupSandbox() {
@@ -124,8 +166,7 @@ public class ModuleManager {
         registry.stream().map(Module::getModuleManifest).forEach(apiScanner::scan);
 
         permissionProviderFactory.getBasePermissionSet().grantPermission("com.google.gson", ReflectPermission.class);
-        permissionProviderFactory.getBasePermissionSet().grantPermission("com.google.gson.internal",
-                ReflectPermission.class);
+        permissionProviderFactory.getBasePermissionSet().grantPermission("com.google.gson.internal", ReflectPermission.class);
 
         Policy.setPolicy(new ModuleSecurityPolicy());
         System.setSecurityManager(new ModuleSecurityManager());
@@ -145,10 +186,9 @@ public class ModuleManager {
 
     public ModuleEnvironment loadEnvironment(Set<Module> modules, boolean asPrimary) {
         Set<Module> finalModules = Sets.newLinkedHashSet(modules);
-        finalModules.addAll(registry.stream().filter((m) -> m.getResources() instanceof ClasspathFileSource).collect(Collectors.toList()));
+        finalModules.add(engineModule);
         ModuleEnvironment newEnvironment;
-        boolean permissiveSecurityEnabled =
-                Boolean.parseBoolean(System.getProperty(SystemConfig.PERMISSIVE_SECURITY_ENABLED_PROPERTY));
+        boolean permissiveSecurityEnabled = Boolean.parseBoolean(System.getProperty(SystemConfig.PERMISSIVE_SECURITY_ENABLED_PROPERTY));
         if (permissiveSecurityEnabled) {
             newEnvironment = new ModuleEnvironment(finalModules, wrappingPermissionProviderFactory);
         } else {
@@ -161,10 +201,27 @@ public class ModuleManager {
     }
 
     public ModuleMetadataJsonAdapter getModuleMetadataReader() {
+        // FIXME: is this used anywhere? (MTE?)
         return metadataReader;
     }
 
     public ModuleFactory getModuleFactory() {
         return moduleFactory;
     }
+
+//    private void enrichReflectionsWithSubsystems(Module engineModule) {
+//        Serializer serializer = new XmlSerializer();
+//        try {
+//            Enumeration<URL> urls = ModuleManager.class.getClassLoader().getResources("reflections.cache");
+//            while (urls.hasMoreElements()) {
+//                URL url = urls.nextElement();
+//                if (url.getPath().contains("subsystem")) {
+//                    Reflections subsystemReflections = serializer.read(url.openStream());
+//                    engineModule.getReflectionsFragment().merge(subsystemReflections);
+//                }
+//            }
+//        } catch (IOException e) {
+//            logger.error("Cannot enrich engine's reflections with subsystems");
+//        }
+//    }
 }
