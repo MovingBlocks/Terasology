b874418eb4b6743cefbb2d2a87fb2321b1dca48f
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-04-01 22:51:56 -0700

    test: reduce duplicate exception logs and wrapping

--- engine-tests/src/main/java/org/terasology/engine/Environment.java
+++ engine-tests/src/main/java/org/terasology/engine/Environment.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License"){ }
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 
 package org.terasology.engine;
 
@@ -20,12 +7,12 @@ import com.badlogic.gdx.physics.bullet.Bullet;
 import com.google.common.collect.Sets;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.engine.context.Context;
 import org.terasology.engine.context.internal.ContextImpl;
 import org.terasology.engine.core.paths.PathManager;
 import org.terasology.engine.recording.RecordAndReplayCurrentStatus;
 import org.terasology.engine.registry.CoreRegistry;
+import org.terasology.gestalt.assets.management.AssetManager;
 import org.terasology.gestalt.naming.Name;
 
 import java.io.IOException;
@@ -50,8 +37,11 @@ public class Environment {
         try {
             reset(Sets.newHashSet(moduleNames));
         } catch (Exception e) {
-            logger.error("Error", e);
-            throw new RuntimeException(e);
+            if (e instanceof RuntimeException) {
+                throw (RuntimeException) e;
+            } else {
+                throw new RuntimeException(e);
+            }
         }
     }
 
