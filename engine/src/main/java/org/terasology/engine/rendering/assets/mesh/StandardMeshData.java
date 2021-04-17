c453f3fcf38a649b52e3ff03b4ebc8472f561503
Michael Pollind <mpollind@gmail.com>
2021-04-17 13:05:30 -0700

    Revert "remove UV1"
    This reverts commit 8d92496004c9d79ff7002f9fe87d9b59f2bf96ae.

--- engine/src/main/java/org/terasology/engine/rendering/assets/mesh/StandardMeshData.java
+++ engine/src/main/java/org/terasology/engine/rendering/assets/mesh/StandardMeshData.java
@@ -12,8 +12,10 @@ public class StandardMeshData extends MeshData {
     public static final int VERTEX_INDEX = 0;
     public static final int NORMAL_INDEX = 1;
     public static final int UV0_INDEX = 2;
-    public static final int COLOR0_INDEX = 3;
-    public static final int LIGHT0_INDEX = 4;
+    public static final int UV1_INDEX = 3;
+    public static final int COLOR0_INDEX = 4;
+    public static final int LIGHT0_INDEX = 5;
+
 
     public final TFloatList vertices;
     public final TFloatList uv0;
@@ -33,6 +35,7 @@ public class StandardMeshData extends MeshData {
         addLayout(new FloatLayout(VERTEX_INDEX, 3, vertices, Layout.FLOATING_POINT));
         addLayout(new FloatLayout(NORMAL_INDEX, 3, normals, Layout.FLOATING_POINT));
         addLayout(new FloatLayout(UV0_INDEX, 2, uv0, Layout.FLOATING_POINT));
+        addLayout(new FloatLayout(UV1_INDEX, 2, uv1, Layout.FLOATING_POINT));
         addLayout(new FloatLayout(COLOR0_INDEX, 4, color0, Layout.FLOATING_POINT));
         addLayout(new FloatLayout(LIGHT0_INDEX, 3, light0, Layout.FLOATING_POINT));
     }
