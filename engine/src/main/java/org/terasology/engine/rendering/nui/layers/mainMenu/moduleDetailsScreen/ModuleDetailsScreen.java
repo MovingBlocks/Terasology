a3130faa5a84974ce07f0ccc1b9565b84d9ffe61
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-04-23 10:30:26 -0700

    chore(ModuleDetailsScreen): switch from versionRange to more flexible versionPredicate
    Allows meeting semantic version compatibility without explicitly naming the top end of the range.

--- engine/src/main/java/org/terasology/engine/rendering/nui/layers/mainMenu/moduleDetailsScreen/ModuleDetailsScreen.java
+++ engine/src/main/java/org/terasology/engine/rendering/nui/layers/mainMenu/moduleDetailsScreen/ModuleDetailsScreen.java
@@ -305,7 +305,7 @@ public class ModuleDetailsScreen extends CoreScreenLayer {
             public void draw(DependencyInfo value, Canvas canvas) {
                 Module module = moduleManager.getRegistry().getLatestModuleVersion(value.getId());
 
-                if (module == null || !(value.versionRange().contains(module.getVersion()))) {
+                if (module == null || !(value.versionPredicate().test(module.getVersion()))) {
                     canvas.setMode("invalid");
                 } else {
                     canvas.setMode("available");
