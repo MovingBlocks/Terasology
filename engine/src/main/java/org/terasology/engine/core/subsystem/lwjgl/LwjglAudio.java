67db33e0af3b2f13e850d7203ac25b902998c3a6
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-04-23 17:40:15 -0700

    chore(Audio): remove commented code

--- engine/src/main/java/org/terasology/engine/core/subsystem/lwjgl/LwjglAudio.java
+++ engine/src/main/java/org/terasology/engine/core/subsystem/lwjgl/LwjglAudio.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2014 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *  http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.engine.core.subsystem.lwjgl;
 
 import org.slf4j.Logger;
@@ -55,8 +42,6 @@ public class LwjglAudio extends BaseLwjglSubsystem {
     public void registerCoreAssetTypes(ModuleAwareAssetTypeManager assetTypeManager) {
         assetTypeManager.createAssetType(StaticSound.class, audioManager.getStaticSoundFactory(), "sounds");
         assetTypeManager.createAssetType(StreamingSound.class, audioManager.getStreamingSoundFactory(), "music");
-//        assetTypeManager.registerCoreAssetType(StaticSound.class, audioManager.getStaticSoundFactory(), "sounds");
-//        assetTypeManager.registerCoreAssetType(StreamingSound.class, audioManager.getStreamingSoundFactory(), "music");
     }
 
     @Override
