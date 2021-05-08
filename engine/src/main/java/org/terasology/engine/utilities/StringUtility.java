4c49efa7d4aa327df64c21a0d3d95e65628376f7
Michael Pollind <mpollind@gmail.com>
2021-05-08 14:02:32 -0700

    chore: minor cleanup for CamelCaseMatcher

--- engine/src/main/java/org/terasology/engine/utilities/StringUtility.java
+++ engine/src/main/java/org/terasology/engine/utilities/StringUtility.java
@@ -10,24 +10,24 @@ import java.util.regex.Pattern;
 
 import com.google.common.collect.Sets;
 
-/**
- * Inspired by
- * <p>
- * http://stackoverflow.com/questions/745415/regex-to-match-from-partial-or-camel-case-string
- * </p>
- */
-public final class CamelCaseMatcher {
+public final class StringUtility {
 
-    private CamelCaseMatcher() {
-        // avoid instantiation
-    }
+    private StringUtility() { }
 
     /**
-     * @param queryStr
-     * @param commands
-     * @return
+     * reference: http://stackoverflow.com/questions/745415/regex-to-match-from-partial-or-camel-case-string
+     *
+     * Match a string by a wild card expression and a collection of strings to match against
+     *
+     * query: MPRString, M, MyP*RString, *PosResString, MyP*RString, My*String, M
+     * match: MyPossibleResultString
+     *
+     * @param queryStr string to query by
+     * @param commands a list of strings that will be matched
+     * @return the collection of matching strings
      */
-    public static Set<String> getMatches(String queryStr, Collection<String> commands, boolean includeCaseInsensitiveStartingWith) {
+    public static Set<String> wildcardMatch(String queryStr, Collection<String> commands,
+                                            boolean includeCaseInsensitiveStartingWith) {
         Set<String> matches = Sets.newHashSet();
 
         String query = queryStr.replaceAll("\\*", ".*?");
@@ -37,19 +37,15 @@ public final class CamelCaseMatcher {
         String re = "\\b(" + query.replaceAll("([A-Z][^A-Z]*)", "$1[^A-Z]*") + ".*?)\\b";
 
         Pattern regex = Pattern.compile(re);
-
         for (String cmd : commands) {
             if (includeCaseInsensitiveStartingWith && cmd.toLowerCase().startsWith(queryStr.toLowerCase())) {
                 matches.add(cmd);
             }
-
             Matcher m = regex.matcher(cmd);
-
             if (m.find()) {
                 matches.add(m.group());
             }
         }
-
         return matches;
     }
 }
