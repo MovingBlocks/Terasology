2379064e34c46f42192280639fd0da63d4dcafec
Michael Pollind <mpollind@gmail.com>
2021-05-08 14:31:41 -0700

    temp

--- engine/src/main/java/org/terasology/engine/utilities/StringUtility.java
+++ engine/src/main/java/org/terasology/engine/utilities/StringUtility.java
@@ -18,7 +18,7 @@ public final class StringUtility {
      * Returns the ordinal indicator of an integer.
      * <br><br>
      * Most readable when called with class name:
-     * OrdinalIndicator.of(22) returns "nd"
+     * getOrdinal(22) returns "nd"
      *
      * @param number the integer
      * @return The ordinal indicator ("st", "nd", "rd" or "th").
@@ -52,7 +52,7 @@ public final class StringUtility {
      * Returns the integer combined with it's ordinal indicator as String.
      * <br><br>
      * Most readable when called with class name:
-     * OrdinalIndicator.addedTo(22) returns "22nd"
+     * toOrdinalString(22) returns "22nd"
      *
      * @param x the integer
      * @return The integer with it's ordinal indicator attached.
