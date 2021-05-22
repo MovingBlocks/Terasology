1e5c8d0e5858e5c6166c7c854b51d96fcd8afce7
Michael Pollind <mpollind@gmail.com>
2021-05-22 12:45:43 -0700

    feature: rework location change event

--- engine/src/main/java/org/terasology/engine/logic/location/LocationChangedEvent.java
+++ engine/src/main/java/org/terasology/engine/logic/location/LocationChangedEvent.java
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.engine.logic.location;
 
+import com.google.common.base.Preconditions;
 import org.joml.Quaternionf;
 import org.joml.Quaternionfc;
 import org.joml.Vector3f;
@@ -9,60 +10,48 @@ import org.joml.Vector3fc;
 import org.terasology.engine.entitySystem.event.Event;
 
 public class LocationChangedEvent implements Event {
-    public final LocationComponent component;
-    public final Vector3fc oldPosition;
-    public final Quaternionfc oldRotation;
-    public final Vector3fc newPosition;
-    public final Quaternionfc newRotation;
+    protected Vector3f lastPosition = new Vector3f();
+    protected Quaternionf lastRotation = new Quaternionf();
+    protected Vector3f currentPosition = new Vector3f();
+    protected Quaternionf currentRotation = new Quaternionf();
 
-    public LocationChangedEvent(LocationComponent newLocation) {
-        this(newLocation, newLocation.position, newLocation.rotation, newLocation.position, newLocation.rotation);
+    public LocationChangedEvent() {
     }
 
-    public LocationChangedEvent(LocationComponent newLocation, Vector3f oPosition) {
-        this(newLocation, oPosition, newLocation.rotation, newLocation.position, newLocation.rotation);
+    public LocationChangedEvent(Vector3fc lastPosition, Quaternionfc lastRotation, Vector3fc currentPosition, Quaternionfc currentRotation) {
+        Preconditions.checkNotNull(lastPosition);
+        Preconditions.checkNotNull(lastRotation);
+        Preconditions.checkNotNull(currentPosition);
+        Preconditions.checkNotNull(currentRotation);
+
+        this.lastPosition.set(lastPosition);
+        this.lastRotation.set(lastRotation);
+        this.currentPosition.set(currentPosition);
+        this.currentRotation.set(currentRotation);
     }
 
-    public LocationChangedEvent(LocationComponent newLocation, Vector3f oPosition, Vector3f nPosition) {
-        this(newLocation, oPosition, newLocation.rotation, nPosition, newLocation.rotation);
+    public Vector3fc lastPosition() {
+        return lastPosition;
     }
 
-    public LocationChangedEvent(LocationComponent newLocation, Quaternionfc oRotation) {
-        this(newLocation, newLocation.position, oRotation, newLocation.position, newLocation.rotation);
+    public Quaternionfc lastRotation() {
+        return lastRotation;
     }
 
-    public LocationChangedEvent(LocationComponent newLocation, Quaternionfc oRotation, Quaternionfc nRotation) {
-        this(newLocation, newLocation.position, oRotation, newLocation.position, nRotation);
+    public Vector3fc currentPosition() {
+        return currentPosition;
     }
 
-    public LocationChangedEvent(LocationComponent newLocation, Vector3f oPosition, Quaternionfc oRotation) {
-        this(newLocation, oPosition, oRotation, newLocation.position, newLocation.rotation);
+    public Quaternionfc currentRotation() {
+        return currentRotation;
     }
 
-    public LocationChangedEvent(LocationComponent newLocation, Vector3f oPosition, Quaternionfc oRotation,
-                                Vector3f nPosition) {
-        this(newLocation, oPosition, oRotation, nPosition, newLocation.rotation);
-    }
-
-    public LocationChangedEvent(LocationComponent newLocation, Vector3f oPosition, Quaternionfc oRotation,
-                                Quaternionfc nRotation) {
-        this(newLocation, oPosition, oRotation, newLocation.position, nRotation);
-    }
-
-    public LocationChangedEvent(LocationComponent nComponent, Vector3fc oPosition, Quaternionfc oRotation,
-                                Vector3fc nPosition, Quaternionfc nRotation) {
-        oldPosition = new Vector3f(oPosition);
-        oldRotation = new Quaternionf(oRotation);
-        newPosition = new Vector3f(nPosition);
-        newRotation = new Quaternionf(nRotation);
-        component = nComponent;
-    }
 
     public Vector3f vectorMoved(Vector3f dest) {
-        return oldPosition != null ? newPosition.sub(oldPosition, dest) : dest.set(0, 0, 0);
+        return currentPosition.sub(lastPosition, dest);
     }
 
     public float distanceMoved() {
-        return oldPosition != null ? newPosition.distance(oldPosition) : 0.0F;
+        return currentPosition.distance(lastPosition);
     }
 }
