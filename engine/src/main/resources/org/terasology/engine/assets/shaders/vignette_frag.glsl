1027052470baf9557302cdb76aa2c3313aa7e2a7
Michael Pollind <mpollind@gmail.com>
2021-05-26 21:51:59 -0700

    modify vignette frag

--- engine/src/main/resources/org/terasology/engine/assets/shaders/vignette_frag.glsl
+++ engine/src/main/resources/org/terasology/engine/assets/shaders/vignette_frag.glsl
@@ -13,12 +13,12 @@ uniform vec3 tint = vec3(1.0,1.0,1.0);
 #endif
 
 void main(){
-    vec4 color = texture2D(texScene, v_uv0.xy);
+    vec4 color = texture(texScene, v_uv0.xy);
 
     #ifdef VIGNETTE
-        float vig = texture2D(texVignette, v_uv0.xy).x;
+        float vig = texture(texVignette, v_uv0.xy).x;
         if (!swimming) {
-            color.rgb *= vec3(vig)+(1-vig)*tint.rgb;
+            color.rgb *= vec3(vig, vig, vig) + (1 - vig) * tint.rgb;
         } else {
             color.rgb *= vig * vig * vig;
             color.rgb *= inLiquidTint;
