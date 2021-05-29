d496022421bec8df4d85a58f30a3314989da6828
Tobias Nett <skaldarnar@googlemail.com>
2021-05-29 23:11:22 +0200

    doc: improve document of PlaySound and PlaySoundForOwner events
    These are just small additions, but I hope they make the behavior of these events a bit clearer and document what I have learned about them.

--- engine/src/main/java/org/terasology/engine/audio/events/PlaySoundEvent.java
+++ engine/src/main/java/org/terasology/engine/audio/events/PlaySoundEvent.java
@@ -8,19 +8,28 @@ import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.network.BroadcastEvent;
 
 /**
- * This event is sent whenever a sound is to be played.
- * The event is not sent to or played for the client of the entity that sent the event as they have simulated it already.
+ * A <i>trigger event</i> to play a sound.
+ * <p>
+ * The event is not sent to or played for the client of the entity that sent the event as they have simulated it
+ * already.
+ * <p>
+ * The target entities {@link org.terasology.engine.logic.location.LocationComponent LocationComponent} defines at which
+ * location (world coordinates) the sound is played at. If the entity does not have a location, the sound is played as
+ * global effect.
+ * <p>
+ * Note, that the volume is only adjusted for static sounds with <b>mono channel</b>.
  */
 @BroadcastEvent(skipInstigator = true)
 public class PlaySoundEvent extends AbstractPlaySoundEvent {
+    /**
+     * INTERNAL: Only required for internal replication of network events
+     */
     protected PlaySoundEvent() {
     }
-
 
     public PlaySoundEvent(StaticSound sound, float volume) {
         super(sound, volume);
     }
-
 
     public PlaySoundEvent(EntityRef exceptOwner, StaticSound sound, float volume) {
         super(exceptOwner, sound, volume);
