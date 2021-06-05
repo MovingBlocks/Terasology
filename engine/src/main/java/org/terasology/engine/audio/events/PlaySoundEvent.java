d19d352d657d7cfbe407c49c1d4d4e8890cc63ac
Tobias Nett <skaldarnar@googlemail.com>
2021-06-05 15:35:31 +0200

    doc: try to clarify broadcast and instigator for PlaySoundEvent

--- engine/src/main/java/org/terasology/engine/audio/events/PlaySoundEvent.java
+++ engine/src/main/java/org/terasology/engine/audio/events/PlaySoundEvent.java
@@ -10,8 +10,9 @@ import org.terasology.engine.network.BroadcastEvent;
 /**
  * A <i>trigger event</i> to play a sound.
  * <p>
- * The event is not sent to or played for the client of the entity that sent the event as they have simulated it
- * already.
+ * The event is {@link BroadcastEvent broadcasted} to all connected systems.
+ * If the {@code instigator}
+ * A client sending this event should have already simulated it locally.
  * <p>
  * The target entities {@link org.terasology.engine.logic.location.LocationComponent LocationComponent} defines at which
  * location (world coordinates) the sound is played at. If the entity does not have a location, the sound is played as
@@ -27,12 +28,30 @@ public class PlaySoundEvent extends AbstractPlaySoundEvent {
     protected PlaySoundEvent() {
     }
 
+    /**
+     * Create a new trigger event to play a sound on all connected clients.
+     *
+     * The instigator of this event is undefined ({@link EntityRef#NULL}).
+     *
+     * @param sound the sound to play; should be mono channel
+     * @param volume the volume of the sound effect in {@code [0.0, 1.0]}
+     */
     public PlaySoundEvent(StaticSound sound, float volume) {
         super(sound, volume);
     }
 
-    public PlaySoundEvent(EntityRef exceptOwner, StaticSound sound, float volume) {
-        super(exceptOwner, sound, volume);
+    /**
+     * Create a new trigger event to play a sound on all connected systems except for the instigating client.
+     *
+     * The instigator client is derived from the {@code instigator} entity by resolving the ownership relation.
+     * The <i>client entity</i> is defined by having the {@link org.terasology.engine.network.ClientComponent ClientComponent}.
+     *
+     * @param instigator an entity denoting the instigator system
+     * @param sound the sound to play; should be mono channel
+     * @param volume the volume of the sound effect in {@code [0.0, 1.0]}
+     */
+    public PlaySoundEvent(EntityRef instigator, StaticSound sound, float volume) {
+        super(instigator, sound, volume);
     }
 
 }
