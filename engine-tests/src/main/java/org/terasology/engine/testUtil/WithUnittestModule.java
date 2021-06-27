1cb62846df8229fb36c8291f0ac075a77b4f843f
Tobias Nett <skaldarnar@googlemail.com>
2021-06-27 13:42:16 +0200

    doc: add docstring for WithUnittestModule

--- engine-tests/src/main/java/org/terasology/engine/testUtil/WithUnittestModule.java
+++ engine-tests/src/main/java/org/terasology/engine/testUtil/WithUnittestModule.java
@@ -9,6 +9,17 @@ import org.terasology.engine.core.module.ModuleManager;
 import org.terasology.engine.core.subsystem.EngineSubsystem;
 import org.terasology.gestalt.module.Module;
 
+/**
+ * This subsystem registers the {@code unittest} module containing stub component classes with the {@link ModuleManager} before the engine
+ * is initialized.
+ * <p>
+ * It is indended to be used on the MTE side by adding it to the list of subsystems given to the builder.
+ * <p>
+ * NOTE: The usage of this subsystem for MTE is potentially fragile as it depends on specific assumptions on when subsystems are initialized.
+ * This makes things work because {@code Subsystem.initialize} is called at a useful place in the engine's boot
+ * sequence, but it's very possible that something else will call {@code ModuleManager.loadEnvironment} later — without including "unittest"
+ * in the environment — and then things might break with an unclear error again.
+ */
 public class WithUnittestModule implements EngineSubsystem {
     @Override
     public String getName() {
