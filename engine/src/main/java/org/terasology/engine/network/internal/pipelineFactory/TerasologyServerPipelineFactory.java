4de39ae1189580cd0e633130881bfc386a5096b2
Michael Pollind <mpollind@gmail.com>
2021-07-10 19:26:34 -0700

    perf: remove extra length prepend

--- engine/src/main/java/org/terasology/engine/network/internal/pipelineFactory/TerasologyServerPipelineFactory.java
+++ engine/src/main/java/org/terasology/engine/network/internal/pipelineFactory/TerasologyServerPipelineFactory.java
@@ -12,8 +12,6 @@ import io.netty.handler.codec.compression.Lz4FrameDecoder;
 import io.netty.handler.codec.compression.Lz4FrameEncoder;
 import io.netty.handler.codec.protobuf.ProtobufDecoder;
 import io.netty.handler.codec.protobuf.ProtobufEncoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32FrameDecoder;
-import io.netty.handler.codec.protobuf.ProtobufVarint32LengthFieldPrepender;
 import org.terasology.engine.network.internal.MetricRecordingHandler;
 import org.terasology.engine.network.internal.NetworkSystemImpl;
 import org.terasology.engine.network.internal.ServerConnectionHandler;
@@ -37,14 +35,12 @@ public class TerasologyServerPipelineFactory extends ChannelInitializer {
         ChannelPipeline p = ch.pipeline();
         p.addLast(MetricRecordingHandler.NAME, new MetricRecordingHandler());
 
-        p.addLast("lengthFrameDecoder", new LengthFieldBasedFrameDecoder(8388608, 0, 3, 0, 3));
         p.addLast("inflateDecoder", new Lz4FrameDecoder());
-        p.addLast("frameDecoder", new ProtobufVarint32FrameDecoder());
+        p.addLast("lengthFrameDecoder", new LengthFieldBasedFrameDecoder(8388608, 0, 3, 0, 3));
         p.addLast("protobufDecoder", new ProtobufDecoder(NetData.NetMessage.getDefaultInstance()));
 
-        p.addLast("frameLengthEncoder", new LengthFieldPrepender(3));
         p.addLast("deflateEncoder", new Lz4FrameEncoder(true));
-        p.addLast("frameEncoder", new ProtobufVarint32LengthFieldPrepender());
+        p.addLast("frameLengthEncoder", new LengthFieldPrepender(3));
         p.addLast("protobufEncoder", new ProtobufEncoder());
 
         p.addLast("authenticationHandler", new ServerHandshakeHandler());
