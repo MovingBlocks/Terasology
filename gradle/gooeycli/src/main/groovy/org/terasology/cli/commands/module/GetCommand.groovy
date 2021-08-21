031256785185095967ada4be6d4d3e4104314df8
Nail Khanipov <foxflameless@gmail.com>
2021-08-21 06:23:51 +0300

    feat: Add parallel module get (#4852)
    * reimplement algorithm. gather dependencies before git cloning.
    * parallel getting at x1.5-x4 faster then sequence getting

--- gradle/gooeycli/src/main/groovy/org/terasology/cli/commands/module/GetCommand.groovy
+++ gradle/gooeycli/src/main/groovy/org/terasology/cli/commands/module/GetCommand.groovy
@@ -3,9 +3,12 @@
 
 package org.terasology.cli.commands.module
 
+import groovy.io.FileType
 import org.eclipse.jgit.api.Git
 import org.terasology.cli.ModuleItem
 import org.terasology.cli.options.GitOptions
+import org.terasology.cli.util.Constants
+import org.terasology.cli.util.ModuleIndex
 import picocli.CommandLine
 import picocli.CommandLine.Command
 import picocli.CommandLine.Mixin
@@ -13,8 +16,7 @@ import picocli.CommandLine.Option
 import picocli.CommandLine.Parameters
 
 @Command(name = "get", description = "get module ")
-class GetCommand implements Runnable{
-    private Set<ModuleItem> fetchedModules = []
+class GetCommand implements Runnable {
 
     @Mixin
     GitOptions gitOptions
@@ -25,46 +27,68 @@ class GetCommand implements Runnable{
     @Parameters(paramLabel = "items", arity = "1", description = "Target item(s) to get")
     List<String> items = []
 
+    private static List<String> availableModuleIds() {
+        return availableModules()*.id
+    }
+
+    private static List<Object> availableModules() {
+        return ModuleIndex.instance.getData()
+    }
+
+    private static List<String> modulesAtModuleDir() {
+        def list = []
+        list << "engine"
+        Constants.ModuleDirectory.eachFile(FileType.DIRECTORIES) { file ->
+            list << file.getName()
+        }
+        return list as List<String>
+    }
+
+    private List<String> gatherAllDependencies(List<String> items) {
+        List<String> list = availableModules()
+                .findAll { it.id in items }
+                .collect { it.dependencies*.id }
+                .flatten()
+
+        if (list.empty) {
+            return items;
+        } else {
+            return items + list + gatherAllDependencies(list)
+        }
+    }
+
     @Override
     void run() {
         String origin = gitOptions.resolveOrigin()
-        for (module in items.collect({k -> new ModuleItem(k)})) {
-            def targetUrl = "https://github.com/${origin}/${module.name()}"
-            if (fetchedModules.contains(module)) {
-                continue
-            }
-            if (module.getDirectory().exists()) {
-                println CommandLine.Help.Ansi.AUTO.string("@|yellow already retrieved ${module.name()} - skipping|@")
-                continue
-            }
-            try {
-                Git.cloneRepository()
-                        .setURI(targetUrl)
-                        .setDirectory(module.getDirectory())
-                        .call()
-                println CommandLine.Help.Ansi.AUTO.string("@|green Retrieving module ${module.name()} from ${targetUrl}|@")
-                fetchedModules << module
-            } catch (Exception ex) {
-                println CommandLine.Help.Ansi.AUTO.string("@|red Unable to clone ${module.name()}, Skipping: ${ex.getMessage()} |@");
-                continue
-            }
 
-            ModuleItem.copyInTemplates(module)
-            if (recurse) {
-                ModuleItem[] dependencies = module.dependencies()
-                if (dependencies.length > 0) {
-                    ModuleItem[] uniqueDependencies = dependencies - fetchedModules
-                    println "After removing dupes already retrieved we have the remaining dependencies left: ${uniqueDependencies*.name()}"
-                    if (uniqueDependencies.length > 0) {
-                        GetCommand cmd = new GetCommand();
-                        cmd.gitOptions = this.gitOptions
-                        cmd.items = uniqueDependencies*.name()
-                        cmd.recurse = true
-                        cmd.fetchedModules = fetchedModules
-                        cmd.run()
+        def modulesCandidatesToReceive = availableModuleIds() - modulesAtModuleDir()
+        def requestedModules
+
+        if (recurse) {
+            requestedModules = gatherAllDependencies(items).unique()
+        } else {
+            requestedModules = items
+        }
+
+        def needsToReceive = requestedModules.intersect(modulesCandidatesToReceive)
+
+        needsToReceive.collect {
+            k -> new ModuleItem(k)
+        }.parallelStream()
+                .forEach { module ->
+                    def targetUrl = "https://github.com/${origin}/${module.name()}"
+                    try {
+
+                        Git.cloneRepository()
+                                .setURI(targetUrl)
+                                .setDirectory(module.getDirectory())
+                                .call()
+                        println CommandLine.Help.Ansi.AUTO.string("@|green Retrieving module ${module.name()} from ${targetUrl}|@")
+
+                        ModuleItem.copyInTemplates(module)
+                    } catch (Exception ex) {
+                        println CommandLine.Help.Ansi.AUTO.string("@|red Unable to clone ${module.name()}, Skipping: ${ex.getMessage()} |@");
                     }
                 }
-            }
-        }
     }
 }
