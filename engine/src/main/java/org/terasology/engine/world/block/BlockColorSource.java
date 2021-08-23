e61a930a742fffe17df02da3955aa5fe6b1a3345
Sam Dirkswager <sam@dirkback.com>
2021-08-23 09:56:17 -0500

    fix: enable block tinting in LOD chunks (#4847)
    Block tinting previously queried biome information from the WorldProvider. But LOD chunks are never added to the world, just meshed and rendered. All the necessary data is still there, so this switches to passing block tinting systems a ChunkView from which to derive necessary information, which really makes more sense anyway.

--- engine/src/main/java/org/terasology/engine/world/block/BlockColorSource.java
+++ engine/src/main/java/org/terasology/engine/world/block/BlockColorSource.java
@@ -3,6 +3,7 @@
 
 package org.terasology.engine.world.block;
 
+import org.terasology.engine.world.ChunkView;
 import org.terasology.nui.Colorc;
 
 /**
@@ -11,14 +12,17 @@ import org.terasology.nui.Colorc;
 @FunctionalInterface
 public interface BlockColorSource {
 
-    default Colorc calcColor() {
-        return calcColor(0, 0, 0);
+    default Colorc calcColor(ChunkView view) {
+        return calcColor(view, 0, 0, 0);
     };
 
-    default Colorc calcColor(int x, int z) {
-        return calcColor(x, 0, z);
+    default Colorc calcColor(ChunkView view, int x, int z) {
+        return calcColor(view, x, 0, z);
     };
 
-    Colorc calcColor(int x, int y, int z);
+    /**
+     * @return the color that should be applied at the coordinates {@code (x, y, z)} relative to {@code view}.
+     */
+    Colorc calcColor(ChunkView view, int x, int y, int z);
 
 }
