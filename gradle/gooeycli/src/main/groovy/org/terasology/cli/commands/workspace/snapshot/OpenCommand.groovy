24a5ff927219f7b776e55830f495465ef84b3473
DarkWeird <foxflameless@gmail.com>
2021-08-31 11:25:37 +0300

    feat(gooey-cli): Add support for headless systems to `./gw workspace snapshot open`

--- gradle/gooeycli/src/main/groovy/org/terasology/cli/commands/workspace/snapshot/OpenCommand.groovy
+++ gradle/gooeycli/src/main/groovy/org/terasology/cli/commands/workspace/snapshot/OpenCommand.groovy
@@ -9,19 +9,36 @@ import picocli.CommandLine.Parameters
 
 import java.awt.*
 
-@Command(name = "open", description = "open snapshot file")
+@Command(name = "open", description = [
+        "open snapshot file in your editor",
+        "Opens at GUI, if your environment supports it, Opens with \$EDITOR otherwise"])
 class OpenCommand implements Runnable {
     @Parameters(paramLabel = "items", arity = "1", description = "tag snapshot with name")
     String name = ""
 
     @Override
     void run() {
-        Desktop desktop = Desktop.getDesktop()
-        Snapshot.find(name).ifPresentOrElse({ it ->
-            desktop.open(it.file)
-        }, {
-            println "failed to resolve snapshot: ${name}"
-        })
+        if (Desktop.desktopSupported && !GraphicsEnvironment.isHeadless()) {
+            Desktop desktop = Desktop.getDesktop()
+            Snapshot.find(name).ifPresentOrElse({ it ->
+                desktop.open(it.file)
+            }, {
+                println "failed to resolve snapshot: ${name}"
+            })
+        } else {
+            Snapshot.find(name).ifPresentOrElse({ it ->
+                String editor = System.getenv('EDITOR')
+                if (editor != null) {
+                    println "$editor $it.file"
+                    ProcessBuilder pb = new ProcessBuilder(editor, it.file.getAbsolutePath())
+                    pb.inheritIO().start().waitFor()
 
+                } else {
+                    println "Cannot determinate editor. Please set EDITOR to environment variables."
+                }
+            }, {
+                println "failed to resolve snapshot: ${name}"
+            })
+        }
     }
 }
