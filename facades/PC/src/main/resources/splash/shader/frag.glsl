e78a7c8ce5accbe26015332bc55e0efbadc94180
Michael Pollind <mpollind@gmail.com>
2021-09-05 16:18:18 -0700

    bugfix: migrate deprecated texture2D
    ref: https://github.com/Terasology/CoreRendering/pull/72

--- facades/PC/src/main/resources/splash/shader/frag.glsl
+++ facades/PC/src/main/resources/splash/shader/frag.glsl
@@ -6,6 +6,6 @@ varying vec2 textureCoord;
 uniform sampler2D texImage;
 
 void main() {
-    vec4 textureColor = texture2D(texImage, textureCoord);
+    vec4 textureColor = texture(texImage, textureCoord);
     gl_FragColor = vertexColor * textureColor;
 }
