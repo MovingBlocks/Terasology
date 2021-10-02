7d41b978452061aaa919cc0d4fa3ab772cac65b3
Cervator <cervator@gmail.com>
2021-10-02 17:30:03 -0500

    fix: Avoid a couple cases where no NUIManager would cause trouble.
    Bonus: filters out typical virtual NICs from listening

--- engine/src/main/java/org/terasology/engine/core/modes/loadProcesses/StartServer.java
+++ engine/src/main/java/org/terasology/engine/core/modes/loadProcesses/StartServer.java
@@ -3,15 +3,19 @@
 
 package org.terasology.engine.core.modes.loadProcesses;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.terasology.engine.config.Config;
 import org.terasology.engine.context.Context;
 import org.terasology.engine.core.modes.SingleStepLoadProcess;
+import org.terasology.engine.core.modes.StateLoading;
 import org.terasology.engine.network.NetworkSystem;
 import org.terasology.engine.network.exceptions.HostingFailedException;
 import org.terasology.engine.rendering.nui.NUIManager;
 import org.terasology.engine.rendering.nui.layers.mainMenu.MessagePopup;
 
 public class StartServer extends SingleStepLoadProcess {
+    private static final Logger logger = LoggerFactory.getLogger(StartServer.class);
 
     private final Context context;
     private final boolean dedicated;
@@ -36,8 +40,14 @@ public class StartServer extends SingleStepLoadProcess {
             int port = config.getNetwork().getServerPort();
             context.get(NetworkSystem.class).host(port, dedicated);
         } catch (HostingFailedException e) {
-            context.get(NUIManager.class).pushScreen(MessagePopup.ASSET_URI, MessagePopup.class).setMessage("Failed to Host",
-                    e.getMessage() + " - Reverting to single player");
+            NUIManager nui = context.get(NUIManager.class);
+            if (nui != null) {
+                nui.pushScreen(MessagePopup.ASSET_URI, MessagePopup.class).setMessage("Failed to Host",
+                        e.getMessage() + " - Reverting to single player");
+            } else {
+                logger.error("Failed to Host. NUI was also unavailable for warning popup (headless?)", e);
+                throw new RuntimeException("Cannot host game successfully from likely headless start, terminating");
+            }
         }
         return true;
     }
