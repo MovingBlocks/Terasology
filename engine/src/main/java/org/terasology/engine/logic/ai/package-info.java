108a64d4e39c35f4135044f2db2aa22493773783
Michael Pollind <polli104@mail.chapman.edu>
2021-10-11 09:30:26 -0700

    chore: remove internal ai system for engine (#4878)

--- engine/src/main/java/org/terasology/engine/logic/ai/package-info.java
+++
@@ -1,6 +0,0 @@
-// Copyright 2021 The Terasology Foundation
-// SPDX-License-Identifier: Apache-2.0
-
-@API package org.terasology.engine.logic.ai;
-
-import org.terasology.gestalt.module.sandbox.API;
