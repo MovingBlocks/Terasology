8e57d792f715071cff381fd0b06d27946ccdec1a
Kevin Turner <83819+keturn@users.noreply.github.com>
2021-11-13 14:44:36 -0800

    doc(MonitoringSubsystem): document micrometer-related methods

--- engine/src/main/java/org/terasology/engine/core/subsystem/common/MonitoringSubsystem.java
+++ engine/src/main/java/org/terasology/engine/core/subsystem/common/MonitoringSubsystem.java
@@ -77,7 +77,6 @@ public class MonitoringSubsystem implements EngineSubsystem {
         }, Clock.SYSTEM);
         Metrics.addRegistry(jmxMeterRegistry);
 
-
         // If we want to make global metrics available to our custom view,
         // we add our custom registry to the global composite:
         //
