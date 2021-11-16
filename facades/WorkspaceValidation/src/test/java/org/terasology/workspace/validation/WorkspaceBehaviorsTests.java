fb1b63f04757a2a292f428f62b1a2b3006d64f7f
darkweird <foxflameless@gmail.com>
2021-11-16 10:37:54 +0300

    feature(Workspace-wide tests): refactor WorkspaceBehaviorsTests. decompose functions and provide comment strings.

--- facades/WorkspaceValidation/src/test/java/org/terasology/workspace/validation/WorkspaceBehaviorsTests.java
+++ facades/WorkspaceValidation/src/test/java/org/terasology/workspace/validation/WorkspaceBehaviorsTests.java
@@ -3,7 +3,6 @@
 
 package org.terasology.workspace.validation;
 
-import com.google.common.collect.Streams;
 import com.google.common.io.ByteStreams;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DynamicContainer;
@@ -19,15 +18,19 @@ import org.terasology.gestalt.assets.Asset;
 import org.terasology.gestalt.assets.AssetData;
 import org.terasology.gestalt.assets.ResourceUrn;
 import org.terasology.gestalt.assets.management.AssetManager;
+import org.terasology.gestalt.entitysystem.component.Component;
 import org.terasology.gestalt.module.Module;
 import org.terasology.gestalt.module.resources.ModuleFileSource;
+import org.terasology.gestalt.naming.Name;
 import org.terasology.moduletestingenvironment.Engines;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
@@ -35,65 +38,102 @@ public class WorkspaceBehaviorsTests {
 
     @TestFactory
     Stream<DynamicNode> behaviours() {
-        System.setProperty(ModuleManager.LOAD_CLASSPATH_MODULES_PROPERTY, "true");
-        ModuleManager temporary = new ModuleManager("");
-        return temporary.getRegistry()
+        return template(
+                // Filter modules
+                haveAsset("behaviors").or(havePrefab(BehaviorComponent.class)),
+                // Create assetTests
+                assetManagerRef -> Stream.of(
+                        asset(
+                                assetManagerRef,
+                                BehaviorTree.class,
+                                (b) -> Assertions.assertNotNull(b.getData())
+                        ),
+                        asset(
+                                assetManagerRef,
+                                Prefab.class,
+                                prefab -> prefab.hasComponent(BehaviorComponent.class),
+                                prefab -> Assertions.assertNotNull(prefab.getComponent(BehaviorComponent.class).tree)
+
+                        ))
+        );
+    }
+
+
+    Stream<DynamicNode> template(
+            BiPredicate<ModuleManager, Name> moduleFilter,
+            Function<AtomicReference<AssetManager>,
+                    Stream<DynamicNode>> testsCreator) {
+        ModuleManager moduleManager = createModuleManager();
+        return moduleManager.getRegistry()
                 .getModuleIds()
                 .stream()
-                .filter(name -> {
-                    Module module = temporary.getRegistry().getLatestModuleVersion(name);
-                    ModuleFileSource resources = module.getResources();
-
-                    boolean haveBehaviors = !resources.getFilesInPath(true, "assets/behaviors").isEmpty();
-                    boolean haveBehaviorOverride =
-                            resources.getFilesInPath(false, "overrides")
-                                    .stream()
-                                    .anyMatch(fr -> fr.getPath().contains("behaviors"));
-                    boolean haveBehaviorDelta =
-                            resources.getFilesInPath(false, "deltas")
-                                    .stream()
-                                    .anyMatch(fr -> fr.getPath().contains("behaviors"));
-
-                    boolean havePrefab = resources.getFiles().stream()
-                            .filter(fr -> fr.toString().contains("behaviours") && fr.toString().contains(".prefab"))
-                            .anyMatch(fr -> {
-                                try (InputStream inputStream = fr.open()) {
-                                    byte[] bytes = ByteStreams.toByteArray(inputStream);
-                                    String content = new String(bytes, TerasologyConstants.CHARSET);
-                                    return content.contains(BehaviorComponent.class.getSimpleName());
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                    return false;
-                                }
-                            });
-
-                    return haveBehaviors || haveBehaviorOverride || haveBehaviorDelta || havePrefab;
-                })
+                // use filter over modules
+                .filter(name -> moduleFilter.test(moduleManager, name))
+                // create tests
                 .map(moduleName -> {
+                    // Create engine
                     EnginesAccessor engine = new EnginesAccessor(Set.of(moduleName.toString()), null);
+                    // Provide `Lazy` reference for asset tests
                     AtomicReference<AssetManager> assetManagerRef = new AtomicReference<>();
-                    return DynamicContainer.dynamicContainer(String.format("Module - %s", moduleName), Streams.concat(
-                            Stream.of(engine).map((e) ->
+                    return DynamicContainer.dynamicContainer(String.format("Module - %s", moduleName),
+                            Stream.of(
+                                    // engine test
                                     DynamicTest.dynamicTest("setup", () -> {
-                                        e.setup();
-                                        assetManagerRef.set(e.getHostContext().get(AssetManager.class));
-                                    })
-                            ),
-                            Stream.of(DynamicContainer.dynamicContainer("Assets tests", Stream.of(
-                                    asset(
-                                            assetManagerRef,
-                                            BehaviorTree.class,
-                                            (b) -> Assertions.assertNotNull(b.getData())
-                                    ),
-                                    asset(
-                                            assetManagerRef,
-                                            Prefab.class,
-                                            prefab -> prefab.hasComponent(BehaviorComponent.class),
-                                            prefab -> Assertions.assertNotNull(prefab.getComponent(BehaviorComponent.class).tree)
-
-                                    )))),
-                            Stream.of(DynamicTest.dynamicTest("tearDown", engine::tearDown))));
+                                        engine.setup();
+                                        assetManagerRef.set(engine.getHostContext().get(AssetManager.class));
+                                    }),
+                                    // Container with asset tests
+                                    DynamicContainer.dynamicContainer("Assets tests", testsCreator.apply(assetManagerRef)),
+                                    // engine teardown test
+                                    DynamicTest.dynamicTest("tearDown", engine::tearDown)));
                 });
+    }
+
+    private ModuleManager createModuleManager() {
+        System.setProperty(ModuleManager.LOAD_CLASSPATH_MODULES_PROPERTY, "true");
+        return new ModuleManager("");
+    }
+
+    private BiPredicate<ModuleManager, Name> haveAsset(String assetName) {
+        return (moduleManager, name) -> {
+            Module module = moduleManager.getRegistry().getLatestModuleVersion(name);
+            ModuleFileSource resources = module.getResources();
+
+            boolean haveAsset = !resources.getFilesInPath(true, "assets/" + assetName).isEmpty();
+
+            boolean haveAssetOverride =
+                    resources.getFilesInPath(false, "overrides")
+                            .stream()
+                            .anyMatch(fr -> fr.getPath().contains(assetName));
+            boolean haveAssetDelta =
+                    resources.getFilesInPath(false, "deltas")
+                            .stream()
+                            .anyMatch(fr -> fr.getPath().contains(assetName));
+
+            return haveAsset || haveAssetOverride || haveAssetDelta;
+        };
+    }
+
+
+    private <T extends Component<T>> BiPredicate<ModuleManager, Name> havePrefab(Class<T> componentClass) {
+        return (moduleManager, name) -> {
+            Module module = moduleManager.getRegistry().getLatestModuleVersion(name);
+            ModuleFileSource resources = module.getResources();
+
+            return resources.getFiles().stream()
+                    .filter(fr -> fr.toString().contains("prefabs") && fr.toString().contains(".prefab"))
+                    .anyMatch(fr -> {
+                        try (InputStream inputStream = fr.open()) {
+                            byte[] bytes = ByteStreams.toByteArray(inputStream);
+                            String content = new String(bytes, TerasologyConstants.CHARSET);
+                            String componentName = componentClass.getSimpleName();
+                            return content.contains(componentName);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                            return false;
+                        }
+                    });
+        };
     }
 
     private <T extends AssetData, A extends Asset<T>> DynamicContainer asset(AtomicReference<AssetManager> assetManager,
@@ -110,19 +150,21 @@ public class WorkspaceBehaviorsTests {
 
         return DynamicContainer.dynamicContainer(String.format("AssetType: %s", assetClazz.getSimpleName()),
                 DynamicTest.stream(
+                        // Special generator for 1 value for `lazy` getting.
                         Stream.generate(() -> assetManager.get().getAvailableAssets(assetClazz).stream())
                                 .limit(1)
                                 .flatMap(s -> s)
                                 .filter(urn -> filter.test(assetManager.get().getAsset(urn, assetClazz).get())),
                         ResourceUrn::toString,
                         urn -> {
-                            A asset = (A) assetManager.get().getAsset(urn, assetClazz).get();
+                            A asset = assetManager.get().getAsset(urn, assetClazz).get();
                             validator.accept(asset);
                         }
                 ));
 
     }
 
+    // Just grant access to protected methods.
     public static class EnginesAccessor extends Engines {
 
         public EnginesAccessor(Set<String> dependencies, String worldGeneratorUri) {
