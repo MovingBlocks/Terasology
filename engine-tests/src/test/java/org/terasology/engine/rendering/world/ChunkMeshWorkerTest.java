e032131dc5b9cb70cc9fb4c2032bb545f2dbc420
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-01-19 19:21:39 -0800

    test(DummyChunk): stub implementation of Chunk

--- engine-tests/src/test/java/org/terasology/engine/rendering/world/ChunkMeshWorkerTest.java
+++ engine-tests/src/test/java/org/terasology/engine/rendering/world/ChunkMeshWorkerTest.java
@@ -3,43 +3,56 @@
 
 package org.terasology.engine.rendering.world;
 
+import org.joml.Vector3i;
+import org.joml.Vector3ic;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.terasology.engine.rendering.primitives.ChunkMesh;
 import org.terasology.engine.world.chunks.Chunk;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class ChunkMeshWorkerTest {
+    static Vector3ic position0 = new Vector3i(123, 456, 789);
+
     ChunkMeshWorker worker;
+
+    static Optional<Tuple2<Chunk, ChunkMesh>> alwaysCreateMesh(Chunk chunk) {
+        return Optional.of(Tuples.of(chunk, mock(ChunkMesh.class)));
+    }
 
     @BeforeEach
     void makeWorker() {
-        worker = new ChunkMeshWorker(null, null, null);
+        worker = new ChunkMeshWorker(ChunkMeshWorkerTest::alwaysCreateMesh, null);
     }
 
     @Test
     void testChunkIsNotProcessedTwice() {
-        var chunk1 = mock(Chunk.class);
-        var chunk2 = mock(Chunk.class);
-        // FIXME: give both chunks the same .position
+        var chunk1 = new DummyChunk(position0);
+        var chunk2 = new DummyChunk(position0);
         worker.add(chunk1);
         worker.add(chunk2);
         // drain
-        // assert number of results == 1
+        fail("TODO: assert number of results == 1");
     }
 
     @Test
     void testChunkIsRegeneratedIfDirty() {
-        var chunk = mock(Chunk.class);
+        var chunk = new DummyChunk(position0);
         worker.add(chunk);
         // tick - work function has started
         chunk.setDirty(true);
         worker.add(chunk);
         // drain
-        // assert number of results == 2
+        fail("TODO: assert number of results == 2");
     }
 
     @Test
@@ -51,51 +64,65 @@ public class ChunkMeshWorkerTest {
 
     @Test
     void testChunkCanBeRemovedBeforeMeshGeneration() {
-        var chunk = mock(Chunk.class);
+        var chunk = new DummyChunk(position0);
         worker.add(chunk);
         worker.remove(chunk);
         // drain
-        // assert no work happened on chunk
+        fail("TODO: assert no work happened on chunk");
+    }
+
+    @Test
+    void testDoubleRemoveIsNoProblem() {
+        var chunk = new DummyChunk(position0);
+        worker.add(chunk);
+        worker.remove(chunk);
+        worker.remove(chunk);
+        // drain
     }
 
     @Test
     void testChunkCanBeRemovedBeforeUpload() {
-        var chunk = mock(Chunk.class);
+        var chunk = new DummyChunk(position0);
         worker.add(chunk);
         // tick so generation is finished, but not upload
         worker.remove(chunk);
         // drain
-        // assert upload did not happen for chunk
+        fail("assert upload did not happen for chunk");
     }
 
     @Test
     void testChunkCanBeRemovedByPosition() {
+        var chunk = new DummyChunk(position0);
+        worker.add(chunk);
+        worker.remove(position0);
+        // drain
+        fail("TODO: assert no work happened on chunk");
     }
 
     @Test
     void testWorkIsPrioritized() {
-        var nearChunk = mock(Chunk.class);
-        var farChunk = mock(Chunk.class);
+        var nearChunk = new DummyChunk(position0);
+        var farChunk = new DummyChunk(new Vector3i(position0).add(100, 0, 0));
 
         worker.add(farChunk);
         worker.add(nearChunk);
         // …add a few more so the result isn't just a coin toss.
 
         // tick
-        // assert first one through the gate was nearChunk
+        fail("TODO: assert first one through the gate was nearChunk");
 
         // change the state of the comparator
         // assert the next one through the gate is the one closest *now*
     }
 
-
     @Test
     void testWorkerStopsWhenShutDown() {
+        fail("TODO: add shutdown method");
     }
 
     @Test
     void testSomethingAboutTheUpdateMethod() {
-        // FIXME: What does this do, and what needs testing?
+        fail("FIXME: What does this do, and what needs testing?");
     }
 
     // What else? More parallelization tests?
---
+++ engine-tests/src/test/java/org/terasology/engine/rendering/world/DummyChunk.java
@@ -0,0 +1,220 @@
+// Copyright 2022 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+
+package org.terasology.engine.rendering.world;
+
+import org.joml.Vector3i;
+import org.joml.Vector3ic;
+import org.terasology.engine.rendering.primitives.ChunkMesh;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockRegionc;
+import org.terasology.engine.world.chunks.Chunk;
+import org.terasology.engine.world.chunks.ChunkBlockIterator;
+import org.terasology.joml.geom.AABBfc;
+import org.terasology.protobuf.EntityData;
+
+public class DummyChunk implements Chunk {
+    private final Vector3ic chunkPos;
+    private boolean dirty;
+    private ChunkMesh mesh;
+    private boolean ready;
+
+    public DummyChunk(Vector3ic position) {
+        this.chunkPos = position;
+    }
+
+    @Override
+    public Vector3ic getPosition() {
+        return chunkPos;
+    }
+
+    @Override
+    public Block getBlock(int x, int y, int z) {
+        return null;
+    }
+
+    @Override
+    public Block setBlock(int x, int y, int z, Block block) {
+        return null;
+    }
+
+    @Override
+    public void setExtraData(int index, int x, int y, int z, int value) {
+
+    }
+
+    @Override
+    public int getExtraData(int index, int x, int y, int z) {
+        return 0;
+    }
+
+    @Override
+    public Vector3i getChunkWorldOffset(Vector3i pos) {
+        return null;
+    }
+
+    @Override
+    public int getChunkWorldOffsetX() {
+        return 0;
+    }
+
+    @Override
+    public int getChunkWorldOffsetY() {
+        return 0;
+    }
+
+    @Override
+    public int getChunkWorldOffsetZ() {
+        return 0;
+    }
+
+    @Override
+    public Vector3i chunkToWorldPosition(int x, int y, int z, Vector3i dest) {
+        return null;
+    }
+
+    @Override
+    public int chunkToWorldPositionX(int x) {
+        return 0;
+    }
+
+    @Override
+    public int chunkToWorldPositionY(int y) {
+        return 0;
+    }
+
+    @Override
+    public int chunkToWorldPositionZ(int z) {
+        return 0;
+    }
+
+    @Override
+    public BlockRegionc getRegion() {
+        return null;
+    }
+
+    @Override
+    public byte getSunlight(int x, int y, int z) {
+        return 0;
+    }
+
+    @Override
+    public boolean setSunlight(int x, int y, int z, byte amount) {
+        return false;
+    }
+
+    @Override
+    public byte getSunlightRegen(int x, int y, int z) {
+        return 0;
+    }
+
+    @Override
+    public boolean setSunlightRegen(int x, int y, int z, byte amount) {
+        return false;
+    }
+
+    @Override
+    public byte getLight(int x, int y, int z) {
+        return 0;
+    }
+
+    @Override
+    public boolean setLight(int x, int y, int z, byte amount) {
+        return false;
+    }
+
+    @Override
+    public int getEstimatedMemoryConsumptionInBytes() {
+        return 0;
+    }
+
+    @Override
+    public ChunkBlockIterator getBlockIterator() {
+        return null;
+    }
+
+    @Override
+    public void markReady() {
+        ready = true;
+    }
+
+    @Override
+    public AABBfc getAABB() {
+        return null;
+    }
+
+    @Override
+    public void setMesh(ChunkMesh newMesh) {
+        this.mesh = newMesh;
+    }
+
+    @Override
+    public void setAnimated(boolean animated) {
+
+    }
+
+    @Override
+    public boolean isAnimated() {
+        return false;
+    }
+
+    @Override
+    public boolean hasMesh() {
+        return mesh != null;
+    }
+
+    @Override
+    public ChunkMesh getMesh() {
+        return mesh;
+    }
+
+    @Override
+    public void disposeMesh() {
+        mesh = null;
+    }
+
+    @Override
+    public boolean isReady() {
+        return ready;
+    }
+
+    @Override
+    public void deflate() {
+
+    }
+
+    @Override
+    public void deflateSunlight() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public boolean isDisposed() {
+        return false;
+    }
+
+    @Override
+    public void prepareForReactivation() {
+
+    }
+
+    @Override
+    public EntityData.ChunkStore.Builder encode() {
+        return null;
+    }
+
+    @Override
+    public boolean isDirty() {
+        return dirty;
+    }
+
+    @Override
+    public void setDirty(boolean dirty) {
+        this.dirty = dirty;
+    }
+}
