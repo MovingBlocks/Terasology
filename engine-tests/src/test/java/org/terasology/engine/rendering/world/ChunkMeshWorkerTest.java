3db427f9b44cf32ebc0c679ab04969491e420206
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-05-05 18:58:52 -0700

    test(ChunkMeshWorker): fix testMultipleChunks so the results are order-independent
    Because they run in parallel, if there are only two of them, either one may return first.

--- engine-tests/src/test/java/org/terasology/engine/rendering/world/ChunkMeshWorkerTest.java
+++ engine-tests/src/test/java/org/terasology/engine/rendering/world/ChunkMeshWorkerTest.java
@@ -16,24 +16,33 @@ import org.terasology.engine.world.chunks.RenderableChunk;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 import reactor.test.StepVerifier;
+import reactor.test.scheduler.VirtualTimeScheduler;
+import reactor.test.subscriber.TestSubscriber;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
 import java.time.Duration;
 import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
 
+import static com.google.common.truth.Truth.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.mock;
 
 @ExtendWith(MockitoExtension.class)
 public class ChunkMeshWorkerTest {
+    static final Duration EXPECTED_DURATION = Duration.ofSeconds(4);
+
     static Vector3ic position0 = new Vector3i(123, 456, 789);
 
     final Vector3i currentPosition = new Vector3i(position0);
 
+    Comparator<RenderableChunk> comparator = Comparator.comparingDouble(chunk ->
+            chunk.getRenderPosition().distanceSquared(currentPosition.x, currentPosition.y, currentPosition.z)
+    );
     ChunkMeshWorker worker;
-    Comparator<RenderableChunk> comparator;
-    StepVerifier.FirstStep<Chunk> verifier;
+    StepVerifier.Step<Chunk> verifier;
 
     static Mono<Tuple2<Chunk, ChunkMesh>> alwaysCreateMesh(Chunk chunk) {
         chunk.setDirty(false);
@@ -42,10 +51,6 @@ public class ChunkMeshWorkerTest {
 
     @BeforeEach
     void makeWorker() {
-        comparator = Comparator.comparingDouble(chunk ->
-                chunk.getRenderPosition().distanceSquared(currentPosition.x, currentPosition.y, currentPosition.z)
-        );
-
         StepVerifier.setDefaultTimeout(Duration.ofSeconds(10));
 
         // Use virtual time so we don't have to wait around in real time
@@ -69,18 +74,40 @@ public class ChunkMeshWorkerTest {
         return chunk;
     }
 
+    protected List<Chunk> getChunksThatResultFrom(Consumer<ChunkMeshWorker> workFunction) {
+        var scheduler = VirtualTimeScheduler.create();
+
+        var workerB = new ChunkMeshWorker(
+                ChunkMeshWorkerTest::alwaysCreateMesh,
+                comparator,
+                scheduler,
+                scheduler
+        );
+
+        var completed = workerB.getCompletedChunks()
+                .subscribeWith(TestSubscriber.create());
+
+        workFunction.accept(workerB);
+
+        // The Worker doesn't mark the flux as complete; it expects it'll still get more work.
+        // That means we can't collect the the complete flux in to a list.
+        // Instead, we use TestSubscriber's methods to see what it has output so far.
+        scheduler.advanceTimeBy(EXPECTED_DURATION);
+        return completed.getReceivedOnNext();
+    }
+
     @Test
     void testMultipleChunks() {
         var chunk1 = newDirtyChunk(position0);
         var chunk2 = newDirtyChunk(new Vector3i(position0).add(1, 0, 0));
 
-        verifier.then(() -> {
-                    worker.add(chunk1);
-                    worker.add(chunk2);
-                    worker.update();
-                })
-                .expectNext(chunk1, chunk2)
-                .verifyTimeout(Duration.ofSeconds(5));
+        var resultingChunks = getChunksThatResultFrom(worker -> {
+            worker.add(chunk1);
+            worker.add(chunk2);
+            worker.update();
+        });
+
+        assertThat(resultingChunks).containsExactly(chunk1, chunk2);
     }
 
     @Test
@@ -163,16 +190,14 @@ public class ChunkMeshWorkerTest {
         var nearChunk = newDirtyChunk(position0);
         var farChunk = newDirtyChunk(new Vector3i(position0).add(100, 0, 0));
 
-        verifier.then(() -> {
-                    worker.add(farChunk);
-                    worker.add(nearChunk);
-                    worker.update();
-                })
-                // TODO: this may be flaky due to parallelization.
-                //   Given a scheduler with N threads, we should test it with more than N chunks.
-                .expectNext(nearChunk)
-                .expectNext(farChunk)
-                .verifyTimeout(Duration.ofSeconds(5));
+        var completed = getChunksThatResultFrom(worker -> {
+            worker.add(farChunk);
+            worker.add(nearChunk);
+            worker.update();
+        });
+        // TODO: this may be flaky due to parallelization.
+        //   Given a scheduler with N threads, we should test it with more than N chunks.
+        assertThat(completed).containsExactly(nearChunk, farChunk).inOrder();
 
         // TODO: change the state of the comparator
         // assert the next one through the gate is the one closest *now*
