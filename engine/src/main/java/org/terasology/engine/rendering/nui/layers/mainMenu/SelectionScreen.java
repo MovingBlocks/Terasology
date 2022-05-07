14a6d88099a06bfe46254afb592bfcb965558315
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-05-07 13:04:16 -0700

    fix(SelectionScreen): be more robust in the face of save files with incomplete manifests
    Fixes #4912

--- engine/src/main/java/org/terasology/engine/rendering/nui/layers/mainMenu/SelectionScreen.java
+++ engine/src/main/java/org/terasology/engine/rendering/nui/layers/mainMenu/SelectionScreen.java
@@ -1,13 +1,13 @@
-// Copyright 2021 The Terasology Foundation
+// Copyright 2022 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.engine.rendering.nui.layers.mainMenu;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.terasology.gestalt.assets.ResourceUrn;
 import org.terasology.engine.config.Config;
 import org.terasology.engine.config.PlayerConfig;
 import org.terasology.engine.core.TerasologyConstants;
+import org.terasology.engine.game.GameManifest;
 import org.terasology.engine.i18n.TranslationSystem;
 import org.terasology.engine.persistence.internal.GamePreviewImageProvider;
 import org.terasology.engine.registry.In;
@@ -20,6 +20,8 @@ import org.terasology.engine.utilities.Assets;
 import org.terasology.engine.utilities.FilesUtil;
 import org.terasology.engine.world.generator.internal.WorldGeneratorInfo;
 import org.terasology.engine.world.generator.internal.WorldGeneratorManager;
+import org.terasology.engine.world.internal.WorldInfo;
+import org.terasology.gestalt.assets.ResourceUrn;
 import org.terasology.gestalt.naming.Name;
 import org.terasology.gestalt.naming.NameVersion;
 import org.terasology.nui.widgets.UIImage;
@@ -78,22 +80,28 @@ public abstract class SelectionScreen extends CoreScreenLayer {
             return;
         }
 
-        final WorldGeneratorInfo wgi = worldGeneratorManager.getWorldGeneratorInfo(
-                gameInfo.getManifest()
-                .getWorldInfo(TerasologyConstants.MAIN_WORLD)
-                .getWorldGenerator());
+        GameManifest manifest = gameInfo.getManifest();
+        WorldInfo worldInfo = manifest.getWorldInfo(TerasologyConstants.MAIN_WORLD);
 
         String mainWorldGenerator = "ERROR: world generator ";
-        if (wgi != null) {
-            mainWorldGenerator = wgi.getDisplayName();
+        if (worldInfo == null) {
+            logger.warn("Could not get MAIN_WORLD for {} with worlds {}",
+                    manifest, manifest.getWorldInfoMap().keySet());
+            mainWorldGenerator = "ERROR: no main world";
         } else {
-            mainWorldGenerator = mainWorldGenerator + gameInfo.getManifest()
-                    .getWorldInfo(TerasologyConstants.MAIN_WORLD)
-                    .getWorldGenerator()
-                    .toString() + " not found";
+            final WorldGeneratorInfo wgi = worldGeneratorManager.getWorldGeneratorInfo(
+                    worldInfo.getWorldGenerator());
+
+            if (wgi != null) {
+                mainWorldGenerator = wgi.getDisplayName();
+            } else {
+                mainWorldGenerator = mainWorldGenerator + worldInfo
+                        .getWorldGenerator()
+                        .toString() + " not found";
+            }
         }
 
-        final String commaSeparatedModules = gameInfo.getManifest()
+        final String commaSeparatedModules = manifest
                 .getModules()
                 .stream()
                 .map(NameVersion::getName)
@@ -120,7 +128,7 @@ public abstract class SelectionScreen extends CoreScreenLayer {
                         try {
                             textureData = AWTTextureFormat.convertToTextureData(buffImage, Texture.FilterMode.LINEAR);
                         } catch (IOException e) {
-                            logger.error("Converting preview image to texture data {} failed", e);
+                            logger.error("Converting preview image to texture data failed", e);
                             return null;
                         }
                         return Assets.generateAsset(new ResourceUrn(PREVIEW_IMAGE_URI_PATTERN + bufferedImages.indexOf(buffImage)),
