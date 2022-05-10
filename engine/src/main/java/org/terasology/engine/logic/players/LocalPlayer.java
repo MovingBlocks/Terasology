MovingBlocks/Terasology

commit c8a53b58777a1c0f7b638c087492c105f8954d45

chore: remove finite check from LocalPlayer (#4795)

engine/src/main/java/org/terasology/engine/logic/players/LocalPlayer.java
@@ -9,11 +9,11 @@ import org.terasology.engine.entitySystem.entity.EntityRef;
 import org.terasology.engine.logic.characters.CharacterComponent;
 import org.terasology.engine.logic.characters.CharacterMovementComponent;
 import org.terasology.engine.logic.characters.CharacterSystem;
+import org.terasology.engine.logic.characters.events.ActivationPredicted;
+import org.terasology.engine.logic.characters.events.ActivationRequest;
 import org.terasology.engine.logic.common.ActivateEvent;
 import org.terasology.engine.logic.common.RangeComponent;
 import org.terasology.engine.logic.location.LocationComponent;
-import org.terasology.engine.logic.characters.events.ActivationPredicted;
-import org.terasology.engine.logic.characters.events.ActivationRequest;
 import org.terasology.engine.math.Direction;
 import org.terasology.engine.network.ClientComponent;
 import org.terasology.engine.physics.HitResult;
@@ -100,13 +100,7 @@ public class LocalPlayer {
      */
     public Vector3f getPosition(Vector3f dest) {
         LocationComponent location = getCharacterEntity().getComponent(LocationComponent.class);
-        if (location != null) {
-            Vector3f result = location.getWorldPosition(new Vector3f());
-            if (result.isFinite()) { //TODO: MP finite check seems to hide a larger underlying problem
-                dest.set(result);
-            }
-        }
-        return dest;
+        return location.getWorldPosition(dest);
     }
 
     /**
@@ -117,13 +111,7 @@ public class LocalPlayer {
      */
     public Quaternionf getRotation(Quaternionf dest) {
         LocationComponent location = getCharacterEntity().getComponent(LocationComponent.class);
-        if (location != null) {
-            Quaternionf result = location.getWorldRotation(new Quaternionf());
-            if (result.isFinite()) { //TODO: MP finite check seems to hide a larger underlying problem
-                dest.set(result);
-            }
-        }
-        return dest;
+        return location.getWorldRotation(dest);
     }
 
     /**
@@ -134,18 +122,8 @@ public class LocalPlayer {
      */
     public Vector3f getViewPosition(Vector3f dest) {
         ClientComponent clientComponent = getClientEntity().getComponent(ClientComponent.class);
-        if (clientComponent == null) {
-            return dest;
-        }
         LocationComponent location = clientComponent.camera.getComponent(LocationComponent.class);
-        if (location != null) {
-            Vector3f result = location.getWorldPosition(new Vector3f());
-            if (result.isFinite()) { //TODO: MP finite check seems to hide a larger underlying problem
-                dest.set(result);
-                return dest;
-            }
-        }
-        return getPosition(dest);
+        return location.getWorldPosition(dest);
     }
 
     /**
@@ -156,18 +134,8 @@ public class LocalPlayer {
      */
     public Quaternionf getViewRotation(Quaternionf dest) {
         ClientComponent clientComponent = getClientEntity().getComponent(ClientComponent.class);
-        if (clientComponent == null) {
-            return new Quaternionf();
-        }
         LocationComponent location = clientComponent.camera.getComponent(LocationComponent.class);
-        if (location != null) {
-            Quaternionf result = location.getWorldRotation(new Quaternionf());
-            if (result.isFinite()) { //TODO: MP finite check seems to hide a larger underlying problem
-                dest.set(result);
-                return dest;
-            }
-        }
-        return getRotation(dest);
+        return location.getWorldRotation(dest);
     }
 
     /**
@@ -183,10 +151,7 @@ public class LocalPlayer {
 
     public Vector3f getVelocity(Vector3f dest) {
         CharacterMovementComponent movement = getCharacterEntity().getComponent(CharacterMovementComponent.class);
-        if (movement != null) {
-            return dest.set(movement.getVelocity());
-        }
-        return dest;
+        return dest.set(movement.getVelocity());
     }
 
 

engine/src/main/java/org/terasology/engine/rendering/nui/layers/ingame/metrics/DebugOverlay.java
@@ -99,6 +99,9 @@ public class DebugOverlay extends CoreScreenLayer {
             debugLine3.bindText(new ReadOnlyBinding<String>() {
                 @Override
                 public String get() {
+                    if (!localPlayer.isValid()) {
+                        return "";
+                    }
                     Vector3f pos = localPlayer.getPosition(new Vector3f());
                     Vector3i chunkPos = Chunks.toChunkPos(pos, new Vector3i());
                     Vector3f rotation = localPlayer.getViewDirection(new Vector3f());

engine/src/main/java/org/terasology/engine/world/chunks/remoteChunkProvider/RemoteChunkProvider.java
@@ -206,6 +206,10 @@ public class RemoteChunkProvider implements ChunkProvider {
         }
 
         private int score(PositionFuture<?> task) {
+            if (!localPlayer.isValid()) {
+                return 1;
+            }
+
             return (int) Chunks.toChunkPos(localPlayer.getPosition(new Vector3f()), new Vector3i()).distance(task.getPosition());
         }
     }

commit b6e85b6e44e58e78ad3cc975fd8bbddcdee3f50e

fix(LocalPlayer): do not crash when the camera is not on
Restores the alternate behavior described in the docstring,
correcting a regression introduced in #4795.

This "LocalPlayer without camera" situation has come up in headless MTE tests.

engine/src/main/java/org/terasology/engine/logic/players/LocalPlayer.java
@@ -1,4 +1,4 @@
-// Copyright 2021 The Terasology Foundation
+// Copyright 2022 The Terasology Foundation
 // SPDX-License-Identifier: Apache-2.0
 package org.terasology.engine.logic.players;
 
@@ -115,27 +115,35 @@ public class LocalPlayer {
     }
 
     /**
-     * position of camera if one is present else use {@link #getPosition(Vector3f)}
+     * position of camera if one is present else use {@link #getPosition}
      *
      * @param dest will hold the result
      * @return dest
      */
     public Vector3f getViewPosition(Vector3f dest) {
         ClientComponent clientComponent = getClientEntity().getComponent(ClientComponent.class);
-        LocationComponent location = clientComponent.camera.getComponent(LocationComponent.class);
-        return location.getWorldPosition(dest);
+        if (clientComponent.camera.exists()) {
+            LocationComponent location = clientComponent.camera.getComponent(LocationComponent.class);
+            return location.getWorldPosition(dest);
+        } else {
+            return getPosition(dest);
+        }
     }
 
     /**
-     * orientation of camera if one is present else use {@link #getPosition(Vector3f)}
+     * orientation of camera if one is present else use {@link #getRotation}
      *
      * @param dest will hold the result
      * @return dest
      */
     public Quaternionf getViewRotation(Quaternionf dest) {
         ClientComponent clientComponent = getClientEntity().getComponent(ClientComponent.class);
-        LocationComponent location = clientComponent.camera.getComponent(LocationComponent.class);
-        return location.getWorldRotation(dest);
+        if (clientComponent.camera.exists()) {
+            LocationComponent location = clientComponent.camera.getComponent(LocationComponent.class);
+            return location.getWorldRotation(dest);
+        } else {
+            return getRotation(dest);
+        }
     }
 
     /**