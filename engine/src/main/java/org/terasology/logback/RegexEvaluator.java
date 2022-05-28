9f0e0be71a0f4742048e6df4b2da82b4e7442161
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-05-28 09:36:04 -0700

    doc(RegexEvaluator): document the constructors

--- engine/src/main/java/org/terasology/logback/RegexEvaluator.java
+++ engine/src/main/java/org/terasology/logback/RegexEvaluator.java
@@ -12,6 +12,11 @@ import java.util.regex.Pattern;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
+/**
+ * Evaluate messages with a regular expression.
+ *
+ * @see RegexFilterAction
+ */
 public class RegexEvaluator<E extends ILoggingEvent> extends EventEvaluatorBase<E> {
     final Pattern prefix;
     final Pattern matchMessage;
@@ -19,6 +24,12 @@ public class RegexEvaluator<E extends ILoggingEvent> extends EventEvaluatorBase<
     private final Predicate<String> prefixTest;
     private final Predicate<String> messageTest;
 
+    /**
+     * @param prefix Test the event's logger name against this pattern.
+     *     May be null to apply to all loggers.
+     *
+     * @param matchMessage Test the event's formatted message against this pattern.
+     */
     public RegexEvaluator(Pattern prefix, Pattern matchMessage) {
         this.prefix = prefix;
         this.matchMessage = matchMessage;
@@ -27,6 +38,13 @@ public class RegexEvaluator<E extends ILoggingEvent> extends EventEvaluatorBase<
         messageTest = checkNotNull(matchMessage).asPredicate();
     }
 
+    /**
+     * @param prefixString   Test the event's logger name against this prefix. This input is a
+     *     simple string, <em>not</em> a regex pattern. May be null to apply to all loggers.
+     *
+     * @param messagePattern A regular expression {@linkplain Pattern pattern} to match against the
+     *     {@linkplain ILoggingEvent#getFormattedMessage() formatted message} of a logging event.
+     */
     public RegexEvaluator(String prefixString, String messagePattern) {
         this(prefixString == null ? null : Pattern.compile("^" + Pattern.quote(prefixString)),
                 Pattern.compile(checkNotNull(messagePattern)));
