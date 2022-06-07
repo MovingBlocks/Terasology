3674bbb93b3f3177d8d12b77d0263ebfaf7afe7e
Kevin Turner <83819+keturn@users.noreply.github.com>
2022-06-07 16:52:26 -0700

    test(MTE)! set default NetworkMode to NONE (#5041)
    * test(MTE)! set default NetworkMode to NONE

--- engine-tests/src/main/java/org/terasology/engine/integrationenvironment/jupiter/IntegrationEnvironment.java
+++ engine-tests/src/main/java/org/terasology/engine/integrationenvironment/jupiter/IntegrationEnvironment.java
@@ -13,5 +13,5 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface IntegrationEnvironment {
-    NetworkMode networkMode() default NetworkMode.LISTEN_SERVER;
+    NetworkMode networkMode() default NetworkMode.NONE;
 }
