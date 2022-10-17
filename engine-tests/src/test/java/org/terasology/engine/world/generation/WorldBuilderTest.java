32d18280b6be02a74f7c96fa20a8124ea478a382
jdrueckert <jd.rueckert@googlemail.com>
2022-10-17 09:05:53 +0200

    chore: add missing import

--- engine-tests/src/test/java/org/terasology/engine/world/generation/WorldBuilderTest.java
+++ engine-tests/src/test/java/org/terasology/engine/world/generation/WorldBuilderTest.java
@@ -11,6 +11,7 @@ import org.terasology.engine.world.generator.plugin.WorldGeneratorPluginLibrary;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WorldBuilderTest {
