307673c4f0deaab399bfd75b681b9a0ce2d2dd4d
soloturn <soloturn@gmail.com>
2023-11-10 12:35:46 +0100

    engine logic qa.
    fix checkstyle, pmd warnings.

--- engine/src/main/java/org/terasology/engine/logic/console/ConsoleSystem.java
+++ engine/src/main/java/org/terasology/engine/logic/console/ConsoleSystem.java
@@ -34,9 +34,11 @@ public class ConsoleSystem extends BaseComponentSystem {
         if (nuiManager != null) {
             overlay = nuiManager.addOverlay(NotificationOverlay.ASSET_URI, NotificationOverlay.class);
             console.subscribe((Message message) -> {
-                if (!nuiManager.isOpen("engine:console") && message.getType() != CoreMessageType.CHAT
-                        && message.getType() != CoreMessageType.NOTIFICATION || !nuiManager.isOpen("engine:chat")) {
-                    // make sure the message isn't already shown in the chat overlay
+                // make sure the message isn't already shown in the chat overlay
+                if (!nuiManager.isOpen("engine:console")
+                        && (message.getType() != CoreMessageType.CHAT
+                        && message.getType() != CoreMessageType.NOTIFICATION
+                        || !nuiManager.isOpen("engine:chat"))) {
                     overlay.setVisible(true);
                 }
             });
