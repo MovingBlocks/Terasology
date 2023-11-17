920286966acd095147df1183789b6fe68ca22372
Josephine Rueckert <jd.rueckert@googlemail.com>
2023-11-17 15:11:15 +0100

    chore: fix minor typos in comment

--- subsystems/DiscordRPC/src/main/java/org/terasology/subsystem/discordrpc/DiscordRPCThread.java
+++ subsystems/DiscordRPC/src/main/java/org/terasology/subsystem/discordrpc/DiscordRPCThread.java
@@ -204,7 +204,7 @@ public final class DiscordRPCThread implements IPCListener, Runnable {
             /* Update the rich presence and keeping the connection alive */
             while (connected) {
                 synchronized (this) {
-                    /* Ping the ipc connection with an rich presnece to keep the connection alive */
+                    /* Ping the ipc connection with a rich presence to keep the connection alive */
                     if (enabled) {
                         /* Allocate a new rich presence when the buffer has changed */
                         if (buffer.hasChanged() && buffer.isEmpty()) {
