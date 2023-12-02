9db5ce91c482f7b536ff42fb94f85163b5414b3b
soloturn <soloturn@gmail.com>
2023-12-02 06:54:41 +0100

    qa engine, audio, fluent logger.

--- engine/src/main/java/org/terasology/engine/audio/openAL/streamingSound/OpenALStreamingSound.java
+++ engine/src/main/java/org/terasology/engine/audio/openAL/streamingSound/OpenALStreamingSound.java
@@ -123,7 +123,7 @@ public final class OpenALStreamingSound extends StreamingSound {
         try {
             GameThread.synch(this::initializeBuffers);
         } catch (InterruptedException e) {
-            logger.error("Failed to reload {}", getUrn(), e);
+            logger.atError().addArgument(() -> getUrn()).addArgument(e).log("Failed to reload {}");
         }
     }
 
