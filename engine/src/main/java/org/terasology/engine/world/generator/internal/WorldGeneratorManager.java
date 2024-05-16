30bc83ab00b98271bd505b11a2244309e146d795
jdrueckert <jd.rueckert@googlemail.com>
2024-05-16 20:19:16 +0200

    chore: screen merge follow-ups (#5241)
    * fix: screen name after merge
    
    * chore: remove no longer used translation tags
    
    * feat: return user to previous screen if no module with world generator in use
    
    * feat: differentiate world generator creation errors
    
    - add popup for user in case selected world generator cannot be resolved
    
    * UnresolvedDependencyException is in package excpeptions.
    
    ---------
    
    Co-authored-by: soloturn <soloturn@gmail.com>

--- engine/src/main/java/org/terasology/engine/world/generator/internal/WorldGeneratorManager.java
+++ engine/src/main/java/org/terasology/engine/world/generator/internal/WorldGeneratorManager.java
@@ -15,6 +15,7 @@ import org.terasology.engine.world.generator.UnresolvedWorldGeneratorException;
 import org.terasology.engine.world.generator.WorldGenerator;
 import org.terasology.gestalt.module.Module;
 import org.terasology.gestalt.module.ModuleEnvironment;
+import org.terasology.gestalt.module.exceptions.UnresolvedDependencyException;
 import org.terasology.gestalt.module.dependencyresolution.DependencyResolver;
 import org.terasology.gestalt.module.dependencyresolution.ResolutionResult;
 import org.terasology.gestalt.naming.Name;
@@ -98,7 +99,8 @@ public class WorldGeneratorManager {
      * @param context objects from this context will be injected into the
      * @return The instantiated world generator.
      */
-    public static WorldGenerator createGenerator(SimpleUri uri, Context context) throws UnresolvedWorldGeneratorException {
+    public static WorldGenerator createGenerator(SimpleUri uri, Context context)
+            throws UnresolvedWorldGeneratorException, UnresolvedDependencyException {
         ModuleManager moduleManager = context.get(ModuleManager.class);
         Module module = moduleManager.getEnvironment().get(uri.getModuleName());
         if (module == null) {
@@ -128,12 +130,12 @@ public class WorldGeneratorManager {
      * @return a new world generator with the specified uri.
      */
     public static WorldGenerator createWorldGenerator(SimpleUri uri, Context context, ModuleEnvironment environment)
-            throws UnresolvedWorldGeneratorException {
+            throws UnresolvedWorldGeneratorException, UnresolvedDependencyException {
         for (Class<?> generatorClass : environment.getTypesAnnotatedWith(RegisterWorldGenerator.class)) {
             RegisterWorldGenerator annotation = generatorClass.getAnnotation(RegisterWorldGenerator.class);
             Name moduleName = environment.getModuleProviding(generatorClass);
             if (moduleName == null) {
-                throw new UnresolvedWorldGeneratorException("Cannot find module for world generator " + generatorClass);
+                throw new UnresolvedDependencyException("Cannot find module for world generator " + generatorClass);
             }
             SimpleUri generatorUri = new SimpleUri(moduleName, annotation.id());
             if (generatorUri.equals(uri)) {
