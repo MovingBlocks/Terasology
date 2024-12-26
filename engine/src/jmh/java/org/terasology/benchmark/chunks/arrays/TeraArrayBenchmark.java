4ff7eed0f5e697aa9b3ad5420c1a068b2f2afb60
BabbaWaagen <73744497+BabbaWaagen@users.noreply.github.com>
2024-12-26 17:46:39 +0100

    Code Optimization in TeraArrayBenchmark.java
    ByteBuffer.allocateDirect-> Better performance through direct ByteBuffer.
    
    Pre-loading of sizes-> Minimizes method calls in inner loops.
    
    Use of flip-> Optimizes ByteBuffer for read operations.
    
    Clarity and consistency-> Uniform names and code style improve readability.

--- engine/src/jmh/java/org/terasology/benchmark/chunks/arrays/TeraArrayBenchmark.java
+++ engine/src/jmh/java/org/terasology/benchmark/chunks/arrays/TeraArrayBenchmark.java
@@ -3,18 +3,7 @@
 
 package org.terasology.benchmark.chunks.arrays;
 
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Param;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
-import org.openjdk.jmh.annotations.Warmup;
+import org.openjdk.jmh.annotations.*;
 import org.terasology.engine.world.chunks.blockdata.TeraArray;
 import org.terasology.engine.world.chunks.blockdata.TeraArray.SerializationHandler;
 import org.terasology.engine.world.chunks.blockdata.TeraDenseArray16Bit;
@@ -23,7 +12,6 @@ import org.terasology.engine.world.chunks.blockdata.TeraDenseArray8Bit;
 import org.terasology.engine.world.chunks.blockdata.TeraSparseArray4Bit;
 import org.terasology.engine.world.chunks.blockdata.TeraSparseArray8Bit;
 
-import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
@@ -37,29 +25,23 @@ public class TeraArrayBenchmark {
 
     public static final int BUFFER_SIZE = 1024 * 1024;
 
-    private static final byte[][] INFLATED_8_BIT = new byte[256][];
+    private static final byte[][] INFLATED_8_BIT = new byte[256][256];
+    private static final byte[][] INFLATED_4_BIT = new byte[256][128];
     private static final byte[] DEFLATED_8_BIT = new byte[256];
-
-    private static final byte[][] INFLATED_4_BIT = new byte[256][];
     private static final byte[] DEFLATED_4_BIT = new byte[256];
-
-    static {
-        for (int i = 0; i < INFLATED_8_BIT.length; i++) {
-            INFLATED_8_BIT[i] = new byte[256];
-        }
-        for (int i = 0; i < INFLATED_4_BIT.length; i++) {
-            INFLATED_4_BIT[i] = new byte[128];
-        }
-    }
-
 
     @Benchmark
     public int fullyRead(ArrayState state) {
+        TeraArray array = state.array;
+        int sizeX = array.getSizeX();
+        int sizeY = array.getSizeY();
+        int sizeZ = array.getSizeZ();
+
         int tmp = 0;
-        for (int y = 0; y < state.array.getSizeY(); y++) {
-            for (int z = 0; z < state.array.getSizeZ(); z++) {
-                for (int x = 0; x < state.array.getSizeX(); x++) {
-                    tmp += state.array.get(x, y, z);
+        for (int y = 0; y < sizeY; y++) {
+            for (int z = 0; z < sizeZ; z++) {
+                for (int x = 0; x < sizeX; x++) {
+                    tmp += array.get(x, y, z);
                 }
             }
         }
@@ -68,10 +50,15 @@ public class TeraArrayBenchmark {
 
     @Benchmark
     public void fullyWrite(ArrayState state) {
-        for (int y = 0; y < state.array.getSizeY(); y++) {
-            for (int z = 0; z < state.array.getSizeZ(); z++) {
-                for (int x = 0; x < state.array.getSizeX(); x++) {
-                    state.array.set(x, y, z, 1);
+        TeraArray array = state.array;
+        int sizeX = array.getSizeX();
+        int sizeY = array.getSizeY();
+        int sizeZ = array.getSizeZ();
+
+        for (int y = 0; y < sizeY; y++) {
+            for (int z = 0; z < sizeZ; z++) {
+                for (int x = 0; x < sizeX; x++) {
+                    array.set(x, y, z, 1);
                 }
             }
         }
@@ -88,13 +75,11 @@ public class TeraArrayBenchmark {
     }
 
     public enum TeraArrayType {
-        DENCE_4BIT(() -> new TeraDenseArray4Bit(16, 256, 16), TeraDenseArray4Bit.SerializationHandler::new),
-        DENCE_8BIT(() -> new TeraDenseArray8Bit(16, 256, 16), TeraDenseArray8Bit.SerializationHandler::new),
-        DENCE_16BIT(() -> new TeraDenseArray16Bit(16, 256, 16), TeraDenseArray16Bit.SerializationHandler::new),
-        SPARCE_4BIT(() -> new TeraSparseArray4Bit(16, 256, 16, INFLATED_4_BIT, DEFLATED_4_BIT),
-                TeraSparseArray4Bit.SerializationHandler::new),
-        SPARCE_8BIT(() -> new TeraSparseArray8Bit(16, 256, 16, INFLATED_8_BIT, DEFLATED_8_BIT),
-                TeraSparseArray8Bit.SerializationHandler::new);
+        DENSE_4BIT(() -> new TeraDenseArray4Bit(16, 256, 16), TeraDenseArray4Bit.SerializationHandler::new),
+        DENSE_8BIT(() -> new TeraDenseArray8Bit(16, 256, 16), TeraDenseArray8Bit.SerializationHandler::new),
+        DENSE_16BIT(() -> new TeraDenseArray16Bit(16, 256, 16), TeraDenseArray16Bit.SerializationHandler::new),
+        SPARSE_4BIT(() -> new TeraSparseArray4Bit(16, 256, 16, INFLATED_4_BIT, DEFLATED_4_BIT), TeraSparseArray4Bit.SerializationHandler::new),
+        SPARSE_8BIT(() -> new TeraSparseArray8Bit(16, 256, 16, INFLATED_8_BIT, DEFLATED_8_BIT), TeraSparseArray8Bit.SerializationHandler::new);
 
         private final Supplier<TeraArray> creator;
         private final Supplier<SerializationHandler> handler;
@@ -113,38 +98,35 @@ public class TeraArrayBenchmark {
         }
     }
 
-
     @State(Scope.Thread)
     public static class ByteBufferState {
-        private ByteBuffer out;
+        ByteBuffer out;
 
         @Setup(Level.Invocation)
-        public void setup() throws IOException {
-            out = ByteBuffer.allocate(BUFFER_SIZE);
+        public void setup() {
+            out = ByteBuffer.allocateDirect(BUFFER_SIZE);
         }
-
     }
 
     @State(Scope.Thread)
     public static class FilledByteBufferState {
-        private ByteBuffer in;
+        ByteBuffer in;
 
         @Setup(Level.Invocation)
         public void setup(ArrayState arrayState) {
-            in = ByteBuffer.allocate(BUFFER_SIZE);
+            in = ByteBuffer.allocateDirect(BUFFER_SIZE);
             arrayState.handler.serialize(arrayState.array, in);
-            in.rewind();
+            in.flip();
         }
-
     }
 
     @State(Scope.Thread)
     public static class ArrayState {
-        @Param({"DENCE_4BIT", "DENCE_8BIT", "DENCE_16BIT", "SPARCE_4BIT", "SPARCE_8BIT"})
+        @Param({"DENSE_4BIT", "DENSE_8BIT", "DENSE_16BIT", "SPARSE_4BIT", "SPARSE_8BIT"})
         private static TeraArrayType arrayType;
 
-        private SerializationHandler handler;
-        private TeraArray array;
+        SerializationHandler handler;
+        TeraArray array;
 
         @Setup
         public void setup() {
@@ -152,5 +134,4 @@ public class TeraArrayBenchmark {
             handler = arrayType.handler();
         }
     }
-
 }
