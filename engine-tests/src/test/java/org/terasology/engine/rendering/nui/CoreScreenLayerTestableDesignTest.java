e09466ac24a10f1a2313b46bc43ba3035fc5f8ad
carrie57 <carrie91507@gmail.com>
2026-02-26 22:20:04 -0800

    Part 5: Add Mockito interaction test for CoreScreenLayer

--- engine-tests/src/test/java/org/terasology/engine/rendering/nui/CoreScreenLayerTestableDesignTest.java
+++ engine-tests/src/test/java/org/terasology/engine/rendering/nui/CoreScreenLayerTestableDesignTest.java
@@ -1,2 +1,36 @@
-package org.terasology.engine.rendering.nui;public class CoreScreenLayerTestableDesignTest {
+// Copyright The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
+package org.terasology.engine.rendering.nui;
+
+import org.junit.jupiter.api.Test;
+import org.terasology.engine.rendering.nui.animation.MenuAnimationSystem;
+
+import static org.mockito.Mockito.*;
+
+public class CoreScreenLayerTestableDesignTest {
+
+    static class TestScreenLayer extends CoreScreenLayer {
+
+        TestScreenLayer(String id, MenuAnimationSystem animationSystem) {
+            super(id, animationSystem);
+        }
+
+        @Override
+        public void initialise() {
+            // no-op for testing
+        }
+    }
+
+    @Test
+    public void onOpened_shouldTriggerAnimation() {
+
+        MenuAnimationSystem animation = mock(MenuAnimationSystem.class);
+
+        TestScreenLayer screen =
+                new TestScreenLayer("test", animation);
+
+        screen.onOpened();
+
+        verify(animation, times(1)).triggerFromPrev();
+    }
 }
