a2a42511748dc3ff8e022df1491b357fcebc0135
RyanAI <ryanai@RyanAIdeMacBook-Pro.local>
2026-03-08 13:03:12 +0800

    refactor: replace whitelist/blacklist with allowlist/denylist
    - Renamed variables and methods in ServerConnectListManager.java
    - Updated file paths from blacklist.json/whitelist.json to denylist.json/allowlist.json
    - Updated comments in SubtypeLayoutBuilder.java
    - Renamed test method in TypeRegistryTest.java
    
    Closes #5275

--- engine-tests/src/test/java/org/terasology/reflection/TypeRegistryTest.java
+++ engine-tests/src/test/java/org/terasology/reflection/TypeRegistryTest.java
@@ -49,10 +49,10 @@ public class TypeRegistryTest extends ModuleEnvironmentTest {
     // TODO: Re-enable as gradlew check seems to still stall even with the Ignore in place?
     @Disabled("Seems to intermittently stall, at least on Win10")
     @Test
-    public void testWhitelistedTypes() {
+    public void testAllowlistedTypes() {
         Set<Class<?>> allTypes = typeRegistry.getSubtypesOf(Object.class);
-        for (Class<?> whitelisted : ExternalApiWhitelist.CLASSES) {
-            assumeTrue(allTypes.contains(whitelisted), () -> allTypes.toString() + " should contain " + whitelisted.getName());
+        for (Class<?> allowlisted : ExternalApiWhitelist.CLASSES) {
+            assumeTrue(allTypes.contains(allowlisted), () -> allTypes.toString() + " should contain " + allowlisted.getName());
         }
     }
 }
